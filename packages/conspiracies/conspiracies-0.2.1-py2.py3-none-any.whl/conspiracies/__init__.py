9e4013bdeb9ba2620c99c5882827544a51f6fabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package conspiracies/0.2.1/conspiracies-0.2.1-py2.py3-none-any.whl

---
+++ packages/conspiracies/conspiracies-0.2.1-py2.py3-none-any.whl/conspiracies/__init__.py
@@ -0,0 +1,6 @@
+from .HeadWordExtractionComponent import (  # noqa
+    create_headwords_component,
+    HeadwordsExtractionComponent,
+)
+from .coref import create_coref_component, CoreferenceComponent  # noqa
+from .wordpiece_length_normalization import wordpiece_length_normalization  # noqa
---
+++ packages/conspiracies/conspiracies-0.2.1-py2.py3-none-any.whl/conspiracies/relationextraction/other/__init__.py
@@ -0,0 +1,3 @@
+from ..model import BERTBiLSTM, Multi2OIE
+from .bio import arg_tag2idx, pred_tag2idx
+from .utils import get_models, get_word2piece, load_pkl
