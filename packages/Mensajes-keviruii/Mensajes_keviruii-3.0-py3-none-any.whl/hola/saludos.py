679e9518fb37423d37b8b5cee689019eba3ccd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:41 +0000

    Add package Mensajes-keviruii/3.0/Mensajes_keviruii-3.0-py3-none-any.whl

---
+++ packages/Mensajes-keviruii/Mensajes_keviruii-3.0-py3-none-any.whl/hola/saludos.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def saludar():
+    print("hola")
+
+def generar_array(numeros):
+	return np.arange(numeros)
+
+def prueba():
+    print("Esto es una prueba de la nueva versión")
+
+
+class Saludo():
+
+	def __init__(self):
+		
+		print("Hola, te estoy saludando desde el __init__ de la clase Saludo")
+
+
+
+
+if __name__ == '__main__':
+    print(generar_array(5))
