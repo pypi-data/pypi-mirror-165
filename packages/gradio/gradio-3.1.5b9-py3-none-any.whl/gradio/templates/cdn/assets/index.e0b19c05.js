2684dea006f4702fc25239d7e909cbfba186fe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package gradio/3.1.5b9/gradio-3.1.5b9-py3-none-any.whl

---
+++ packages/gradio/gradio-3.1.5b9-py3-none-any.whl/gradio/templates/cdn/assets/index.e0b19c05.js
@@ -0,0 +1,5 @@
+export { M as Component } from './index.8ba27fc4.js';
+
+const modes = ["static"];
+
+export { modes };
---
+++ packages/gradio/gradio-3.1.5b9-py3-none-any.whl/gradio/templates/cdn/gradio.js
@@ -0,0 +1,10 @@
+
+
+function make_script(src) {
+    const script = document.createElement('script');
+    script.type = 'module';
+    script.setAttribute("crossorigin", "");
+    script.src = src;
+    document.head.appendChild(script);
+}
+make_script("https://gradio.s3-us-west-2.amazonaws.com/3.1.5b9/assets/index.8ba27fc4.js");
