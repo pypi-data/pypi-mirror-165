a7f0557c7e1ce1d6d68367f0a064d6d635406cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:14 +0000

    Add package cuny-python-package-demo/0.0.1/cuny_python_package_demo-0.0.1-py3-none-any.whl

---
+++ packages/cuny-python-package-demo/cuny_python_package_demo-0.0.1-py3-none-any.whl/cunyue_python_package_demo/helloworld.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time     : 2022/8/27 14:11
+@Author   : cuny
+@File     : helloworld.py
+@Software : PyCharm
+@Introduce: 
+简单的helloworld程序
+"""
+
+
+def hello():
+    print("hello world!")
