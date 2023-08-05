59394ef9662d7967233176255703b40a73f8dfc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:56 +0000

    Add package hydra-genetics/0.14.1/hydra-genetics-0.14.1.tar.gz

---
+++ packages/hydra-genetics/hydra-genetics-0.14.1.tar.gz/hydra-genetics-0.14.1/hydra_genetics/utils/misc.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from collections.abc import Mapping
+from copy import deepcopy
+from os.path import exists
+
+
+def merge(dict1, dict2):
+    ''' Return a new dictionary by merging two dictionaries recursively. '''
+
+    result = deepcopy(dict1)
+    for key, value in dict2.items():
+        if isinstance(value, Mapping):
+            result[key] = merge(result.get(key, {}), value)
+        else:
+            result[key] = deepcopy(dict2[key])
+    return result
+
+
+def extract_chr(file, filter_out=["chrM"]):
+    chr = None
+    if exists(file):
+        with open(file) as lines:
+            chr = [line.split("\t")[0] for line in lines]
+        return [c for c in chr if c not in filter_out]
+    return [""]
