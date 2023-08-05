c90c6c5fe22315d4115c51810a5abfdb66248b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:28 +0000

    Add package python-office/0.1.19/python_office-0.1.19-py3-none-any.whl

---
+++ packages/python-office/python_office-0.1.19-py3-none-any.whl/office/__init__.py
@@ -0,0 +1,15 @@
+from office.api import word
+from office.api import pdf
+from office.api import image
+from office.api import file
+from office.api import video
+from office.api import tools
+from office.api import ppt
+from office.api import excel
+
+print('=' * 30)
+print('【python-office库】，功能持续更新中')
+print( '全部功能 - 说明书 -：https://www.python-office.com/')
+print('使用有问题 or 提交你的功能需求 or 参与项目开发')
+print('请+【开发者微信】：http://t.cn/A6XVQXAk')
+print('=' * 30)
