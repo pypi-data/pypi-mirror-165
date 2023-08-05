76782a1ed3051467588d922d78901b94ac8998f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package tintx/2022.8.0/tintx-2022.8.0-py3.10.egg

---
+++ packages/tintx/tintx-2022.8.0-py3.10.egg/tintx/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+========================================
+Cell Tracking (:mod:`tracking.core`)
+========================================
+.. currentmodule:: tracking.core
+TITAN cell tracking
+================
+.. autosummary::
+    :toctree: generated/
+    Cell_tracks
+
+"""
+
+from .reader import RunDirectory
+
+__version__ = "2022.8.0"
