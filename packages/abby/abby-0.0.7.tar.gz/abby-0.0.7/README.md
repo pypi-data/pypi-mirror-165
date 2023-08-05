84e882d8c21da65a67267910810b0747fe9ceed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:51 +0000

    Add package abby/0.0.7/abby-0.0.7.tar.gz

---
+++ packages/abby/abby-0.0.7.tar.gz/abby-0.0.7/README.md
@@ -0,0 +1,15 @@
+# abby
+
+> AB testing for Python
+
+Abby is a python library package for A/B testing for human. Abby aims to make A/B testing easy and accessible to anyone.
+
+[![PyPI Version][pypi-image]][pypi-url]
+[![Build Status][build-image]][build-url]
+
+<!-- Badges -->
+
+[pypi-image]: https://img.shields.io/pypi/v/abby
+[pypi-url]: https://pypi.org/project/abby/
+[build-image]: https://github.com/farhanreynaldo/abby/actions/workflows/test.yml/badge.svg
+[build-url]: https://github.com/farhanreynaldo/abby/actions/workflows/test.yml
---
+++ packages/abby/abby-0.0.7.tar.gz/abby-0.0.7/abby/tests/test_delta.py
@@ -0,0 +1,38 @@
+from typing import Callable
+
+import pandas as pd
+import pytest
+
+from abby.compare import compare_delta
+
+
+@pytest.fixture
+def click_df() -> pd.DataFrame:
+    data = pd.read_csv("abby/tests/datasets/click_impression.csv")
+    return data
+
+
+@pytest.fixture
+def click_df_wrong(click_df):
+    return click_df.rename(columns={"variant_name": "group"})
+
+
+class TestCompareTtest:
+    def test_ttest_result(self, click_df: Callable):
+        result = compare_delta(
+            click_df, ["control", "experiment"], "click", "impression"
+        )
+        assert result["control_mean"] == pytest.approx(0.135135, rel=4)
+        assert result["experiment_mean"] == pytest.approx(0.162371, rel=4)
+        assert result["control_var"] == pytest.approx(0.000296, rel=4)
+        assert result["experiment_var"] == pytest.approx(0.000475, rel=4)
+        assert result["absolute_difference"] == pytest.approx(0.027236, rel=4)
+        assert result["lower_bound"] == pytest.approx(-0.027189, rel=4)
+        assert result["upper_bound"] == pytest.approx(0.081661, rel=4)
+        assert result["p_values"] == pytest.approx(0.326668, rel=4)
+
+    def test_wrong_variant_column_name(self, click_df_wrong: Callable):
+        with pytest.raises(AssertionError):
+            compare_delta(
+                click_df_wrong, ["control", "experiment"], "click", "impression"
+            )
