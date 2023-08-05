0f3d904859ecb38ee1d2ff5841f2f2adf3a9b615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:20 +0000

    Add package fesnd-probability/1.2/fesnd_probability-1.2.tar.gz

---
+++ packages/fesnd-probability/fesnd_probability-1.2.tar.gz/fesnd_probability-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fesnd_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['fesnd_probability'],
+      author='Adegbenga Festus',
+      author_email = 'festlove2007@yahoo.com',
+      zip_safe=False)
