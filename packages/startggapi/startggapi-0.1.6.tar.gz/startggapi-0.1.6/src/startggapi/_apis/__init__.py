2bdcb067616c71cd028bce630953193948daf461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:24 +0000

    Add package startggapi/0.1.6/startggapi-0.1.6.tar.gz

---
+++ packages/startggapi/startggapi-0.1.6.tar.gz/startggapi-0.1.6/src/startggapi/_apis/__init__.py
@@ -0,0 +1,10 @@
+from .BaseApi import BaseApi
+from .TournamentApi import TournamentApi
+from .EntrantApi import EntrantApi
+from .EventApi import EventApi
+
+__all__ = [
+    "TournamentApi",
+    "EntrantApi",
+    "EventApi"
+]
