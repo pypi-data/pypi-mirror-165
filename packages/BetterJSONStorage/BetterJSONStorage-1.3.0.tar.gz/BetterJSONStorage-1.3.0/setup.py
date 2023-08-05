aef8d0d96f55815ae80e21b6f5619a3d4fce2dee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:50 +0000

    Add package BetterJSONStorage/1.3.0/BetterJSONStorage-1.3.0.tar.gz

---
+++ packages/BetterJSONStorage/BetterJSONStorage-1.3.0.tar.gz/BetterJSONStorage-1.3.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BetterJSONStorage",
+    version="1.3.0",
+    author="Thomas Eeckhout",
+    author_email="Thomas.Eeckhout@outlook.be",
+    description="An optimized tinyDB storage extension",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/MrPigss/BetterJSONStorage",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Database",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=["tinydb", "orjson", "blosc2", "mypy"],
+    python_requires=">=3.8",
+    setup_requires=["isort"],
+)
