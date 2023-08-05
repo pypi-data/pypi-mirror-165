8baab8ea36a24ad6255866555b780ec94eaff6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:23 +0000

    Add package fast-sentence-tokenize/0.1.3/fast_sentence_tokenize-0.1.3-py3-none-any.whl

---
+++ packages/fast-sentence-tokenize/fast_sentence_tokenize-0.1.3-py3-none-any.whl/fast_sentence_tokenize/datablock/__init__.py
@@ -0,0 +1,6 @@
+from .wordnet import *
+from .dto import *
+from .find_wordnet import FindWordnet
+from .find_dictionaries import FindDictionaries
+from .find_stopwords import FindStopWords
+from .find_pronouns import FindPronouns
