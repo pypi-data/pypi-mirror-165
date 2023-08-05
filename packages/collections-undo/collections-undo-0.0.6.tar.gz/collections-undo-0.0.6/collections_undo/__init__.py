eca5ea5df0066fac1478633ccd019767f41b7e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:39 +0000

    Add package collections-undo/0.0.6/collections-undo-0.0.6.tar.gz

---
+++ packages/collections-undo/collections-undo-0.0.6.tar.gz/collections-undo-0.0.6/collections_undo/__init__.py
@@ -0,0 +1,33 @@
+__version__ = "0.0.6"
+
+from ._stack import UndoManager
+from ._const import empty
+from . import abc, fmt, containers
+
+__all__ = [
+    "UndoManager",
+    "empty",
+    "AbstractUndoableDict",
+    "AbstractUndoableList",
+    "AbstractUndoableSet",
+    "UndoableDict",
+    "UndoableList",
+    "UndoableSet",
+    "abc",
+    "containers",
+    "fmt",
+]
+
+
+def __getattr__(name):
+    cls = getattr(containers, name, None)
+    if cls is None:
+        raise AttributeError(f"module 'collections_undo' has no attribute '{name}'")
+    import warnings
+
+    warnings.warn(
+        f"Importing {name} from 'collections_undo' is deprecated. Import it from "
+        "'collections_undo.containers' instead.",
+        DeprecationWarning,
+    )
+    return cls
