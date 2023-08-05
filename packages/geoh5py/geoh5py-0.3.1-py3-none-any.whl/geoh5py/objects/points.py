000b95283246b483b487e453793bff548a56dc76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:41 +0000

    Add package geoh5py/0.3.1/geoh5py-0.3.1-py3-none-any.whl

---
+++ packages/geoh5py/geoh5py-0.3.1-py3-none-any.whl/geoh5py/objects/points.py
@@ -0,0 +1,69 @@
+#  Copyright (c) 2022 Mira Geoscience Ltd.
+#
+#  This file is part of geoh5py.
+#
+#  geoh5py is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  geoh5py is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+import uuid
+
+import numpy as np
+
+from .object_base import ObjectBase, ObjectType
+
+
+class Points(ObjectBase):
+    """
+    Points object made up of vertices.
+    """
+
+    __TYPE_UID = uuid.UUID("{202C5DB1-A56D-4004-9CAD-BAAFD8899406}")
+
+    def __init__(self, object_type: ObjectType, **kwargs):
+        self._vertices: np.ndarray | None = None
+
+        super().__init__(object_type, **kwargs)
+
+        object_type.workspace._register_object(self)
+
+    @classmethod
+    def default_type_uid(cls) -> uuid.UUID:
+        return cls.__TYPE_UID
+
+    @property
+    def vertices(self) -> np.ndarray | None:
+        """
+        :obj:`~geoh5py.objects.object_base.ObjectBase.vertices`
+        """
+        if self._vertices is None and self.on_file:
+            self._vertices = self.workspace.fetch_array_attribute(self, "vertices")
+
+        if self._vertices is not None:
+            return self._vertices.view("<f8").reshape((-1, 3))
+
+        return None
+
+    @vertices.setter
+    def vertices(self, xyz: np.ndarray):
+        assert (
+            xyz.shape[1] == 3
+        ), f"Array of vertices must be of shape (*, 3). Array of shape {xyz.shape} provided."
+        self._vertices = np.asarray(
+            np.core.records.fromarrays(
+                xyz.T.tolist(),
+                dtype=[("x", "<f8"), ("y", "<f8"), ("z", "<f8")],
+            )
+        )
+        self.workspace.update_attribute(self, "vertices")
