1c667de20219b655d194d4c3edea8753a2727dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:05 +0000

    Add package ueli-calculation/0.1/ueli_calculation-0.1.tar.gz

---
+++ packages/ueli-calculation/ueli_calculation-0.1.tar.gz/ueli_calculation-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ueli_calculation',
+      version='0.1',
+      description='My package in pypi',
+      packages=['ueli_calculation'],
+      author='Eliane Netto',
+      author_email='elianenetto82@gmail.com',
+      zip_safe=False)
---
+++ packages/ueli-calculation/ueli_calculation-0.1.tar.gz/ueli_calculation-0.1/ueli_calculation/GeneralCalculation.py
@@ -0,0 +1,8 @@
+class Calculation:
+    
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
+                                                        
+    def sumNumber(self):
+        return self.number1 + self.number2
