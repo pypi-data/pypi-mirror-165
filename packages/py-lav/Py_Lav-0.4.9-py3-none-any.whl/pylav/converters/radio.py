6bf4043b0d48ac6c9bb5506c24a82e099f1396a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:06 +0000

    Add package py-lav/0.4.9/Py_Lav-0.4.9-py3-none-any.whl

---
+++ packages/py-lav/Py_Lav-0.4.9-py3-none-any.whl/pylav/converters/radio.py
@@ -0,0 +1,298 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, TypeVar
+
+import asyncstdlib
+from discord.app_commands import Choice, Transformer
+from discord.ext import commands
+
+from pylav import getLogger
+from pylav.types import ContextT, InteractionT
+
+LOGGER = getLogger("red.3pt.PyLav-Shared.converters.radio")
+
+if TYPE_CHECKING:
+    from pylav.radio.objects import Codec, Country, CountryCode, Language, State, Station, Tag
+
+    StationConverter = TypeVar("StationConverter", bound="list[Station]")
+    TagConverter = TypeVar("TagConverter", bound=list[Tag])
+    LanguageConverter = TypeVar("LanguageConverter", bound=list[Language])
+    StateConverter = TypeVar("StateConverter", bound=list[State])
+    CodecConverter = TypeVar("CodecConverter", bound=list[Codec])
+    CountryCodeConverter = TypeVar("CountryCodeConverter", bound=list[CountryCode])
+    CountryConverter = TypeVar("CountryConverter", bound=list[Country])
+else:
+
+    class StationConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[Station]:
+            """Converts a station name to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                stations = await ctx.lavalink.radio_browser.stations()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"Station with name `{arg}` not found.") from e
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), stations)):
+                return r
+            raise commands.BadArgument(f"Station with name `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[Station]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            data = interaction.data
+            options = data.get("options", [])
+            kwargs = {"order": "clickcount"}
+            if options:
+                country_code = [v for v in options if v.get("name") == "countrycode"]
+                country = [v for v in options if v.get("name") == "country"]
+                state = [v for v in options if v.get("name") == "state"]
+                language = [v for v in options if v.get("name") == "language"]
+                tags = [v for v in options if v.get("name", "").startswith("tag")]
+                if country_code and (val := country_code[0].get("value")):
+                    kwargs["countrycode"] = val
+                if country and (val := country[0].get("value")):
+                    kwargs["country"] = val
+                    kwargs["country_exact"] = any(
+                        True
+                        for t in await interaction.client.lavalink.radio_browser.countries(
+                            code=kwargs.get("countrycode")
+                        )
+                        if t.name == val
+                    )
+                if state and (val := state[0].get("value")):
+                    kwargs["state"] = val
+                    kwargs["state_exact"] = any(
+                        True
+                        for t in await interaction.client.lavalink.radio_browser.states(
+                            country=kwargs["country"] if kwargs.get("country_exact") else None
+                        )
+                        if t.name == val
+                    )
+                if language and (val := language[0].get("value")):
+                    kwargs["language"] = val
+                    kwargs["language_exact"] = any(
+                        True for t in await interaction.client.lavalink.radio_browser.languages() if t.name == val
+                    )
+                if tags:
+                    if len(tags) == 1 and (val := tags[0].get("value")):
+                        kwargs["tag"] = val
+                        kwargs["tag_exact"] = any(
+                            True for t in await interaction.client.lavalink.radio_browser.tags() if t.name == val
+                        )
+                    elif len(tags) > 1:
+                        kwargs["tag_list"] = [tv for t in tags if (tv := t.get("value"))]
+
+            if current:
+                kwargs["name"] = current
+            stations = await interaction.client.lavalink.radio_browser.search(limit=25, **kwargs)
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in stations
+                if current.lower() in n.name.lower()
+            ][:25]
+
+    class TagConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[Tag]:
+            """Converts a Tag name to to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                tags = await ctx.lavalink.radio_browser.tags()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"Tag with name `{arg}` not found.") from e
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), tags)):
+                return r
+            raise commands.BadArgument(f"Tag with name `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[Tag]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            tags = await interaction.client.lavalink.radio_browser.tags()
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in tags
+                if current.lower() in n.name.lower()
+            ][:25]
+
+    class LanguageConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[Language]:
+            """Converts a Language name to to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                langs = await ctx.lavalink.radio_browser.languages()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"Language with name `{arg}` not found.") from e
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), langs)):
+                return r
+            raise commands.BadArgument(f"Language with name `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[Language]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            languages = await interaction.client.lavalink.radio_browser.languages()
+
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in languages
+                if current.lower() in n.name.lower()
+            ][:25]
+
+    class StateConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[State]:
+            """Converts a State name to to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                states = await ctx.lavalink.radio_browser.states()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"State with name `{arg}` not found.") from e
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), states)):
+                return r
+            raise commands.BadArgument(f"State with name `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[State]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            data = interaction.data
+            options = data.get("options", [])
+            kwargs = {}
+            if options:
+                country = [v for v in options if v.get("name") == "country"]
+
+                if country and (val := country[0].get("value")):
+                    kwargs["country"] = val
+            LOGGER.debug(f"StateConverter Autocompleting {current} with {kwargs} and {options}")
+
+            states = await interaction.client.lavalink.radio_browser.states(**kwargs)
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in states
+                if current.lower() in n.name.lower()
+            ][:25]
+
+    class CodecConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[Codec]:
+            """Converts a Codec name to to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                codecs = await ctx.lavalink.radio_browser.codecs()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"Codec with name `{arg}` not found.") from e
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), codecs)):
+                return r
+            raise commands.BadArgument(f"Codec with name `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[Codec]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            codecs = await interaction.client.lavalink.radio_browser.codecs()
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in codecs
+                if current.lower() in n.name.lower()
+            ][:25]
+
+    class CountryCodeConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[CountryCode]:
+            """Converts a CountryCode name to to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                countrycodes = await ctx.lavalink.radio_browser.countrycodes()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"Country code `{arg}` not found.") from e
+
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), countrycodes)):
+                return r
+            raise commands.BadArgument(f"Country code `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[CountryCode]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            countrycodes = await interaction.client.lavalink.radio_browser.countrycodes()
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in countrycodes
+                if current.lower() in n.name.lower()
+            ][:25]
+
+    class CountryConverter(Transformer):
+        @classmethod
+        async def convert(cls, ctx: ContextT, arg: str) -> list[Country]:
+            """Converts a Country name to to a matching object."""
+            from pylav import EntryNotFoundError
+
+            try:
+                countries = await ctx.lavalink.radio_browser.countries()
+            except EntryNotFoundError as e:
+                raise commands.BadArgument(f"Country with name `{arg}` not found.") from e
+            if r := await asyncstdlib.list(asyncstdlib.filter(lambda n: arg.lower() in n.name.lower(), countries)):
+                return r
+            raise commands.BadArgument(f"Country with name `{arg}` not found.")
+
+        @classmethod
+        async def transform(cls, interaction: InteractionT, argument: str) -> list[Country]:
+            ctx = await interaction.client.get_context(interaction)
+            if interaction and not interaction.response.is_done():
+                await ctx.defer(ephemeral=True)
+            return await cls.convert(ctx, argument)
+
+        @classmethod
+        async def autocomplete(cls, interaction: InteractionT, current: str) -> list[Choice]:
+            data = interaction.data
+            options = data.get("options", [])
+            kwargs = {}
+            if options:
+                code = [v for v in options if v.get("name") in ["countrycode", "code"]]
+                if code and (val := code[0].get("value")):
+                    kwargs["code"] = val
+            countries = await interaction.client.lavalink.radio_browser.countries(**kwargs)
+            return [
+                Choice(name=n.name[:99] if n.name else "Unnamed", value=f"{n.name}")
+                for n in countries
+                if current.lower() in n.name.lower()
+            ][:25]
---
+++ packages/py-lav/Py_Lav-0.4.9-py3-none-any.whl/pylav/sql/tables.py
@@ -0,0 +1,185 @@
+# sourcery skip: avoid-builtin-shadow
+from __future__ import annotations
+
+import os
+
+from piccolo.columns import JSONB, Array, BigInt, Boolean, Bytea, Float, Integer, Text, Timestamptz
+from piccolo.engine import PostgresEngine, SQLiteEngine
+from piccolo.table import Table
+
+from pylav._logging import getLogger
+from pylav.envvars import (
+    JAVA_EXECUTABLE,
+    POSTGRES_DATABASE,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+)
+
+LOGGER = getLogger("PyLav.Postgres")
+config = {
+    "host": POSTGRES_HOST,
+    "port": int(POSTGRES_PORT) if isinstance(POSTGRES_PORT, str) else POSTGRES_PORT,
+    "database": POSTGRES_DATABASE,
+    "user": POSTGRES_USER,
+    "password": POSTGRES_PASSWORD,
+}
+LOGGER.info("Connecting to Postgres server using %r", config)
+if os.getenv("BUILDING_DOCS", False):
+    DB = SQLiteEngine()
+else:
+    DB = PostgresEngine(config=config)
+
+
+class PlaylistRow(Table, db=DB, tablename="playlist"):
+    id = BigInt(primary_key=True)
+    scope = BigInt(null=False, index=True)
+    author = BigInt(null=False, index=True)
+    name = Text(null=False, index=True)
+    url = Text(null=True)
+    tracks = JSONB(null=False, default=[])
+
+
+class LibConfigRow(Table, db=DB, tablename="lib_config"):
+    bot = BigInt(primary_key=True, index=True)
+    id = BigInt(index=True)
+    config_folder = Text(null=False)
+    localtrack_folder = Text(null=True)
+    java_path = Text(null=False, default=JAVA_EXECUTABLE)
+    enable_managed_node = Boolean(null=False, default=True)
+    use_bundled_lava_link_external = Boolean(null=False, default=True)
+    use_bundled_pylav_external = Boolean(null=False, default=True)
+    auto_update_managed_nodes = Boolean(null=False, default=True)
+    download_id = BigInt(index=True, default=0)
+    extras: JSONB = JSONB(null=False, default={})
+    next_execution_update_bundled_playlists = Timestamptz(null=True)
+    next_execution_update_bundled_external_playlists = Timestamptz(null=True)
+    next_execution_update_external_playlists = Timestamptz(null=True)
+    update_bot_activity = Boolean(null=False, default=False)
+
+
+class EqualizerRow(Table, db=DB, tablename="equalizer"):
+    id = BigInt(primary_key=True, index=True)
+    scope = BigInt(null=False, index=True)
+    name = Text(null=True, index=True)
+    description = Text(null=True)
+    author = BigInt(null=False, index=True)
+    band_25 = Float(null=False, default=0.0)
+    band_40 = Float(null=False, default=0.0)
+    band_63 = Float(null=False, default=0.0)
+    band_100 = Float(null=False, default=0.0)
+    band_160 = Float(null=False, default=0.0)
+    band_250 = Float(null=False, default=0.0)
+    band_400 = Float(null=False, default=0.0)
+    band_630 = Float(null=False, default=0.0)
+    band_1000 = Float(null=False, default=0.0)
+    band_1600 = Float(null=False, default=0.0)
+    band_2500 = Float(null=False, default=0.0)
+    band_4000 = Float(null=False, default=0.0)
+    band_6300 = Float(null=False, default=0.0)
+    band_10000 = Float(null=False, default=0.0)
+    band_16000 = Float(null=False, default=0.0)
+
+
+class PlayerStateRow(Table, db=DB, tablename="player_state"):
+    id = BigInt(primary_key=True, index=True)
+    bot = BigInt(index=True, null=False)
+    channel_id = BigInt(null=False)
+    volume = Integer(null=False, default=100)
+    position = Float(null=False, default=0.0)
+    auto_play_playlist_id = BigInt(null=True)
+    forced_channel_id = BigInt(null=True)
+    text_channel_id = BigInt(null=True)
+    notify_channel_id = BigInt(null=True)
+
+    paused = Boolean(null=False, default=False)
+    repeat_current = Boolean(null=False, default=False)
+    repeat_queue = Boolean(null=False, default=False)
+    shuffle = Boolean(null=False, default=True)
+    auto_shuffle = Boolean(null=False, default=False)
+    auto_play = Boolean(null=False, default=False)
+    playing = Boolean(null=False, default=False)
+    effect_enabled = Boolean(null=False, default=False)
+    self_deaf = Boolean(null=False, default=False)
+
+    current = JSONB(null=True)
+    queue = JSONB(null=False, default=[])
+    history = JSONB(null=False, default=[])
+    effects = JSONB(null=False, default={})
+    extras = JSONB(null=False, default={})
+
+
+class PlayerRow(Table, db=DB, tablename="player"):
+    id = BigInt(primary_key=True, index=True)
+    bot = BigInt(index=True, null=False)
+    volume = Integer(null=False, default=100)
+    max_volume = Integer(null=False, default=1000)
+    auto_play_playlist_id = BigInt(null=False, default=1)
+
+    text_channel_id = BigInt(null=True)
+    notify_channel_id = BigInt(null=True)
+    forced_channel_id = BigInt(null=True)
+
+    repeat_current = Boolean(null=False, default=False)
+    repeat_queue = Boolean(null=False, default=False)
+    shuffle = Boolean(null=False, default=True)
+    auto_shuffle = Boolean(null=False, default=False)
+    auto_play = Boolean(null=False, default=True)
+    self_deaf = Boolean(null=False, default=True)
+    empty_queue_dc = JSONB(
+        null=False,
+        default={
+            "enabled": False,
+            "time": 60,
+        },
+    )
+    alone_dc = JSONB(
+        null=False,
+        default={
+            "enabled": False,
+            "time": 60,
+        },
+    )
+    alone_pause = JSONB(
+        null=False,
+        default={
+            "enabled": False,
+            "time": 60,
+        },
+    )
+    extras = JSONB(null=False, default={})
+    effects = JSONB(null=False, default={})
+    dj_users = Array(null=False, default=[], base_column=BigInt())
+    dj_roles = Array(null=False, default=[], base_column=BigInt())
+
+
+class NodeRow(Table, db=DB, tablename="node"):
+    id = BigInt(primary_key=True, index=True)
+    name = Text(null=False)
+    ssl = Boolean(null=False, default=False)
+    resume_key = Text(null=True)
+    resume_timeout = Integer(null=False, default=600)
+    reconnect_attempts = Integer(null=False)
+    search_only = Boolean(null=False, default=False)
+    managed = Boolean(null=False, default=False)
+    disabled_sources = JSONB(null=False, default=[])
+    extras = JSONB(null=True)
+    yaml = JSONB(null=True)
+
+
+class QueryRow(Table, db=DB, tablename="query"):
+    identifier = Text(null=False, index=True, primary_key=True)
+    name = Text(null=True)
+    tracks = JSONB(null=False, default=[])
+    last_updated = Timestamptz(null=False, index=True)
+
+
+class BotVersionRow(Table, db=DB, tablename="version"):
+    bot = BigInt(primary_key=True, index=True)
+    version = Text(null=False)
+
+
+class AioHttpCacheRow(Table, db=DB, tablename="aiohttp_client_cache"):
+    key = Text(primary_key=True, index=True)
+    value = Bytea()
