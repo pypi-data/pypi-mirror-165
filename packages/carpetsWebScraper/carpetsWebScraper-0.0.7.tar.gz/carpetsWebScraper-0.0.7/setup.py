337947aa690d4de0eda4c3658a7eff963d3a17e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:38 +0000

    Add package carpetsWebScraper/0.0.7/carpetsWebScraper-0.0.7.tar.gz

---
+++ packages/carpetsWebScraper/carpetsWebScraper-0.0.7.tar.gz/carpetsWebScraper-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.7'
+DESCRIPTION = "A simplified webscraper that's based on Selenium."
+LONG_DESCRIPTION = "A simplified webscraper that's based on Selenium."
+
+# Setting up
+setup(
+    name="carpetsWebScraper",
+    version=VERSION,
+    author="thecarpetjasp",
+    author_email="thecarpetjasp@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['selenium','pandas','bs4','requests'],
+    keywords=['python', 'selenium', 'webscraper'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
