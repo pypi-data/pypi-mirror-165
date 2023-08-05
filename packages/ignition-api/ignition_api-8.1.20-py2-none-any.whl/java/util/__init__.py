023f73ad31606d66dfd32a0fd67a4e1dbedb9dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:40 +0000

    Add package ignition-api/8.1.20/ignition_api-8.1.20-py2-none-any.whl

---
+++ packages/ignition-api/ignition_api-8.1.20-py2-none-any.whl/java/util/__init__.py
@@ -0,0 +1,327 @@
+"""Contains the collections framework, legacy collection classes, event
+model, date and time facilities, internationalization, and miscellaneous
+utility classes (a string tokenizer, a random-number generator, and a
+bit array).
+"""
+
+from __future__ import print_function
+
+__all__ = ["Comparator", "Date", "EventObject", "Locale", "UUID"]
+
+from typing import Any, Optional, TypeVar
+
+from dev.thecesrom.utils.decorators import classproperty
+from java.lang import Object, String
+from java.util.function import Function, ToDoubleFunction, ToIntFunction, ToLongFunction
+
+T = TypeVar("T")
+
+
+class Comparator(object):
+    def compare(self, o1, o2):
+        # type: (T, T) -> int
+        raise NotImplementedError
+
+    @staticmethod
+    def comparing(keyExtractor, keyComparator):
+        # type: (Function, Comparator) -> Comparator
+        pass
+
+    @staticmethod
+    def comparingDouble(keyExtractor):
+        # type: (ToDoubleFunction) -> Comparator
+        pass
+
+    @staticmethod
+    def comparingInt(keyExtractor):
+        # type: (ToIntFunction) -> Comparator
+        pass
+
+    @staticmethod
+    def comparingLong(keyExtractor):
+        # type: (ToLongFunction) -> Comparator
+        pass
+
+    def equals(self, obj):
+        # type: (Object) -> bool
+        raise NotImplementedError
+
+    @staticmethod
+    def naturalOrder():
+        # type: () -> Comparator
+        pass
+
+    @staticmethod
+    def nullsFirst(comparator):
+        # type: (Comparator) -> Comparator
+        pass
+
+    @staticmethod
+    def nullsLast(comparator):
+        # type: (Comparator) -> Comparator
+        pass
+
+    def reversed(self):
+        # type: () -> Comparator
+        pass
+
+    @staticmethod
+    def reverseOrder():
+        # type: () -> Comparator
+        pass
+
+    def thenComparing(self, *args):
+        # type: (Any) -> Comparator
+        pass
+
+    def thenComparingDouble(self, keyExtractor):
+        # type: (ToDoubleFunction) -> Comparator
+        pass
+
+    def thenComparingInt(self, keyExtractor):
+        # type: (ToIntFunction) -> Comparator
+        pass
+
+    def thenComparingLong(self, keyExtractor):
+        # type: (ToLongFunction) -> Comparator
+        pass
+
+
+class Date(Object):
+    """The class Date represents a specific instant in time, with
+    millisecond precision.
+    """
+
+    def __init__(self, date=None):
+        # type: (Optional[long]) -> None
+        print(self, date)
+
+    def after(self, when):
+        # type: (Date) -> bool
+        pass
+
+    def before(self, when):
+        # type: (Date) -> bool
+        pass
+
+    def compareTo(self, anotherDate):
+        # type: (Date) -> int
+        pass
+
+    def getTime(self):
+        # type: () -> long
+        pass
+
+    def setTime(self, time):
+        # type: (long) -> None
+        pass
+
+
+class EventObject(Object):
+    """The root class from which all event state objects shall be
+    derived.
+
+    All Events are constructed with a reference to the object, the
+    "source", that is logically deemed to be the object upon which the
+    Event in question initially occurred upon.
+    """
+
+    source = None  # type: Object
+
+    def __init__(self, source):
+        # type: (Object) -> None
+        self.source = source
+
+    def getSource(self):
+        return self.source
+
+
+class Locale(Object):
+    """A Locale object represents a specific geographical, political, or
+    cultural region. An operation that requires a Locale to perform its
+    task is called locale-sensitive and uses the Locale to tailor
+    information for the user. For example, displaying a number is a
+    locale-sensitive operation; the number should be formatted according
+    to the customs and conventions of the user's native country, region,
+    or culture.
+    """
+
+    country = None  # type: Optional[str]
+    language = None  # type: str
+    variant = None  # type: Optional[str]
+
+    def __init__(self, language, country=None, variant=None):
+        # type: (str, Optional[str], Optional[str]) -> None
+        self.language = language
+        self.country = country
+        self.variant = variant
+
+    def __repr__(self):
+        return "{!r}".format(self.__str__())
+
+    def __str__(self):
+        ret = self.language
+        if self.country:
+            ret += "_{}".format(self.country)
+        if self.variant:
+            ret += "_{}".format(self.variant)
+        return unicode(ret)
+
+    @classproperty
+    def CANADA(self):
+        # type: () -> Locale
+        return Locale("en", "CA")
+
+    @classproperty
+    def CANADA_FRENCH(self):
+        # type: () -> Locale
+        return Locale("fr", "CA")
+
+    @classproperty
+    def CHINA(self):
+        # type: () -> Locale
+        return Locale("zh", "CN")
+
+    @classproperty
+    def CHINESE(self):
+        # type: () -> Locale
+        return Locale("zh")
+
+    @classproperty
+    def ENGLISH(self):
+        # type: () -> Locale
+        return Locale("en")
+
+    @classproperty
+    def FRANCE(self):
+        # type: () -> Locale
+        return Locale("fr", "FR")
+
+    @classproperty
+    def FRENCH(self):
+        # type: () -> Locale
+        return Locale("fr")
+
+    @classproperty
+    def GERMAN(self):
+        # type: () -> Locale
+        return Locale("de")
+
+    @classproperty
+    def GERMANY(self):
+        # type: () -> Locale
+        return Locale("de", "DE")
+
+    @classproperty
+    def ITALIAN(self):
+        # type: () -> Locale
+        return Locale("it")
+
+    @classproperty
+    def ITALY(self):
+        # type: () -> Locale
+        return Locale("it", "IT")
+
+    @classproperty
+    def JAPAN(self):
+        # type: () -> Locale
+        return Locale("ja", "JP")
+
+    @classproperty
+    def JAPANESE(self):
+        # type: () -> Locale
+        return Locale("ja")
+
+    @classproperty
+    def KOREA(self):
+        # type: () -> Locale
+        return Locale("ko", "KR")
+
+    @classproperty
+    def KOREAN(self):
+        # type: () -> Locale
+        return Locale("ko")
+
+    @classproperty
+    def PRC(self):
+        # type: () -> Locale
+        return Locale("zh", "CN")
+
+    @classproperty
+    def SIMPLIFIED_CHINESE(self):
+        # type: () -> Locale
+        return Locale("zh", "CN")
+
+    @classproperty
+    def TAIWAN(self):
+        # type: () -> Locale
+        return Locale("zh", "TW")
+
+    @classproperty
+    def TRADITIONAL_CHINESE(self):
+        # type: () -> Locale
+        return Locale("zh", "TW")
+
+    @classproperty
+    def UK(self):
+        # type: () -> Locale
+        return Locale("en", "GB")
+
+    @classproperty
+    def US(self):
+        # type: () -> Locale
+        return Locale("en", "US")
+
+
+class UUID(Object):
+    def __init__(self, mostSigBits, leastSigBits):
+        # type: (long, long) -> None
+        self._leastSigbits = leastSigBits
+        self._mostSigBits = mostSigBits
+
+    def clockSequence(self):
+        # type: () -> int
+        pass
+
+    def compareTo(self, val):
+        # type: (UUID) -> int
+        pass
+
+    @staticmethod
+    def fromString(name):
+        # type: (String) -> UUID
+        pass
+
+    def getLeastSignificantBits(self):
+        # type: () -> long
+        return self._leastSigbits
+
+    def getMostSignificantBits(self):
+        # type: () -> long
+        return self._mostSigBits
+
+    @staticmethod
+    def nameUUIDFromBytes(name):
+        # type: (bytearray) -> UUID
+        pass
+
+    def node(self):
+        # type: () -> long
+        pass
+
+    @staticmethod
+    def randomUUID():
+        # type: () -> UUID
+        pass
+
+    def timestamp(self):
+        # type: () -> long
+        pass
+
+    def variant(self):
+        # type: () -> int
+        pass
+
+    def version(self):
+        # type: () -> int
+        pass
---
+++ packages/ignition-api/ignition_api-8.1.20-py2-none-any.whl/javax/swing/__init__.py
@@ -0,0 +1,292 @@
+from __future__ import print_function
+
+__all__ = [
+    "Icon",
+    "JComponent",
+    "JDesktopPane",
+    "JFrame",
+    "JInternalFrame",
+    "JLabel",
+    "JLayeredPane",
+    "JOptionPane",
+    "JPanel",
+    "JPopupMenu",
+    "JTextField",
+]
+
+from typing import Any, List, Optional
+
+from java.awt import Container, Frame
+from java.lang import String
+from javax.swing.text import JTextComponent
+
+
+class Icon(object):
+    def getIconHeight(self):
+        raise NotImplementedError
+
+    def getIconWidth(self):
+        raise NotImplementedError
+
+    def paintIcon(self, c, g, x, y):
+        raise NotImplementedError
+
+
+class JComponent(Container):
+    def __init__(self):
+        # type: () -> None
+        pass
+
+
+class JFrame(Frame):
+    def __init__(self, *args):
+        # type: (Any) -> None
+        super(JFrame, self).__init__(*args)
+
+
+class JInternalFrame(JComponent):
+    def __init__(
+        self,
+        title=None,  # type: Optional[String]
+        resizable=None,  # type: Optional[bool]
+        closable=None,  # type: Optional[bool]
+        maximizable=None,  # type: Optional[bool]
+        iconifiable=None,  # type: Optional[bool]
+    ):
+        # type: (...) -> None
+        super(JInternalFrame, self).__init__()
+        print(title, resizable, closable, maximizable, iconifiable)
+
+
+class JLabel(JComponent):
+    """A display area for a short text string or an image, or both."""
+
+    def __init__(self, *args):
+        # type: (Any) -> None
+        super(JLabel, self).__init__()
+        print(args)
+
+
+class JLayeredPane(JComponent):
+    DEFAULT_LAYER = 0
+    DRAG_LAYER = 400
+    FRAME_CONTENT_LAYER = -30000
+    LAYER_PROPERTY = "layeredContainerLayer"
+    MODAL_LAYER = 200
+    PALETTE_LAYER = 100
+    POPUP_LAYER = 300
+
+    def __init__(self):
+        # type: () -> None
+        super(JLayeredPane, self).__init__()
+
+    def getComponentCountInLayer(self, layer):
+        pass
+
+    def getLayer(self, c):
+        pass
+
+    def highestLayer(self):
+        pass
+
+    def lowestLayer(self):
+        pass
+
+    def setPosition(self, c, position):
+        pass
+
+
+class JDesktopPane(JLayeredPane):
+    def __init__(self):
+        # type: () -> None
+        super(JDesktopPane, self).__init__()
+
+    def getAllFrames(self):
+        pass
+
+    def getAllFramesInLayer(self):
+        pass
+
+    def getDesktopManager(self):
+        pass
+
+    def getDragMode(self):
+        pass
+
+    def getSelectedFrame(self):
+        pass
+
+    def getUI(self):
+        pass
+
+    def getUIClassID(self):
+        pass
+
+    def updateUI(self):
+        pass
+
+
+class JOptionPane(JComponent):
+    """JOptionPane makes it easy to pop up a standard dialog box that
+    prompts users for a value or informs them of something. For
+    information about using JOptionPane, see How to Make Dialogs, a
+    section in The Java Tutorial.
+    """
+
+    # messageType.
+    PLAIN_MESSAGE = -1
+    ERROR_MESSAGE = 0
+    INFORMATION_MESSAGE = 1
+    WARNING_MESSAGE = 2
+    QUESTION_MESSAGE = 3
+
+    # optionType.
+    DEFAULT_OPTION = -1
+    YES_NO_OPTION = 0
+    YES_NO_CANCEL_OPTION = 1
+    OK_CANCEL_OPTION = 2
+
+    # When one of the showXxxDialog methods returns an integer, the
+    # possible values are:
+    CLOSED_OPTION = -1
+    OK_OPTION = 0
+    YES_OPTION = 0
+    NO_OPTION = 1
+    CANCEL_OPTION = 2
+
+    @staticmethod
+    def showConfirmDialog(
+        parentComponent,  # type: Optional[Any]
+        message,  # type: Any
+        title=None,  # type: Optional[String]
+        optionType=None,  # type: Optional[int]
+        messageType=None,  # type: Optional[int]
+        icon=None,  # type: Optional[Icon]
+    ):
+        # type: (...) -> int
+        print(parentComponent, message, title, optionType, messageType, icon)
+        return JOptionPane.YES_OPTION
+
+    @staticmethod
+    def showInputDialog(
+        parentComponent,  # type: Optional[Any]
+        message,  # type: Any
+        title=None,  # type: Optional[String]
+        messageType=None,  # type: Optional[int]
+        icon=None,  # type: Optional[Icon]
+        selectionValues=None,  # type: Optional[List[Any]]
+        initialSelectionValue=None,  # type: Optional[Any]
+    ):
+        # type: (...) -> String
+        print(
+            parentComponent,
+            message,
+            title,
+            messageType,
+            icon,
+            selectionValues,
+            initialSelectionValue,
+        )
+        return "Input"
+
+    @staticmethod
+    def showMessageDialog(
+        parentComponent,  # type: Optional[Any]
+        message,  # type: Any
+        title=None,  # type: Optional[String]
+        messageType=None,  # type: Optional[int]
+        icon=None,  # type: Optional[Icon]
+    ):
+        # type: (...) -> None
+        print(parentComponent, message, title, messageType, icon)
+
+    @staticmethod
+    def showOptionDialog(
+        parentComponent,  # type: Optional[Any]
+        message,  # type: Any
+        title=None,  # type: Optional[String]
+        optionType=None,  # type: Optional[int]
+        messageType=None,  # type: Optional[int]
+        icon=None,  # type: Optional[Icon]
+        options=None,  # type: Optional[List[Any]]
+        initialValue=None,  # type: Optional[Any]
+    ):
+        # type: (...) -> int
+        print(
+            parentComponent,
+            message,
+            title,
+            optionType,
+            messageType,
+            icon,
+            options,
+            initialValue,
+        )
+        return JOptionPane.YES_OPTION
+
+
+class JPanel(JComponent):
+    def __init__(self, *args):
+        # type: (Any) -> None
+        super(JPanel, self).__init__()
+        print(args)
+
+
+class JToolTip(JComponent):
+    def getAccessibleContext(self):
+        pass
+
+    def getComponent(self):
+        print(self)
+        return JComponent()
+
+    def getTipText(self):
+        pass
+
+
+class JPopupMenu(JComponent):
+    """An implementation of a popup menu -- a small window that pops up
+    and displays a series of choices. A JPopupMenu is used for the menu
+    that appears when the user selects an item on the menu bar. It is
+    also used for "pull-right" menu that appears when the selects a menu
+    item that activates it. Finally, a JPopupMenu can also be used
+    anywhere else you want a menu to appear. For example, when the user
+    right-clicks in a specified area.
+    """
+
+    def __init__(self, label=None):
+        # type: (Optional[String]) -> None
+        super(JPopupMenu, self).__init__()
+        print(label)
+
+    def addAncestorListener(self, listener):
+        pass
+
+    def addNotify(self):
+        pass
+
+    def createToolTip(self):
+        print(self)
+        return JToolTip()
+
+    @staticmethod
+    def getDefaultLocale():
+        pass
+
+    @staticmethod
+    def isLightweightComponent(c):
+        pass
+
+    @staticmethod
+    def setDefaultLocale(l):
+        pass
+
+
+class JTextField(JTextComponent):
+    """JTextField is a lightweight component that allows the editing of
+    a single line of text.
+    """
+
+    def __init__(self, *args):
+        # type: (Any) -> None
+        pass
