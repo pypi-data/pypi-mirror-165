5b7585c9b29583e59b2b74eb67939edd297596c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:59 +0000

    Add package PyFstat/1.17.0/PyFstat-1.17.0-py3-none-any.whl

---
+++ packages/PyFstat/PyFstat-1.17.0-py3-none-any.whl/pyfstat/__init__.py
@@ -0,0 +1,42 @@
+from ._version import get_versions
+from .core import (
+    BaseSearchClass,
+    ComputeFstat,
+    SearchForSignalWithJumps,
+    SemiCoherentGlitchSearch,
+    SemiCoherentSearch,
+)
+from .grid_based_searches import (
+    DMoff_NO_SPIN,
+    FrequencySlidingWindow,
+    GridGlitchSearch,
+    GridSearch,
+    GridUniformPriorSearch,
+    SliceGridSearch,
+    TransientGridSearch,
+)
+from .gridcorner import gridcorner
+from .injection_parameters import (
+    AllSkyInjectionParametersGenerator,
+    InjectionParametersGenerator,
+)
+from .make_sfts import (
+    BinaryModulatedWriter,
+    FrequencyAmplitudeModulatedArtifactWriter,
+    FrequencyModulatedArtifactWriter,
+    GlitchWriter,
+    LineWriter,
+    Writer,
+)
+from .mcmc_based_searches import (
+    MCMCFollowUpSearch,
+    MCMCGlitchSearch,
+    MCMCSearch,
+    MCMCSemiCoherentSearch,
+    MCMCTransientSearch,
+)
+from .snr import DetectorStates, SignalToNoiseRatio
+from .tcw_fstat_map_funcs import pyTransientFstatMap
+
+__version__ = get_versions()["version"]
+del get_versions
