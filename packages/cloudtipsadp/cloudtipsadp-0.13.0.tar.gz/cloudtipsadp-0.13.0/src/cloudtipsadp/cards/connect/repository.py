c1d1d8ef9627291e0886cf2e1dd0ef013af23bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:24 +0000

    Add package cloudtipsadp/0.13.0/cloudtipsadp-0.13.0.tar.gz

---
+++ packages/cloudtipsadp/cloudtipsadp-0.13.0.tar.gz/cloudtipsadp-0.13.0/src/cloudtipsadp/cards/connect/repository.py
@@ -0,0 +1,53 @@
+import json
+
+import requests
+
+from cloudtipsadp.connect.clients import Connect
+from cloudtipsadp.connect.repository import Repository
+
+
+class CardRepository(Repository):
+    """Заведения."""
+
+    def __init__(self,
+                 req: requests = None,
+                 session: Connect = None,
+                 base_path: str = None, ):
+        super(CardRepository, self).__init__(req, session, base_path)
+
+    def get(self, obj_id: str):
+        """Список карт получателя."""
+        url = self(self.base_path)
+        parsed = self.req.get(url, params=dict(userId=obj_id),
+                              headers=self.session.get_headers()).json()
+        return parsed
+
+    def list(self):
+        pass
+
+    def save(self, obj):
+        pass
+
+    def update(self, obj):
+        pass
+
+    def default(self, user_id, card_token):
+        """Изменить карту, на которую выплачиваются чаевые по умолчанию."""
+        url = self(self.base_path, 'default')
+        return self.req.post(url, dict(
+            userId=user_id, cardToken=card_token),
+                             headers=self.session.get_headers()).json()
+
+    def delete(self, user_id, card_token):
+        """Удаление карты получателя. Карту по умолчанию удалить нельзя."""
+        url = self(self.base_path)
+        return self.req.delete(
+            url, data=json.dumps(dict(userId=user_id, cardToken=card_token)),
+            headers=self.session.get_headers()).json()
+
+    def auth(self, user_id, checkout):
+        """Привязка карты получателю."""
+        url = self(self.base_path, 'auth')
+        return self.req.post(url, json.dumps(
+            dict(CardholderName='NONE', CardCryptogramPacket=checkout,
+                 UserId=user_id)), headers=self.session.get_headers()).json()
---
+++ packages/cloudtipsadp/cloudtipsadp-0.13.0.tar.gz/cloudtipsadp-0.13.0/src/cloudtipsadp/main.py
@@ -0,0 +1,56 @@
+from cloudtipsadp.accumulations.services import (accum_payout_receiver,
+                                                     accum_summary)
+from cloudtipsadp.cards.services import (card_auth, card_default,
+                                             card_delete, card_get)
+from cloudtipsadp.payouts.services import payout
+from cloudtipsadp.places.services import (place_confirm, place_list,
+                                              place_send)
+from cloudtipsadp.receivers.services import (photo_load, receiver_create,
+                                                 receiver_detach_agent,
+                                                 receiver_get, receiver_pages)
+
+
+class Cloudtipsadp:
+    __link = None
+
+    def __init__(self):
+        self.accums_summary = accum_summary
+        self.accums_payout_receiver = accum_payout_receiver
+        self.cards_auth = card_auth
+        self.cards_delete = card_delete
+        self.cards_default = card_default
+        self.cards_get = card_get
+        self.payouts = payout
+        self.places_confirm = place_confirm
+        self.places_get = place_list
+        self.places_send_sms = place_send
+        self.receivers_create = receiver_create
+        self.receivers_get = receiver_get
+        self.receivers_detach_agent = receiver_detach_agent
+        self.receivers_pages = receiver_pages
+        self.receivers_photo = photo_load
+
+    def connect(self, sandbox=False):
+        """Подключение."""
+        if sandbox:
+            self.__link = self.__connect(self.__sandbox_client())
+        self.__link = self.__connect()
+
+    def get_token(self):
+        return self.__connect.get_token()
+
+    def refresh_token(self):
+        return self.__link.refresh_token()
+
+
+if __name__ == '__main__':
+    cta = Cloudtipsadp()
+    user_id = '44a38440-595d-494e-a028-09804355757a'
+    photo_path = '/home/setter/Изображения/Adam.jpg'
+
+    response = cta.accums_payout_receiver(user_id)
+    if type(response) == dict and response.get('succeed'):
+        print('Выплата накопления получателю:')
+        print(response.get('data'))
+    else:
+        print(f'RESPONSE-0: {response}')
