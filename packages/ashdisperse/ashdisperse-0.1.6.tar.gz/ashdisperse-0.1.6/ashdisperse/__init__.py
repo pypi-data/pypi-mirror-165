29f46145284931faec60922150f94f82b072e39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:38 +0000

    Add package ashdisperse/0.1.6/ashdisperse-0.1.6.tar.gz

---
+++ packages/ashdisperse/ashdisperse-0.1.6.tar.gz/ashdisperse-0.1.6/ashdisperse/__init__.py
@@ -0,0 +1,8 @@
+from ashdisperse.version import __version__
+from ashdisperse.ashdisperse import setup, solve
+from ashdisperse.params import copy_parameters, update_parameters, save_parameters, load_parameters
+from ashdisperse.interface import set_met, set_parameters
+from ashdisperse.met import save_met, load_met, wind_plot
+
+
+__author__ = 'Mark Woodhouse'
---
+++ packages/ashdisperse/ashdisperse-0.1.6.tar.gz/ashdisperse-0.1.6/ashdisperse/interface/__init__.py
@@ -0,0 +1,7 @@
+from .interface import set_grain_parameters
+from .interface import set_met
+from .interface import set_output_parameters
+from .interface import set_parameters
+from .interface import set_physical_parameters
+from .interface import set_solver_parameters
+from .interface import set_source_parameters
