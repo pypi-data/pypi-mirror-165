dfe2c45136248f5cbca9535be72302092f94d637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:40 +0000

    Add package dequeai/0.5/dequeai-0.5.tar.gz

---
+++ packages/dequeai/dequeai-0.5.tar.gz/dequeai-0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+
+setup(
+    name='dequeai',
+    version='0.000005',
+    description='Python Package for Deque tools',
+    author="The Deque Team",
+    author_email='team@deque.app',
+    packages=["deque"],
+    url='https://github.com/rijupahwa/deque',
+    keywords='deque client for experiment tracking, sweep and other deep learning tooling',
+    install_requires=[
+          "coolname","requests","pillow","numpy","psutil","GPUtil","altair"
+      ],
+)
