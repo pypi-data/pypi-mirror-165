3ea42b50e9be41a548aac245944e2d4111899c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:33 +0000

    Add package pawnlib/0.0.16/pawnlib-0.0.16-py3-none-any.whl

---
+++ packages/pawnlib/pawnlib-0.0.16-py3-none-any.whl/pawnlib/output/__init__.py
@@ -0,0 +1,32 @@
+from .file import (
+    check_file_overwrite,
+    get_file_path,
+    get_parent_path,
+    get_abs_path,
+    get_real_path,
+    is_binary_file,
+    is_file,
+    is_json,
+    open_json,
+    open_file,
+    open_yaml_file,
+    write_file,
+    write_json,
+    write_yaml
+
+)
+from .color_print import (
+    bcolors,
+    colored,
+    cprint,
+    TablePrinter,
+    get_bcolors,
+    colored_input,
+    dump,
+    debug_print,
+    classdump,
+    kvPrint,
+    print_json,
+    debug_logging,
+    print_progress_bar,
+)
---
+++ packages/pawnlib/pawnlib-0.0.16-py3-none-any.whl/pawnlib/resource/net.py
@@ -0,0 +1,87 @@
+from pawnlib.config.globalconfig import pawnlib_config as pawn
+import socket
+from pawnlib.utils import http
+
+prev_getaddrinfo = socket.getaddrinfo
+
+
+class OverrideDNS:
+    _dns_cache = {}
+
+    def __init__(self, domain="", ipaddr="", port=80):
+        self._dns_cache[domain] = ipaddr
+        self.prv_getaddrinfo = prev_getaddrinfo
+
+    def new_getaddrinfo(self, *args):
+        if args[0] in self._dns_cache:
+            if pawn.verbose:
+                print("Forcing FQDN: {} to IP: {}".format(args[0], self._dns_cache[args[0]]))
+            return self.prv_getaddrinfo(self._dns_cache[args[0]], *args[1:])
+        else:
+            return self.prv_getaddrinfo(*args)
+
+    def set(self):
+        socket.getaddrinfo = self.new_getaddrinfo
+
+    def unset(self):
+        socket.getaddrinfo = self.prv_getaddrinfo
+
+
+def get_public_ip():
+    """
+    Get the public IP address
+
+    :return:
+
+    Example:
+
+        .. code-block:: python
+
+            from pawnlib.resource import net
+            net.get_public_ip()
+
+    """
+    return http.jequest("http://checkip.amazonaws.com").get('text', "").strip()
+
+
+def get_local_ip():
+    """
+
+    Get the local IP address
+
+    :return:
+
+    Example:
+
+        .. code-block:: python
+
+            from pawnlib.resource import net
+            net.get_local_ip()
+
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        ipaddr = s.getsockname()[0]
+    except Exception:
+        ipaddr = '127.0.0.1'
+    finally:
+        s.close()
+    return ipaddr
+
+
+def get_hostname():
+    """
+
+    Get the local hostname
+
+    :return:
+    Example:
+
+        .. code-block:: python
+
+            from pawnlib.resource import net
+            net.get_hostname()
+
+    """
+    return socket.gethostname()
