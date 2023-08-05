925325b24c3a49c85a25d434fdd08839b3b7d103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:25 +0000

    Add package valorant.py/1.0.0/valorant.py-1.0.0.tar.gz

---
+++ packages/valorant.py/valorant.py-1.0.0.tar.gz/valorant.py-1.0.0/setup.py
@@ -0,0 +1,46 @@
+import re
+from setuptools import setup
+
+# inspired by https://github.com/Rapptz/discord.py/blob/master/setup.py
+
+requirements = ['aiohttp']
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
+
+version = ''
+with open('valorant/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+packages = ['valorant']
+
+setup(
+    name='valorant.py',
+    author='xStacia',
+    url='https://github.com/staciax/valorantx',
+    project_urls={
+        "Issue tracker": "https://github.com/staciax/valorantx/issues",
+    },
+    version=version,
+    packages=packages,
+    license='MIT',
+    description='An Asynchronous Valorant API Wrapper for Python',
+    long_description=readme,
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires='>=3.8.0',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
