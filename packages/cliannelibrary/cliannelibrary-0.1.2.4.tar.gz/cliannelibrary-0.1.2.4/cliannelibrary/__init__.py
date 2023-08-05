1d0ab041e5f54ee595f239a6d75bae0c06d47665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:30 +0000

    Add package cliannelibrary/0.1.2.4/cliannelibrary-0.1.2.4.tar.gz

---
+++ packages/cliannelibrary/cliannelibrary-0.1.2.4.tar.gz/cliannelibrary-0.1.2.4/cliannelibrary/__init__.py
@@ -0,0 +1,21 @@
+"""
+:author: Dmitry Ostroushko
+:copyright: (c) 2021 Dmitry Ostroushko
+"""
+
+
+from file_library import (FileLibrary)
+from exception_library.files_exception import (FileAlreadyExistsException)
+from exception_library.xml_exception import (XMLEmptyNodeListException, \
+                                                XMLManyNodeInListException, \
+                                                XMLNodeNotExistException, \
+                                                XMLInvalidOperationException, \
+                                                XMLInvalidNodeKindException, \
+                                                XMLВiscrepancyNodeKindOperationException)
+from .xml_library import (XMLLibrary)
+import cliannelibrary.parse_library as parse_library
+
+
+__author__ = 'Dmitry Ostroushko'
+__version__ = '0.1.2.2'
+__email__ = 'dmitrylianne@gmail.com'
