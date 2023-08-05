b8be7b7b8d5071ed898e41846cd697fe4f8da560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:51 +0000

    Add package abby/0.0.7/abby-0.0.7-py3-none-any.whl

---
+++ packages/abby/abby-0.0.7-py3-none-any.whl/abby/__init__.py
@@ -0,0 +1,4 @@
+"""Seamless A/B testing with Abby."""
+from abby import compare  # noqa
+
+__version__ = "0.0.7"
---
+++ packages/abby/abby-0.0.7-py3-none-any.whl/abby/tests/test_ttest.py
@@ -0,0 +1,36 @@
+from typing import Callable
+
+import pandas as pd
+import pytest
+
+from abby.compare import compare_ttest
+
+
+@pytest.fixture
+def sleeps() -> pd.DataFrame:
+    data = pd.read_csv("abby/tests/datasets/sleeps.csv").assign(
+        variant_name=lambda df: df["group"].map({1: "control", 2: "experiment"})
+    )
+    return data
+
+
+@pytest.fixture
+def sleeps_wrong(sleeps):
+    return sleeps.rename(columns={"variant_name": "group"})
+
+
+class TestCompareTtest:
+    def test_ttest_result(self, sleeps: Callable):
+        result = compare_ttest(sleeps, ["control", "experiment"], "extra")
+        assert result["control_mean"] == pytest.approx(0.75, rel=4)
+        assert result["experiment_mean"] == pytest.approx(2.33, rel=4)
+        assert result["control_var"] == pytest.approx(3.200556, rel=4)
+        assert result["experiment_var"] == pytest.approx(4.009000, rel=4)
+        assert result["absolute_difference"] == pytest.approx(1.5800, rel=4)
+        assert result["lower_bound"] == pytest.approx(-0.2054832, rel=4)
+        assert result["upper_bound"] == pytest.approx(3.3654832, rel=4)
+        assert result["p_values"] == pytest.approx(0.07939, rel=4)
+
+    def test_wrong_variant_column_name(self, sleeps_wrong: Callable):
+        with pytest.raises(AssertionError):
+            compare_ttest(sleeps_wrong, ["control", "experiment"], "extra")
