e0e18fc78cbfdcff4bd8db56629d6af81e385f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:06 +0000

    Add package hanlp/2.1.0b41/hanlp-2.1.0b41-py3-none-any.whl

---
+++ packages/hanlp/hanlp-2.1.0b41-py3-none-any.whl/hanlp/pretrained/classifiers.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Author: hankcs
+# Date: 2020-01-01 03:51
+from hanlp_common.constant import HANLP_URL
+
+CHNSENTICORP_BERT_BASE_ZH = HANLP_URL + 'classification/chnsenticorp_bert_base_20211228_163210.zip'
+SST2_ALBERT_BASE_EN = HANLP_URL + 'classification/sst2_albert_base_20211228_164917.zip'
+
+ALL = {}
