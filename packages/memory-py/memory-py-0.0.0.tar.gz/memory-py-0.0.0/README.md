a0cb27496593aac3c05e0951a57193bd2a9eb749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:06 +0000

    Add package memory-py/0.0.0/memory-py-0.0.0.tar.gz

---
+++ packages/memory-py/memory-py-0.0.0.tar.gz/memory-py-0.0.0/README.md
@@ -0,0 +1,15 @@
+# `memory`
+
+Approximate memory usage profiler.
+
+## Usage
+
+```
+time memory sleep 1
+```
+
+## Install
+
+```
+pip3 install memory
+```
---
+++ packages/memory-py/memory-py-0.0.0.tar.gz/memory-py-0.0.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+setuptools.setup(
+    name="memory-py",
+    version="0.0.0",
+    description="Approximate memory usage profiler",
+    long_description="Approximate memory usage profiler.",
+    long_description_content_type="text/markdown",
+    author="Vladimir Chebotarev",
+    author_email="vladimir.chebotarev@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",  # FIXME
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    keywords=[],  # FIXME
+    project_urls={
+        "Documentation": "https://github.com/excitoon/memory/blob/master/README.md",
+        "Source": "https://github.com/excitoon/memory",
+        "Tracker": "https://github.com/excitoon/memory/issues",
+    },
+    url="https://github.com/excitoon/memory",
+    packages=[],
+    scripts=["memory", "memory.cmd"],
+    install_requires=["psutil"],
+)
