a42144208ff66202eae8a200245c2f119d963653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:44 +0000

    Add package libdev/0.38/libdev-0.38.tar.gz

---
+++ packages/libdev/libdev-0.38.tar.gz/libdev-0.38/libdev/num.py
@@ -0,0 +1,24 @@
+"""
+Numbers functionality
+"""
+
+def is_float(value: str) -> bool:
+    """ Check value for float """
+
+    try:
+        float(value)
+    except (ValueError, TypeError):
+        return False
+
+    return True
+
+def to_num(value) -> bool:
+    """ Convert value to int or float """
+
+    if isinstance(value, str):
+        value = float(value.strip())
+
+    if not value % 1:
+        value = int(value)
+
+    return value
