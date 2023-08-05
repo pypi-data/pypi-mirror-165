dd2a9d1838b05293a4359389c754583ef556be7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:54 +0000

    Add package unredoable/0.0.4/unredoable-0.0.4.tar.gz

---
+++ packages/unredoable/unredoable-0.0.4.tar.gz/unredoable-0.0.4/README.md
@@ -0,0 +1,49 @@
+# __unredoable__
+Python package providing object-specific undoing & redoing functionality through wrapper class
+
+[![Build](https://github.com/w2sv/unredoable/actions/workflows/build.yaml/badge.svg)](https://github.com/w2sv/unredoable/actions/workflows/build.yaml)
+[![codecov](https://codecov.io/gh/w2sv/unredoable/branch/master/graph/badge.svg?token=9EESND69PG)](https://codecov.io/gh/w2sv/unredoable)
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+![PyPI](https://img.shields.io/pypi/v/unredoable)
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+## Download
+```
+pip install unredoable
+```
+
+## Usage
+
+```python
+from unredoable import Unredoable
+
+class StateManager:
+    def __init__(self, state_variable):
+        self.unredoable_state_variable = Unredoable(state_variable, max_stack_depths=10, craft_deep_copies=False)
+        
+        # state_variable may be of whatever type, whether custom or not, 
+        # the sole restraint it's subject to, is that is needs to implement 
+        # either __copy__ or __deepcopy__, depending on the passed 
+        # 'craft_deep_copies' parameter
+        
+    def alter_state_variable(self):
+        self.unredoable_state_variable.push_state()
+        
+        self.unredoable_state_variable.obj += 1
+
+if __name__ == '__main__':
+    manager = StateManager(69)
+    
+    manager.alter_state_variable()
+    manager.alter_state_variable()
+    
+    manager.unredoable_state_variable.undo()  # unredoable_state_variable = 70
+    manager.unredoable_state_variable.redo()  # unredoable_state_variable = 71
+```
+
+
+## Author
+Janek Zangenberg
+
+## License
+[MIT](LICENSE)
---
+++ packages/unredoable/unredoable-0.0.4.tar.gz/unredoable-0.0.4/unredoable/_unredoing.py
@@ -0,0 +1,104 @@
+from collections import deque
+from copy import copy, deepcopy
+from functools import wraps
+from typing import Any, Callable, Deque, TypeVar
+
+
+_T = TypeVar('_T')
+_FuncT = TypeVar('_FuncT', bound=Callable[..., Any])
+
+
+class Unredoable:
+    """ Wrapper class adding undo & redo functionality to whatever kind of object
+        implementing __copy__/__deepcopy__ """
+
+    def __init__(self, obj: _T, max_stack_depths: int, craft_deep_copies=True):
+        """ Parameters:
+                obj: object getting wrapped and unresolvable calls will be forwarded to via __getattr__
+                max_stack_depths: maximal number of obj copies to be respectively held by both undo & redo stack
+                craft_deep_copies: whether to craft copies by calling __copy__ or __deepcopy__ """
+
+        self.obj: _T = obj
+        self._craft_deep_copies: bool = craft_deep_copies
+
+        self._undo_stack: Deque[_T] = deque(maxlen=max_stack_depths)
+        self._redo_stack: Deque[_T] = deque(maxlen=max_stack_depths)
+
+    def __getattr__(self, item):
+        """ Forward unresolvable calls to obj """
+
+        return getattr(self.obj, item)
+
+    #################
+    # State pushing #
+    #################
+
+    def push_state(self):
+        """ Pushes copy of current object to undo stack """
+
+        self._push_state_to(self._undo_stack)
+
+    def state_pusher(self, f: _FuncT) -> _FuncT:
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            self.push_state()
+            return f(*args, **kwargs)
+        return wrapper  # type: ignore
+
+    def _push_state_to(self, stack: deque):
+        stack.append(deepcopy(self.obj) if self._craft_deep_copies else copy(self.obj))
+
+    ##########################
+    # Operation availability #
+    ##########################
+
+    @property
+    def is_undoable(self) -> bool:
+        return bool(self._undo_stack)
+
+    @property
+    def is_redoable(self) -> bool:
+        return bool(self._redo_stack)
+
+    #############
+    # Execution #
+    #############
+
+    def undo(self):
+        """ Pushes current state to redo stack and set obj to the uppermost
+        popped element from undo stack
+
+        Raises:
+            AttributeError if stack empty """
+
+        self._push_state_to(self._redo_stack)
+        try:
+            self.obj = self._undo_stack.pop()
+        except IndexError:
+            raise AttributeError('Undo stack empty')
+
+    def redo(self):
+        """ Pushes current state to undo stack and set obj to popped
+        uppermost element from redo stack
+
+        Raises:
+            AttributeError if stack empty """
+
+        self._push_state_to(self._undo_stack)
+        try:
+            self.obj = self._redo_stack.pop()
+        except IndexError:
+            raise AttributeError('Redo stack empty')
+
+    ########
+    # Misc #
+    ########
+
+    def __str__(self):
+        return str(self.obj)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} | ' \
+               f'wrapped obj: {self.obj} | ' \
+               f'undo stack depth: {len(self._undo_stack)}, redo stack depth: {len(self._redo_stack)} | ' \
+               f'max stack depth: {self._redo_stack.maxlen}'
