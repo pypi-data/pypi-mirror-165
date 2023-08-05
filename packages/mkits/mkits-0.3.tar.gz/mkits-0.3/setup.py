c4dca5e0fcf4381d77ac979b0eb80b674b84e25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:30 +0000

    Add package mkits/0.3/mkits-0.3.tar.gz

---
+++ packages/mkits/mkits-0.3.tar.gz/mkits-0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="mkits",
+    description="multi-DFT codes assistant program.",
+    long_description=
+    """mkits 
+    """,
+    version="0.3",
+    author="Leon Ma",
+    author_email="blustery.med@hotmail.com",
+    license="GPLv3+",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    keywords="DFT code assistant",
+    packages=["mkits"],
+    entry_points={"console_scripts": ["mkits = mkits.main:mkits_main"]},
+    
+    install_requires=[
+        "spglib", "numpy", "ase", "pandas"
+    ],
+    python_requires=">=3.5",
+    url="https://github.com/leon-venir/mkits",
+    download_url=(
+        "https://github.com/leon-venir/mkits"
+    )
+)
