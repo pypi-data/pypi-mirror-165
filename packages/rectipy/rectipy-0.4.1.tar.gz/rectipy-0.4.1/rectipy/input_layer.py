3d99355b692e2f7e9832aaf35aea6af606151728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:30 +0000

    Add package rectipy/0.4.1/rectipy-0.4.1.tar.gz

---
+++ packages/rectipy/rectipy-0.4.1.tar.gz/rectipy-0.4.1/rectipy/input_layer.py
@@ -0,0 +1,45 @@
+import torch
+from torch.nn import Module, Linear
+from typing import Iterator
+import numpy as np
+
+
+class InputLayer(Module):
+
+    def __init__(self, n: int, m: int, weights: np.ndarray = None, trainable: bool = False,
+                 dtype: torch.dtype = torch.float64):
+        super().__init__()
+        if trainable:
+            self.layer = Linear(m, n, bias=False, dtype=dtype)
+        else:
+            if weights is None:
+                weights = torch.randn(n, m, dtype=dtype)
+            else:
+                weights = torch.tensor(weights, dtype=dtype)
+            if weights.shape[0] != n or weights.shape[1] != m:
+                raise ValueError("Shape of the provided weights does not match the input and output dimensions of the"
+                                 "input layer.")
+            self.layer = LinearStatic(weights)
+
+    def forward(self, x):
+        return self.layer(x)
+
+    def parameters(self, recurse: bool = True) -> Iterator:
+        return self.layer.parameters(recurse=recurse)
+
+
+class LinearStatic(Module):
+
+    def __init__(self, weights: torch.Tensor):
+        super().__init__()
+        if not isinstance(weights, torch.Tensor):
+            raise TypeError("Weights provided to the input layer have to be of type `torch.Tensor`.")
+        weights.detach()
+        self.weights = weights
+
+    def forward(self, x):
+        return self.weights @ x
+
+    def parameters(self, recurse: bool = True) -> Iterator:
+        for p in []:
+            yield p
