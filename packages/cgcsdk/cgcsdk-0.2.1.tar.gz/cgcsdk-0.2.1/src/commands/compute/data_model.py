c3a98bd33ea71a57f04c4df9e53593c775778d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package cgcsdk/0.2.1/cgcsdk-0.2.1.tar.gz

---
+++ packages/cgcsdk/cgcsdk-0.2.1.tar.gz/cgcsdk-0.2.1/src/commands/compute/data_model.py
@@ -0,0 +1,27 @@
+def compute_create_payload_validator(
+    name, entity, cpu, memory, volumes: list, gpu: int, gpu_type: str
+):
+    """
+    Create payload for pod creation.
+    """
+
+    payload = {
+        "name": name,
+        "entity": entity,
+        "cpu": cpu,
+        "memory": memory,
+        "gpu_type": gpu_type,
+    }
+    if len(volumes) != 0:
+        payload["pv_volume"] = volumes
+    return payload
+
+
+def compute_delete_payload_validator(name):
+    """
+    Create payload for pod creation.
+    """
+    payload = {
+        "name": name,
+    }
+    return payload
