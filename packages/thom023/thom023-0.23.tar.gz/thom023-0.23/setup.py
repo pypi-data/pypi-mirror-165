aab4f283d49e68a82e16d034b33a6c6f82a6f026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:19 +0000

    Add package thom023/0.23/thom023-0.23.tar.gz

---
+++ packages/thom023/thom023-0.23.tar.gz/thom023-0.23/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='thom023',
+      version='0.23',
+      description='Gaussian distributions',
+      packages=['thom023'],
+      author = 'Nguyen Huong Thom',
+      author_email = 'rajatsharma369007@gmail.com',
+      zip_safe=False)
