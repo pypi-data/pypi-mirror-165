fd5c8ff8dd191f4c60ae2722240e340d414cfca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:17 +0000

    Add package cmem-plugin-kafka/1.0.1/cmem-plugin-kafka-1.0.1.tar.gz

---
+++ packages/cmem-plugin-kafka/cmem-plugin-kafka-1.0.1.tar.gz/cmem-plugin-kafka-1.0.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cmem_plugin_kafka', 'cmem_plugin_kafka.workflow']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cmem-plugin-base>=2.1.0,<3.0.0',
+ 'confluent-kafka==1.8.2',
+ 'defusedxml>=0.7.1,<0.8.0']
+
+setup_kwargs = {
+    'name': 'cmem-plugin-kafka',
+    'version': '1.0.1',
+    'description': 'Send messages to Apache Kafka',
+    'long_description': '# cmem-plugin-kafka\n\nThis eccenca Corporate Memory plugin allows for sending messages to Apache Kafka.\n\n',
+    'author': 'eccenca',
+    'author_email': 'cmempy-developer@eccenca.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/eccenca/cmem-plugin-kafka',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
