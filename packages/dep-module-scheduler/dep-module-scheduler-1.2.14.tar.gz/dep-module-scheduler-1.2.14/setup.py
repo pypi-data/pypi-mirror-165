0fa54dca845dc125c4f2fa9f4f5e18e7fdd9f6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:20 +0000

    Add package dep-module-scheduler/1.2.14/dep-module-scheduler-1.2.14.tar.gz

---
+++ packages/dep-module-scheduler/dep-module-scheduler-1.2.14.tar.gz/dep-module-scheduler-1.2.14/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dep_scheduler', 'tests']
+
+package_data = \
+{'': ['*']}
+
+modules = \
+['pyproject']
+install_requires = \
+['APScheduler>=3.9.1,<4.0.0', 'dep-spec>=1.2.14,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dep-module-scheduler',
+    'version': '1.2.14',
+    'description': '',
+    'long_description': None,
+    'author': 'everhide',
+    'author_email': 'i.tolkachnikov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
