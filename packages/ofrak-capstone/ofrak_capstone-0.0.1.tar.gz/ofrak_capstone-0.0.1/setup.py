fc5d0dc1c34517a188a84a5334ccfe2b9789e279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:19 +0000

    Add package ofrak-capstone/0.0.1/ofrak_capstone-0.0.1.tar.gz

---
+++ packages/ofrak-capstone/ofrak_capstone-0.0.1.tar.gz/ofrak_capstone-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="ofrak_capstone",
+    version="0.0.1",
+    description=("OFRAK: unpack, modify, and repack binaries."),
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Red Balloon Security",
+    url="https://ofrak.com",
+    packages=["ofrak_capstone"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Security",
+        "Topic :: Software Development :: Embedded Systems",
+        "Typing :: Typed",
+    ],
+    python_requires=">=3.7",
+)
