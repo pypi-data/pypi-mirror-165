113a410beeb46a205ac8f4992bce7ce9c82eb6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:43 +0000

    Add package easyencryption/0.1.4/easyencryption-0.1.4.tar.gz

---
+++ packages/easyencryption/easyencryption-0.1.4.tar.gz/easyencryption-0.1.4/src/easyencryption/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.0"
+
+from .fernet import fernetencrypt
+from .fernet import fernetdecrypt
+from .pubprivate import pubprivencrypt
+from .pubprivate import pubprivdecrypt
+from .aes import aesencrypt
+from .aes import aesdecrypt
