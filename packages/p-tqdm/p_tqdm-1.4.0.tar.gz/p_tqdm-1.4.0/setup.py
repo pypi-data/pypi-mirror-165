439e5275fd5bfb2de83f90a742b086b62218683a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:40 +0000

    Add package p-tqdm/1.4.0/p_tqdm-1.4.0.tar.gz

---
+++ packages/p-tqdm/p_tqdm-1.4.0.tar.gz/p_tqdm-1.4.0/setup.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+# Load version number
+__version__ = None
+
+src_dir = Path(__file__).parent.absolute()
+version_file = src_dir / 'p_tqdm' / '_version.py'
+
+# Long README
+with open(version_file) as fd:
+    exec(fd.read())
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='p_tqdm',
+    version='1.4.0',
+    author='Kyle Swanson',
+    author_email='swansonk.14@gmail.com',
+    description='Parallel processing with progress bars',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/swansonk14/p_tqdm',
+    download_url='https://github.com/swansonk14/p_tqdm/v_1.3.3.tar.gz',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm>=4.45.0',
+        'pathos>=0.2.5',
+        'six>=1.13.0'
+    ],
+    tests_require=['pytest'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords=[
+        'tqdm',
+        'progress bar',
+        'parallel'
+    ],
+)
