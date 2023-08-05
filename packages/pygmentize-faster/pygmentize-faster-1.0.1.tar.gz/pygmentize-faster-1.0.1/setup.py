3b40f68ceb67ab6d1f753238f0bb7bac0e7cccc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:30 +0000

    Add package pygmentize-faster/1.0.1/pygmentize-faster-1.0.1.tar.gz

---
+++ packages/pygmentize-faster/pygmentize-faster-1.0.1.tar.gz/pygmentize-faster-1.0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pygmentize_faster']
+install_requires = \
+['pygments-cache>=0.1.3,<0.2.0']
+
+entry_points = \
+{'console_scripts': ['pygmentize = pygmentize_faster:main']}
+
+setup_kwargs = {
+    'name': 'pygmentize-faster',
+    'version': '1.0.1',
+    'description': 'Provides a faster version of pygmentize',
+    'long_description': "# Pygmentize Faster\n\nThis package uses [pygments-cache](https://github.com/xonsh/pygments-cache) to run `pygmentize` just a little bit faster.\n\nIt overrides the `pygmentize` script provided by `pygments`.\n\n## Benkmarks\n\n`pygmentize-faster` runs about twice as fast as `pygmentize`:\n\n```bash\n\n$ time fd -d1 -j1 '.py$' -x python -m pygmentize {} > /dev/null ';' /usr/lib/python3.10\nuser=65.25s system=7.81s cpu=98% total=1:14.45\n\n$ time fd -d1 -j1 '.py$' -x python -m pygmentize_faster {} > /dev/null ';' /usr/lib/python3.10\nuser=33.61s system=4.96s cpu=99% total=38.663\n\n```\n",
+    'author': 'Josiah Outram Halstead',
+    'author_email': 'josiah@halstead.email',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/joouha/pygmentize-faster',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
