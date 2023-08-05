30b79ce03fffc0be0c38954ea6ab25e2e58d3c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:05 +0000

    Add package MuhekoNikolasVersionsDumpTest/0.0.1/MuhekoNikolasVersionsDumpTest-0.0.1.tar.gz

---
+++ packages/MuhekoNikolasVersionsDumpTest/MuhekoNikolasVersionsDumpTest-0.0.1.tar.gz/MuhekoNikolasVersionsDumpTest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='MuhekoNikolasVersionsDumpTest',
+    author= "Muheko Nikolas",
+    version= "0.0.1",
+    packages=find_packages(),
+    install_requires=[""]
+)
+
