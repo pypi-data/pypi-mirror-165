a808ed011c130c0a31f735b50e8135e2f0460ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:31 +0000

    Add package webcode-tk/0.3.1/webcode-tk-0.3.1.tar.gz

---
+++ packages/webcode-tk/webcode-tk-0.3.1.tar.gz/webcode-tk-0.3.1/webcode_tk/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+"""Deals with html and css documents to check their code.
+
+Modules exported by this package:
+- `html`: gets html files from a project folder, gets the HTML code
+          from files, gets number of a particular element in a file
+          or folder, gets elements as tags, and much more.
+- `validator`: sends HTML or CSS code to the W3C Validator to check
+          for errors.
+
+"""
+__version__ = "0.3.1"
