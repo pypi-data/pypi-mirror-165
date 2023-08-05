1eb25e5b8250193ff3f3c2b98fb493f63d1c8032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:34 +0000

    Add package cli-command-parser/2022.8.27/cli_command_parser-2022.8.27.tar.gz

---
+++ packages/cli-command-parser/cli_command_parser-2022.8.27.tar.gz/cli_command_parser-2022.8.27/lib/cli_command_parser/inputs/base.py
@@ -0,0 +1,38 @@
+"""
+Custom input handlers for Parameters
+
+:author: Doug Skrypa
+"""
+
+from abc import ABC, abstractmethod
+from typing import Any, Callable
+
+__all__ = ['InputType']
+
+TypeFunc = Callable[[str], Any]
+# TODO: Accept a tuple of types to apply to nargs=>1 in order
+
+
+class InputType(ABC):
+    @abstractmethod
+    def __call__(self, value: str) -> Any:
+        """Process the parsed argument and convert it to the desired type"""
+        raise NotImplementedError
+
+    def is_valid_type(self, value: str) -> bool:  # pylint: disable=W0613
+        """
+        Called during parsing when :meth:`.Parameter.would_accept` is called to determine if the value would be
+        accepted later for processing / conversion via :meth:`.__call__`.  May be overridden in subclasses to
+        provide actual validation, if necessary.
+
+        Not called by :meth:`.Parameter.take_action` - value validation should happen in :meth:`.__call__`
+
+        :param value: A parsed argument
+        :return: True if this input would accept it for processing later (where it may still be rejected), False if
+          it should be rejected before attempting to process / convert / store it.
+        """
+        return True
+
+    def format_metavar(self, choice_delim: str = ',') -> str:
+        # TODO: Optional/required arg, or handle wrapping in []/{} in formatter
+        raise NotImplementedError
