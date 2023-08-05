9309e418ee0002c84c093574c8d88c9bcb8b7bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:58 +0000

    Add package dep-module-mongo/1.3.14/dep-module-mongo-1.3.14.tar.gz

---
+++ packages/dep-module-mongo/dep-module-mongo-1.3.14.tar.gz/dep-module-mongo-1.3.14/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dep_mongo', 'tests']
+
+package_data = \
+{'': ['*']}
+
+modules = \
+['pyproject']
+install_requires = \
+['beanie>=1.11.9,<2.0.0', 'dep-spec>=1.2.14,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dep-module-mongo',
+    'version': '1.3.14',
+    'description': '',
+    'long_description': None,
+    'author': 'everhide',
+    'author_email': 'i.tolkachnikov@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
