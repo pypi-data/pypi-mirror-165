c932cb07caff88d4a3c4da7df1df30dc67b3ddb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:17 +0000

    Add package clastline/1.1.0/clastline-1.1.0.tar.gz

---
+++ packages/clastline/clastline-1.1.0.tar.gz/clastline-1.1.0/clastline/test_clastline.py
@@ -0,0 +1,30 @@
+import io
+import shutil
+
+from .clastline import cLastLine
+
+
+def test_simple():
+    stream = io.StringIO()
+
+    with cLastLine(stream) as cll:
+        cll.write('hi')
+        cll.write('word')
+        cll.write('test', clearBeforeWrite=False)
+
+    clearText = shutil.get_terminal_size().columns * ' '
+    assert stream.getvalue() == f'''hi\r{clearText}\rword\rtest\r\n'''
+
+def test_last_line_empty():
+    with cLastLine() as cll:
+        pass
+
+    assert cll._lastLine == ''
+
+def test_one_line_with_alt_end():
+    stream = io.StringIO()
+
+    with cLastLine(stream, lineEnd='bleh') as cll:
+        cll.write('a')
+
+    assert stream.getvalue() == 'a\rbleh'
