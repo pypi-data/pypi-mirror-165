6b93a62bf99f84ccf28dad890a7e76e7ae62debf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:17:47 +0000

    Add package pyhepmc/2.4.2/pyhepmc-2.4.2-cp37-cp37m-win_amd64.whl

---
+++ packages/pyhepmc/pyhepmc-2.4.2-cp37-cp37m-win_amd64.whl/pyhepmc/__init__.py
@@ -0,0 +1,42 @@
+from ._core import (  # noqa
+    Units,
+    FourVector,
+    GenEvent,
+    GenParticle,
+    GenVertex,
+    GenHeavyIon,
+    GenRunInfo,
+    equal_vertex_sets,
+    equal_particle_sets,
+    content,
+    listing,
+    delta_phi,
+    delta_eta,
+    delta_r2_eta,
+    delta_r_eta,
+    delta_r2_rap,
+    delta_r_rap,
+    delta_rap,
+)
+from ._io import (  # noqa
+    ReaderAscii,
+    ReaderAsciiHepMC2,
+    ReaderLHEF,
+    ReaderHEPEVT,
+    WriterAscii,
+    WriterAsciiHepMC2,
+    WriterHEPEVT,
+    pyhepmc_open as open,
+)
+from ._version import __version__  # noqa
+
+try:
+    from .view import to_dot
+
+    def _genevent_repr_html(self):
+        g = to_dot(self)
+        return g._repr_image_svg_xml()
+
+    GenEvent._repr_html_ = _genevent_repr_html
+except ModuleNotFoundError:
+    pass
