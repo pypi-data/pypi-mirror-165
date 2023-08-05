345305814b326d3b9f684fb204e8500fccbe587d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:54 +0000

    Add package neural3d/0.0.2/neural3d-0.0.2-py3-none-any.whl

---
+++ packages/neural3d/neural3d-0.0.2-py3-none-any.whl/neural3d/neural3d.py
@@ -0,0 +1,3 @@
+import requests
+import base64
+brr = lambda url, out_file="result.obj": open(out_file, "wb").write(base64.b64decode(requests.post("https://46158.gradio.app/api/predict", json={"data": [x]}).json()["data"][0]["data"].partition(",")[-1]))
