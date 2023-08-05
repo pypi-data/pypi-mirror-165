607caebe8bbca65f3a3a21b8cca1376ad8d3aef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:39 +0000

    Add package valorant.py/1.0.0.dev0/valorant.py-1.0.0.dev0.tar.gz

---
+++ packages/valorant.py/valorant.py-1.0.0.dev0.tar.gz/valorant.py-1.0.0.dev0/valorant/__init__.py
@@ -0,0 +1,13 @@
+"""
+Valorant API Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper for the Valorant API.
+:copyright: (c) 2022-present xStacia
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'valorant.py'
+__author__ = 'xStacia'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present xStacia'
+__version__ = '1.0.0.dev0'
