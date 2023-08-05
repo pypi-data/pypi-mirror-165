f764de0679b8a036522d75b32a8cbef4360be340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package speed-predict/0.0/speed_predict-0.0.tar.gz

---
+++ packages/speed-predict/speed_predict-0.0.tar.gz/speed_predict-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='speed_predict',
+      version='0.0',
+      description='forecasting package',
+      packages=['speed_predict'],
+      author_email='jacoblouiswright@gmail.com',
+      zip_safe=False)
