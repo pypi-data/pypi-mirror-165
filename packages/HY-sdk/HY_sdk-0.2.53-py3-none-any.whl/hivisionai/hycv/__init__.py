ed1f603f43fa89bd7b5d41521ed2f221c6ad3d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:24 +0000

    Add package HY-sdk/0.2.53/HY_sdk-0.2.53-py3-none-any.whl

---
+++ packages/HY-sdk/HY_sdk-0.2.53-py3-none-any.whl/hivisionai/hycv/__init__.py
@@ -0,0 +1,5 @@
+from src.hivisionai.hycv.utils import cover_mask, get_box, get_box_pro, filtering, cut, zoom_image_without_change_size
+
+VERSION:str = "0.2.51"
+DIARY:str = "update hyService"
+print(f'\033[1;32;8m Welcome! You are using HY-sdk V{VERSION}\033[0m')
