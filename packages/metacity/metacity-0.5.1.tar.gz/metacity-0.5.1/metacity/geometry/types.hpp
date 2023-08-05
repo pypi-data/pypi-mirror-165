624308c7e2eea21b3054c74bfa8671ae834199ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:11 +0000

    Add package metacity/0.5.1/metacity-0.5.1.tar.gz

---
+++ packages/metacity/metacity-0.5.1.tar.gz/metacity-0.5.1/metacity/geometry/types.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "deps/glm/glm.hpp"
+#include "deps/cppcodec/base64_rfc4648.hpp"
+#include <vector>
+#include <iostream>
+#include <memory>
+
+using tvec3 = glm::highp_f32vec3;
+using tvec2 = glm::highp_f32vec2;
+using tfloat = float;
+
