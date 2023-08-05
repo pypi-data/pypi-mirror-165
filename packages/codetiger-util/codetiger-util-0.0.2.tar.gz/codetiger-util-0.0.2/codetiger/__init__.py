1c3abf66244ef26944bb09c58f8e436b8e61f171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:38 +0000

    Add package codetiger-util/0.0.2/codetiger-util-0.0.2.tar.gz

---
+++ packages/codetiger-util/codetiger-util-0.0.2.tar.gz/codetiger-util-0.0.2/codetiger/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+"""
+    @program: utils
+    @filename: __init__.py
+    @author: codetiger
+    @create: 2022/8/26 21:58
+"""
+import os
+import sys
+
+# 添加项目根目录模块扫描，方便自定义模块扫描
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
