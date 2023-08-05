0fdd3cbfb3ccc3e102a6fa6c8609b9e4d156ac79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:45 +0000

    Add package heinlein/0.2.3/heinlein-0.2.3.tar.gz

---
+++ packages/heinlein/heinlein-0.2.3.tar.gz/heinlein-0.2.3/heinlein/cmds.py
@@ -0,0 +1,121 @@
+from genericpath import isfile
+import multiprocessing
+from heinlein.locations import INSTALL_DIR, MAIN_CONFIG_DIR
+from heinlein.config import globalConfig
+from heinlein.manager.managers import FileManager
+from heinlein.utilities import warning_prompt_tf, split_catalog
+from heinlein.manager.dataManger import get_all
+import numpy as np
+from pathlib import Path
+
+def add(args) -> bool:
+    """
+    Add a location on disk to a dataset
+    """
+    cwd = Path.cwd()
+    name = args.dataset_name
+    dtype = args.dtype
+    path = args.path
+
+    if path != 'cwd':
+        path = cwd / path
+    else:
+        path = cwd
+    if not path.exists():
+        print(f"Error: {path} not found!")
+        return
+    manager = FileManager(name)
+    manager.add_data(dtype, path)
+    return True
+
+def remove(args):
+    name = args.dataset_name
+    if not FileManager.exists(name):
+        print(f"Error: dataset {name} does not exist!")
+        return True
+    dtype = args.dtype
+    mgr = FileManager(name)
+    mgr.remove_data(dtype)
+    return True
+
+def clear(args) -> bool:
+    """
+    Clear all data from a dataset
+    """
+    name = args.dataset_name
+    if not FileManager.exists(name):
+        print(f"Error: dataset {name} does not exist!")
+        return True
+    msg = f"This will delete all references to data for the {name} dataset." \
+                                " Are you sure you want to do this?"
+    delete = warning_prompt_tf(msg)
+
+    if delete:
+        mgr = FileManager(name)
+        mgr.clear_all_data()
+    return True
+
+def get(args) -> bool:
+    """
+    Get the path to a specific data type in a specific datset
+    """
+    name = args.dataset_name
+    if not FileManager.exists(name):
+        print(f"Error: dataset {name} does not exist!")
+        return True
+    dtype = args.dtype
+    mgr = FileManager(name)
+    path = mgr.get_path(dtype)
+    if path:
+        print(str(path))
+    return True
+
+def list_all(args) -> None:
+    surveys = get_all()
+    data = {name: d.get("data", []) for name, d in surveys.items()}
+    if len(data) == 0 or all([len(d) == 0 for d in data.values()]):
+        print("No data found!")
+        return
+    header = "DATASET".ljust(20) + "AVAILABLE DATA"
+    print(header)
+    print("-"*len(header))
+    for name, dtypes in data.items():
+        s1 = name
+        s2 = ",".join(list(dtypes.keys()))
+        print(s1.ljust(20) + s2)
+
+def split(args) -> None:
+    path = Path(args.input_path)
+    args.input_path = path
+    if not path.exists():
+        print(f"Error: path {path} does not exist!")
+    else:
+        args.input_path = path
+    if args.threads == 1 and globalConfig.interactive:
+        print("Warning: Splitting large datasets takes a long time")
+        print("We recommend using more than a single thread")
+        print("You can increase this number with the -t flag in the future")
+        while True:
+            nthreads = input("How many threads would you like to use? ")
+            try:
+                max_threads = multiprocessing.cpu_count()
+                nthreads = int(nthreads)
+                if max_threads < nthreads:
+                    print(f"Error: Maximum number of threads available on this machine is {max_threads}")
+                    continue
+                elif nthreads < 1:
+                    print(f"Error: I need at least one thread!")
+                    continue
+
+                break
+            except ValueError:
+                print("Number of threads must be an integer")
+        args.threads = nthreads
+
+    if args.output == None:
+        if path.is_file():
+            output_path = path.parents[0]
+        else:
+            output_path = path
+        args.output = output_path
+    split_catalog(args)
