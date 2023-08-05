7fffbc4bf1293da9a6f87fa7f7e24aad7c16d7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package ducks/0.0.1/ducks-0.0.1.tar.gz

---
+++ packages/ducks/ducks-0.0.1.tar.gz/ducks-0.0.1/ducks/exceptions.py
@@ -0,0 +1,11 @@
+class FrozenError(Exception):
+    """Raised when attempting to modify a FrozenFilterBox"""
+
+
+class AttributeNotFoundError(Exception):
+    """Raised when querying an attribute we don't have"""
+
+
+class MissingAttribute(Exception):
+    """Raise this in your attribute functions to denote that the object is missing this attribute. Finds that
+    match the attribute will never return this object. Finds that exclude the attribute will."""
