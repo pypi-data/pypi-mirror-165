8ba2a0c8b022168c8cf8a01a724d7eca482cfec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:33 +0000

    Add package bagbag/0.32.4/bagbag-0.32.4.tar.gz

---
+++ packages/bagbag/bagbag-0.32.4.tar.gz/bagbag-0.32.4/src/bagbag/Os/__init__.py
@@ -0,0 +1,27 @@
+import os
+import sys 
+from . import Path
+import shutil
+
+def Exit(num:int=0):
+    sys.exit(num)
+
+System = os.system 
+
+def Mkdir(path:str):
+    os.makedirs(path, exist_ok=True)
+
+def ListDir(path:str) -> list[str]:
+    return os.listdir(path)
+
+Args = sys.argv 
+
+def Getenv(varname:str, defaultValue:str=None) -> str | None:
+    v = os.environ.get(varname)
+    if not v:
+        return defaultValue
+    else:
+        return v
+
+def Unlink(path:str):
+    shutil.rmtree(path)
