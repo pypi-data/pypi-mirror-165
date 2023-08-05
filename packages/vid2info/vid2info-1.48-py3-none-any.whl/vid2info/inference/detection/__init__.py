ad9146c0cf864f741ae01b08f83a86695889f065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:35 +0000

    Add package vid2info/1.48/vid2info-1.48-py3-none-any.whl

---
+++ packages/vid2info/vid2info-1.48-py3-none-any.whl/vid2info/inference/detection/__init__.py
@@ -0,0 +1,3 @@
+from .yolov6_detector import YoloV6Detector
+from .yolov7_detector import YoloV7Detector
+from .config import CLASS_ID_POSITION_IN_BBOX, DETECTOR_MIN_CONFIDENCE_TH, IOU_NMS_TH, DETECTOR_IMAGE_SIZE
