aa5ef06d3d0420de9c1b0a91ce9c8e7cb9590aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:13 +0000

    Add package MuhekoNikolasVersionsDumpTest/0.0.5/MuhekoNikolasVersionsDumpTest-0.0.5.tar.gz

---
+++ packages/MuhekoNikolasVersionsDumpTest/MuhekoNikolasVersionsDumpTest-0.0.5.tar.gz/MuhekoNikolasVersionsDumpTest-0.0.5/MuhekoNikolas/test/__init__.py
@@ -0,0 +1,3 @@
+
+def j():
+    print("Hi")
---
+++ packages/MuhekoNikolasVersionsDumpTest/MuhekoNikolasVersionsDumpTest-0.0.5.tar.gz/MuhekoNikolasVersionsDumpTest-0.0.5/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup,find_packages
+
+
+setup(
+    name="MuhekoNikolasVersionsDumpTest",
+    version="0.0.5",
+    author="Muheko Nikolas",
+    packages = find_packages()
+    
+)
