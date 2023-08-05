005545b4bacc24cfd3526bb02e8c739394a14712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:40 +0000

    Add package parameterspace/0.7.18/parameterspace-0.7.18.tar.gz

---
+++ packages/parameterspace/parameterspace-0.7.18.tar.gz/parameterspace-0.7.18/parameterspace/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 - for information on the respective copyright owner see the
+# NOTICE file and/or the repository https://github.com/boschresearch/parameterspace
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import parameterspace.priors
+import parameterspace.transformations
+from parameterspace.parameters import *
+from parameterspace.parameterspace import *
