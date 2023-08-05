d870f4d77a836f1ff9b9c56cde5d41dce4a737b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package plonemeeting.restapi/1.0rc18/plonemeeting.restapi-1.0rc18.tar.gz

---
+++ packages/plonemeeting.restapi/plonemeeting.restapi-1.0rc18.tar.gz/plonemeeting.restapi-1.0rc18/src/plonemeeting/restapi/config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+CONFIG_ID_ERROR = 'The "config_id" parameter must be given!'
+CONFIG_ID_TYPE_ERROR = 'A "config_id" can not be given when parameter "type" is "config"!'
+CONFIG_ID_NOT_FOUND_ERROR = 'The given "config_id" named "%s" was not found!'
+ANNEXES_FILTER_VALUES = [
+    "to_print", "confidential", "publishable", "to_sign", "signed"]
+INDEX_CORRESPONDENCES = {
+    'getConfigId': 'config_id',
+    'review_state': 'state',
+    'portal_type': 'type',
+    'UID': 'uid'}
+HAS_MEETING_DX = False
+try:
+    from Products.PloneMeeting.content.meeting import IMeeting
+    HAS_MEETING_DX = True
+except ImportError:
+    pass
---
+++ packages/plonemeeting.restapi/plonemeeting.restapi-1.0rc18.tar.gz/plonemeeting.restapi-1.0rc18/src/plonemeeting/restapi/tests/base.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from os import path
+from plone.restapi.testing import RelativeSession
+from plonemeeting.restapi.config import HAS_MEETING_DX
+from plonemeeting.restapi.testing import PM_REST_TEST_PROFILE_FUNCTIONAL
+from Products.PloneMeeting import tests as pm_tests
+from Products.PloneMeeting.tests.PloneMeetingTestCase import DEFAULT_USER_PASSWORD
+from Products.PloneMeeting.tests.PloneMeetingTestCase import PloneMeetingTestCase
+
+import os
+import transaction
+
+
+class BaseTestCase(PloneMeetingTestCase):
+    """Base class for defining PM restapi test cases."""
+
+    layer = PM_REST_TEST_PROFILE_FUNCTIONAL
+
+    def setUp(self):
+        """ """
+        super(BaseTestCase, self).setUp()
+        self.portal_url = self.portal.absolute_url()
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = ("pmManager", DEFAULT_USER_PASSWORD)
+        self.changeUser("siteadmin")
+        self.getMeetingFolder(userId="pmManager")
+        # enable RESTAPI_DEBUG for every tests
+        # this will display the result of a restapi call in the log
+        os.environ['RESTAPI_DEBUG'] = "True"
+        # XXX 4.1.x compatibility, to be removed in 4.2+
+        if not HAS_MEETING_DX:
+            self.meetingConfig.setWorkflowAdaptations(('creator_initiated_decisions', ))
+            self.meetingConfig.at_post_edit_script()
+        try:
+            transaction.commit()
+        except Exception:
+            transaction.begin()
+
+    def get_review_state(self, obj):
+        """Backward compat method that manage queryState/query_state."""
+        if HAS_MEETING_DX:
+            return obj.query_state()
+        else:
+            return obj.queryState()
+
+    def _add_image(self, obj):
+        """ """
+        file_path = path.join(path.dirname(pm_tests.__file__), "dot.gif")
+        data = open(file_path, "r")
+        img_id = obj.invokeFactory("Image", id="dot.gif", title="Image", file=data.read())
+        img = getattr(obj, img_id)
+        return img
