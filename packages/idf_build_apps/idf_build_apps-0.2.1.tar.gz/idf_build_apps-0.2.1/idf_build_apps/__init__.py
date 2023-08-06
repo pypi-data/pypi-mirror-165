507b57916e31a7f0755009db009c623eee698344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:53 +0000

    Add package idf_build_apps/0.2.1/idf_build_apps-0.2.1.tar.gz

---
+++ packages/idf_build_apps/idf_build_apps-0.2.1.tar.gz/idf_build_apps-0.2.1/idf_build_apps/__init__.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Tools for building ESP-IDF related apps.
+"""
+
+import logging
+
+__version__ = '0.2.1'
+
+LOGGER = logging.getLogger('idf_build_apps')
+
+from .app import App, CMakeApp
+from .main import find_apps, build_apps
+from .utils import setup_logging
+
+__all__ = [
+    'App',
+    'CMakeApp',
+    'find_apps',
+    'build_apps',
+    'setup_logging',
+]
