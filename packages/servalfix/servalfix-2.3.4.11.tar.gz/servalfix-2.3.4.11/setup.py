de37544c17c6d2600cb169af842dc048aeadfe2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:54 +0000

    Add package servalfix/2.3.4.11/servalfix-2.3.4.11.tar.gz

---
+++ packages/servalfix/servalfix-2.3.4.11.tar.gz/servalfix-2.3.4.11/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "requests",
+    "websocket-client==1.3.1", 
+    "setuptools", 
+    "json_minify", 
+    "six",
+    "aiohttp",
+    "pysocks",
+    "poetry",
+    "websockets"
+]
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name="servalfix",
+    license='MIT',
+    author="Capuchino",
+    version="2.3.4.11",
+    author_email="chocolabienid@gmail.com",
+    description="Library for AminoApps. Discord - https://discord.gg/7NmhxQBmET",
+    url="https://github.com/TheSabaru/servalfix",
+    packages=find_packages(),
+    long_description=long_description,
+    install_requires=requirements,
+    keywords=[
+        'aminoapps',
+        'servalfix',
+        'servalfix',
+        'serval',
+        'serval-bot',
+        'narvii',
+        'amino',
+        'api',
+        'python',
+        'python3',
+        'python3.x',
+        'servalfix.py',
+        'serval-amino',
+        'capuchino'
+    ],
+    python_requires='>=3.6',
+)
