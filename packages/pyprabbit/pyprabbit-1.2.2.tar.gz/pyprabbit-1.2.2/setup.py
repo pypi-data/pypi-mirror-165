0807adbba7f40e097f312519d9d407188478a9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:34 +0000

    Add package pyprabbit/1.2.2/pyprabbit-1.2.2.tar.gz

---
+++ packages/pyprabbit/pyprabbit-1.2.2.tar.gz/pyprabbit-1.2.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='pyprabbit',
+    version='1.2.2',
+    author='zhoufangfang',
+    author_email='xczff001@163.com',
+    maintainer='zhoufangfang',
+    maintainer_email='xczff001@163.com',
+    keywords=['web','nodejs'],
+    descriotion='这是rabbit编程语言的Python包版',
+    url='http://192.168.2.110:8000/index.html',
+    packages=['rabbit'],
+    install_requires=['pgzero'],
+    python_requires='>=3'
+)
+
