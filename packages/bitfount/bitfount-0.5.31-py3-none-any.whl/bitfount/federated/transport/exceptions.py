5e2c0c7294b34c40fbb22e8591405d3dfdedd8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:15 +0000

    Add package bitfount/0.5.31/bitfount-0.5.31-py3-none-any.whl

---
+++ packages/bitfount/bitfount-0.5.31-py3-none-any.whl/bitfount/federated/transport/exceptions.py
@@ -0,0 +1,8 @@
+"""Transport layer exceptions."""
+from bitfount.exceptions import BitfountError
+
+
+class BitfountMessageServiceError(BitfountError):
+    """Bitfount exception related to interaction with the message service."""
+
+    pass
