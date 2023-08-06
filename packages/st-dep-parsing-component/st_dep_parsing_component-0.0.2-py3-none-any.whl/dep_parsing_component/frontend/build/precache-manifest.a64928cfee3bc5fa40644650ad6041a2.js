e35491f1fe54a1ca1e520fe543f5242188cadb0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:51 +0000

    Add package st-dep-parsing-component/0.0.2/st_dep_parsing_component-0.0.2-py3-none-any.whl

---
+++ packages/st-dep-parsing-component/st_dep_parsing_component-0.0.2-py3-none-any.whl/dep_parsing_component/frontend/build/precache-manifest.a64928cfee3bc5fa40644650ad6041a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9433dd4d19ca97673fad23a8a1e7aaea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "618917a5d8eff9b901df",
+    "url": "./static/js/2.77162d2d.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.77162d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97159d1507cba09e5238",
+    "url": "./static/js/main.8d014288.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
---
+++ packages/st-dep-parsing-component/st_dep_parsing_component-0.0.2-py3-none-any.whl/dep_parsing_component/frontend/build/precache-manifest.bc4819c15f9c9c37303c7ee976481c9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0077318652463a59f05f2a57fd74874e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "618917a5d8eff9b901df",
+    "url": "./static/js/2.77162d2d.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.77162d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c08b2e55997171868b",
+    "url": "./static/js/main.06d197db.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
