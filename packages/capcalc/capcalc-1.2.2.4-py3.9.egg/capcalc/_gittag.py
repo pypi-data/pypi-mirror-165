745d4c1f85f1e29106d7ea4fdaf66cb66a39966c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package capcalc/1.2.2.4/capcalc-1.2.2.4-py3.9.egg

---
+++ packages/capcalc/capcalc-1.2.2.4-py3.9.egg/capcalc/_gittag.py
@@ -0,0 +1,3 @@
+# This file is originally generated from Git information by running 'setup.py
+# install'. Distribution tarballs contain a pre-generated copy of this file.
+__gittag__ = "f38f9f0-dirty"
