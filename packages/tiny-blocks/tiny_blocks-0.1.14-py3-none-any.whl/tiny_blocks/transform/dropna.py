49257e704b4f3e16e4c1e74d81347885a966e469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package tiny-blocks/0.1.14/tiny_blocks-0.1.14-py3-none-any.whl

---
+++ packages/tiny-blocks/tiny_blocks-0.1.14-py3-none-any.whl/tiny_blocks/transform/dropna.py
@@ -0,0 +1,50 @@
+import logging
+from typing import Iterator, Literal
+
+import pandas as pd
+from tiny_blocks.transform.base import KwargsTransformBase, TransformBase
+
+__all__ = ["DropNa", "KwargsDropNa"]
+
+
+logger = logging.getLogger(__name__)
+
+
+class KwargsDropNa(KwargsTransformBase):
+    """
+    Kwargs for DropNa
+    """
+
+    ignore_index: bool = None
+
+
+class DropNa(TransformBase):
+    """
+    Drop Nan Block. Defines the drop None values functionality
+
+    Basic example:
+        >>> import pandas as pd
+        >>> from tiny_blocks.transform import DropNa
+        >>> from tiny_blocks.extract import FromCSV
+        >>>
+        >>> extract_csv = FromCSV(path='/path/to/file.csv')
+        >>> drop_na = DropNa()
+        >>>
+        >>> generator = extract_csv.get_iter()
+        >>> generator = drop_na.get_iter(generator)
+        >>> df = pd.concat(generator)
+
+    For more Kwargs info:
+    https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.dropna.html
+    """
+
+    name: Literal["drop_na"] = "drop_na"
+    kwargs: KwargsDropNa = KwargsDropNa()
+
+    def get_iter(
+        self, source: Iterator[pd.DataFrame]
+    ) -> Iterator[pd.DataFrame]:
+
+        for chunk in source:
+            chunk = chunk.dropna(**self.kwargs.to_dict())
+            yield chunk
