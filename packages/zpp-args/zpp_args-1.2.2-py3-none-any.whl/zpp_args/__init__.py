70f00a12f390cb674e097c2c10b803665b7998d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:24 +0000

    Add package zpp-args/1.2.2/zpp_args-1.2.2-py3-none-any.whl

---
+++ packages/zpp-args/zpp_args-1.2.2-py3-none-any.whl/zpp_args/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.2.2'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = "Module pour le traitement des arguments d'une ligne de commande"
+__lastupdate__ = '29/08/2022'
+__releasenote__ = 'Mistike in translation class'
+
+from .args import *
