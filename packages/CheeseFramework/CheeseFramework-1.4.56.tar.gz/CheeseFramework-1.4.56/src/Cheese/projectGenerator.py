da82b590764fd53e7c7f75ba51da4963aa535513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:21 +0000

    Add package CheeseFramework/1.4.56/CheeseFramework-1.4.56.tar.gz

---
+++ packages/CheeseFramework/CheeseFramework-1.4.56.tar.gz/CheeseFramework-1.4.56/src/Cheese/projectGenerator.py
@@ -0,0 +1,42 @@
+#cheese
+
+import os
+import git
+import shutil
+import stat
+
+from Cheese.resourceManager import ResMan
+
+"""
+Generates structure of Cheese Application
+"""
+
+class ProjectGenerator:
+
+    @staticmethod
+    def generate(path, generateFiles):
+        repo = git.Repo.clone_from("https://github.com/KubaBoi/CheeseFramework.git", path)
+
+        repo.git.checkout("template")  
+
+        for root, dirs, files in os.walk(os.path.join(path, ".git")):  
+            for dir in dirs:
+                os.chmod(os.path.join(root, dir), stat.S_IRWXU)
+            for file in files:
+                os.chmod(os.path.join(root, file), stat.S_IRWXU)
+        shutil.rmtree(os.path.join(path, ".git"))
+
+        src = os.path.join(path, "src")
+
+        if (not generateFiles):
+            os.remove(os.path.join(src, "controllers", "HelloWorldController.py"))
+            os.remove(os.path.join(src, "models", "Hello.py"))
+            os.remove(os.path.join(src, "repositories", "helloRepository.py"))
+
+        with open(os.path.join(path, "mainTemplate.py"), "r") as f:
+            data = f.read()
+
+        with open(os.path.join(path, f"{os.path.basename(os.path.abspath(os.path.curdir))}.py"), "w") as f:
+            f.write(data)
+
+        os.remove(os.path.join(path, "mainTemplate.py"))
