2f60cfed8cb8d9cdaf474fb1df53c3d654b10d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:48 +0000

    Add package JovyTesting/0.0.4/JovyTesting-0.0.4.tar.gz

---
+++ packages/JovyTesting/JovyTesting-0.0.4.tar.gz/JovyTesting-0.0.4/JovyTesting/__init__.py
@@ -0,0 +1,22 @@
+
+import testModule.JovyTesting.jmaths as jmaths
+
+#from testModule.JovyTesting.jmaths import *
+
+
+class JMATHCLASS():
+    """The Mathematics class for JovyTesting"""
+    def __init__(self):
+        self.add = jmaths.add
+        self.sub = self.subtract= jmaths.sub
+
+    def __repr__(self):
+        return "The mathematics class for JovyTesting"
+
+    def __str__(self):
+        return "The mathematics class for JovyTesting"
+
+
+
+
+jmath = JMATHCLASS()
