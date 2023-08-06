728e7fa3fba333d156d404c1f83a1f1202d3490b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:33 +0000

    Add package clemoni-utilities/1.0.2/clemoni_utilities-1.0.2.tar.gz

---
+++ packages/clemoni-utilities/clemoni_utilities-1.0.2.tar.gz/clemoni_utilities-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='clemoni_utilities',
+    version='1.0.2',
+    url='https://github.com/clemoni/clemoni_utilities',
+    author='Clement Liscoet',
+    author_email='clement.liscoet@gmail.com',
+    license='MIT',
+    description='A short utlitiy library for python',
+    packages=['utilities'],
+    py_modules=['tools']
+)
