de2d85826dd44c5254a6f905169883c1c52fecd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:12 +0000

    Add package ndx-franklab-novela/0.1.0/ndx-franklab-novela-0.1.0.tar.gz

---
+++ packages/ndx-franklab-novela/ndx-franklab-novela-0.1.0.tar.gz/ndx-franklab-novela-0.1.0/src/pynwb/tests/test_cameraDevice.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from src.pynwb.ndx_franklab_novela import CameraDevice
+
+
+class TestCameraDevice(TestCase):
+
+    def test_cameraDevice_created_successfully(self):
+
+        cam_1 = CameraDevice(
+            name='1',
+            meters_per_pixel=0.20,
+            camera_name='test name',
+            model='ndx2000',
+            lens='500dpt',
+            manufacturer='sony'
+        )
+
+        self.assertEqual(cam_1.name, '1')
+        self.assertEqual(cam_1.camera_name, 'test name')
+        self.assertEqual(cam_1.model, 'ndx2000')
+        self.assertEqual(cam_1.lens, '500dpt')
+        self.assertEqual(cam_1.manufacturer, 'sony')
