d0c4946d962708d9557465de783c2dcd3aaca96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:41 +0000

    Add package django-rrweb/4.0.5/django-rrweb-4.0.5.tar.gz

---
+++ packages/django-rrweb/django-rrweb-4.0.5.tar.gz/django-rrweb-4.0.5/django_rrweb/management/commands/rrwebpurge.py
@@ -0,0 +1,56 @@
+import datetime as dt
+import logging
+import time
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+from django.utils import timezone as tz
+
+from ...models import Page, Session
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Purges pages and sessions'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--delay', default=600, type=int)
+        parser.add_argument('--limit', default=float('inf'), type=int)
+        parser.add_argument('--max-age', default=7, type=int)
+
+    def handle(self, *args, **options):
+        delay = options['delay']
+        limit = options['limit']
+        max_age = dt.timedelta(days=options['max_age'])
+        count = 0
+
+        while True:
+            now = tz.now()
+            past = now - max_age
+            sessions = Session.objects.filter(
+                notes='',
+                pages__notes='',
+                create_time__lt=past,
+            )
+            log.warning('Deleting %s sessions', sessions.count())
+            sessions.delete()
+            sessions = (
+                Session.objects.filter(notes='', create_time__lt=past)
+                .annotate(pages_count=Count('pages__id'))
+                .filter(pages_count=0)
+            )
+            log.warning('Deleting %s sessions', sessions.count())
+            sessions.delete()
+            pages = Page.objects.filter(
+                notes='',
+                session__notes='',
+                create_time__lt=past,
+            )
+            log.warning('Deleting %s pages', pages.count())
+            pages.delete()
+            count += 1
+            if count >= limit:
+                break
+            log.info('Sleeping for %s seconds', delay)
+            time.sleep(delay)
