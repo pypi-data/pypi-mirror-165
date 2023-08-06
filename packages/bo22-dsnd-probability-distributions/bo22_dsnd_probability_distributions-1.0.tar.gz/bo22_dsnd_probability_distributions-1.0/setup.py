49498335551b7d0a46a3655f49e94f9ab0a2fa03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:55 +0000

    Add package bo22-dsnd-probability-distributions/1.0/bo22_dsnd_probability_distributions-1.0.tar.gz

---
+++ packages/bo22-dsnd-probability-distributions/bo22_dsnd_probability_distributions-1.0.tar.gz/bo22_dsnd_probability_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bo22_dsnd_probability_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bo22_dsnd_probability_distributions'],
+      autor= 'Boris Eyambe',
+      author_email= 'bolbor22@gmail.com',
+      zip_safe=False)
