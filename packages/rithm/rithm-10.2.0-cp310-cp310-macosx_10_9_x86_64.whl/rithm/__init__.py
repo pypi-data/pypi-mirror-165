ea8e11112523c7dac2163f044ead8acb4bebf676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:25 +0000

    Add package rithm/10.2.0/rithm-10.2.0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/rithm/rithm-10.2.0-cp310-cp310-macosx_10_9_x86_64.whl/rithm/__init__.py
@@ -0,0 +1,14 @@
+"""Arbitrary precision arithmetic."""
+
+__version__ = '10.2.0'
+
+try:
+    from ._crithm import (Endianness,
+                          Fraction,
+                          Int,
+                          TieBreaking)
+except ImportError:
+    from ._rithm import (Endianness,
+                         Fraction,
+                         Int,
+                         TieBreaking)
