b5e187f4e489c201f9b86d51e2c54c23866f0cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:41 +0000

    Add package pgw/0.4.3/pgw-0.4.3.tar.gz

---
+++ packages/pgw/pgw-0.4.3.tar.gz/pgw-0.4.3/setup.py
@@ -0,0 +1,15 @@
+version = '0.4.3'
+from setuptools import setup
+setup(
+  name='pgw',
+  packages=['pygwin'],
+  version=version,
+  author='themixray',
+  description='A library for creating Python applications.',
+  license='MIT',
+  install_requires=[
+    'attrdict','cython','pywin32','pygame','inputs',
+    'randstr','pydub','wxPython','pyautogui','moviepy',
+    'pipwin','wave','opencv-python','mutagen'
+  ]
+)
