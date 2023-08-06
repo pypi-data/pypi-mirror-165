3384752cb483cd499e4fe9d48e5bdf727023944b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:46 +0000

    Add package phonologic/0.1.1a1/phonologic-0.1.1a1.tar.gz

---
+++ packages/phonologic/phonologic-0.1.1a1.tar.gz/phonologic-0.1.1a1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name='phonologic',
+    version='0.1.1a1',
+    author='Robert Gale',
+    author_email='galer@ohsu.edu',
+    packages=setuptools.find_packages(),
+    url='https://github.com/rcgale/phonologic',
+    description='',
+    install_requires=[
+        'regex',
+    ],
+    include_package_data=True,
+    package_data={
+        'phonologic': [
+            '**/*.py',
+            '**/*.phl',
+            '**/*.json',
+            '**/*.html',
+            '**/*.css',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'phonologic-viewer = phonologic.viewer:main',
+        ],
+    },
+)
