7540f70d37e774068dd0e5a509d2a595be80c6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:59 +0000

    Add package pytiers/1.1.2/pytiers-1.1.2.tar.gz

---
+++ packages/pytiers/pytiers-1.1.2.tar.gz/pytiers-1.1.2/pytiers/__init__.py
@@ -0,0 +1,2 @@
+from pytiers.core import Point, PitchTier, DurationTier
+from pytiers.utils import read_Tier
---
+++ packages/pytiers/pytiers-1.1.2.tar.gz/pytiers-1.1.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='pytiers',
+    version='1.1.2',
+    author='Huang Bo-Xuan',
+    author_email='r09142003@gmail.com',
+    install_requires=[
+        'seaborn',
+        'matplotlib',
+        'pandas'
+    ]
+)
