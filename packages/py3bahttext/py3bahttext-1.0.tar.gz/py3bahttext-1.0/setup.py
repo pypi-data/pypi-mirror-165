3c557d5ae3e1eec5a53f5b1942e089a0bf778c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:46 +0000

    Add package py3bahttext/1.0/py3bahttext-1.0.tar.gz

---
+++ packages/py3bahttext/py3bahttext-1.0.tar.gz/py3bahttext-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='py3bahttext',
+      version='1.0',
+      description='Tool for convert currency number to Thai text',
+      author='Deeprom S',
+      author_email='grooveeeeee@gmail.com',
+      url='https://github.com/tstxni97/py3bahttext',
+      download_url='https://github.com/tstxni97/py3bahttext/tarball/master',
+      packages=['py3bahttext'],
+      license="BSD",
+)
+
