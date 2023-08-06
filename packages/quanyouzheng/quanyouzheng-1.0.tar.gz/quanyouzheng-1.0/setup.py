b6e99639a9a21af7cd873ba98af634863ac3ea35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:24 +0000

    Add package quanyouzheng/1.0/quanyouzheng-1.0.tar.gz

---
+++ packages/quanyouzheng/quanyouzheng-1.0.tar.gz/quanyouzheng-1.0/setup.py
@@ -0,0 +1,10 @@
+
+
+from distutils.core import  setup
+import setuptools
+packages = ['healthautotest']# 唯一的包名，自己取名
+setup(name='quanyouzheng',
+      version='1.0',
+      author='Quan-u',
+      packages=packages,
+      package_dir={'requests': 'requests'},)
