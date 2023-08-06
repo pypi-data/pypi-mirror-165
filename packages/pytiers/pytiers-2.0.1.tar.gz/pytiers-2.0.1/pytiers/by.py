57ba70db97796c263be12cd4289cdb6e02ab5493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:26 +0000

    Add package pytiers/2.0.1/pytiers-2.0.1.tar.gz

---
+++ packages/pytiers/pytiers-2.0.1.tar.gz/pytiers-2.0.1/pytiers/by.py
@@ -0,0 +1,35 @@
+"""Methods for referenceing Point objects: by.time, by.index, by.point\nUsage example:\n pytiers.get_point(1, by.Index())\n returns the point in the Tier object with the #1 index."""
+
+class Time(object):
+    """When passed to \'by\', functions to state the Point object should be referenced by time. Essentially returns a string."""
+    __slots__ = ['__by_method']
+    def __init__(self):
+        self.__by_method = 'by_time'
+    
+    @property
+    def by_method(self):
+        return self.__by_method
+
+class Index(object):
+    """When passed to \'by\', functions to state the Point object should be referenced by point index. Essentially returns a string."""
+    __slots__ = ['__by_method']
+    def __init__(self):
+        self.__by_method = 'by_index'
+    
+    @property
+    def by_method(self):
+        return self.__by_method
+
+class Point(object):
+    """When passed to \'by\', functions to state the Point object should be referenced by the Point per se. Essentially returns a string."""
+    __slots__ = ['__by_method']
+    def __init__(self):
+        self.__by_method = 'by_point'
+    
+    @property
+    def by_method(self):
+        return self.__by_method
+
+by_methods = [Time().by_method,
+              Index().by_method,
+              Point().by_method]
