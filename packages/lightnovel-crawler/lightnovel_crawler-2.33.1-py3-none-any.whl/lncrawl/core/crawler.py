bd4c20be93c46de02a63c7500bffd809d86e020c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package lightnovel-crawler/2.33.1/lightnovel_crawler-2.33.1-py3-none-any.whl

---
+++ packages/lightnovel-crawler/lightnovel_crawler-2.33.1-py3-none-any.whl/lncrawl/core/crawler.py
@@ -0,0 +1,378 @@
+# -*- coding: utf-8 -*-
+"""
+Crawler application
+"""
+import logging
+import random
+import ssl
+import time
+from abc import ABC, abstractmethod
+from concurrent.futures import ThreadPoolExecutor
+from threading import Semaphore
+from typing import Dict, List
+from urllib.parse import urlparse
+
+import cloudscraper
+from bs4 import BeautifulSoup
+from requests import Response, Session
+
+from ..assets.user_agents import user_agents
+from ..utils.cleaner import TextCleaner
+from ..utils.ssl_no_verify import no_ssl_verification
+from .exeptions import LNException
+
+logger = logging.getLogger(__name__)
+
+MAX_CONCURRENT_REQUEST_PER_DOMAIN = 15
+REQUEST_SEMAPHORES: Dict[str, Semaphore] = {}
+
+def get_domain_semaphore(url):
+    host = urlparse(url).hostname or url
+    if host not in REQUEST_SEMAPHORES:
+        REQUEST_SEMAPHORES[host] = Semaphore(MAX_CONCURRENT_REQUEST_PER_DOMAIN)
+    return REQUEST_SEMAPHORES[host]
+
+
+class Crawler(ABC):
+    '''Blueprint for creating new crawlers'''
+
+    # ------------------------------------------------------------------------- #
+    # Internal methods
+    # ------------------------------------------------------------------------- #
+    def __init__(self) -> None:
+        self._destroyed = False
+        self.executor = ThreadPoolExecutor(max_workers=5)
+        self.cleaner = TextCleaner()
+
+        # Initialize cloudscrapper
+        try:
+            ctx = ssl.create_default_context()
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+            self.scraper = cloudscraper.create_scraper(
+                # debug=True,
+                ssl_context=ctx,
+                browser={
+                    'custom': random.choice(user_agents),
+                    #'browser': 'chrome',
+                    #'platform': 'windows',
+                    #'mobile': False
+                }
+            )
+        except Exception as err:
+            logger.exception('Failed to initialize cloudscraper')
+            self.scraper = Session()
+        # end try
+
+        # Must resolve these fields inside `read_novel_info`
+        self.novel_title = ''
+        self.novel_author = ''
+        self.novel_cover = None
+        self.is_rtl = False
+
+        # Each item must contain these keys:
+        # `id` - 1 based index of the volume
+        # `title` - the volume title (can be ignored)
+        self.volumes = []
+
+        # Each item must contain these keys:
+        # `id` - 1 based index of the chapter
+        # `title` - the title name
+        # `volume` - the volume id of this chapter
+        # `volume_title` - the volume title (can be ignored)
+        # `url` - the link where to download the chapter
+        self.chapters = []
+
+        # Other stuffs - not necessary to resolve from crawler instance.
+        self.home_url = ''
+        self.novel_url = ''
+        self.last_visited_url = None
+
+        # Setup an automatic proxy switcher
+        self.auto_proxy_switch = True
+        self.__proxy_worker = ThreadPoolExecutor(max_workers=2)
+        self.__generate_next_proxy()
+    # end def
+
+    def __generate_next_proxy(self):
+        if not self.auto_proxy_switch or not self.home_url:
+            return
+        # end if
+
+        try:
+            from fp.fp import FreeProxy
+        except ImportError as e:
+            logger.debug('free-proxy package is not found.')
+            return
+        # end try
+
+        scheme = urlparse(self.home_url).scheme
+        https = scheme == 'https'
+
+        def generator():
+            gen_time_key = '__last_http_proxy_gen_time'
+            last_gen = getattr(self, gen_time_key, 0)
+            if time.time() - last_gen > 30:
+                setattr(self, gen_time_key, time.time())
+                proxy = FreeProxy(https=https).get()
+                self.set_proxy(scheme, proxy)
+            # end if
+        # end def
+
+        self.__proxy_worker.submit(generator)
+    # end def
+
+    def __process_response(self, response: Response) -> Response:
+        # generate a proxy after each response received
+        self.__generate_next_proxy()
+
+        if response.status_code in [403, 429, 503]:
+            logger.debug('%s\n%s\n%s', '-' * 60, response.text, '-' * 60)
+            raise LNException(f'{response.status_code} {response.reason} -- could not bypass cloudflare.')
+        # end if
+
+        response.raise_for_status()
+        response.encoding = 'utf8'
+        self.cookies.update({
+            x.name: x.value
+            for x in response.cookies
+        })
+        return response
+    # end def
+
+    # ------------------------------------------------------------------------- #
+    # Implement these methods
+    # ------------------------------------------------------------------------- #
+
+    def initialize(self) -> None:
+        pass
+    # end def
+
+    def login(self, email: str, password: str) -> None:
+        pass
+    # end def
+
+    def logout(self) -> None:
+        pass
+    # end def
+
+    def search_novel(self, query) -> List[Dict[str, str]]:
+        '''Gets a list of results matching the given query'''
+        return []
+    # end def
+
+    @abstractmethod
+    def read_novel_info(self) -> None:
+        '''Get novel title, autor, cover etc'''
+        raise NotImplementedError()
+    # end def
+
+    @abstractmethod
+    def download_chapter_body(self, chapter) -> str:
+        '''Download body of a single chapter and return as clean html format.'''
+        raise NotImplementedError()
+    # end def
+
+    def download_image(self, url) -> bytes:
+        '''Download image from url'''
+        logger.info('Downloading image: ' + url)
+        response = self.get_response(url, headers={
+            'accept': 'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.9'
+        })
+        return response.content
+    # end def
+
+    def get_chapter_index_of(self, url) -> int:
+        '''Return the index of chapter by given url or 0'''
+        url = (url or '').strip().strip('/')
+        for chapter in self.chapters:
+            if chapter['url'] == url:
+                return chapter['id']
+            # end if
+        # end for
+        return 0
+    # end def
+
+    # ------------------------------------------------------------------------- #
+    # Helper methods to be used
+    # ------------------------------------------------------------------------- #
+
+    def destroy(self) -> None:
+        self._destroyed = True
+        self.volumes.clear()
+        self.chapters.clear()
+        self.scraper.close()
+        self.executor.shutdown(False)
+        self.__proxy_worker.shutdown(False)
+    # end def
+
+    @property
+    def headers(self) -> dict:
+        return dict(self.scraper.headers)
+    # end def
+
+    def set_header(self, key: str, value: str) -> None:
+        self.scraper.headers[key.lower()] = value
+    # end def
+
+    @property
+    def cookies(self) -> dict:
+        return {x.name: x.value for x in self.scraper.cookies}
+    # end def
+    
+    def set_cookie(self, name: str, value: str) -> None:
+        self.scraper.cookies[name] = value
+    # end def
+
+    def set_proxy(self, scheme: str, proxy_url: str) -> None:
+        self.scraper.proxies[scheme] = proxy_url
+    # end def
+
+    def absolute_url(self, url, page_url=None) -> str:
+        url = (url or '').strip()
+        if len(url) > 1000 or url.startswith('data:'):
+            return url
+        # end if
+        if not page_url:
+            page_url = self.last_visited_url
+        # end if
+        if not url or len(url) == 0:
+            return url
+        elif url.startswith('//'):
+            return self.home_url.split(':')[0] + ':' + url
+        elif url.find('//') >= 0:
+            return url
+        elif url.startswith('/'):
+            return self.home_url.strip('/') + url
+        elif page_url:
+            return page_url.strip('/') + '/' + url
+        else:
+            return self.home_url + url
+        # end if
+    # end def
+
+    def is_relative_url(self, url) -> bool:
+        page = urlparse(self.novel_url)
+        url = urlparse(url)
+        return (page.hostname == url.hostname
+                and url.path.startswith(page.path))
+    # end def
+
+    def get_response(self, url, **kargs) -> Response:
+        if self._destroyed:
+            raise LNException('Instance is detroyed')
+        # end if
+
+        kargs = kargs or dict()
+        #kargs.setdefault('verify', False)
+        #kargs.setdefault('allow_redirects', True)
+        kargs.setdefault('timeout', (7, 301))  # in seconds
+        headers = kargs.setdefault('headers', {})
+        headers = {k.lower(): v for k, v in headers.items()}
+        headers.setdefault('host', urlparse(self.home_url).hostname)
+        headers.setdefault('origin', self.home_url.strip('/'))
+        headers.setdefault('referer', self.novel_url.strip('/'))
+        logger.debug('GET url=%s, headers=%s', url, headers)
+
+        with get_domain_semaphore(url):
+            with no_ssl_verification():
+                response = self.scraper.get(url, **kargs)
+
+        self.last_visited_url = url.strip('/')
+        return self.__process_response(response)
+    # end def
+
+    def post_response(self, url, data={}, headers={}) -> Response:
+        if self._destroyed:
+            raise LNException('Instance is detroyed')
+        # end if
+
+        headers = {k.lower(): v for k, v in headers.items()}
+        headers.setdefault('content-type', 'application/json')
+        headers.setdefault('host', urlparse(self.home_url).hostname)
+        headers.setdefault('origin', self.home_url.strip('/'))
+        headers.setdefault('referer', self.novel_url.strip('/'))
+        logger.debug('POST url=%s, data=%s, headers=%s', url, data, headers)
+
+        with get_domain_semaphore(url):
+            with no_ssl_verification():
+                response = self.scraper.post(
+                    url,
+                    data=data,
+                    headers=headers,
+                    # verify=False,
+                    # allow_redirects=True,
+                )
+
+        return self.__process_response(response)
+    # end def
+
+    def submit_form(self, url, data={}, multipart=False, headers={}) -> Response:
+        '''Submit a form using post request'''
+        if self._destroyed:
+            raise LNException('Instance is detroyed')
+        # end if
+ 
+        headers = {k.lower(): v for k, v in headers.items()}
+        headers.setdefault('content-type', 'multipart/form-data' if multipart \
+            else 'application/x-www-form-urlencoded; charset=UTF-8')
+        headers.setdefault('host', urlparse(self.home_url).hostname)
+        headers.setdefault('origin', self.home_url.strip('/'))
+        headers.setdefault('referer', self.novel_url.strip('/'))
+        logger.debug('SUBMIT url=%s, data=%s, headers=%s', url, data, headers)
+
+        return self.post_response(url, data, headers)
+    # end def
+
+    def get_soup(self, *args, **kwargs) -> BeautifulSoup:
+        parser = kwargs.pop('parser', None)
+        response = self.get_response(*args, **kwargs)
+        return self.make_soup(response, parser)
+    # end def
+
+    def make_soup(self, response, parser=None) -> BeautifulSoup:
+        if isinstance(response, Response):
+            html = response.content.decode('utf8', 'ignore')
+        elif isinstance(response, bytes):
+            html = response.decode('utf8', 'ignore')
+        elif isinstance(response, str):
+            html = str(response)
+        else:
+            raise LNException('Could not parse response')
+        # end if
+        soup = BeautifulSoup(html, parser or 'lxml')
+        if not soup.find('body'):
+            raise ConnectionError('HTML document was not loaded properly')
+        # end if
+        return soup
+    # end def
+
+    def get_json(self, *args, **kwargs) -> dict:
+        kwargs = kwargs or dict()
+        headers = kwargs.setdefault('headers', {})
+        headers = {k.lower(): v for k, v in headers.items()}
+        headers.setdefault('accept', 'application/json, text/javascript, */*')
+        response = self.get_response(*args, **kwargs)
+        return response.json()
+    # end def
+
+    def post_soup(self, url, data={}, headers={}, parser='lxml') -> BeautifulSoup:
+        response = self.post_response(url, data, headers)
+        return self.make_soup(response, parser)
+    # end def
+
+    def post_json(self, url, data={}, headers={}) -> dict:
+        headers = {k.lower(): v for k, v in headers.items()}
+        headers.setdefault('accept', 'application/json, text/plain, */*')
+        response = self.post_response(url, data, headers)
+        return response.json()
+    # end def
+
+    def download_cover(self, output_file) -> None:
+        response = self.get_response(self.novel_cover)
+        with open(output_file, 'wb') as f:
+            f.write(response.content)
+        # end with
+    # end def
+    
+# end class
---
+++ packages/lightnovel-crawler/lightnovel_crawler-2.33.1-py3-none-any.whl/lncrawl/core/downloader.py
@@ -0,0 +1,301 @@
+# -*- coding: utf-8 -*-
+"""
+To download chapter bodies
+"""
+import base64
+import hashlib
+import json
+import logging
+import os
+import time
+from io import BytesIO
+
+import bs4
+from PIL import Image
+from tqdm import tqdm
+from requests.exceptions import RequestException
+    
+from ..core.exeptions import LNException
+from .arguments import get_args
+
+logger = logging.getLogger(__name__)
+
+def resolve_all_futures(futures_to_check, desc='', unit=''):
+    if not futures_to_check:
+        return
+    # end if
+    is_debug_mode = os.getenv('debug_mode') == 'yes'
+    bar = tqdm(desc=desc, unit=unit,
+               total=len(futures_to_check), 
+               disable=is_debug_mode)
+    try:
+        for future in futures_to_check:
+            try:
+                message = future.result()
+                if message and not is_debug_mode:
+                    bar.clear()
+                    logger.warning(message)
+                # end if
+            except KeyboardInterrupt as ex:
+                raise LNException('Cancelled by user')
+            finally:
+                bar.update()
+            # end try
+        # end for
+    finally:
+        bar.close()
+        for future in futures_to_check:
+            future.cancel()
+        # end for
+    # end try
+# end def
+
+def get_chapter_filename(app, chapter):
+    from .app import App
+    assert isinstance(app, App)
+
+    dir_name = os.path.join(app.output_path, 'json')
+    if app.pack_by_volume:
+        vol_name = 'Volume ' + str(chapter['volume']).rjust(2, '0')
+        dir_name = os.path.join(dir_name, vol_name)
+    # end if
+
+    chapter_name = str(chapter['id']).rjust(5, '0')
+    return os.path.join(dir_name, chapter_name + '.json')
+# end def
+
+
+def extract_chapter_images(app, chapter):
+    from .app import App
+    assert isinstance(app, App)
+    assert app.crawler is not None
+    assert isinstance(chapter, dict), 'Invalid chapter'
+
+    if not chapter['body']:
+        return
+    # end if 
+
+    chapter.setdefault('images', {})
+    soup = app.crawler.make_soup(chapter['body'])
+    for img in soup.select('img'):
+        if not img or not img.has_attr('src'):
+            continue
+        # end if
+        full_url = app.crawler.absolute_url(img['src'], page_url=chapter['url'])   
+        filename = hashlib.md5(full_url.encode()).hexdigest() + '.jpg'
+        img.attrs = {'src': 'images/' + filename, 'alt': filename}
+        chapter['images'][filename] = full_url
+    # end for
+    
+    soup_body = soup.select_one('body')
+    assert isinstance(soup_body, bs4.Tag), 'Invalid soup body'
+    chapter['body'] = ''.join([str(x) for x in soup_body.contents])
+# end def
+
+
+def save_chapter_body(app, chapter):
+    from .app import App
+    assert isinstance(app, App)
+
+    file_name = get_chapter_filename(app, chapter)
+
+    title = chapter['title'].replace('>', '&gt;').replace('<', '&lt;')
+    if title not in chapter['body']:
+        chapter['body'] = '<h1>%s</h1>\n%s' % (title, chapter['body'])
+    if get_args().add_source_url and chapter['url'] not in chapter['body']:
+        chapter['body'] += '<br><p>Source: <a href="%s">%s</a></p>' % (
+            chapter['url'], chapter['url'])
+    # end if
+
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, 'w', encoding="utf-8") as file:
+        file.write(json.dumps(chapter, ensure_ascii=False))
+    # end with
+# end def
+
+
+def download_chapter_body(app, chapter):
+    assert isinstance(chapter, dict)
+    from .app import App
+    assert isinstance(app, App)
+    assert app.crawler is not None
+
+    try:
+        # Read old chapter
+        file_name = get_chapter_filename(app, chapter)
+        if os.path.exists(file_name):
+            logger.debug('Restoring from %s', file_name)
+            with open(file_name, 'r', encoding="utf-8") as file:
+                old_chapter = json.load(file)
+            # end with
+            if old_chapter.get('body') and old_chapter.get('success'):
+                chapter.update(**old_chapter)
+                return
+            # end if
+        # end def
+
+        # Fetch new chapter body
+        retry_count = 2
+        for i in range(retry_count):
+            try:
+                logger.debug('Downloading chapter %d: %s', chapter['id'], chapter['url'])
+                chapter['body'] = app.crawler.download_chapter_body(chapter)
+                extract_chapter_images(app, chapter)
+                chapter['success'] = True
+            except KeyboardInterrupt as ex:
+                raise LNException('Cancelled by user')
+            except RequestException as e:
+                if i < retry_count:
+                    logger.debug('Error: %s | Retrying...', str(e))
+                    time.sleep(3 + 5 * i)  # wait before next retry
+                else:
+                    logger.debug('Failed after %d retries', i, e)
+                    return f"[{chapter['id']}] Failed to get chapter body ({e.__class__.__name__}: {e})"
+                # end if
+            except Exception as e:
+                logger.debug('Failed', e)
+                return f"[{chapter['id']}] Failed to get chapter body ({e.__class__.__name__}: {e})"
+            # end try
+        # end for
+    finally:
+        chapter.setdefault('body', '')
+        save_chapter_body(app, chapter)
+        app.progress += 1
+    # end try
+# end def
+
+
+def download_chapters(app):
+    from .app import App
+    assert isinstance(app, App)
+    assert app.crawler is not None
+
+    if not app.output_formats:
+        app.output_formats = {}
+    # end if
+
+    app.progress = 0
+    futures_to_check = [
+        app.crawler.executor.submit(
+            download_chapter_body,
+            app,
+            chapter,
+        )
+        for chapter in app.chapters
+    ]
+
+    try:
+        resolve_all_futures(futures_to_check, desc='Chapters', unit='item')
+    finally:
+        logger.info('Processed %d chapters' % app.progress)
+    # end try
+# end def
+
+
+def download_image(app, url) -> Image.Image:
+    from .app import App
+    assert isinstance(app, App)
+    assert app.crawler is not None
+
+    assert isinstance(url, str), 'Invalid image url'
+    if len(url) > 1000 or url.startswith('data:'):
+        content = base64.b64decode(url.split('base64,')[-1])
+    else:
+        content = app.crawler.download_image(url)
+    # end if
+    return Image.open(BytesIO(content))
+# end def
+
+
+def download_cover_image(app):
+    from .app import App
+    assert isinstance(app, App)
+    assert app.crawler is not None
+
+    filename = os.path.join(app.output_path, 'cover.jpg')
+
+    if not os.path.isfile(filename):
+        cover_urls = [
+            app.crawler.novel_cover,
+            'https://source.unsplash.com/featured/800x1032?abstract',
+        ]
+        for url in cover_urls:
+            try:
+                logger.info('Downloading cover image: %s', url)
+                img = download_image(app, url)
+                img.convert('RGB').save(filename, "JPEG")
+                logger.debug('Saved cover: %s', filename)
+                app.progress += 1
+                break
+            except KeyboardInterrupt as e:
+                raise LNException('Cancelled by user')
+            except Exception as e:
+                logger.debug('Failed to get cover: %s', url, e)
+            # end try
+        # end for
+    # end if
+             
+    if not os.path.isfile(filename):
+        return f"[{filename}] Failed to download cover image"
+    # end if
+    
+    app.crawler.novel_cover = filename
+    app.book_cover = filename
+# end def
+
+
+def download_content_image(app, url, filename):
+    from .app import App
+    assert isinstance(app, App)
+    image_folder = os.path.join(app.output_path, 'images')
+    image_file = os.path.join(image_folder, filename)
+    try:
+        if os.path.isfile(image_file):
+            return
+        # end if
+        img = download_image(app, url)        
+        os.makedirs(image_folder, exist_ok=True)
+        with open(image_file, 'wb') as f:
+            img.convert('RGB').save(f, "JPEG")
+            logger.debug('Saved image: %s', image_file)
+        # end with
+    except KeyboardInterrupt as e:
+        raise LNException('Cancelled by user')
+    except Exception as e:
+        return f"[{filename}] Failed to get content image: {url} | {e.__class__.__name__}: {e}"
+    finally:
+        app.progress += 1
+    # end try
+# end def
+
+
+def download_chapter_images(app):
+    from .app import App
+    assert isinstance(app, App)
+    assert app.crawler is not None
+
+    # download or generate cover
+    app.progress = 0
+    futures_to_check = [
+        app.crawler.executor.submit(
+            download_cover_image,
+            app,
+        )
+    ]
+    futures_to_check += [
+        app.crawler.executor.submit(
+            download_content_image,
+            app,
+            url,
+            filename,
+        )
+        for chapter in app.chapters
+        for filename, url in chapter.get('images', {}).items()
+    ]
+
+    try:
+        resolve_all_futures(futures_to_check, desc='  Images', unit='item')
+    finally:
+        logger.info('Processed %d images' % app.progress)
+    # end try
+# end def
