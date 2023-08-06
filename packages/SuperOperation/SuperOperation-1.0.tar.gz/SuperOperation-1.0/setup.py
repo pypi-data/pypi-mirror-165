4903d5ba4d2d15248f18ed201a85e15adc9503b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:42 +0000

    Add package SuperOperation/1.0/SuperOperation-1.0.tar.gz

---
+++ packages/SuperOperation/SuperOperation-1.0.tar.gz/SuperOperation-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="SuperOperation",
+    version="1.0",
+    description="首次发布，进行连加，连乘，多加，多乘的运算，敬请测试",
+    author="Yz",
+    author_email="825508423@qq.com",
+    py_module=["SuperOperation"]
+)
