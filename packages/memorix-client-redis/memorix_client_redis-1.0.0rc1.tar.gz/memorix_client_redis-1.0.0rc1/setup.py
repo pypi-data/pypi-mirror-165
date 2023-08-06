eac30184bc1a5bbcb7a53c09b5f4fdcca01adbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:41 +0000

    Add package memorix-client-redis/1.0.0rc1/memorix_client_redis-1.0.0rc1.tar.gz

---
+++ packages/memorix-client-redis/memorix_client_redis-1.0.0rc1.tar.gz/memorix_client_redis-1.0.0rc1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['memorix_client_redis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dacite>=1.6.0,<2.0.0', 'redis>=4.3.4,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['start = memorix_client_redis.start:start']}
+
+setup_kwargs = {
+    'name': 'memorix-client-redis',
+    'version': '1.0.0rc1',
+    'description': '',
+    'long_description': 'None',
+    'author': 'Yuval Saraf',
+    'author_email': 'unimonkiez@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
