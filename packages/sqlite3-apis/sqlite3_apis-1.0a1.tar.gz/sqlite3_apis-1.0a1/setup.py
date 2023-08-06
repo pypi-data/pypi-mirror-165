21c838e1bf3d0968790515d3d772640cf8e240d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:23 +0000

    Add package sqlite3-apis/1.0a1/sqlite3_apis-1.0a1.tar.gz

---
+++ packages/sqlite3-apis/sqlite3_apis-1.0a1.tar.gz/sqlite3_apis-1.0a1/setup.py
@@ -0,0 +1,20 @@
+"""
+作者:陈泊文
+日期:2022年09月02日
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sqlite3_apis',
+    version = '1.0a1',
+    description = 'None',
+    include_package_data= True,
+    author= '北悸安凉',
+    author_email= '2591623190@qq.com',
+    license = 'MIT License',
+    classifiers= [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
