ff0e00cbd7a3405ec72fde0c9977a44bdb99a7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package jaical/0.0.1/jaical-0.0.1.tar.gz

---
+++ packages/jaical/jaical-0.0.1.tar.gz/jaical-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9'
+]
+setup(
+    name='jaical',
+    version='0.0.1',
+    description='this is a trial package used for learning.',
+    url='',
+    author='Jayashree',
+    author_email='jayukudari@gmail.com',
+    licence='MIT',
+    packages=['jaical'],
+    install_requires=[]
+)
+
