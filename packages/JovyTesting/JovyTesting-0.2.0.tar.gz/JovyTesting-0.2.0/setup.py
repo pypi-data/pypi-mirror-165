abe5d7996d6bd2f038c34bf38741a13ace9833a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:29 +0000

    Add package JovyTesting/0.2.0/JovyTesting-0.2.0.tar.gz

---
+++ packages/JovyTesting/JovyTesting-0.2.0.tar.gz/JovyTesting-0.2.0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import *
+
+
+setup(
+    name="JovyTesting",
+    description= "The testing package for Jomy",
+    version= "0.2.0",
+    author = "Muheko Nikolas",
+    packages = find_packages()
+    
+)
