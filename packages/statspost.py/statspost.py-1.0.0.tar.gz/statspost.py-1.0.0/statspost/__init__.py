e66fcd902eb4968fe95fdcafc39f0a6f76576f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:58 +0000

    Add package statspost.py/1.0.0/statspost.py-1.0.0.tar.gz

---
+++ packages/statspost.py/statspost.py-1.0.0.tar.gz/statspost.py-1.0.0/statspost/__init__.py
@@ -0,0 +1,32 @@
+__title__ = "statspost"
+__author__ = "Dhruva Shaw"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022-present Dhruvacube"
+__version__ = "1.0.0"
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from typing import NamedTuple, Literal
+import logging
+
+from .http import *
+from .enums import *
+from .errors import *
+from ._type import *
+from .client import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(
+    major=1, minor=0, micro=0, releaselevel="final", serial=0
+)
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+del logging, NamedTuple, Literal, VersionInfo
---
+++ packages/statspost.py/statspost.py-1.0.0.tar.gz/statspost.py-1.0.0/statspost/_type.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+
+class _MissingSentinel:
+    """A type safe sentinel for the `None` type."""
+
+    __slots__ = ()
+
+    def __eq__(self, other):
+        return False
+
+    def __bool__(self):
+        return False
+
+    def __hash__(self):
+        return 0
+
+    def __repr__(self):
+        return "..."
+
+    def __iter__(self):
+        return iter([])
+
+    def __len__(self):
+        return 0
+
+
+MISSING: Any = _MissingSentinel()
