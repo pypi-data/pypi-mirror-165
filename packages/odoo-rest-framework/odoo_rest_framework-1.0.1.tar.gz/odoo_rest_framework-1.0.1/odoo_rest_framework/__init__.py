bc6c3764f304c915051f5a4dd94466f8a731e509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:45 +0000

    Add package odoo-rest-framework/1.0.1/odoo_rest_framework-1.0.1.tar.gz

---
+++ packages/odoo-rest-framework/odoo_rest_framework-1.0.1.tar.gz/odoo_rest_framework-1.0.1/odoo_rest_framework/__init__.py
@@ -0,0 +1,12 @@
+# from . import (
+#     JwtHttp,
+#     Util,
+#     Validator,
+#     firebase
+# )
+#
+
+from .JwtHttp import JwtHttp
+from .Validator import Validator
+from .Util import Util
+from .Firebase import send_notifications
