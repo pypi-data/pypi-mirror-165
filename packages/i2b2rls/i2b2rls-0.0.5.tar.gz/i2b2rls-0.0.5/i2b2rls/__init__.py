db782628a59077fa5f9ef17cbafde0ab3fb827ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:27 +0000

    Add package i2b2rls/0.0.5/i2b2rls-0.0.5.tar.gz

---
+++ packages/i2b2rls/i2b2rls-0.0.5.tar.gz/i2b2rls-0.0.5/i2b2rls/__init__.py
@@ -0,0 +1,14 @@
+"""
+The python package `i2b2rls` provides commands to manage users
+in the [i2b2](https://www.i2b2.org/) clinical data warehousing platform
+and adds security enhancements with PostgreSQL Row Level Security templates.
+
+This includes:
+    * Frontend user_manager and project management
+    * Datasources
+
+## TODO
+* Project as class
+* POSTGRES UPDATE:
+https://www.rapid7.com/db/vulnerabilities/postgres-cve-2020-25696/
+"""
