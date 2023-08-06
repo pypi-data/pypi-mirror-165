8373ca8c30fcd2852357d8b9558f5c90c1edcfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:25 +0000

    Add package agrolens/0.0.1/agrolens-0.0.1.tar.gz

---
+++ packages/agrolens/agrolens-0.0.1.tar.gz/agrolens-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='agrolens',
+    version='0.0.1',
+    packages=['agrolens'],
+    license='MIT',
+    author='myname',
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
+    description='Some description'
+)
