8cbdd2ec42b867ddac90a3c11535271e61ece3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:34 +0000

    Add package paradime/0.3.3/paradime-0.3.3-py3-none-any.whl

---
+++ packages/paradime/paradime-0.3.3-py3-none-any.whl/paradime/types.py
@@ -0,0 +1,28 @@
+"""Type definitions for paraDime."""
+
+from typing import Tuple, Union, Callable
+
+import numpy as np
+import numpy.typing as npt
+import scipy.sparse
+import torch
+
+Rels = Union[
+    np.ndarray,
+    torch.Tensor,
+    scipy.sparse.spmatrix,
+    Tuple[np.ndarray, np.ndarray],
+]
+
+TensorLike = Union[
+    np.ndarray,
+    torch.Tensor,
+]
+
+BinaryTensorFun = Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
+
+IndexList = Union[list[int], npt.NDArray[np.integer], torch.Tensor]
+
+Data = Union[
+    np.ndarray, torch.Tensor, dict[str, Union[np.ndarray, torch.Tensor]]
+]
