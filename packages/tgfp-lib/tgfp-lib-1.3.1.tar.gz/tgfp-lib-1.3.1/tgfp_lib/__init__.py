272d49d7543fbcfaaaa9c2a52c11114113e1963e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:47 +0000

    Add package tgfp-lib/1.3.1/tgfp-lib-1.3.1.tar.gz

---
+++ packages/tgfp-lib/tgfp-lib-1.3.1.tar.gz/tgfp-lib-1.3.1/tgfp_lib/__init__.py
@@ -0,0 +1,10 @@
+""" Init file for tgfp_lib module """
+from tgfp import TGFP, TGFPGame, TGFPTeam, TGFPPick, TGFPPlayer
+
+__all__ = [
+    'TGFP',
+    'TGFPTeam',
+    'TGFPPick',
+    'TGFPPlayer',
+    'TGFPGame'
+]
