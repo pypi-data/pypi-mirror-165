fc81e3d54d4e66b3257f4d54fad760d6cff60cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:15 +0000

    Add package cloudtipsadp/0.14.2/cloudtipsadp-0.14.2.tar.gz

---
+++ packages/cloudtipsadp/cloudtipsadp-0.14.2.tar.gz/cloudtipsadp-0.14.2/src/cloudtipsadp/payouts/connect/repository.py
@@ -0,0 +1,19 @@
+from cloudtipsadp.connect.repository import Repository
+
+
+class PayoutRepository(Repository):
+    """Выплата."""
+
+    def list(self):
+        """Получение всех транзакций выплат получателям менеджера."""
+        url = self(self.base_path)
+        return self.req.get(url, headers=self.session.get_headers()).json()
+
+    def get(self, obj_id: str):
+        raise NotImplementedError()
+
+    def save(self, obj):
+        self.session.add(obj)
+
+    def update(self, obj):
+        raise NotImplementedError()
