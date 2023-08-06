435bd7abaaa7c1d27d878428fc8f6ede49f8d980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:55 +0000

    Add package iptoolbox/0.0.3/iptoolbox-0.0.3.tar.gz

---
+++ packages/iptoolbox/iptoolbox-0.0.3.tar.gz/iptoolbox-0.0.3/iptoolbox/__init__.py
@@ -0,0 +1,9 @@
+from .tools import RandomIp4
+from . import validation
+
+__name__ = "iptoolbox"
+__version__ = "0.0.3"
+__description__ = f'{__name__} is a tool for purposefully creating IP, testing and scanning it'
+__url_repository__ = f'https://github.com/V70024/{__name__}/tree/{__version__}'
+__url_documentation__ = f'https://github.com/V70024/{__name__}/wiki/{__name__}-{__version__}'
+__url_bog_tracker__ = f'https://github.com/V70024/{__name__}/issues/'
