3bafc239d2677d5b7c12ab803ee668e5759ccd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:23 +0000

    Add package napari-turing/0.3.1/napari-turing-0.3.1.tar.gz

---
+++ packages/napari-turing/napari-turing-0.3.1.tar.gz/napari-turing-0.3.1/src/napari_turing/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.3.1"
+from ._widget import TuringViewer
+from .Models._TuringPattern import TuringPattern
+from .Models._model_list import *
+
+# __all__ = ("TuringViewer", "TuringPattern", "GrayScott", "Brusselator", "FitzHughNagumo")
