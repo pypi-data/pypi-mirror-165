0b49d61d5a8b421bef5d6609307479d3026f1701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:52 +0000

    Add package dbt-flink/1.2.1/dbt-flink-1.2.1.tar.gz

---
+++ packages/dbt-flink/dbt-flink-1.2.1.tar.gz/dbt-flink-1.2.1/dbt/adapters/flink/__init__.py
@@ -0,0 +1,10 @@
+from dbt.adapters.base import AdapterPlugin
+
+from dbt.adapters.flink.connections import FlinkConnectionManager  # noqa
+from dbt.adapters.flink.connections import FlinkCredentials
+from dbt.adapters.flink.impl import FlinkAdapter
+from dbt.include import flink
+
+Plugin = AdapterPlugin(
+    adapter=FlinkAdapter, credentials=FlinkCredentials, include_path=flink.PACKAGE_PATH
+)
