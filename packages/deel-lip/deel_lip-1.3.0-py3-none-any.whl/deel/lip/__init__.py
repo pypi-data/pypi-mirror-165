93f679e6da180dd1019ad521d572cf26cf2a29a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:23 +0000

    Add package deel-lip/1.3.0/deel_lip-1.3.0-py3-none-any.whl

---
+++ packages/deel-lip/deel_lip-1.3.0-py3-none-any.whl/deel/lip/__init__.py
@@ -0,0 +1,15 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+from . import activations
+from . import callbacks
+from . import constraints
+from . import initializers
+from . import layers
+from . import unconstrained_layers
+from . import losses
+from . import metrics
+from .model import Sequential, Model, vanillaModel
+from . import normalizers
+from . import utils
