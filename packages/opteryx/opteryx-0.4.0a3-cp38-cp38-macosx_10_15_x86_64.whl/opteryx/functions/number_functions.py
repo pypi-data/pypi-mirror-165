c2888715556d924dc5f4476a09b5ef6b413392b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:53 +0000

    Add package opteryx/0.4.0a3/opteryx-0.4.0a3-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/opteryx/opteryx-0.4.0a3-cp38-cp38-macosx_10_15_x86_64.whl/opteryx/functions/number_functions.py
@@ -0,0 +1,43 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+
+import numpy
+
+from pyarrow import compute
+
+
+def pi():
+    return 3.1415926535897932384626433
+
+
+def round(*args):
+    if len(args) == 1:
+        return compute.round(args[0])
+    # the second parameter is a fixed value
+    return compute.round(args[0], args[1][0])  # [#325]
+
+
+def random(size):
+    return numpy.random.uniform(size=size)
+
+
+def random_normal(size):
+    from numpy.random import default_rng
+
+    rng = default_rng()
+    return rng.standard_normal(size)
+
+
+def random_string(width):
+    random_ints = numpy.random.uniform(low=97, high=123, size=int(width))
+    return "".join([chr(int(i)) for i in random_ints])
