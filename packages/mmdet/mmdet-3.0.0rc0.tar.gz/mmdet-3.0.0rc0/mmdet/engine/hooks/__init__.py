243c4fb0b4647b399a0f3db1ffc2b6177185e744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:05 +0000

    Add package mmdet/3.0.0rc0/mmdet-3.0.0rc0.tar.gz

---
+++ packages/mmdet/mmdet-3.0.0rc0.tar.gz/mmdet-3.0.0rc0/mmdet/engine/hooks/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .checkloss_hook import CheckInvalidLossHook
+from .mean_teacher_hook import MeanTeacherHook
+from .memory_profiler_hook import MemoryProfilerHook
+from .num_class_check_hook import NumClassCheckHook
+from .set_epoch_info_hook import SetEpochInfoHook
+from .sync_norm_hook import SyncNormHook
+from .visualization_hook import DetVisualizationHook
+from .yolox_mode_switch_hook import YOLOXModeSwitchHook
+
+__all__ = [
+    'YOLOXModeSwitchHook', 'SyncNormHook', 'CheckInvalidLossHook',
+    'SetEpochInfoHook', 'MemoryProfilerHook', 'DetVisualizationHook',
+    'NumClassCheckHook', 'MeanTeacherHook'
+]
