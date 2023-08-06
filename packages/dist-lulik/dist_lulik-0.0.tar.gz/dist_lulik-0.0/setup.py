7cd9f50ba2ff1deb09c34857dfcad35d8c44d073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:38 +0000

    Add package dist-lulik/0.0/dist_lulik-0.0.tar.gz

---
+++ packages/dist-lulik/dist_lulik-0.0.tar.gz/dist_lulik-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_lulik',
+      version='0.0',
+      description='мій проект',
+      packages=['dist_lulik'],
+      author_email='lulik1988@gmail.com',
+      zip_safe=False)
