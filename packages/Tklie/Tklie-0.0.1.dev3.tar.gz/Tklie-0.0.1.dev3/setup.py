3ac5c1b3ddb3789e4bacf33ea4fb06c948a955dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:41 +0000

    Add package Tklie/0.0.1.dev3/Tklie-0.0.1.dev3.tar.gz

---
+++ packages/Tklie/Tklie-0.0.1.dev3.tar.gz/Tklie-0.0.1.dev3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="Tklie",
+    version="0.0.1.dev3",
+    author="XiangQinxi",
+    author_email="XiangQinxi@outlook.com",
+    description="使用tkinter和tkdev4开发的功能库。",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://xiangqinxidevelopment.jetbrains.space/p/tkinterdev",
+    python_requires=">=3.6",
+    install_requires=[
+        "Pillow",
+        "colorama",
+        "tkdev4",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/Tklie/Tklie-0.0.1.dev3.tar.gz/Tklie-0.0.1.dev3/src/tkile/Constant.py
@@ -0,0 +1,25 @@
+Auto = "auto"
+Dark = "dark"
+Light = "light"
+
+MainWindow = "mainwindow"
+TransientWindow = "transientwindow"
+TabbedWindow = "tabbedwindow"
+
+Default = "default"
+
+Round = "round"
+RoundSmall = "smallround"
+RoundNone = "noneround"
+
+BGRRed = 0x000000FF
+BGRGreen = 0x0000FF00
+BGRBlue = 0x00FF0000
+BGRBlack = 0x00000000
+BGRWhite = 0x00FFFFFF
+
+Flat = "flat"
+Raised = "raised"
+Sunken = "sunken"
+Groove = "groove"
+Ridge = "ridge"
