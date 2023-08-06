500c0dd236b51dfb2d62670f38c710e5b8c6db60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package scPANTHEON/0.0.14/scPANTHEON-0.0.14.tar.gz

---
+++ packages/scPANTHEON/scPANTHEON-0.0.14.tar.gz/scPANTHEON-0.0.14/scpantheon/main.py
@@ -0,0 +1,17 @@
+try:
+    from scpantheon import source
+except:
+    import source
+from bokeh.server.server import Server
+
+server = Server({'/': source.main}, num_procs=1)
+server.start()
+
+# if __name__ == '__main__':
+def run():
+    print('Opening Bokeh application on http://localhost:5006/')
+
+    server.io_loop.add_callback(server.show, "/")
+    server.io_loop.start()
+run()
+# source.main()
