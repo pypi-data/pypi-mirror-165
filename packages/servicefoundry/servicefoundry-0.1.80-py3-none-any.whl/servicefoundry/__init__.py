183473b64bcdf7e1826d45de7bcc60f6407e34ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:15 +0000

    Add package servicefoundry/0.1.80/servicefoundry-0.1.80-py3-none-any.whl

---
+++ packages/servicefoundry/servicefoundry-0.1.80-py3-none-any.whl/servicefoundry/__init__.py
@@ -0,0 +1,17 @@
+from servicefoundry.auto_gen.models import Env, HealthProbe, Image, Port, Resources
+from servicefoundry.v2 import (
+    Application,
+    Build,
+    DockerFileBuild,
+    GithubSource,
+    HttpProbe,
+    Job,
+    LocalSource,
+    ManualTrigger,
+    Notebook,
+    PythonBuild,
+    RemoteSource,
+    ScheduledTrigger,
+    Service,
+)
+from servicefoundry.version import __version__
