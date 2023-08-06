010ffdb0de1bebd94d10b5265925fad5328171a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:57 +0000

    Add package clixdev/0.2.5/clixdev-0.2.5-py3-none-any.whl

---
+++ packages/clixdev/clixdev-0.2.5-py3-none-any.whl/clixdev/terminal.py
@@ -0,0 +1,21 @@
+import argparse
+from .generators.django_writer import generate
+
+
+def terminal():
+    try:
+        parser = argparse.ArgumentParser(prog="clixdev", description="Clix.dev command line tool.")
+        parser.add_argument('action', choices=['sync'])
+        parser.add_argument('terminal_token', type=str)
+        parser.add_argument('project_token', type=str)
+        parser.add_argument('-dir', type=str, required=False)
+        args = parser.parse_args()
+
+        if args.action == 'sync':
+            res = generate(args.terminal_token, args.project_token, args.dir)
+            return True
+        
+        return False
+    except Exception as e:
+        print(e)
+        return False
