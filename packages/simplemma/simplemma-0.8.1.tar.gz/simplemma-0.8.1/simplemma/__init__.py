d18224afdcc7c8b77de67e02619ce4cfb5573c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:27 +0000

    Add package simplemma/0.8.1/simplemma-0.8.1.tar.gz

---
+++ packages/simplemma/simplemma-0.8.1.tar.gz/simplemma-0.8.1/simplemma/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Simplemma."""
+
+__title__ = "simplemma"
+__author__ = "Adrien Barbaresi"
+__email__ = "barbaresi@bbaw.de"
+__license__ = "MIT"
+__version__ = "0.8.1"
+
+
+from .simplemma import lemmatize, lemma_iterator, text_lemmatizer, is_known
+from .tokenizer import simple_tokenizer
