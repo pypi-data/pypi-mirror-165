1ec43bef0f9a1054cec57a2307c8d82a9d6ce693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:11 +0000

    Add package tosa-checker/0.1.0/tosa_checker-0.1.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tosa-checker/tosa_checker-0.1.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tosa_checker/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright 2022, Arm Limited and/or its affiliates.
+# SPDX-License-Identifier: Apache-2.0
+
+"""the package provides a way to check if a TFLite model is compatible with the TOSA specification."""
+
+from _tosa_checker_wrapper import *
+
+__version__ = "0.1.0"
