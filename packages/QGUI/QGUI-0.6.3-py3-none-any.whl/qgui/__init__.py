c10c45924713d8f9e9ae49ab1b9b444d4a771d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:00 +0000

    Add package QGUI/0.6.3/QGUI-0.6.3-py3-none-any.whl

---
+++ packages/QGUI/QGUI-0.6.3-py3-none-any.whl/qgui/__init__.py
@@ -0,0 +1,16 @@
+# Author: Acer Zhang
+# Datetime: 2021/9/16 
+# Copyright belongs to the author.
+# Please indicate the source for reprinting.
+
+import qgui.base_tools
+import qgui.factory
+import qgui.notebook_tools
+import qgui.banner_tools
+
+
+from qgui.factory import CreateQGUI
+
+from qgui.manager import *
+
+from qgui.base_tools import ArgInfo
