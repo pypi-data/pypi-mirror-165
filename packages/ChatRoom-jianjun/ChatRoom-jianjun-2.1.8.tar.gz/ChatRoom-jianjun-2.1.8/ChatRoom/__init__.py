d7e99baf5bd7570c7285d22d2527d01acd09a704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:08 +0000

    Add package ChatRoom-jianjun/2.1.8/ChatRoom-jianjun-2.1.8.tar.gz

---
+++ packages/ChatRoom-jianjun/ChatRoom-jianjun-2.1.8.tar.gz/ChatRoom-jianjun-2.1.8/ChatRoom/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import sys
+
+__version__ = "1.0.0"
+
+from ChatRoom.main import Room, User
+from ChatRoom.net import Server, Client
+from ChatRoom.net import hash_encryption
+from ChatRoom.gui import RunRoom as _RunRoom
+
+del log, encrypt, main, net, sys, gui, MessyServerHardware
