db6bae6ce64716480201293f21dab1083adb11f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:33 +0000

    Add package ferret-xai/0.4.0/ferret_xai-0.4.0-py2.py3-none-any.whl

---
+++ packages/ferret-xai/ferret_xai-0.4.0-py2.py3-none-any.whl/ferret/__init__.py
@@ -0,0 +1,30 @@
+"""Top-level package for ferret."""
+
+__author__ = """Giuseppe Attanasio"""
+__email__ = "giuseppeattanasio6@gmail.com"
+__version__ = "0.4.0"
+
+
+from .explainers.shap import SHAPExplainer
+from .explainers.gradient import GradientExplainer, IntegratedGradientExplainer
+from .explainers.lime import LIMEExplainer
+from .explainers.dummy import DummyExplainer
+
+from .benchmark import Benchmark
+
+from .evaluators.faithfulness_measures import (
+    AOPC_Comprehensiveness_Evaluation,
+    AOPC_Sufficiency_Evaluation,
+    TauLOO_Evaluation,
+)
+from .evaluators.plausibility_measures import (
+    AUPRC_PlausibilityEvaluation,
+    Tokenf1_PlausibilityEvaluation,
+    TokenIOU_PlausibilityEvaluation,
+)
+
+from .datasets.datamanagers import HateXplainDataset
+
+# from .evaluators.classes_evaluation_measures import (
+#    AOPC_Comprehensiveness_Evaluation_by_class,
+# )
---
+++ packages/ferret-xai/ferret_xai-0.4.0-py2.py3-none-any.whl/ferret_xai-0.4.0.dist-info/AUTHORS.rst
@@ -0,0 +1,14 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Giuseppe Attanasio <giuseppeattanasio6@gmail.com>
+* Eliana Pastor <eliana.pastor@centai.eu>
+
+Contributors
+------------
+
+None yet. Why not be the first?
