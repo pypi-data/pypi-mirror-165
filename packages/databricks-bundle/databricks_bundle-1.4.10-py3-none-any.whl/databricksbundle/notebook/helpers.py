669d9b9fc125cb8101fcc90cdbfca9f364e69186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:06 +0000

    Add package databricks-bundle/1.4.10/databricks_bundle-1.4.10-py3-none-any.whl

---
+++ packages/databricks-bundle/databricks_bundle-1.4.10-py3-none-any.whl/databricksbundle/notebook/helpers.py
@@ -0,0 +1,25 @@
+import sys
+from databricksbundle.dbutils.IPythonDbUtilsResolver import resolve_dbutils
+
+
+def get_notebook_context():
+    return resolve_dbutils().notebook.entry_point.getDbutils().notebook().getContext()
+
+
+def get_user_email():
+    return get_notebook_context().tags().get("user").get()
+
+
+def get_notebook_path():
+    return get_notebook_context().notebookPath().get()
+
+
+def is_notebook_environment():
+    return sys.argv and sys.argv[0][-15:] == "/PythonShell.py"
+
+
+def ipython_display(obj):
+    # pylint: disable=import-outside-toplevel
+    import IPython
+
+    IPython.get_ipython().user_ns["display"](obj)
