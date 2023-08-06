a514e15754c5af63c600bff0fbdd2ba7a6d4a2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:55 +0000

    Add package monitor-logs/0.1.2/monitor_logs-0.1.2.tar.gz

---
+++ packages/monitor-logs/monitor_logs-0.1.2.tar.gz/monitor_logs-0.1.2/setup.py
@@ -0,0 +1,21 @@
+##setup.py
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "monitor_logs/README.md").read_text()
+
+# This call to setup() does all the work
+setup(name = 'monitor_logs', 
+    version = '0.1.2', 
+    description = 'Monitor your logs from anywhere', 
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages = ['monitor_logs'], 
+    url="https://github.com/Vinithavn/Monitor-model-logs/",
+    zip_safe = False
+)
