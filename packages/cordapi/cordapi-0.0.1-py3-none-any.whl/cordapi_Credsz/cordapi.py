081f497bfda76505ef886eee9805b5b1cbcd36ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:07 +0000

    Add package cordapi/0.0.1/cordapi-0.0.1-py3-none-any.whl

---
+++ packages/cordapi/cordapi-0.0.1-py3-none-any.whl/cordapi_Credsz/cordapi.py
@@ -0,0 +1,25 @@
+import requests
+
+def create_session(token, proxy = None):
+    session = requests.Session()
+    response = str(session.get('https://discord.com/app').cookies)
+    if proxy:
+        session.proxies.update(proxy)
+    session.headers.update({
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.6',
+        'authorization': token,
+        'content-type': 'application/json',
+        'cookie': '__dcfduid=%s; __sdcfduid=%s; locale=en-US' % (response.split('dcfduid=')[1][:-31], response.split('sdcfduid=')[1][:-20]),
+        'origin': 'https://discord.com',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'sec-gpc': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+        'x-debug-options': 'bugReporterEnabled',
+        'x-discord-locale': 'en-US',
+        'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEwNS4wLjAuMCBTYWZhcmkvNTM3LjM2IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTA1LjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE0NTI1OSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbH0='
+    })
+    return session
