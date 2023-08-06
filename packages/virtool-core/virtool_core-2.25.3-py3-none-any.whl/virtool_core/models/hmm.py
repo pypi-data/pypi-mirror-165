ec69f3d4cc91af84b3cf0c33104220589b8f4e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:30 +0000

    Add package virtool-core/2.25.3/virtool_core-2.25.3-py3-none-any.whl

---
+++ packages/virtool-core/virtool_core-2.25.3-py3-none-any.whl/virtool_core/models/hmm.py
@@ -0,0 +1,41 @@
+from typing import List, Dict, Any
+
+from pydantic import validator
+
+from virtool_core.models.basemodel import BaseModel
+from virtool_core.models.searchresult import SearchResult
+
+
+class HMMMinimal(BaseModel):
+    id: str
+    cluster: int
+    count: int
+    families: Dict[str, int]
+    name: List[str]
+
+    @validator("name")
+    def is_name_valid(cls, name: List[str]) -> List[str]:
+        if len(name) > 3:
+            raise ValueError("The length of name should be a maximum of 3")
+        return name
+
+
+class HMMSequenceEntry(BaseModel):
+    accession: str
+    gi: str
+    name: str
+    organism: str
+
+
+class HMM(HMMMinimal):
+    entries: List[HMMSequenceEntry]
+    genera: Dict[str, int]
+    hidden: bool
+    length: int
+    mean_entropy: float
+    total_entropy: float
+
+
+class HMMSearchResult(SearchResult):
+    documents: List[HMMMinimal]
+    status: Dict[str, Any]
