63ea27fb790a6ed72606b8244e245f0a6655fee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:31 +0000

    Add package friendly-styles/0.2/friendly_styles-0.2.tar.gz

---
+++ packages/friendly-styles/friendly_styles-0.2.tar.gz/friendly_styles-0.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    import os
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='friendly_styles',
+    version='0.2',
+    description='Pygments styles designed for friendly/friendly-traceback',
+    long_description=read('README.md'),
+    license='MIT',
+
+    author='André Roberge',
+    author_email='andre.roberge@gmail.com',
+
+    url='https://github.com/friendly-traceback/friendly_styles',
+    packages=find_packages(),
+    install_requires=['pygments >= 2'],
+
+    entry_points={
+        "pygments.styles": [
+            "friendly_light = friendly_styles.friendly_light:FriendlyLightStyle",
+            "friendly_dark = friendly_styles.friendly_dark:FriendlyDarkStyle",
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
+
