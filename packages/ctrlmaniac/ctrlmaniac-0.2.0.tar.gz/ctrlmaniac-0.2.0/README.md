a9ff849eae80362e9961795ae26c20287bc23cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:07 +0000

    Add package ctrlmaniac/0.2.0/ctrlmaniac-0.2.0.tar.gz

---
+++ packages/ctrlmaniac/ctrlmaniac-0.2.0.tar.gz/ctrlmaniac-0.2.0/README.md
@@ -0,0 +1,108 @@
+# Davide Di Criscito
+
+```python
+class CtrlManiac:
+    """CtrlManiac because i overuse the ctrl key."""
+
+    def __init__(self):
+        """My specifications."""
+        self.name = "Davide"
+        self.surname = "Di Criscito"
+        self.nickname = "Dave"
+        self.pronouns = (
+            "He",
+            "Him",
+        )
+
+        self.languages_spoken = ["it_IT", "en_US", "en_GB"]
+
+        self.description = (
+            "I'm currently an Artisan, soon-to-be a full-stack web developer!"
+        )
+
+        self.website = "https://ctrlmaniac.me"
+
+        self.hobbies = [
+            "coding",
+            "hiking",
+            "photography",
+            "watching movies & TV series",
+            "listening to music",
+            "reading books and manga",
+            "going out with my friends and have fun",
+        ]
+
+        self.coding_languages = [
+            "Python",  # I simply love it
+            "JavaScript",
+        ]
+
+        self.favourite_tools = [
+            "poetry",  # makes it simpler to manage a python project
+            "black",  # chooses a coding style for me and makes my code pretty
+            "isort",  # sorts python imports so that everything is really clear
+            "flake8",  # tells me whether I've made a mistake
+            "pydocstyle",  # helps me write better documentation
+            "yarn",  # I love it for the workspace feature
+            "lerna",  # I use it to manage my monorepos
+            "prettier",  # chooses a coding style for me and makes my code pretty
+        ]
+
+        self.IDEs = [
+            "VScode",  # because it's awesome!
+        ]
+
+    def greet(self) -> None:
+        """Say hi."""
+        print(
+            f"Hi! I'm {self.name} {self.surname}, but you can call me {self.nickname}."
+        )
+        print(self.description)
+        print(f"You can know more about me by visiting my website: {self.website}")
+
+    def learn_new_coding_languange(self, language) -> None:
+        """Print a string that tells what new coding language I'm learning.
+
+        :param language: the coding language to learn.
+        """
+        print(f"I'm studying a new coding language: {language}")
+```
+
+## Fun Facts
+
+You can install this package via pip by running `pip install ctrlmaniac` and then excecute the program by typing into your terminal `python -m ctrlmaniac` and see the output!
+
+Or import the package:
+
+```
+>>> from ctrlmaniac import ctrlmaniac
+>>> me = ctrlmaniac.CtrlManiac()
+
+>>> me.greet()
+Hi! I'm Davide Di Criscito, but you can call me Dave.
+I'm currently an Artisan, soon-to-be a full-stack web developer!
+You can know more about me by visiting my website: https://ctrlmaniac.me
+
+>>> me.learn_new_coding_languange("Java")
+I'm studying a new coding language: Java
+
+>>> me.learn_new_coding_languange("Typescript")
+I'm studying a new coding language: Typescript
+```
+
+## Follow Me on:
+
+- [Twitter](https://twitter.com/ctrlmaniac)
+- [Instagram](https://instagram.com/meldinco)
+- [Linkedin](https://www.linkedin.com/in/dcdavide/)
+- [Pinterest](https://pin.it/4erq4kP)
+
+## :pray: Help me test The House!
+
+Help me test my text-based game written in python!
+You can find the repository [here](https://github.com/ctrlmaniac/the-house) or you can install it via pip `pip install thehouse` and then run `thehouse` to make the game start!
+
+## Stats
+
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=ctrlmaniac)](https://github.com/anuraghazra/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ctrlmaniac)](https://github.com/anuraghazra/github-readme-stats)
---
+++ packages/ctrlmaniac/ctrlmaniac-0.2.0.tar.gz/ctrlmaniac-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ctrlmaniac']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ctrlmaniac',
+    'version': '0.2.0',
+    'description': "Davide DC's GitHub Readme profile",
+    'long_description': '# Davide Di Criscito\n\n```python\nclass CtrlManiac:\n    """CtrlManiac because i overuse the ctrl key."""\n\n    def __init__(self):\n        """My specifications."""\n        self.name = "Davide"\n        self.surname = "Di Criscito"\n        self.nickname = "Dave"\n        self.pronouns = (\n            "He",\n            "Him",\n        )\n\n        self.languages_spoken = ["it_IT", "en_US", "en_GB"]\n\n        self.description = (\n            "I\'m currently an Artisan, soon-to-be a full-stack web developer!"\n        )\n\n        self.website = "https://ctrlmaniac.me"\n\n        self.hobbies = [\n            "coding",\n            "hiking",\n            "photography",\n            "watching movies & TV series",\n            "listening to music",\n            "reading books and manga",\n            "going out with my friends and have fun",\n        ]\n\n        self.coding_languages = [\n            "Python",  # I simply love it\n            "JavaScript",\n        ]\n\n        self.favourite_tools = [\n            "poetry",  # makes it simpler to manage a python project\n            "black",  # chooses a coding style for me and makes my code pretty\n            "isort",  # sorts python imports so that everything is really clear\n            "flake8",  # tells me whether I\'ve made a mistake\n            "pydocstyle",  # helps me write better documentation\n            "yarn",  # I love it for the workspace feature\n            "lerna",  # I use it to manage my monorepos\n            "prettier",  # chooses a coding style for me and makes my code pretty\n        ]\n\n        self.IDEs = [\n            "VScode",  # because it\'s awesome!\n        ]\n\n    def greet(self) -> None:\n        """Say hi."""\n        print(\n            f"Hi! I\'m {self.name} {self.surname}, but you can call me {self.nickname}."\n        )\n        print(self.description)\n        print(f"You can know more about me by visiting my website: {self.website}")\n\n    def learn_new_coding_languange(self, language) -> None:\n        """Print a string that tells what new coding language I\'m learning.\n\n        :param language: the coding language to learn.\n        """\n        print(f"I\'m studying a new coding language: {language}")\n```\n\n## Fun Facts\n\nYou can install this package via pip by running `pip install ctrlmaniac` and then excecute the program by typing into your terminal `python -m ctrlmaniac` and see the output!\n\nOr import the package:\n\n```\n>>> from ctrlmaniac import ctrlmaniac\n>>> me = ctrlmaniac.CtrlManiac()\n\n>>> me.greet()\nHi! I\'m Davide Di Criscito, but you can call me Dave.\nI\'m currently an Artisan, soon-to-be a full-stack web developer!\nYou can know more about me by visiting my website: https://ctrlmaniac.me\n\n>>> me.learn_new_coding_languange("Java")\nI\'m studying a new coding language: Java\n\n>>> me.learn_new_coding_languange("Typescript")\nI\'m studying a new coding language: Typescript\n```\n\n## Follow Me on:\n\n- [Twitter](https://twitter.com/ctrlmaniac)\n- [Instagram](https://instagram.com/meldinco)\n- [Linkedin](https://www.linkedin.com/in/dcdavide/)\n- [Pinterest](https://pin.it/4erq4kP)\n\n## :pray: Help me test The House!\n\nHelp me test my text-based game written in python!\nYou can find the repository [here](https://github.com/ctrlmaniac/the-house) or you can install it via pip `pip install thehouse` and then run `thehouse` to make the game start!\n\n## Stats\n\n[![Anurag\'s GitHub stats](https://github-readme-stats.vercel.app/api?username=ctrlmaniac)](https://github.com/anuraghazra/github-readme-stats)\n[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ctrlmaniac)](https://github.com/anuraghazra/github-readme-stats)\n',
+    'author': 'Davide Di Criscito',
+    'author_email': 'davide.dicriscito@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
