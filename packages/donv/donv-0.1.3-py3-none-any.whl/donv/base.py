f66b5c89d774fadd6791301c07388f181961269c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:16 +0000

    Add package donv/0.1.3/donv-0.1.3-py3-none-any.whl

---
+++ packages/donv/donv-0.1.3-py3-none-any.whl/donv/base.py
@@ -0,0 +1,55 @@
+import os
+from . import option
+from . import version
+
+
+
+class Docker_Base:
+    def __init__(self, opt):
+        super(Docker_Base, self).__init__()
+        self.opt = opt
+        self.init_cmd()
+        self.set_cmd()
+
+    def init_cmd(self):
+        self.format = f''
+
+    def set_cmd(self):
+        self.add_option('echo [DOCKER INFO]')
+        self.add_option('echo')
+        self.add_option('echo [IMAGE]')
+        self.add_option('docker images')
+        self.add_option('echo')
+        self.add_option('echo [CONTAINER]')
+        self.add_option('docker ps -a')
+
+    def add_option(self, arg: str, parser=''):
+        self.format += f'{parser}\n'
+        self.format += arg
+
+    def do_cmd(self):
+        try:
+            os.system(self.format)
+        except:
+            print('[!] wrong command')
+
+    def print_cmd(self):        
+        LINE = '--------------------------------------------------------------------------------'
+        print(LINE)
+        print(version.DONV.format(version.VERSION))
+        print(LINE)
+        print(self.format.strip())
+        print(LINE)
+
+    @property
+    def cmd(self):
+        return self.format
+
+def main():
+    opt = option.Options().get_option()
+    docker = Docker_Base(opt)
+    docker.print_cmd()
+    docker.do_cmd()
+
+if __name__ == '__main__':
+    main()
