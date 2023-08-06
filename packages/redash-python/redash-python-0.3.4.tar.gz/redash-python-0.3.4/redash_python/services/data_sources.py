dc6434e070cd9d529e66394c2a90aca91650c964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:42 +0000

    Add package redash-python/0.3.4/redash-python-0.3.4.tar.gz

---
+++ packages/redash-python/redash-python-0.3.4.tar.gz/redash-python-0.3.4/redash_python/services/data_sources.py
@@ -0,0 +1,12 @@
+from .base import BaseService
+from .mixins import CommonMixin, NameMixin, PrintMixin
+
+
+class DataSourcesService(CommonMixin, NameMixin, PrintMixin):
+    def __init__(self, base: BaseService) -> None:
+
+        # init mixins
+        CommonMixin.__init__(self, base)
+
+        self.__base = base
+        self.endpoint = "/api/data_sources"
