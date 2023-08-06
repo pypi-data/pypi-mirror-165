b139e6108a7f26ae690f7f9a124037a111681d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:52 +0000

    Add package pya2ldb/0.12.92/pya2ldb-0.12.92-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/pya2ldb/pya2ldb-0.12.92-cp310-cp310-macosx_10_9_x86_64.whl/pya2l/examples/ex03.py
@@ -0,0 +1,8 @@
+from pya2l import DB
+from pya2l import model
+
+db = DB()
+session = db.open_existing("ASAP2_Demo_V161")
+measurements = session.query(model.Measurement).order_by(model.Measurement.name).all()
+for m in measurements:
+    print("{:48} {:12} 0x{:08x}".format(m.name, m.datatype, m.ecu_address.address))
