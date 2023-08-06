4c5e38151358e80f45dcbce5d4a2ce5351bfcb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:29 +0000

    Add package metacity/0.5.2/metacity-0.5.2-cp39-cp39-macosx_10_9_universal2.whl

---
+++ packages/metacity/metacity-0.5.2-cp39-cp39-macosx_10_9_universal2.whl/metacity-0.5.2.data/data/_skbuild/macosx-10.9-universal2-3.9/cmake-build/CMakeFiles/geometry.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/runner/work/Metacity/Metacity/src/cpp/geometry.cpp" "CMakeFiles/geometry.dir/src/cpp/geometry.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/geometry.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/attribute.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/attribute.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/attribute.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/bbox.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bbox.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bbox.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/bvh.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bvh.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bvh.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/graham.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/graham.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/graham.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/grid.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/grid.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/grid.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/layer.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/layer.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/layer.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/model.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/model.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/model.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/modifiers.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/modifiers.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/modifiers.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/mesh_pipeline/triangulation.cpp" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/triangulation.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/triangulation.cpp.o.d"
+  "/Users/runner/work/Metacity/Metacity/src/cpp/vector_pipeline/graph.cpp" "CMakeFiles/geometry.dir/src/cpp/vector_pipeline/graph.cpp.o" "gcc" "CMakeFiles/geometry.dir/src/cpp/vector_pipeline/graph.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
