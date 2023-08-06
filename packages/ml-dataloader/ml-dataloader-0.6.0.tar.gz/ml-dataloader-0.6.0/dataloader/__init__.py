3324e926ee0cce2e45f602b6ea6ad7d6b16b8b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:38 +0000

    Add package ml-dataloader/0.6.0/ml-dataloader-0.6.0.tar.gz

---
+++ packages/ml-dataloader/ml-dataloader-0.6.0.tar.gz/ml-dataloader-0.6.0/dataloader/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import logging
+import os
+import sys
+
+logging_level_registry = {
+    'CRITICAL': logging.CRITICAL,
+    'ERROR': logging.ERROR,
+    'WARNING': logging.WARNING,
+    'INFO': logging.INFO,
+    'DEBUG': logging.DEBUG,
+    None: logging.CRITICAL
+}
+
+LOG_FMT = '[%(levelname)1.1s %(asctime)s.%(msecs)03d %(module)s:%(lineno)d] %(message)s'
+
+logging_level = logging_level_registry[os.getenv('DL_LOGGING_LEVEL')]
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging_level)
+logger.propagate = False
+
+formatter = logging.Formatter(LOG_FMT)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
