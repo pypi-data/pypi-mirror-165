cec7eeba8a90884e43bae7f1e578bdcc036f073d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:44 +0000

    Add package gtasker/1.0.0/gtasker-1.0.0-py3-none-any.whl

---
+++ packages/gtasker/gtasker-1.0.0-py3-none-any.whl/gtasker/scheduler.py
@@ -0,0 +1,269 @@
+from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+import os
+from .task import Task, TaskStatus
+from .utils import get_children_pids, parse_str_to_list, follow, read_last_lines, SERVER_INTERVAL
+from .tracker import GPUTracker
+import threading
+import time
+import subprocess
+import json
+import sys
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s %(thread)s %(funcName)s %(message)s"
+)
+
+os.environ["PYTHONUNBUFFERED"] = "1"
+
+class TaskScheduler:
+    def __init__(self):
+        self.mutex = threading.Lock()
+        self.cur_id = 0
+        self.gpu_tracker = GPUTracker()
+
+        self.tasks = {}
+
+        self.serving = False
+
+
+    def get_status(self):
+        task_status_tuple_list = []
+        for task_id, task in self.tasks.items():
+            task_status_tuple_list.append(task._get_status_tuple())
+        return task_status_tuple_list
+
+    def add_task(self, cmd, req_memory, path, req_gpu_index, pre_reqt):
+        logging.info(f"Add task: {cmd}")
+        self.mutex.acquire()
+        logging.info(f"Add Task {self.cur_id}")
+        self.cur_id += 1
+        
+        
+        pre_reqt = parse_str_to_list(pre_reqt, int)
+        req_gpu_index = parse_str_to_list(req_gpu_index, int)
+
+        task = Task(
+            id=self.cur_id,
+            cmd=cmd,
+            req_memory=req_memory,
+            path=path,
+            req_gpu_index=req_gpu_index,
+            pre_reqt=pre_reqt,
+            priority=0
+        )
+        self.tasks[self.cur_id] = task
+
+        self.mutex.release()
+
+        ret_msg = f"New Task Added ({self.cur_id})"
+        return ret_msg
+
+    def remove_task(self, task_id):
+        self.mutex.acquire()
+        if task_id in self.tasks:
+            if self.tasks[task_id].status == TaskStatus.RUNNING:
+                ret_msg = f"Task {task_id} is running. Please kill it first."
+            else:
+                del self.tasks[task_id]
+                ret_msg = f"Task {task_id} Removed"
+        else:
+            ret_msg = f"Task {task_id} Not Found"
+        
+        self.mutex.release()
+        return ret_msg
+    
+    def stash_task(self, task_id):
+        self.mutex.acquire()
+        if task_id in self.tasks:
+            if self.tasks[task_id].status != TaskStatus.PENDING:
+                ret_msg = f"Task {task_id} is not pending. Please check."
+            else:
+                self.tasks[task_id].status = TaskStatus.STASHED
+                ret_msg = f"Task {task_id} Stashed"
+        else:
+            ret_msg = f"Task {task_id} Not Found"
+        
+        self.mutex.release()
+        return ret_msg
+
+    def kill_task(self, task_id):
+        self.mutex.acquire()
+        if task_id in self.tasks:
+            if self.tasks[task_id].status == TaskStatus.RUNNING:
+                #kill task
+                pid = self.tasks[task_id].executed_proc.pid
+                proc_list = get_children_pids(pid, include_self=True)
+                for p in proc_list:
+                    p.kill()
+                ret_msg = f"Task {task_id} Killed"
+            else:
+                ret_msg = f"Task {task_id} Not Running"
+        else:
+            ret_msg = f"Task {task_id} Not Found"
+        
+        self.mutex.release()
+        return ret_msg
+
+
+    def restart_task(self, task_id, in_place=False):
+        self.mutex.acquire()
+        if task_id in self.tasks:
+            # clone task
+            new_task = Task(
+                id=task_id,
+                cmd=self.tasks[task_id].cmd,
+                req_memory=self.tasks[task_id].req_memory,
+                path=self.tasks[task_id].path,
+                req_gpu_index=self.tasks[task_id].req_gpu_index,
+                pre_reqt=self.tasks[task_id].pre_reqt,
+                priority=self.tasks[task_id].priority
+            )
+            if in_place:
+                self.tasks[task_id] = new_task
+                ret_msg = f"Task {task_id} Restarted In Place"
+            else:
+                self.cur_id += 1
+                new_task.id = self.cur_id
+                self.tasks[self.cur_id] = new_task
+
+                # modify pre-requisit tasks
+                for id, task in self.tasks.items():
+                    task.modify_pre_reqt(task_id, self.cur_id)
+                
+                ret_msg = f"Task {task_id} Restarted As New Task ({self.cur_id})"
+        else:
+            ret_msg = f"Task {task_id} Not Found"
+        
+        self.mutex.release()
+        return ret_msg
+
+    def clean_task(self):
+        # remove all success tasks, modify the pre-requisit tasks of pending tasks
+        self.mutex.acquire()
+        success_task_ids = [id for id, task in self.tasks.items() if task.status == TaskStatus.SUCCESS]
+        for id in success_task_ids:
+            del self.tasks[id]
+        
+        for id, task in self.tasks.items():
+            if task.status == TaskStatus.PENDING:
+                for pre_id in task.pre_reqt:
+                    if pre_id in success_task_ids:
+                        task.modify_pre_reqt(pre_id, -1)
+
+        self.mutex.release()
+
+        ret_msg = f"{len(success_task_ids)} Success Tasks Removed"
+        return ret_msg
+
+    def task_is_running(self, task_id):
+        return task_id in self.tasks and self.tasks[task_id].status == TaskStatus.RUNNING
+
+    def task_exists(self, task_id):
+        return task_id in self.tasks
+
+    def follow_task(self, task_id, callback=sys.stdout.write):
+        logging.info(f"Follow task: {task_id}")
+        if task_id in self.tasks:
+            if self.tasks[task_id].status == TaskStatus.RUNNING:
+                task_log_file = self.tasks[task_id].log_file
+                stop_func = lambda: (task_id not in self.tasks or self.tasks[task_id].status != TaskStatus.RUNNING)
+                follow(task_log_file, stop_func=stop_func, callback=callback)
+                ret_msg = f"Task {task_id} Follow Done"
+            else:
+                ret_msg = f"Task {task_id} Not Running"
+        else:
+            ret_msg = f"Task {task_id} Not Found"
+        
+        return ret_msg
+    
+    
+
+    def log_task(self, task_id, lines=10):
+        logging.info(f"Log task: {task_id}")
+        if task_id in self.tasks:
+            task_log_file = self.tasks[task_id].log_file
+            if task_log_file is not None:
+                ret_msg = read_last_lines(task_log_file, lines)
+            else:
+                ret_msg = f"Task {task_id} Log File Not Found"
+        else:
+            ret_msg = f"Task {task_id} Not Found"
+        
+        return ret_msg
+
+
+    def _run_task(self, task_id, assigned_gpu=None):
+        if task_id not in self.tasks: return
+        task = self.tasks[task_id]
+        t = threading.Thread(target=task.spawn, args=(self.mutex, assigned_gpu, self.gpu_tracker))
+        t.start()
+
+    def _check_pre_reqt(self, task_id):
+        task = self.tasks[task_id]
+        for pre_task_id in task.pre_reqt:
+            if self.tasks[pre_task_id].status != TaskStatus.SUCCESS:
+                return False
+        return True
+
+
+    def _find_task_to_run(self):
+        self.gpu_tracker.update()
+        for task_id, task in self.tasks.items():
+            if not task.check_exec_status(): continue
+            if not self._check_pre_reqt(task_id): continue
+            
+            # check if task is ready to run
+            if task.req_memory > 0:
+                ok, indexes = task.check_exec_mem(self.gpu_tracker.free_memory)
+                if not ok: continue
+                assigned_gpu = indexes[0]
+
+            else:
+                assigned_gpu = None
+
+            return task_id, assigned_gpu
+        return None, None
+
+
+    def _serve(self):
+        while self.serving:
+            self.mutex.acquire()
+            task_id, assigned_gpu = self._find_task_to_run()
+            self.mutex.release()
+
+            if task_id is not None:
+                logging.info(f"Task {task_id} Found. Assigned GPU: {assigned_gpu}")
+                self._run_task(task_id, assigned_gpu)
+
+            time.sleep(SERVER_INTERVAL)
+
+            
+    def _kill_all_tasks(self):
+        for task_id, task in self.tasks.items():
+            self.kill_task(task_id)
+
+    def serve_forever(self):
+        self.serving = True
+        t = threading.Thread(target=self._serve)
+        t.daemon = True
+        t.start()
+        ret_msg = "Server Started"
+        return ret_msg
+
+    def shutdown(self):
+        self.serving = False
+        self._kill_all_tasks()
+        time.sleep(1)
+        ret_msg = "Server Shutdown\nGoodbye!\n"
+        return ret_msg
+
+    def _undone_task_count(self):
+        return sum([1 for task_id, task in self.tasks.items() if task.status not in [TaskStatus.SUCCESS, TaskStatus.FAILED, TaskStatus.KILLED]])
+
+    def ready_to_shutdown(self):
+        return self._undone_task_count() == 0
+    
+if __name__ == "__main__":
+    
+    print('done')
---
+++ packages/gtasker/gtasker-1.0.0-py3-none-any.whl/gtasker/task.py
@@ -0,0 +1,216 @@
+import subprocess
+import os
+import time
+import datetime
+from enum import Enum 
+from .utils import get_log_file_path, pack_command
+
+class TaskStatus(Enum):
+    PENDING = 0 # Task is waiting to be executed
+    STASHED = 1 # Task is stashed, will not be executed until it is unstashed manually
+    RUNNING = 2 # Task is running
+    SUCCESS = 3 # Task is done successfully
+    FAILED = 4 # Task is done with error
+    KILLED = 5 # Task is killed by user
+    LOCKED = 6 # Task is being edited 
+
+
+class Task:
+    def __init__(self,
+        id: int,                        # task id, start from 1, 0 is reserved.
+        cmd: str,                       # command to be executed
+        req_memory: int,                # required GPU memory in MB
+        path: str,                      # working directory, default is current directory
+        req_gpu_index: list = [],       # required GPU index, default is empty, means any GPU is OK
+        pre_reqt: list = [],            # prerequisite tasks, default is empty, means no prerequisite
+        priority: int = 0,              # priority, default is 0, higher priority task will be executed first
+    ) -> None:
+        self.id = id
+        self.cmd = cmd 
+        self.req_memory = req_memory
+        self.req_gpu_index = req_gpu_index
+        self.pre_reqt = pre_reqt
+        self.path = os.path.abspath(path)
+
+        self.priority = priority
+
+
+        # runtime variables
+        self.status = TaskStatus.PENDING
+
+        self.executed_proc = None
+        self.assigned_gpu = None
+        self.start_time = None
+        self.end_time = None
+        self.log_file = None
+
+
+    def check_exec_mem(self, free_memory):
+        if len(self.req_gpu_index) == 0:
+            available_gpus = [i for i in free_memory if free_memory[i] > self.req_memory] 
+        else:
+            available_gpus = [i for i in self.req_gpu_index if free_memory[i] > self.req_memory]
+        return len(available_gpus) > 0, available_gpus
+
+    def check_exec_status(self):
+        return self.status in [TaskStatus.PENDING, TaskStatus.STASHED]
+
+    def modify_pre_reqt(self, old_id, new_id = -1):
+        if new_id == -1:
+            self.pre_reqt = [i for i in self.pre_reqt if i != old_id]
+        else:
+            self.pre_reqt = [new_id if i == old_id else i for i in self.pre_reqt]
+
+
+    def spawn(self, mutex, assigned_gpu=None, gpu_tracker=None):
+        # lock
+        mutex.acquire()
+        self.assigned_gpu = assigned_gpu
+        self.status = TaskStatus.RUNNING
+        self.start_time = datetime.datetime.now().strftime("%m-%d %H:%M:%S")
+
+        self.log_file = get_log_file_path(self.id)
+        
+        log_file = open(self.log_file, "w")
+        log_file.write(self._meta_str())
+        log_file.flush()
+        log_file.close()
+
+        # execute
+        cmd = pack_command(self.cmd, None)
+        print('Executing: {}'.format(cmd))
+        
+        log_file = open(self.log_file, "ab")
+
+        subprocess_env = os.environ.copy()
+        if assigned_gpu is not None:
+            subprocess_env["CUDA_VISIBLE_DEVICES"] = f"{assigned_gpu}"
+        subprocess_env["PYTHONUNBUFFERED"] = "1"
+        proc = subprocess.Popen(cmd, shell=True, cwd=self.path, stdout=log_file, stderr=log_file, bufsize=1, start_new_session=True, env = subprocess_env, executable="/bin/bash")
+
+        self.executed_proc = proc
+
+        if assigned_gpu is not None and gpu_tracker is not None:
+            gpu_tracker.book_memory(self.assigned_gpu, self.req_memory, proc.pid)
+
+        mutex.release()
+
+        exit_code = self.executed_proc.wait()
+
+        log_file.close()
+
+        mutex.acquire()
+        self.end_time = datetime.datetime.now().strftime("%m-%d %H:%M:%S")
+
+        if assigned_gpu is not None and gpu_tracker is not None:
+            gpu_tracker.unbook_memory(assigned_gpu, proc.pid)
+        
+        if exit_code == 0:
+            self.status = TaskStatus.SUCCESS
+        elif exit_code == -9:
+            self.status = TaskStatus.KILLED
+        else:
+            self.status = TaskStatus.FAILED
+        mutex.release()
+        
+        log_file = open(self.log_file, "a+")
+        log_file.write("\n"*3 + self._ending_str())
+        log_file.flush()
+        log_file.close()
+
+        return self.executed_proc
+
+    def _meta_str(self):
+        ret_str = "-" * 50 + "\n"
+        ret_str += "Task ID: {}\n".format(self.id)
+        ret_str += "Command: {}\n".format(self.cmd)
+        if self.req_memory > 0:
+            ret_str += "Type: GPU ({} MB)\n".format(self.req_memory)
+        else:
+            ret_str += "Type: CPU\n"
+        if self.start_time is not None:
+            ret_str += "Execute Time: {}\n".format(self.start_time)
+        if self.assigned_gpu is not None:
+            ret_str += "Assigned GPU: {}\n".format(self.assigned_gpu)
+        ret_str += "_" * 50 + "\n\n"
+        return ret_str
+
+    def _ending_str(self):
+        ret_str = "-" * 50 + "\n"
+        ret_str += "Task Status: {}\n".format(self.status.name)
+        if self.end_time is not None:
+            ret_str += "End Time: {}\n".format(self.end_time)
+        ret_str += "_" * 50 + "\n\n"
+        return ret_str
+    
+    def __str__(self):
+        return "Task {}: {}".format(self.id, self.cmd)
+
+    def detail_str(self):
+        return self.__str__()
+
+    def _get_status_tuple(self):
+        id = str(self.id)
+        status = self.status.name
+        command = self.cmd
+        path = self.path
+        if self.req_memory > 0:
+            type_str = "GPU({} MB)".format(self.req_memory)
+            if self.assigned_gpu is not None:
+                type_str += "-{}".format(self.assigned_gpu)
+        else:
+            type_str = "CPU"
+        gpu_index = ",".join(str(i) for i in self.req_gpu_index)
+        pre_reqt = ",".join(str(i) for i in self.pre_reqt)
+        start_time = "" if self.start_time is None else self.start_time
+        end_time = "" if self.end_time is None else self.end_time
+
+        return (id, status, command, path, type_str, gpu_index, pre_reqt, start_time, end_time)
+
+
+# predefined tasks for testing
+def echo_task():
+    return Task(
+        id=1,
+        cmd="echo 'Hello World'",
+        req_memory=0,
+        path=os.getcwd(),
+        req_gpu_index=[],
+        pre_reqt=[],
+        priority=0
+    )
+
+def sleep_task():
+    return Task(
+        id=2,
+        cmd="sleep 5",
+        req_memory=0,
+        path=os.getcwd(),
+        req_gpu_index=[],
+        pre_reqt=[],
+        priority=0
+    )
+
+def failing_task():
+    return Task(
+        id=3,
+        cmd="echo 'Hello World' && exit 1",
+        req_memory=0,
+        path=os.getcwd(),
+        req_gpu_index=[],
+        pre_reqt=[],
+        priority=0
+    )
+
+def ls_task():
+    return Task(
+        id=4,
+        cmd="ls",
+        req_memory=0,
+        path="/home/fangyan/Workspace/ColBERT/",
+        req_gpu_index=[],
+        pre_reqt=[],
+        priority=0
+    )
+
+
