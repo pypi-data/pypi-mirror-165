50855b5256454252f46c9f1e83efe6c7537907f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:53 +0000

    Add package pybamm/22.8/pybamm-22.8-cp38-cp38-win_amd64.whl

---
+++ packages/pybamm/pybamm-22.8-cp38-cp38-win_amd64.whl/pybamm/models/submodels/interface/sei/no_sei.py
@@ -0,0 +1,44 @@
+#
+# Class for no SEI
+#
+import pybamm
+from .base_sei import BaseModel
+
+
+class NoSEI(BaseModel):
+    """
+    Class for no SEI.
+
+    Parameters
+    ----------
+    param : parameter class
+        The parameters to use for this submodel
+    options : dict, optional
+        A dictionary of options to be passed to the model.
+
+    **Extends:** :class:`pybamm.sei.BaseModel`
+    """
+
+    def __init__(self, param, options=None, cracks=False):
+        super().__init__(param, options=options, cracks=cracks)
+        if self.half_cell:
+            self.reaction_loc = "interface"
+        else:
+            self.reaction_loc = "full electrode"
+
+    def get_fundamental_variables(self):
+        if self.reaction_loc == "interface":
+            zero = pybamm.PrimaryBroadcast(pybamm.Scalar(0), "current collector")
+        else:
+            zero = pybamm.FullBroadcast(
+                pybamm.Scalar(0), "negative electrode", "current collector"
+            )
+        variables = self._get_standard_thickness_variables(zero, zero)
+        variables.update(self._get_standard_reaction_variables(zero, zero))
+        return variables
+
+    def get_coupled_variables(self, variables):
+        variables.update(self._get_standard_concentration_variables(variables))
+        # Update whole cell variables, which also updates the "sum of" variables
+        variables.update(super().get_coupled_variables(variables))
+        return variables
