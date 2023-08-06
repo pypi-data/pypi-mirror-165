1888b24755b80c6b102f1f846310008037c8969c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:24 +0000

    Add package Winstree/0.0.1a4/Winstree-0.0.1a4.tar.gz

---
+++ packages/Winstree/Winstree-0.0.1a4.tar.gz/Winstree-0.0.1a4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Winstree",
+    version="0.0.1.alpha4",
+    author="XiangQinxi",
+    author_email="XiangQinxi@outlook.com",
+    description="Windows环境.NET快速开发工具包",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=[
+        "pythonnet>=3.0.0a1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'gui_scripts': [
+            'winstreer = Winstreer:empty_demo',
+        ],
+    },
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data = True
+)
