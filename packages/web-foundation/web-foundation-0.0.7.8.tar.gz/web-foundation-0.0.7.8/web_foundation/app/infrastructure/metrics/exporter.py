94584fabffa4f5ad42832beff5cbe480d036fd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:02 +0000

    Add package web-foundation/0.0.7.8/web-foundation-0.0.7.8.tar.gz

---
+++ packages/web-foundation/web-foundation-0.0.7.8.tar.gz/web-foundation-0.0.7.8/web_foundation/app/infrastructure/metrics/exporter.py
@@ -0,0 +1,43 @@
+from typing import List, Dict, Any
+
+from web_foundation.app.infrastructure.metrics.basemetric import BaseMetric
+from web_foundation.app.resources.stores.store import TypeJSON
+# from web_foundation import settings
+# try:
+#     from prometheus_client import metrics, registry
+# except ImportError:
+
+
+class MetricExporter:
+    @classmethod
+    def export(cls, metrics: List[BaseMetric]) -> Dict[str, Any]:
+        raise NotImplementedError
+
+    @classmethod
+    def empty(cls):
+        return {}
+
+
+class JsonExporter(MetricExporter):
+    @classmethod
+    def export(cls, metrics: List[BaseMetric]) -> Dict[str, TypeJSON]:
+        dic = {}
+        for metric in metrics:
+            if not metric:
+                continue
+            if not dic.get(metric.name):
+                dic[metric.name] = [{"value": metric.collect(),
+                                     "labels": metric.labels,
+                                     "timestamp": metric.timestamp}]
+            else:
+                dic[metric.name].append({"value": metric.collect(),
+                                         "labels": metric.labels,
+                                         "timestamp": metric.timestamp})
+        return dic
+
+
+class PrometheusExporter(MetricExporter):
+    @classmethod
+    def export(cls, metrics: List[BaseMetric]) -> Dict[str, Any]:
+        # registry = registry.CollectorRegistry()
+        pass
