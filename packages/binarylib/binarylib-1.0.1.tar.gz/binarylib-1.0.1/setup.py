125bc36a21eebad8631349e54b670b9abe6a3a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:35 +0000

    Add package binarylib/1.0.1/binarylib-1.0.1.tar.gz

---
+++ packages/binarylib/binarylib-1.0.1.tar.gz/binarylib-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='binarylib',
+    version='1.0.1',
+    author='zhoufangfang',
+    author_email='xczff001@163.com',
+    maintainer='zhoufangfang',
+    maintainer_email='xczff001@163.com',
+    keywords=['binary','bin'],
+    descriotion='这是一个Python的二进制库',
+    py_modules=['binarylib'],
+    python_requires='>=3'
+)
+
+
