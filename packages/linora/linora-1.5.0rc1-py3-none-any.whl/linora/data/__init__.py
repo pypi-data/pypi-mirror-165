4aba2246ead146f8f9374cd5756e95564666eb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:44 +0000

    Add package linora/1.5.0rc1/linora-1.5.0rc1-py3-none-any.whl

---
+++ packages/linora/linora-1.5.0rc1-py3-none-any.whl/linora/data/__init__.py
@@ -0,0 +1,5 @@
+from linora.data import Dataset
+from linora.data import datasets
+from linora.data import TextLineDataset
+from linora.data._compress import *
+from linora.data._utils import *
---
+++ packages/linora/linora-1.5.0rc1-py3-none-any.whl/linora/train/_terminateonnan.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from linora.utils._config import Config
+
+__all__ = ['TerminateOnNaN']
+
+
+class TerminateOnNaN():
+    """Callback that terminates training when a NaN loss is encountered.
+    
+    Args:
+        monitor: Quantity to be monitored.
+    """
+    def __init__(self, monitor):
+        self._params = Config()
+        self._params.monitor = monitor
+        self._params.state = False
+        self._params.name = 'TerminateOnNaN'
+        
+    def _update(self, batch, log):
+        """update log.
+        
+        Args:
+            batch: Integer, index of batch.
+            log: dict, name and value of loss or metrics;
+        """
+        if self._params.monitor in log:
+            if np.isnan(log[self._params.monitor]).any():
+                self._params.state = True
