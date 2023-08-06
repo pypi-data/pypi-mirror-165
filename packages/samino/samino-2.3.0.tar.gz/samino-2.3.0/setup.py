fd09f501b70bd7b23efa8f52f282ea9ab5610701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:22 +0000

    Add package samino/2.3.0/samino-2.3.0.tar.gz

---
+++ packages/samino/samino-2.3.0.tar.gz/samino-2.3.0/setup.py
@@ -0,0 +1,63 @@
+import pathlib
+from setuptools import setup, find_packages
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="samino",
+    version="2.3.0",
+    url="https://github.com/SirLez/SAmino",
+    download_url="https://github.com/SirLez/SAmino/archive/refs/heads/main.zip",
+    description="Amino Bots with python!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="SirLez",
+    author_email="botsirlez@gmail.com",
+    license="MIT",
+    keywords=[
+        "api",
+        "python",
+        "python3",
+        "python3.x",
+        "SirLez",
+        "sirlez",
+        "srlz",
+        "سيرلز"
+        "bovonos",
+        "Texaz"
+        "Smile"
+        "Bovo",
+        "Marshall Amino",
+        "Texaz Amino",
+        "a7rf",
+        "A7RF",
+        "A7rf",
+        "bovonus",
+        "Amino",
+        "samino",
+        "samino py"
+        "S-Amino",
+        "samino",
+        "samino",
+        "samino-bot",
+        "samino-bots",
+        "samino-bot",
+        "ndc",
+        "narvii.apps",
+        "aminoapps",
+        "samino-py",
+        "samino",
+        "samino-bot",
+        "narvii",
+    ],
+    include_package_data=True,
+    install_requires=[
+        "JSON_minify",
+        "setuptools",
+        "requests",
+        "websocket-client==1.3.1",
+        "websockets",
+    ],
+    setup_requires=["wheel"],
+    packages=find_packages(),
+)
