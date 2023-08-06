78f80b99d1c5f70254c89c33acadaff352bf48f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:10 +0000

    Add package colink/0.1.8/colink-0.1.8.tar.gz

---
+++ packages/colink/colink-0.1.8.tar.gz/colink-0.1.8/README.md
@@ -0,0 +1,52 @@
+# CoLink Python SDK for Application
+CoLink Python SDK  provides a Python3 language support toolkit for application developers which allows them to update storage, manage computation requests, and monitor CoLink server status.
+
+## Requirements
+
+- Python 3.9
+- pytest
+
+## Examples
+
+```
+python3 examples/host_import_user.py
+```
+```
+python3 examples/host_import_user_exchange_jwt.py <address> <host_jwt> <number> 
+```
+```
+python3 examples/host_import_users_and_set_registry.py <address> <host_jwt> <number>
+```
+```
+python3 examples/user_run_task.py <address> <user_jwt A> <user_jwt B> <message> # <message> is optional
+```
+```
+python3 examples/user_greetings_to_multiple_users.py <address> <initiator_jwt> <receiver_jwt A> <receiver_jwt B> <receiver_jwt...
+```
+```
+python3 examples/auto_confirm.py <address> <user_jwt> <protocol_name>
+```
+```
+python3 examples/get_next_greeting_message.py <address> <user_jwt> 
+```
+```
+python3 examples/protocol_greetings.py --addr <address> --jwt <user_jwt> 
+```
+```
+python3 examples/user_remote_storage.py <address> <user_jwt A> <user_jwt B> <message> # <message> is optional
+```
+```
+python3 examples/user_lock.py <address> <user_jwt>
+```
+## Test
+
+```
+pip3 install colink
+pip3 install pytest
+bash pull-and-build-server.sh
+pytest test/test_python.py
+```
+
+## Release
+In order to handle server creation, remote storage management, user creation...etc more conveniently, 
+download ColinkCTL [here](https://github.com/CoLearn-Dev/colinkctl/releases).
---
+++ packages/colink/colink-0.1.8.tar.gz/colink-0.1.8/setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup
+import pip
+import os
+import pkg_resources
+
+pip.main(["install", "grpcio-tools==1.45.0"])
+import grpc_tools.protoc
+
+
+def generate_grpc_template(
+    proto_file, proto_dir="./proto", python_out="./colink", grpc_out="./colink"
+):
+    proto_include = pkg_resources.resource_filename("grpc_tools", "_proto")
+    grpc_tools.protoc.main(
+        (
+            ".",
+            "-I{}".format(proto_include),  # import well known protos
+            "-I{}".format(proto_dir),
+            "--python_out={}".format(python_out),
+            "--grpc_python_out={}".format(grpc_out),
+            "{}/{}".format(proto_dir, proto_file),
+        )
+    )
+
+
+generate_grpc_template("colink.proto")
+generate_grpc_template("colink_remote_storage.proto")
+generate_grpc_template("colink_policy_module.proto")
+generate_grpc_template("colink_registry.proto")
+
+
+def update_import_path_in_pb2_grpc():
+    with open("./colink/colink_pb2_grpc.py", "r") as f:
+        lines = f.readlines()
+    with open("./colink/colink_pb2_grpc.py", "w") as f:
+        for line in lines:
+            if "import colink_pb2 as colink__pb2" in line:
+                line = line.replace("colink_pb2", "colink.colink_pb2")
+            f.write(line)
+
+
+update_import_path_in_pb2_grpc()
+
+setup(
+    name="colink",
+    version="0.1.8",
+    description="colink python module",
+    author="Wenjie Qu",
+    author_email="",
+    packages=["colink"],  # same as name
+    install_requires=[
+        "secp256k1==0.14.0",
+        "pika==1.2.0",
+    ],  # external packages as dependencies
+)
