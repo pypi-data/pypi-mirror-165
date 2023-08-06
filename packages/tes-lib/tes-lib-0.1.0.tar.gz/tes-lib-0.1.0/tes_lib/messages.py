d10f7c8c0d807e0a7f4e9e7063f4ff3c6c41a69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:50 +0000

    Add package tes-lib/0.1.0/tes-lib-0.1.0.tar.gz

---
+++ packages/tes-lib/tes-lib-0.1.0.tar.gz/tes-lib-0.1.0/tes_lib/messages.py
@@ -0,0 +1,85 @@
+"""
+This file contains the message definitions for messages passed to the event store process via
+the request multiprocessing queue
+"""
+
+
+class _BaseMessage:
+    def __init__(self, message_type):
+        self.message_type = message_type
+
+
+class ShutdownMessage(_BaseMessage):
+    TYPE = "SHUTDOWN"
+
+    def __init__(self):
+        super().__init__(ShutdownMessage.TYPE)
+
+
+class ResetMessage(_BaseMessage):
+    TYPE = "RESET"
+
+    def __init__(self):
+        super().__init__(ResetMessage.TYPE)
+
+
+class GetAllEventsMessage(_BaseMessage):
+    TYPE = "GET_ALL"
+
+    def __init__(self):
+        super().__init__(GetAllEventsMessage.TYPE)
+
+
+class GetFullEventLogMessage(_BaseMessage):
+    TYPE = "GET_FULL"
+
+    def __init__(self):
+        super().__init__(GetFullEventLogMessage.TYPE)
+
+
+class GetEventMessage(_BaseMessage):
+    TYPE = "GET_EVENT"
+
+    def __init__(self, expectations):
+        super().__init__(GetEventMessage.TYPE)
+        self.expectations = expectations
+
+
+class GetAllMatchingEventsMessage(_BaseMessage):
+    TYPE = "GET_MATCHING"
+
+    def __init__(self, expectations):
+        super().__init__(GetAllMatchingEventsMessage.TYPE)
+        self.expectations = expectations
+
+
+class AddEventMessage(_BaseMessage):
+    TYPE = "ADD"
+
+    def __init__(self, event_fields):
+        super().__init__(AddEventMessage.TYPE)
+        self.event_fields = event_fields
+
+
+class ExpectEventMessage(_BaseMessage):
+    TYPE = "EXPECT"
+
+    def __init__(self, expectations):
+        super().__init__(ExpectEventMessage.TYPE)
+        self.expectations = expectations
+
+
+class RemoveExpectedEventMessage(_BaseMessage):
+    TYPE = "REMOVE_EXPECTED"
+
+    def __init__(self, event):
+        super().__init__(RemoveExpectedEventMessage.TYPE)
+        self.event = event
+
+
+class DeleteMatchingEventsMessage(_BaseMessage):
+    TYPE = "DELETE_MATCHING"
+
+    def __init__(self, expectations):
+        super().__init__(DeleteMatchingEventsMessage.TYPE)
+        self.expectations = expectations
