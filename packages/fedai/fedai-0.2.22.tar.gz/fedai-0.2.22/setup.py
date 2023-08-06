36571f2d928a5b254eb0f3f1427dd6a74144e06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:00 +0000

    Add package fedai/0.2.22/fedai-0.2.22.tar.gz

---
+++ packages/fedai/fedai-0.2.22.tar.gz/fedai-0.2.22/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requires_list = [x.strip() for x in f.readlines()]
+
+setup(
+    name="fedai",
+    version="0.2.22",
+    description="Xeniro Federated Learning Tools",
+    install_requires=requires_list,
+    packages=["fedai", "fedai.utils", "fedai.saas"]
+)
