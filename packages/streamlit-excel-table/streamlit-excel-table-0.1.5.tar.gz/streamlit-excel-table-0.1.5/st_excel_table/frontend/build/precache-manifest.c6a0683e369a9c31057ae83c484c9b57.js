a16c0c3da031a3de5db66342e1163559ad1f56ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:19 +0000

    Add package streamlit-excel-table/0.1.5/streamlit-excel-table-0.1.5.tar.gz

---
+++ packages/streamlit-excel-table/streamlit-excel-table-0.1.5.tar.gz/streamlit-excel-table-0.1.5/st_excel_table/frontend/build/precache-manifest.c6a0683e369a9c31057ae83c484c9b57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4365a88186157c3d17e6a3e15b41213",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d852903bbf999e5ab6aa",
+    "url": "./static/js/2.0b501eff.chunk.js"
+  },
+  {
+    "revision": "c84fe3a6fba229bd8a4fa43bc50a834a",
+    "url": "./static/js/2.0b501eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6f7ca8fef84f75f6e9",
+    "url": "./static/js/main.088f05f6.chunk.js"
+  },
+  {
+    "revision": "ddc0798ca4473527d7d9",
+    "url": "./static/js/runtime-main.4eedf725.js"
+  }
+]);
