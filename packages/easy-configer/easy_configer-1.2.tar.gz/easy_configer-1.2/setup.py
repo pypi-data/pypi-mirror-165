42b35bebe35c05d05ae0c75dc3cd6d609af4f61a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:12 +0000

    Add package easy-configer/1.2/easy_configer-1.2.tar.gz

---
+++ packages/easy-configer/easy_configer-1.2.tar.gz/easy_configer-1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os.path import abspath, join, dirname
+
+
+# read the contents of your README file
+this_directory = abspath(dirname(__file__))
+with open(join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='easy_configer',
+    version='1.2',
+    description='An easy-way for configurating pyhon program by the given config file or config str',
+    author='JosefHuang',
+    author_email='a3285556aa@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/HuangChiEn/easy_config',
+    packages=['easy_configer'],
+    keywords=["configuration", "commendline argument", "argument"],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+	    'Programming Language :: Python :: 3.9',
+    ]
+)
