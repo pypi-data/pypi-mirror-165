df21e556032ac19dbb8126f4a69d74f8ab12c7c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:56 +0000

    Add package divisible/0.1.2/divisible-0.1.2.tar.gz

---
+++ packages/divisible/divisible-0.1.2.tar.gz/divisible-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['divisible']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'divisible',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'DukeNgn',
+    'author_email': 'triduc2311m@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
