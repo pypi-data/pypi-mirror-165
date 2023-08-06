67d8b60bd04c5909d410b15f737abc307eea6ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:47 +0000

    Add package pytest-mock-resources/2.3.0/pytest-mock-resources-2.3.0.tar.gz

---
+++ packages/pytest-mock-resources/pytest-mock-resources-2.3.0.tar.gz/pytest-mock-resources-2.3.0/src/pytest_mock_resources/fixture/database/relational/generic.py
@@ -0,0 +1,219 @@
+import abc
+import fnmatch
+from typing import Tuple
+
+import attr
+import six
+from sqlalchemy import MetaData, text
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.sql.ddl import CreateSchema
+from sqlalchemy.sql.schema import Table
+
+from pytest_mock_resources import compat
+from pytest_mock_resources.compat.sqlalchemy import DeclarativeMeta
+
+
+@six.add_metaclass(abc.ABCMeta)
+class AbstractAction(object):
+    @abc.abstractmethod
+    def run(self, engine_manager):
+        """Run an action on a database via the passed-in engine_manager instance."""
+
+
+class Rows(AbstractAction):
+    def __init__(self, *rows):
+        self.rows = rows
+
+    def run(self, engine_manager):
+        rows = self._get_stateless_rows(self.rows)
+
+        metadatas = self._get_metadatas(rows)
+
+        for metadata in metadatas:
+            engine_manager.create_ddl(metadata)
+
+        self._create_rows(engine_manager.engine, rows)
+
+    @staticmethod
+    def _get_stateless_rows(rows):
+        """Create rows that aren't associated with any other SQLAlchemy session."""
+        stateless_rows = []
+        for row in rows:
+            row_args = row.__dict__
+            row_args.pop("_sa_instance_state", None)
+
+            stateless_row = type(row)(**row_args)
+
+            stateless_rows.append(stateless_row)
+        return stateless_rows
+
+    @staticmethod
+    def _get_metadatas(rows):
+        return {row.metadata for row in rows}
+
+    @staticmethod
+    def _create_rows(engine, rows):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        session.add_all(rows)
+
+        session.commit()
+        session.close()
+
+
+class Statements(AbstractAction):
+    def __init__(self, *statements):
+        self.statements = statements
+
+    def run(self, engine_manager):
+        with engine_manager.engine.begin() as connection:
+            for statement in self.statements:
+                if isinstance(statement, str):
+                    statement = text(statement)
+                connection.execute(statement)
+
+
+@attr.s
+class EngineManager(object):
+    engine = attr.ib()
+    ordered_actions = attr.ib(default=attr.Factory(tuple))
+    tables: Tuple = attr.ib(default=None, converter=attr.converters.optional(tuple))
+    session = attr.ib(default=False)
+    default_schema = attr.ib(default=None)
+
+    _ddl_created = False
+
+    def _run_actions(self):
+        for action in self.ordered_actions:
+            if isinstance(action, MetaData):
+                self.create_ddl(action)
+            elif isinstance(action, DeclarativeMeta):
+                self.create_ddl(action.metadata)
+            elif isinstance(action, AbstractAction):
+                action.run(self)
+            elif callable(action):
+                self._execute_function(action)
+            else:
+                raise ValueError(
+                    "create_fixture function takes in sqlalchemy.MetaData or actions as inputs only."
+                )
+
+    def _create_schemas(self, metadata):
+        if self._ddl_created:
+            return
+
+        all_schemas = {table.schema for table in metadata.tables.values() if table.schema}
+
+        with self.engine.begin() as connection:
+            for schema in all_schemas:
+                if self.default_schema == schema:
+                    continue
+
+                statement = CreateSchema(schema, quote=True)
+                connection.execute(statement)
+
+    def _create_tables(self, metadata):
+        if not self.tables:
+            metadata.create_all(self.engine)
+            return
+
+        table_objects = {
+            table_object
+            for table in self.tables
+            for table_object in identify_matching_tables(metadata, table)
+        }
+
+        metadata.create_all(self.engine, tables=list(table_objects))
+
+    def _execute_function(self, fn):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+
+        fn(session)
+
+        session.commit()
+        session.close()
+
+    def create_ddl(self, metadata):
+        self._create_schemas(metadata)
+        self._create_tables(metadata)
+        self._ddl_created = True
+
+    def manage_sync(self, session=None):
+        try:
+            self._run_actions()
+
+            if session:
+                if isinstance(session, sessionmaker):
+                    session_factory = session
+                else:
+                    session_factory = sessionmaker(bind=self.engine)
+
+                Session = scoped_session(session_factory)
+                session = Session(bind=self.engine)
+                yield session
+                session.close()
+            else:
+                yield self.engine
+        finally:
+            self.engine.dispose()
+
+    async def manage_async(self, session=None):
+        try:
+            self._run_actions()
+
+            async_engine = self._get_async_engine()
+
+            if session:
+                if isinstance(session, sessionmaker):
+                    session_factory = session
+                else:
+                    session_factory = sessionmaker(
+                        async_engine,
+                        expire_on_commit=False,
+                        class_=compat.sqlalchemy.asyncio.AsyncSession,
+                    )
+                async with session_factory() as session:
+                    yield session
+            else:
+                yield async_engine
+        finally:
+            self.engine.dispose()
+
+    def _get_async_engine(self, isolation_level=None):
+        url = compat.sqlalchemy.URL(
+            drivername="postgresql+asyncpg",
+            username=self.engine.pmr_credentials.username,
+            password=self.engine.pmr_credentials.password,
+            host=self.engine.pmr_credentials.host,
+            port=self.engine.pmr_credentials.port,
+            database=self.engine.pmr_credentials.database,
+            query=dict(ssl="disable"),
+        )
+        options = {}
+        if isolation_level:
+            options["isolation_level"] = isolation_level
+        return compat.sqlalchemy.asyncio.create_async_engine(url, **options)
+
+
+def identify_matching_tables(metadata, table_specifier):
+    if isinstance(table_specifier, DeclarativeMeta):
+        return [table_specifier.__table__]
+
+    if isinstance(table_specifier, Table):
+        return [table_specifier]
+
+    tables = [
+        table
+        for table_name, table in metadata.tables.items()
+        if fnmatch.fnmatch(table_name, table_specifier)
+    ]
+
+    if tables:
+        return tables
+
+    table_names = ", ".join(sorted(metadata.tables.keys()))
+    raise ValueError(
+        'Could not identify any tables matching "{}" from: {}'.format(table_specifier, table_names)
+    )
---
+++ packages/pytest-mock-resources/pytest-mock-resources-2.3.0.tar.gz/pytest-mock-resources-2.3.0/src/pytest_mock_resources/fixture/database/relational/postgresql.py
@@ -0,0 +1,129 @@
+import pytest
+import sqlalchemy
+from sqlalchemy import text
+
+from pytest_mock_resources.container.base import get_container
+from pytest_mock_resources.container.postgres import get_sqlalchemy_engine, PostgresConfig
+from pytest_mock_resources.fixture.database.generic import assign_fixture_credentials
+from pytest_mock_resources.fixture.database.relational.generic import EngineManager
+
+
+@pytest.fixture(scope="session")
+def pmr_postgres_config():
+    """Override this fixture with a :class:`PostgresConfig` instance to specify different defaults.
+
+    Examples:
+        >>> @pytest.fixture(scope='session')
+        ... def pmr_postgres_config():
+        ...     return PostgresConfig(image="postgres:9.6.10", root_database="foo")
+    """
+    return PostgresConfig()
+
+
+@pytest.fixture(scope="session")
+def pmr_postgres_container(pytestconfig, pmr_postgres_config: PostgresConfig):
+    yield from get_container(pytestconfig, pmr_postgres_config)
+
+
+def create_postgres_fixture(
+    *ordered_actions,
+    scope="function",
+    tables=None,
+    session=None,
+    async_=False,
+    createdb_template="template1",
+    engine_kwargs=None,
+):
+    """Produce a Postgres fixture.
+
+    Any number of fixture functions can be created. Under the hood they will all share the same
+    database server.
+
+    Arguments:
+        ordered_actions: Any number of ordered actions to be run on test setup.
+        scope: Passthrough pytest's fixture scope.
+        tables: Subsets the tables created by `ordered_actions`. This is generally
+            most useful when a model-base was specified in `ordered_actions`.
+        session: Whether to return a session instead of an engine directly. This can
+            either be a bool or a callable capable of producing a session.
+        async_: Whether to return an async fixture/client.
+        createdb_template: The template database used to create sub-databases. "template1" is the
+            default chosen when no template is specified.
+        engine_kwargs: Optional set of kwargs to send into the engine on creation.
+    """
+    engine_manager_kwargs = dict(
+        ordered_actions=ordered_actions,
+        tables=tables,
+        createdb_template=createdb_template,
+        engine_kwargs=engine_kwargs,
+    )
+
+    @pytest.fixture(scope=scope)
+    def _sync(pmr_postgres_container, pmr_postgres_config):
+        engine_manager = create_engine_manager(pmr_postgres_config, **engine_manager_kwargs)
+        yield from engine_manager.manage_sync(session=session)
+
+    @pytest.fixture(scope=scope)
+    async def _async(pmr_postgres_container, pmr_postgres_config):
+        engine_manager = create_engine_manager(pmr_postgres_config, **engine_manager_kwargs)
+        async for engine in engine_manager.manage_async(session=session):
+            yield engine
+
+    if async_:
+        return _async
+    else:
+        return _sync
+
+
+def create_engine_manager(
+    pmr_postgres_config, ordered_actions, tables, createdb_template="template1", engine_kwargs=None
+):
+    database_name = produce_clean_database(pmr_postgres_config, createdb_template=createdb_template)
+
+    engine = get_sqlalchemy_engine(pmr_postgres_config, database_name, **(engine_kwargs or {}))
+    assign_fixture_credentials(
+        engine,
+        drivername="postgresql+psycopg2",
+        host=pmr_postgres_config.host,
+        port=pmr_postgres_config.port,
+        database=database_name,
+        username=pmr_postgres_config.username,
+        password=pmr_postgres_config.password,
+    )
+    return EngineManager(engine, ordered_actions, tables=tables, default_schema="public")
+
+
+def produce_clean_database(config, createdb_template="template1"):
+    root_engine = get_sqlalchemy_engine(config, config.root_database, isolation_level="AUTOCOMMIT")
+    with root_engine.connect() as conn:
+        database_name = _create_clean_database(conn, createdb_template=createdb_template)
+
+    return database_name
+
+
+def _create_clean_database(conn, createdb_template="template1"):
+    with conn.begin():
+        try:
+            conn.execute(text("CREATE TABLE IF NOT EXISTS pytest_mock_resource_db(id serial);"))
+        except (sqlalchemy.exc.IntegrityError, sqlalchemy.exc.ProgrammingError):
+            # A race condition may occur during table creation if:
+            #  - another process has already created the table
+            #  - the current process begins creating the table
+            #  - the other process commits the table creation
+            #  - the current process tries to commit the table creation
+            pass
+
+        result = conn.execute(
+            text("INSERT INTO pytest_mock_resource_db VALUES (DEFAULT) RETURNING id")
+        )
+        id_ = tuple(result)[0][0]
+        database_name = "pytest_mock_resource_db_{}".format(id_)
+
+        conn.execute(
+            text('CREATE DATABASE "{}" template={}'.format(database_name, createdb_template))
+        )
+        conn.execute(
+            text('GRANT ALL PRIVILEGES ON DATABASE "{}" TO CURRENT_USER'.format(database_name))
+        )
+
+        return database_name
