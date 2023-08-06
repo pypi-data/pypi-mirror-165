64327bcf8e7739f023ee4ca3d79396318dd1818d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:10 +0000

    Add package My-little-test-here/1.0/My_little_test_here-1.0-py2.py3-none-any.whl

---
+++ packages/My-little-test-here/My_little_test_here-1.0-py2.py3-none-any.whl/MyPackage/main.py
@@ -0,0 +1,17 @@
+def max_number(a, b):
+    print("hello, this is a package demo by rs")
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def min_number(a, b):
+    print("hello, this is a package demo by rs")
+    if a > b:
+        return a
+    else:
+        return b
+
+
+
