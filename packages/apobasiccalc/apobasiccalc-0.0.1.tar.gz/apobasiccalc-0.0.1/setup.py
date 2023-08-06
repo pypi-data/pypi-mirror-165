ef325934bc9ca844f556e171f0056af5ba7f5a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:41 +0000

    Add package apobasiccalc/0.0.1/apobasiccalc-0.0.1.tar.gz

---
+++ packages/apobasiccalc/apobasiccalc-0.0.1.tar.gz/apobasiccalc-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='apobasiccalc',
+  version='0.0.1',
+  description='A very basic calculator',
+  url='',
+  author='Apo',
+  author_email='apoalquaary@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='calculator',
+  packages=find_packages(),
+  install_requires=[''],
+)
