06465b20bd05795f933fbb7b0a266a69a362bdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:47 +0000

    Add package awstokenex/0.0.1/awstokenex-0.0.1.tar.gz

---
+++ packages/awstokenex/awstokenex-0.0.1.tar.gz/awstokenex-0.0.1/README.md
@@ -0,0 +1,21 @@
+```bash
+┌──(||||||||||||||)-[~/Scripts]
+└─$ python3 export-token.py -h                                                                                                                                           2 ⨯
+usage: python3 awsTokenEx.py -f [env_file] -p [ProfileName] 
+
+-------------------------------------------------------------
+--------------- | Bulk SSH Login check |---------------------
+-------------------------------------------------------------
+            _            _____    _            ___     
+           /_\__ __ ____|_   _|__| |_____ _ _ | __|_ __
+          / _ \ V  V (_-< | |/ _ \ / / -_) ' \| _|\ \ /
+         /_/ \_\_/\_//__/ |_|\___/_\_\___|_||_|___/_\_
+                                by h4rith.com
+-------------------------------------------------------------
+
+[!] Required arguments:
+  -f , --file      Environment variable file that contain token.
+  -p , --profile   Valid profile name.
+
+---------------- Script from h4rithd.com ----------------
+```
---
+++ packages/awstokenex/awstokenex-0.0.1.tar.gz/awstokenex-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awstokenex',
+    version='0.0.1',
+    url='https://github.com/h4rithd/awsTokenEx',
+    license='MIT',
+    author='h4rithd',
+    author_email='h4rithd@pm.me',
+    description='This script is for read the env file and then search for the aws temporary credentials after that set it on ~/.aws/credentials file with profile name',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    keywords='awstokenex aws aws-token aws-secret AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'textwrap',
+        'argparse',
+        'flask_httpauth',
+        'pathlib',
+        'colorama'
+    ],
+)
