ed377cb409718c66e00727ed6b1dc4ab16c2cc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:53 +0000

    Add package packperry10/0.1.0/packperry10-0.1.0.tar.gz

---
+++ packages/packperry10/packperry10-0.1.0.tar.gz/packperry10-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='packperry10',
+   version='0.1.0',
+   author='Perihane',
+   author_email='perihane.youssef@vodafone.com',
+   packages=['packperry10']
+)
