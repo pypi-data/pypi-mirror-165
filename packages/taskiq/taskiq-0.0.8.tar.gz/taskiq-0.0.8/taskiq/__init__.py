1e78575874faa3b1d002a9b73282bdd0493646bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:41 +0000

    Add package taskiq/0.0.8/taskiq-0.0.8.tar.gz

---
+++ packages/taskiq/taskiq-0.0.8.tar.gz/taskiq-0.0.8/taskiq/__init__.py
@@ -0,0 +1,30 @@
+"""Distributed task manager."""
+from taskiq.abc.broker import AsyncBroker, AsyncTaskiqDecoratedTask
+from taskiq.abc.formatter import TaskiqFormatter
+from taskiq.abc.middleware import TaskiqMiddleware
+from taskiq.abc.result_backend import AsyncResultBackend
+from taskiq.brokers.inmemory_broker import InMemoryBroker
+from taskiq.brokers.shared_broker import async_shared_broker
+from taskiq.brokers.zmq_broker import ZeroMQBroker
+from taskiq.exceptions import TaskiqError
+from taskiq.funcs import gather
+from taskiq.message import BrokerMessage, TaskiqMessage
+from taskiq.result import TaskiqResult
+from taskiq.task import AsyncTaskiqTask
+
+__all__ = [
+    "gather",
+    "AsyncBroker",
+    "TaskiqError",
+    "TaskiqResult",
+    "ZeroMQBroker",
+    "TaskiqMessage",
+    "BrokerMessage",
+    "InMemoryBroker",
+    "TaskiqFormatter",
+    "AsyncTaskiqTask",
+    "TaskiqMiddleware",
+    "AsyncResultBackend",
+    "async_shared_broker",
+    "AsyncTaskiqDecoratedTask",
+]
