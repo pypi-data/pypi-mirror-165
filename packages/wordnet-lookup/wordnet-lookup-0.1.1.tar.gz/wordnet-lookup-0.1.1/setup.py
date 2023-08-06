c18e93d14fa6de8c484d673c653ebe7e539dcc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:15 +0000

    Add package wordnet-lookup/0.1.1/wordnet-lookup-0.1.1.tar.gz

---
+++ packages/wordnet-lookup/wordnet-lookup-0.1.1.tar.gz/wordnet-lookup-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wordnet_lookup', 'wordnet_lookup.os']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['baseblock']
+
+setup_kwargs = {
+    'name': 'wordnet-lookup',
+    'version': '0.1.1',
+    'description': 'Static Dictionaries for Rapid Wordnet Lookups',
+    'long_description': None,
+    'author': 'Craig Trim',
+    'author_email': 'craigtrim@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '==3.8.5',
+}
+
+
+setup(**setup_kwargs)
