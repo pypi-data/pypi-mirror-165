414b8432b939a3aee457867c2d9cdd06f1a22713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:58 +0000

    Add package planet-mercury/0.0.1/planet-mercury-0.0.1.tar.gz

---
+++ packages/planet-mercury/planet-mercury-0.0.1.tar.gz/planet-mercury-0.0.1/mercury/core/server/server.py
@@ -0,0 +1,17 @@
+import logging
+
+
+logger = logging.getLogger("mercury.error")
+
+
+class Server:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def loop(self):
+        while True:
+            pass
---
+++ packages/planet-mercury/planet-mercury-0.0.1.tar.gz/planet-mercury-0.0.1/mercury/type/__init__.py
@@ -0,0 +1,13 @@
+from mercury.type.asgi import Message, Scope, Receive, Send, ASGIApp, ASGIApplication
+
+empty = object()
+
+__all__ = [
+    "empty",
+    "Message",
+    "Scope",
+    "Receive",
+    "Send",
+    "ASGIApp",
+    "ASGIApplication"
+]
