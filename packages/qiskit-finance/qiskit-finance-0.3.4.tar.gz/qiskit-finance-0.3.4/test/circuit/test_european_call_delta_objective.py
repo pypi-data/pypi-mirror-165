a763c2911873ede87db32bcca4794126b04f512c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:52 +0000

    Add package qiskit-finance/0.3.4/qiskit-finance-0.3.4.tar.gz

---
+++ packages/qiskit-finance/qiskit-finance-0.3.4.tar.gz/qiskit-finance-0.3.4/test/circuit/test_european_call_delta_objective.py
@@ -0,0 +1,111 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020, 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Test EuropeanCallDelta."""
+
+import unittest
+from test import QiskitFinanceTestCase
+
+import numpy as np
+from qiskit.circuit.library import IntegerComparator
+from qiskit.quantum_info import Operator
+from qiskit.utils import QuantumInstance, optionals
+from qiskit.algorithms import IterativeAmplitudeEstimation, EstimationProblem
+from qiskit_finance.circuit.library import LogNormalDistribution
+
+from qiskit_finance.circuit.library.payoff_functions import EuropeanCallDeltaObjective
+
+
+class TestEuropeanCallDelta(QiskitFinanceTestCase):
+    """Tests EuropeanCallDelta."""
+
+    def test_circuit(self):
+        """Test the expected circuit.
+
+        If it equals the correct ``IntegerComparator`` we know the circuit is correct.
+        """
+        num_qubits = 3
+        strike_price = 0.5
+        bounds = (0, 2)
+        ecd = EuropeanCallDeltaObjective(
+            num_state_qubits=num_qubits, strike_price=strike_price, bounds=bounds
+        )
+
+        # map strike_price to a basis state
+        x = (strike_price - bounds[0]) / (bounds[1] - bounds[0]) * (2**num_qubits - 1)
+        comparator = IntegerComparator(num_qubits, x)
+
+        self.assertTrue(Operator(ecd).equiv(comparator))
+
+    @unittest.skipUnless(optionals.HAS_AER, "qiskit-aer is required to run this test")
+    def test_application(self):
+        """Test an end-to-end application."""
+
+        num_qubits = 3
+
+        # parameters for considered random distribution
+        s_p = 2.0  # initial spot price
+        vol = 0.4  # volatility of 40%
+        r = 0.05  # annual interest rate of 4%
+        t_m = 40 / 365  # 40 days to maturity
+
+        # resulting parameters for log-normal distribution
+        mu = (r - 0.5 * vol**2) * t_m + np.log(s_p)
+        sigma = vol * np.sqrt(t_m)
+        mean = np.exp(mu + sigma**2 / 2)
+        variance = (np.exp(sigma**2) - 1) * np.exp(2 * mu + sigma**2)
+        stddev = np.sqrt(variance)
+
+        # lowest and highest value considered for the spot price;
+        # in between, an equidistant discretization is considered.
+        low = np.maximum(0, mean - 3 * stddev)
+        high = mean + 3 * stddev
+        bounds = (low, high)
+
+        # construct circuit for uncertainty model
+        uncertainty_model = LogNormalDistribution(
+            num_qubits, mu=mu, sigma=sigma**2, bounds=bounds
+        ).decompose()
+
+        # set the strike price (should be within the low and the high value of the uncertainty)
+        strike_price = 1.896
+
+        # create amplitude function
+        european_call_delta = EuropeanCallDeltaObjective(
+            num_state_qubits=num_qubits, strike_price=strike_price, bounds=bounds
+        )
+
+        # create state preparation
+        state_preparation = european_call_delta.compose(uncertainty_model, front=True)
+
+        problem = EstimationProblem(
+            state_preparation=state_preparation,
+            objective_qubits=[num_qubits],
+            post_processing=european_call_delta.post_processing,
+        )
+
+        # run amplitude estimation
+        import importlib
+
+        aer = importlib.import_module("qiskit.providers.aer")
+        q_i = QuantumInstance(
+            aer.Aer.get_backend("aer_simulator"),
+            seed_simulator=125,
+            seed_transpiler=80,
+        )
+        iae = IterativeAmplitudeEstimation(epsilon_target=0.01, alpha=0.05, quantum_instance=q_i)
+        result = iae.estimate(problem)
+        self.assertAlmostEqual(result.estimation_processed, 0.8088790606143996)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/qiskit-finance/qiskit-finance-0.3.4.tar.gz/qiskit-finance-0.3.4/test/circuit/test_fixed_income_pricing_objective.py
@@ -0,0 +1,107 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020, 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+""" Test FixedIncomePricingObjective"""
+
+import unittest
+from test import QiskitFinanceTestCase
+
+import numpy as np
+
+from qiskit import QuantumCircuit
+from qiskit.utils import QuantumInstance, optionals
+from qiskit.algorithms import IterativeAmplitudeEstimation, EstimationProblem
+from qiskit.quantum_info import Operator
+from qiskit_finance.circuit.library import NormalDistribution
+from qiskit_finance.circuit.library.payoff_functions import FixedIncomePricingObjective
+
+
+class TestFixedIncomePricingObjective(QiskitFinanceTestCase):
+    """Tests FixedIncomePricingObjective"""
+
+    def test_circuit(self):
+        """Test the expected circuit."""
+        num_qubits = [2, 2]
+        pca = np.eye(2)
+        initial_interests = np.zeros(2)
+        cash_flow = np.array([1, 2])
+        rescaling_factor = 0.125
+        bounds = [(0, 0.12), (0, 0.24)]
+
+        circuit = FixedIncomePricingObjective(
+            num_qubits, pca, initial_interests, cash_flow, rescaling_factor, bounds
+        )
+
+        expected = QuantumCircuit(5)
+        expected.cry(-np.pi / 216, 0, 4)
+        expected.cry(-np.pi / 108, 1, 4)
+        expected.cry(-np.pi / 27, 2, 4)
+        expected.cry(-0.23271, 3, 4)
+        expected.ry(9 * np.pi / 16, 4)
+
+        self.assertTrue(Operator(circuit).equiv(expected))
+
+    @unittest.skipUnless(optionals.HAS_AER, "qiskit-aer is required to run this test")
+    def test_application(self):
+        """Test an end-to-end application."""
+
+        a_n = np.eye(2)
+        b = np.zeros(2)
+
+        num_qubits = [2, 2]
+
+        # specify the lower and upper bounds for the different dimension
+        bounds = [(0, 0.12), (0, 0.24)]
+        mu = [0.12, 0.24]
+        sigma = 0.01 * np.eye(2)
+
+        # construct corresponding distribution
+        dist = NormalDistribution(num_qubits, mu, sigma, bounds=bounds)
+
+        # specify cash flow
+        c_f = [1.0, 2.0]
+
+        # specify approximation factor
+        rescaling_factor = 0.125
+
+        # get fixed income circuit appfactory
+        fixed_income = FixedIncomePricingObjective(
+            num_qubits, a_n, b, c_f, rescaling_factor, bounds
+        )
+
+        # build state preparation operator
+        state_preparation = fixed_income.compose(dist, front=True)
+
+        problem = EstimationProblem(
+            state_preparation=state_preparation,
+            objective_qubits=[4],
+            post_processing=fixed_income.post_processing,
+        )
+
+        # run simulation
+        import importlib
+
+        aer = importlib.import_module("qiskit.providers.aer")
+        q_i = QuantumInstance(
+            aer.Aer.get_backend("aer_simulator"),
+            seed_simulator=2,
+            seed_transpiler=2,
+        )
+        iae = IterativeAmplitudeEstimation(epsilon_target=0.01, alpha=0.05, quantum_instance=q_i)
+        result = iae.estimate(problem)
+
+        # compare to precomputed solution
+        self.assertAlmostEqual(result.estimation_processed, 2.3389012822103044)
+
+
+if __name__ == "__main__":
+    unittest.main()
