c85abff6fcc62e543d08185ea31fa17a62dd8bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:46 +0000

    Add package handyscikit/0.0.7.11/handyscikit-0.0.7.11-py3-none-any.whl

---
+++ packages/handyscikit/handyscikit-0.0.7.11-py3-none-any.whl/handyscikit/__init__.py
@@ -0,0 +1,9 @@
+from .common import *
+
+import handyscikit.analytical_solution
+import handyscikit.lbfs
+import handyscikit.lbm
+import handyscikit.mesh
+import handyscikit.physical_field
+import handyscikit.plot
+import handyscikit.simulation
