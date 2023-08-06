e60fb703c7137ee85b2f86254349e1b3044f0ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:51 +0000

    Add package cubicweb-fluid-design-system/1.9.2/cubicweb-fluid-design-system-1.9.2.tar.gz

---
+++ packages/cubicweb-fluid-design-system/cubicweb-fluid-design-system-1.9.2.tar.gz/cubicweb-fluid-design-system-1.9.2/cubicweb_fluid_design_system/__pkginfo__.py
@@ -0,0 +1,27 @@
+# pylint: disable=W0622
+"""cubicweb-fluid-design-system application packaging information"""
+
+modname = "fluid_design_system"
+distname = "cubicweb-fluid-design-system"
+
+numversion = (1, 9, 2)
+version = ".".join(str(num) for num in numversion)
+
+license = "LGPL"
+author = "LOGILAB S.A. (Paris, FRANCE)"
+author_email = "contact@logilab.fr"
+description = ""
+web = "https://www.cubicweb.org/project/%s" % distname
+
+__depends__ = {
+    "cubicweb": ">= 3.35.0",
+}
+__recommends__ = {}
+
+classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: CubicWeb",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: JavaScript",
+]
