f68461ca09676c0fb2edfd0057605aab34d1d5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:39 +0000

    Add package pomice/1.1.7/pomice-1.1.7-py3-none-any.whl

---
+++ packages/pomice/pomice-1.1.7-py3-none-any.whl/pomice/__init__.py
@@ -0,0 +1,32 @@
+"""
+Pomice
+~~~~~~
+The modern Lavalink wrapper designed for discord.py.
+
+:copyright: 2021, cloudwithax
+:license: GPL-3.0
+"""
+import discord
+
+if not discord.__version__.startswith("2.0"):
+    class DiscordPyOutdated(Exception):
+        pass
+
+    raise DiscordPyOutdated(
+        "You must have discord.py 2.0 to use this library. "
+        "Uninstall your current version and install discord.py 2.0 "
+        "using 'pip install discord.py'"
+    )
+
+__version__ = "1.1.7"
+__title__ = "pomice"
+__author__ = "cloudwithax"
+
+from .enums import SearchType
+from .events import *
+from .exceptions import *
+from .filters import *
+from .objects import *
+from .player import Player
+from .pool import *
+from .queue import *
---
+++ packages/pomice/pomice-1.1.7-py3-none-any.whl/pomice/enums.py
@@ -0,0 +1,67 @@
+from enum import Enum, auto
+
+
+class SearchType(Enum):
+    """The enum for the different search types for Pomice.
+       This feature is exclusively for the Spotify search feature of Pomice.
+       If you are not using this feature, this class is not necessary.
+
+       SearchType.ytsearch searches using regular Youtube,
+       which is best for all scenarios.
+
+       SearchType.ytmsearch searches using YouTube Music,
+       which is best for getting audio-only results.
+
+       SearchType.scsearch searches using SoundCloud,
+       which is an alternative to YouTube or YouTube Music.
+    """
+    ytsearch = "ytsearch"
+    ytmsearch = "ytmsearch"
+    scsearch = "scsearch"
+
+    def __str__(self) -> str:
+        return self.value
+
+class NodeAlgorithm(Enum):
+    """The enum for the different node algorithms in Pomice.
+    
+        The enums in this class are to only differentiate different
+        methods, since the actual method is handled in the
+        get_best_node() method.
+
+        NodeAlgorithm.by_ping returns a node based on it's latency,
+        preferring a node with the lowest response time
+
+        NodeAlgorithm.by_region returns a node based on its voice region,
+        which the region is specified by the user in the method as an arg. 
+        This method will only work if you set a voice region when you create a node.
+
+        NodeAlgorithm.by_players return a nodes based on how many players it has.
+        This algorithm prefers nodes with the least amount of players.
+    """
+
+    # We don't have to define anything special for these, since these just serve as flags
+    by_ping = "BY_PING"
+    by_region = "BY_REGION"
+    by_players = "BY_PLAYERS"
+
+    def __str__(self) -> str:
+        return self.value
+
+class LoopMode(Enum):
+    """The enum for the different loop modes.
+       This feature is exclusively for the queue utility of pomice.
+       If you are not using this feature, this class is not necessary.
+
+       LoopMode.TRACK sets the queue loop to the current track.
+
+       LoopMode.QUEUE sets the queue loop to the whole queue.
+
+    """
+    # We don't have to define anything special for these, since these just serve as flags
+    TRACK = "TRACK"
+    QUEUE = "queue"
+    
+
+    def __str__(self) -> str:
+        return self.value
