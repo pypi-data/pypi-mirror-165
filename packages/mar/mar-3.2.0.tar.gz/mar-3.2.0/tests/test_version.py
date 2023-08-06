2a0f1690039eb21fcd08b8364b594a4b004a00d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:02 +0000

    Add package mar/3.2.0/mar-3.2.0.tar.gz

---
+++ packages/mar/mar-3.2.0.tar.gz/mar-3.2.0/tests/test_version.py
@@ -0,0 +1,22 @@
+import os
+import re
+
+from pkg_resources import get_distribution
+
+import mardor
+
+
+def test_version_in_setuppy():
+    dist = get_distribution('mar')
+    assert mardor.version_str == dist.version
+    assert ".".join(str(_) for _ in mardor.version) == dist.version
+
+
+def test_version_in_changelog():
+    dist = get_distribution('mar')
+    here = os.path.abspath(os.path.dirname(__file__))
+    changelog_path = os.path.join(here, '..', 'CHANGELOG.rst')
+    with open(changelog_path) as f:
+        changelog = f.read()
+    assert re.search('^{}'.format(re.escape(mardor.version_str)), changelog,
+                     re.M)
