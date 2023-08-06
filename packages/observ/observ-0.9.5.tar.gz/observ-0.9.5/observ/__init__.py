6c7a454dd4f4b98ccbf9d7cd1ec173aa85a3af90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:27 +0000

    Add package observ/0.9.5/observ-0.9.5.tar.gz

---
+++ packages/observ/observ-0.9.5.tar.gz/observ-0.9.5/observ/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.9.5"
+
+
+from .observables import (
+    reactive,
+    readonly,
+    shallow_reactive,
+    shallow_readonly,
+    to_raw,
+)
+from .scheduler import scheduler
+from .watcher import computed, watch
