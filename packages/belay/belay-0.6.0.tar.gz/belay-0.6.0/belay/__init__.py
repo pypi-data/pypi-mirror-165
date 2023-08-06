24f582e53169bfbc6e63d40f8735cb5221fc0da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:41 +0000

    Add package belay/0.6.0/belay-0.6.0.tar.gz

---
+++ packages/belay/belay-0.6.0.tar.gz/belay-0.6.0/belay/__init__.py
@@ -0,0 +1,20 @@
+# Don't manually change, let poetry-dynamic-versioning-plugin handle it.
+__version__ = "0.6.0"
+
+__all__ = [
+    "AuthenticationError",
+    "minify",
+    "Device",
+    "SpecialFunctionNameError",
+    "PyboardException",
+    "Implementation",
+    "FeatureUnavailableError",
+]
+from ._minify import minify
+from .device import Device, Implementation
+from .exceptions import (
+    AuthenticationError,
+    FeatureUnavailableError,
+    SpecialFunctionNameError,
+)
+from .pyboard import PyboardException
