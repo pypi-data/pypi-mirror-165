027efb40501146b4e6d8ab72f90e56aca775e982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:19 +0000

    Add package signal-cli-socket/0.7/signal_cli_socket-0.7-py2.py3-none-any.whl

---
+++ packages/signal-cli-socket/signal_cli_socket-0.7-py2.py3-none-any.whl/signal_cli_socket.py
@@ -0,0 +1,69 @@
+from random import randint
+from json import dumps
+import socket
+
+
+class Signal:
+    def __init__(self, account, socket_path="/tmp/signal-cli/socket", r_id=randint(0, 5000)):
+        self.signal = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.signal.connect(socket_path)
+        self.id = r_id
+        self.account = account
+
+    def send_message(self, recipient, message, group=False):
+        if group:
+            message_string = dumps({
+                "jsonrpc": "2.0",
+                "method": "send",
+                "params":
+                    {
+                        "account": self.account,
+                        "groupId": recipient,
+                        "message": message
+                    },
+                "id": self.id
+            })
+        else:
+            message_string = dumps({
+                "jsonrpc": "2.0",
+                "method": "send",
+                "params":
+                    {
+                        "account": self.account,
+                        "recipient": recipient,
+                        "message": message
+                    },
+                "id": self.id
+            })
+
+        self.signal.send(message_string.encode('utf-8'))
+
+    def send_message_attachment(self, recipient, message, attachment, group=False):
+        if group:
+            message_string = dumps({
+                "jsonrpc": "2.0",
+                "method": "send",
+                "params":
+                    {
+                        "account": self.account,
+                        "groupId": recipient,
+                        "message": message,
+                        "attachments": [attachment]
+                    },
+                "id": self.id
+            })
+        else:
+            message_string = dumps({
+                "jsonrpc": "2.0",
+                "method": "send",
+                "params":
+                    {
+                        "account": self.account,
+                        "recipient": recipient,
+                        "message": message,
+                        "attachments": [attachment]
+                    },
+                "id": self.id
+            })
+
+        self.signal.send(message_string.encode('utf-8'))
