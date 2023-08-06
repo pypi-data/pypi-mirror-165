82dfbdd8c4441136710a0dffc5d14e7af172d953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:20 +0000

    Add package yuldistributions/0.1/yuldistributions-0.1.tar.gz

---
+++ packages/yuldistributions/yuldistributions-0.1.tar.gz/yuldistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='yuldistributions',
+      version='0.1',
+      description='Gaussian and Binomal distributions',
+      packages=['yuldistributions'],
+      author='Yulika Wijayanti',
+      author_email='yulika.wijayanti@gmail.com',
+      zip_safe=False)
