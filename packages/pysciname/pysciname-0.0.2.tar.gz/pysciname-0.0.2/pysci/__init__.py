ea44d61ff8c2a5f385f872fc633aa958ea7a50d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:43 +0000

    Add package pysciname/0.0.2/pysciname-0.0.2.tar.gz

---
+++ packages/pysciname/pysciname-0.0.2.tar.gz/pysciname-0.0.2/pysci/__init__.py
@@ -0,0 +1,1 @@
+from pysci.pysci import Py
---
+++ packages/pysciname/pysciname-0.0.2.tar.gz/pysciname-0.0.2/setup.py
@@ -0,0 +1,38 @@
+﻿import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'pysciname',      
+  packages = ['pysci'], 
+  version = '0.0.2', 
+  license='MIT', 
+  description = 'pysci by ****',
+  long_description=DESCRIPTION,
+  author = 'drxprichkrup',                 
+  author_email = 'drxprichkrup@gmail.com',     
+  url = 'https://github.com/drxprichkrup/pysci',  
+  download_url = 'https://github.com/drxprichkrup/pysci/archive/v0.0.1.zip',  
+  keywords = ['pysci', 'pysci01', 'pysci02'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
