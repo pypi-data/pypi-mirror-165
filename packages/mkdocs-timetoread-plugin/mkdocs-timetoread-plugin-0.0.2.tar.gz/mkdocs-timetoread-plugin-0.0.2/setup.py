607d02cf1f2ebc27d8f75219c21ab7b9bb1fe011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:23 +0000

    Add package mkdocs-timetoread-plugin/0.0.2/mkdocs-timetoread-plugin-0.0.2.tar.gz

---
+++ packages/mkdocs-timetoread-plugin/mkdocs-timetoread-plugin-0.0.2.tar.gz/mkdocs-timetoread-plugin-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='mkdocs-timetoread-plugin',
+    version='0.0.2',
+    description='"Estimated Time To Read" generator for MkDocs',
+    long_description='long_description',
+    keywords='mkdocs',
+    url='https://github.com/ntt4/mkdocs-timetoread-plugin',
+    author='Tavis Booth',
+    author_email='code@entityfour.io',
+    license='MIT',
+    python_requires='>=3.7',
+    install_requires=[
+        'mkdocs>=1.3'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': ["timetoread = mkdocs_timetoread_plugin.plugin:TimeToRead"]}
+    )
+
