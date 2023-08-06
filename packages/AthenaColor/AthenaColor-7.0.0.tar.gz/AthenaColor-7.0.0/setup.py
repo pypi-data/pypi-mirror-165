2408b02488e4ab52992dc035c4b8803e4cc11213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:44 +0000

    Add package AthenaColor/7.0.0/AthenaColor-7.0.0.tar.gz

---
+++ packages/AthenaColor/AthenaColor-7.0.0.tar.gz/AthenaColor-7.0.0/setup.py
@@ -0,0 +1,43 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+# General Packages
+import setuptools
+
+# Custom Packages
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Code -
+# ----------------------------------------------------------------------------------------------------------------------
+def readme_handler() -> str:
+    with open("README.md", "r") as readme_file:
+        return readme_file.read()
+
+def version_handler() -> str:
+    # ------------------------------------------------------------------------------------------------------------------
+    version = 7,0,0 # <-- DEFINE THE VERSION IN A TUPLE FORMAT HERE
+    # ------------------------------------------------------------------------------------------------------------------
+    version_str = ".".join(str(i) for i in version)
+
+    return version_str
+
+# ----------------------------------------------------------------------------------------------------------------------
+# - Actual Setup -
+# ----------------------------------------------------------------------------------------------------------------------
+setuptools.setup(
+    name="AthenaColor",
+    version=version_handler(),
+    author="Andreas Sas",
+    author_email="",
+    description="Package to support full usage of RGB colors in the console",
+    long_description=readme_handler(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/DirectiveAthena/AthenaColor",
+    project_urls={
+        "Bug Tracker": "https://github.com/DirectiveAthena/AthenaColor/issues",
+    },
+    license="GPLv3",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7"
+)
