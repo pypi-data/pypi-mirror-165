eb889ffb7bd92c5c15a33c57dc4e56dc9c87b5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:46 +0000

    Add package emo-platform-api-sdk/1.0.3/emo_platform_api_sdk-1.0.3-py3-none-any.whl

---
+++ packages/emo-platform-api-sdk/emo_platform_api_sdk-1.0.3-py3-none-any.whl/emo_platform/__init__.py
@@ -0,0 +1,17 @@
+from .api import BizAdvancedClient, BizBasicClient, Client
+from .api_async import AsyncClient, BizAdvancedAsyncClient, BizBasicAsyncClient
+from .exceptions import (
+    BadRequestError,
+    EmoPlatformError,
+    NoRoomError,
+    NotFoundError,
+    RateLimitError,
+    TokenError,
+    UnauthorizedError,
+    UnavailableError,
+    UnknownError,
+    WebhookCallbackError,
+    WebhookRequestError,
+)
+from .models import AccountInfo, BroadcastMsg, Color, Head, Tokens, WebHook
+from .response import parse_webhook_body
