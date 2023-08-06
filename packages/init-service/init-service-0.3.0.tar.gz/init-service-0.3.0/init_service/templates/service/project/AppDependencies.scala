8933df2e6941cdcaf4e5b1b85f0a173b689872cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:53 +0000

    Add package init-service/0.3.0/init-service-0.3.0.tar.gz

---
+++ packages/init-service/init-service-0.3.0.tar.gz/init-service-0.3.0/init_service/templates/service/project/AppDependencies.scala
@@ -0,0 +1,28 @@
+import play.core.PlayVersion
+import play.sbt.PlayImport._
+import sbt.Keys.libraryDependencies
+import sbt._
+
+object AppDependencies {
+
+  val compile = Seq(
+    <!--(if type=="FRONTEND")-->
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "$!bootstrapPlayVersion!$",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "$!playFrontendHmrcVersion!$"<!--(if MONGO)-->,<!--(end)-->
+    <!--(end)-->
+    <!--(if type=="BACKEND")-->
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "$!bootstrapPlayVersion!$"<!--(if MONGO)-->,<!--(end)-->
+    <!--(end)-->
+    <!--(if MONGO)-->
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "$!mongoVersion!$"
+    <!--(end)-->
+  )
+
+  val test = Seq(
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "$!bootstrapPlayVersion!$"             % "test, it",
+    <!--(if MONGO)-->"uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "$!mongoVersion!$"            % Test,<!--(end)-->
+    <!--(if type=="FRONTEND")-->
+    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
+    <!--(end)-->
+  )
+}
