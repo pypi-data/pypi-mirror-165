88d517a20131cf772d02dc3d8ba3b51732fd8664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:43 +0000

    Add package vsndist/1.0/vsndist-1.0.tar.gz

---
+++ packages/vsndist/vsndist-1.0.tar.gz/vsndist-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vsndist',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['vsndist'],
+      zip_safe=False)
