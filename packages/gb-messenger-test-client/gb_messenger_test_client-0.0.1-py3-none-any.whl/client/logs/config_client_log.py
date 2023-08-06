8984e49b5a60ee93182fd3bc9024d48f32cd5751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:58 +0000

    Add package gb-messenger-test-client/0.0.1/gb_messenger_test_client-0.0.1-py3-none-any.whl

---
+++ packages/gb-messenger-test-client/gb_messenger_test_client-0.0.1-py3-none-any.whl/client/logs/config_client_log.py
@@ -0,0 +1,29 @@
+"""
+Файл содержит конфигурацию логирования для клиентской части приложения
+"""
+
+import sys
+import os
+sys.path.append('../client_dist/client/client/')
+import logging
+from common.variables import LOGGING_LEVEL
+
+client_formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, 'client.log')
+steam = logging.StreamHandler(sys.stderr)
+steam.setFormatter(client_formatter)
+steam.setLevel(logging.INFO)
+log_file = logging.FileHandler(path, encoding='utf8')
+log_file.setFormatter(client_formatter)
+logger = logging.getLogger('client_dist')
+logger.addHandler(steam)
+logger.addHandler(log_file)
+logger.setLevel(LOGGING_LEVEL)
+
+
+if __name__ == '__main__':
+    logger.critical('Test critical event')
+    logger.error('Test error ivent')
+    logger.debug('Test debug ivent')
+    logger.info('Test info ivent')
