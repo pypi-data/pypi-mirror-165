830e34e565b300993a91fde7ff212048ea895712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:26 +0000

    Add package vmu-tools/0.1.9/vmu_tools-0.1.9.tar.gz

---
+++ packages/vmu-tools/vmu_tools-0.1.9.tar.gz/vmu_tools-0.1.9/setup.py
@@ -0,0 +1,33 @@
+"""Install packages as defined in this file into the Python environment."""
+import pathlib
+from setuptools import setup, find_packages
+
+# The version of this tool is based on the following steps:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="vmu_tools",
+    author="slurmking",
+    url="https://github.com/slurmking/vmu-tools",
+    description="A set of tools for Dreamcast VMUcle icon",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    version="0.1.9",
+    packages=find_packages(include=['vmut', 'vmut.*']),
+    install_requires=[
+        "Pillow",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
