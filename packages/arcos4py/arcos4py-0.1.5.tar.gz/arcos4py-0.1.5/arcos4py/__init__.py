541b6f981065e7add1df4435d04fe29fdbc9bd82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:33 +0000

    Add package arcos4py/0.1.5/arcos4py-0.1.5.tar.gz

---
+++ packages/arcos4py/arcos4py-0.1.5.tar.gz/arcos4py-0.1.5/arcos4py/__init__.py
@@ -0,0 +1,13 @@
+"""Arcos4py top level module.
+
+This package is a python port of an R package
+to detect and analyse collective phenomena.
+"""
+
+__author__ = """Benjamin Graedel"""
+__email__ = "benjamin.graedel@unibe.ch"
+__version__ = '0.1.5'
+
+from .arcos4py import ARCOS
+
+__all__ = ["ARCOS"]
