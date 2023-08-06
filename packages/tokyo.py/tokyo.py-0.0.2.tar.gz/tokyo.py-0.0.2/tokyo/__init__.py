856303dc07744f75c8ee5c187187417cfecd953c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:45 +0000

    Add package tokyo.py/0.0.2/tokyo.py-0.0.2.tar.gz

---
+++ packages/tokyo.py/tokyo.py-0.0.2.tar.gz/tokyo.py-0.0.2/tokyo/__init__.py
@@ -0,0 +1,9 @@
+from .endpoints.json import Json
+from .endpoints.buffer import Buffer
+from .endpoints.anime import Anime
+
+__title__ = 'tokyo'
+__author__ = 'Loxitoh'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022-present Loxitoh'
+__version__ = '0.0.2'
