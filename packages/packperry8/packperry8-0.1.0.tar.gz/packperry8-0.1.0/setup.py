1a291f05c8796385f57f30d3d2c1e1ca9ccaa205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:51 +0000

    Add package packperry8/0.1.0/packperry8-0.1.0.tar.gz

---
+++ packages/packperry8/packperry8-0.1.0.tar.gz/packperry8-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='packperry8',
+   version='0.1.0',
+   author='Perihane',
+   author_email='perihane.youssef@vodafone.com',
+   packages=['packperry8']
+)
