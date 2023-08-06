b4cb471956313e5f77ebbee86dfcf1e83c88ea7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:35 +0000

    Add package tinyticker/0.3.0/tinyticker-0.3.0-py3-none-any.whl

---
+++ packages/tinyticker/tinyticker-0.3.0-py3-none-any.whl/tinyticker/waveshare_lib/__init__.py
@@ -0,0 +1,7 @@
+"""Python interface for ePaper displays.
+
+Adapted from:
+https://github.com/waveshare/e-Paper/tree/master/RaspberryPi_JetsonNano/python/lib/waveshare_epd
+"""
+
+from .epdconfig import CONFIG
