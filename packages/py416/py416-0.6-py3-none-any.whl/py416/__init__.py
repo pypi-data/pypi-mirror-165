e17d1ba2af6f1b879b33a25129821f7e917b814f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:43 +0000

    Add package py416/0.6/py416-0.6-py3-none-any.whl

---
+++ packages/py416/py416-0.6-py3-none-any.whl/py416/__init__.py
@@ -0,0 +1,11 @@
+'''
+Name:    py416
+Author:  Ezio416
+Created: 2022-08-15
+Updated: 2022-09-01
+Version: 0.6
+
+A collection of my custom-built functions
+'''
+from .general import *
+__version__ = 0, 6
