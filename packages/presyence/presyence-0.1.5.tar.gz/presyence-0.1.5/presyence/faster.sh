d81cbbe507d8e099a90cbd8e85c4da6f46d17fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:48 +0000

    Add package presyence/0.1.5/presyence-0.1.5.tar.gz

---
+++ packages/presyence/presyence-0.1.5.tar.gz/presyence-0.1.5/presyence/faster.sh
@@ -0,0 +1,6 @@
+poetry build
+pip uninstall presyence --yes
+pip install ../dist/presyence-0.1.4-py3-none-any.whl
+cd ..
+presyence
+
---
+++ packages/presyence/presyence-0.1.5.tar.gz/presyence-0.1.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['presyence', 'presyence.reporter', 'presyence.testrunner']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.80.0,<0.81.0',
+ 'pandas>=1.4.3,<2.0.0',
+ 'typer>=0.6.1,<0.7.0',
+ 'uvicorn>=0.18.3,<0.19.0']
+
+entry_points = \
+{'console_scripts': ['presyence = presyence.main:app']}
+
+setup_kwargs = {
+    'name': 'presyence',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Rémi Connesson',
+    'author_email': 'remiconnesson@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
