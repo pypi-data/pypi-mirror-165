bf4d2f2a233ab916b18ef1cbe43bde846e2f328b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:04 +0000

    Add package az-probability/1.0/az_probability-1.0.tar.gz

---
+++ packages/az-probability/az_probability-1.0.tar.gz/az_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='az_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['az_probability'],
+      author = 'Anna Zybina',
+      zip_safe=False)
