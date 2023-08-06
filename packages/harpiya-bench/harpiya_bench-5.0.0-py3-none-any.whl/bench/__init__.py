7fd64d7b1b32d08bf0684f8cbb5fab326cdb4116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:01 +0000

    Add package harpiya-bench/5.0.0/harpiya_bench-5.0.0-py3-none-any.whl

---
+++ packages/harpiya-bench/harpiya_bench-5.0.0-py3-none-any.whl/bench/__init__.py
@@ -0,0 +1,14 @@
+VERSION = "5.0.0"
+PROJECT_NAME = "harpiya-bench"
+HARPIYA_VERSION = None
+current_path = None
+updated_path = None
+LOG_BUFFER = []
+
+
+def set_harpiya_version(bench_path="."):
+	from .utils.app import get_current_harpiya_version
+
+	global HARPIYA_VERSION
+	if not HARPIYA_VERSION:
+		HARPIYA_VERSION = get_current_harpiya_version(bench_path=bench_path)
