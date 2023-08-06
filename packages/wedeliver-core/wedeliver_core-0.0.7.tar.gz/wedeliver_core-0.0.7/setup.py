721f4557b175c55dcd75ac8d09101ec32c517900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:50 +0000

    Add package wedeliver-core/0.0.7/wedeliver_core-0.0.7.tar.gz

---
+++ packages/wedeliver-core/wedeliver_core-0.0.7.tar.gz/wedeliver_core-0.0.7/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_namespace_packages
+
+# reading long description from file
+# with open('DESCRIPTION.txt') as file:
+#     long_description = file.read()
+
+
+# specify requirements of your package here
+REQUIREMENTS = [
+    "SQLAlchemy==1.3.19",
+    "Flask-SQLAlchemy==2.4.4",
+    "flask-marshmallow==0.14.0",
+    "confluent-kafka==1.9.0",
+    "requests==2.27.1",
+    "python-dotenv==0.20.0",
+]
+
+# some more details
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Internet',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+]
+
+setup(
+    name='wedeliver_core',
+    version='0.0.7',
+    description='weDeliverCore package',
+    long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
+    long_description_content_type='text/markdown',
+    url='https://www.wedeliverapp.com/',
+    author='Eyad Farra',
+    author_email='info@wedeliverapp.com',
+    license='MIT',
+    packages=find_namespace_packages(include=['wedeliver_core', 'wedeliver_core.*']),  # ['wedeliver_core'],
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIREMENTS
+)
