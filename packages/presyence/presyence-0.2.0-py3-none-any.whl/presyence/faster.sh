1a0346c06cba457d77d96d48a4bb626f70994296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:32 +0000

    Add package presyence/0.2.0/presyence-0.2.0-py3-none-any.whl

---
+++ packages/presyence/presyence-0.2.0-py3-none-any.whl/presyence/faster.sh
@@ -0,0 +1,6 @@
+poetry build
+pip uninstall presyence --yes
+pip install ../dist/presyence-0.2.0-py3-none-any.whl
+cd ..
+presyence
+
---
+++ packages/presyence/presyence-0.2.0-py3-none-any.whl/presyence/reporter/www/assets/worker.da4df076.js
@@ -0,0 +1,124 @@
+(function(){"use strict";var n={beginPayload:!0,testReport:[{test:{title:"DUMMY DATA Identity function should return the same object",function:`def identity(x):
+    return x
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+3,3
+`},result:{successful:!1,status:"WrongResult",testrun_output:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,assertion_error_message:`DataFrame.iloc[:, 0] (column name="Unnamed: 0") are different
+
+DataFrame.iloc[:, 0] (column name="Unnamed: 0") values are different (25.0 %)
+[index]: [0, 1, 2, 3]
+[left]:  [1, 2, 3, 3]
+[right]: [1, 2, 3, 4]`}},{test:{title:"DUMMY DATA Drop Unnamed should drop the column 'Unnamed: 0'",function:`def drop_unnamed(df):
+    return df.drop(columns=['Unnamed: 0'])
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`},result:{successful:!1,status:"WrongResult",testrun_output:`useful col
+1
+2
+4
+3
+`,assertion_error_message:`DataFrame are different
+
+DataFrame shape mismatch
+[left]:  (4, 2)
+[right]: (4, 1)`}},{test:{title:"DUMMY DATA Drop Unnamed v2 should drop the column 'Unnamed: 0'",function:`def drop_unnamed2(df):
+    return df.drop(columns=['Unnamed: 0'])
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`useful col
+1
+2
+4
+3
+`},result:{successful:!0,status:"Success"}},{test:{title:"DUMMY DATA Drop Unnamed should drop the column 'Unnamed: 0'",function:`def drop_unnamed5(df):
+    return drop_unnamed3(df.yolo)
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`useful col
+1
+2
+4
+3
+`},result:{successful:!1,status:"Crash",traceback:`Traceback (most recent call last):
+  File "/home/remiconn/python-static-spa/presyence/env/lib/python3.10/site-packages/presyence/simpletests.py", line 138, in run
+    testrun_output = self.function(self.input)
+  File "/home/remiconn/python-static-spa/presyence/experience_zone/realcode.py", line 14, in drop_unnamed5
+    return drop_unnamed3(df.yolo)
+  File "/home/remiconn/python-static-spa/presyence/env/lib/python3.10/site-packages/pandas/core/generic.py", line 5575, in __getattr__
+    return object.__getattribute__(self, name)
+AttributeError: 'DataFrame' object has no attribute 'yolo'
+`}},{test:{title:"DUMMY DATA Identity function should return the same object",function:`def identity(x):
+    return x
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`},result:{successful:!0,status:"Success"}},{test:{title:"DUMMY DATA Drop Unnamed should drop the column 'Unnamed: 0'",function:`def drop_unnamed(df):
+    return df.drop(columns=['Unnamed: 0'])
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`useful col
+1
+2
+4
+3
+`},result:{successful:!0,status:"Success"}},{test:{title:"DUMMY DATA Drop Unnamed v2 should drop the column 'Unnamed: 0'",function:`def drop_unnamed2(df):
+    return df.drop(columns=['Unnamed: 0'])
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`useful col
+1
+2
+4
+3
+`},result:{successful:!0,status:"Success"}},{test:{title:"DUMMY DATA Drop Unnamed v4 should drop the column 'Unnamed: 0'",function:`def drop_unnamed4(df):
+    return drop_unnamed3(df)
+`,input:`Unnamed: 0,useful col
+1,1
+2,2
+3,4
+4,3
+`,expected_output:`useful col
+1
+2
+4
+3
+`},result:{successful:!0,status:"Success"}}],endPayload:!0};const e=n.testReport;console.log("Worker is executed"),self.onmessage=()=>{self.postMessage(e)}})();
