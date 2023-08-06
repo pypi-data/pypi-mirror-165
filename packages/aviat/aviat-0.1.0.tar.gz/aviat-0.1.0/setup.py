e727a06178c651092cb4063b53e5947841a8225a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:14 +0000

    Add package aviat/0.1.0/aviat-0.1.0.tar.gz

---
+++ packages/aviat/aviat-0.1.0.tar.gz/aviat-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['aviat']
+entry_points = \
+{'console_scripts': ['aviat = entry:main']}
+
+setup_kwargs = {
+    'name': 'aviat',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'RedMooner',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
