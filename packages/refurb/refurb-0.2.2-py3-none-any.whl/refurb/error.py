edc0bd4dbdcdda8e9726c274526e183e8d075616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:35 +0000

    Add package refurb/0.2.2/refurb-0.2.2-py3-none-any.whl

---
+++ packages/refurb/refurb-0.2.2-py3-none-any.whl/refurb/error.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import ClassVar, Optional
+
+
+@dataclass
+class Error:
+    code: ClassVar[int]
+    line: int
+    column: int
+    msg: str
+    filename: Optional[str] = None
+
+    def __str__(self) -> str:
+        return f"{self.filename}:{self.line}:{self.column + 1} [FURB{self.code}]: {self.msg}"  # noqa
