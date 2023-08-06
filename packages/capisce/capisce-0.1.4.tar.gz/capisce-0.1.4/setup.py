fc156f7534cb9dd6372c9722b6d9f62e8cd0dc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:27 +0000

    Add package capisce/0.1.4/capisce-0.1.4.tar.gz

---
+++ packages/capisce/capisce-0.1.4.tar.gz/capisce-0.1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='capisce',
+    version='0.1.4',
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True
+)
