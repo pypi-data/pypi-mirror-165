9c1079324535fb64321c4c49909469d2b52520bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package certbot-dns-metaname/0.1.2/certbot_dns_metaname-0.1.2.tar.gz

---
+++ packages/certbot-dns-metaname/certbot_dns_metaname-0.1.2.tar.gz/certbot_dns_metaname-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['certbot_dns_metaname']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['certbot>=1.29.0,<2.0.0', 'mock>=4.0.3,<5.0.0', 'requests-mock>=1.9.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'certbot-dns-metaname',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Taylor Kettle',
+    'author_email': 'tin.teapot@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
