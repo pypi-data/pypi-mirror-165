845b7be41959b9070987cb307f087a2d6b86ce94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:45 +0000

    Add package humanloop/0.1.3/humanloop-0.1.3-py3-none-any.whl

---
+++ packages/humanloop/humanloop-0.1.3-py3-none-any.whl/humanloop/sdk/__init__.py
@@ -0,0 +1,5 @@
+from .feedback import Feedback, feedback
+from .init import init
+from .log import Log, log
+from .model import ModelConfig, register, generate, ModelGenerate
+from .experiment import trial
