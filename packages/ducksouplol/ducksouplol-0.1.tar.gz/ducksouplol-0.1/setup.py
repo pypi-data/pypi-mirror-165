84793e84669239e449bc64ab00d660de3a945f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:12 +0000

    Add package ducksouplol/0.1/ducksouplol-0.1.tar.gz

---
+++ packages/ducksouplol/ducksouplol-0.1.tar.gz/ducksouplol-0.1/setup.py
@@ -0,0 +1,21 @@
+﻿from distutils.core import setup
+setup(
+  name = 'ducksouplol',         
+  packages = ['ducksouplol'],   
+  version = '0.1',      
+  license='LICENSE.txt',        
+  description = 'This is a package uploaded with OTC PyPI Uploader!',   
+  author = 'oliverstech',                   
+  author_email = 'imrolii9876@gmail.com',      
+  url = 'https://www.google.com',   
+  install_requires=[''],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
