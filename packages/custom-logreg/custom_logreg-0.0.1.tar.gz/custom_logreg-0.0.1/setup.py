8fce960ec1d7662b658eb22439dde0baee2e1d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:46 +0000

    Add package custom-logreg/0.0.1/custom_logreg-0.0.1.tar.gz

---
+++ packages/custom-logreg/custom_logreg-0.0.1.tar.gz/custom_logreg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='custom_logreg',
+    version='0.0.1',
+    url='https://github.com/schwarzan/custom_logreg',
+    author='Anna Schwarz',
+    author_email='schwarz.annak@gmail.com',
+    packages=find_packages(),
+)
