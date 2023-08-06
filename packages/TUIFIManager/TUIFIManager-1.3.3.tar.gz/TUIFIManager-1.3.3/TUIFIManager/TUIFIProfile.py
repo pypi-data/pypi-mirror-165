eb1f612dc7385f500222a4002a40fc26c612651d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:54 +0000

    Add package TUIFIManager/1.3.3/TUIFIManager-1.3.3.tar.gz

---
+++ packages/TUIFIManager/TUIFIManager-1.3.3.tar.gz/TUIFIManager-1.3.3/TUIFIManager/TUIFIProfile.py
@@ -0,0 +1,464 @@
+from unicurses import OPERATING_SYSTEM
+from shutil    import which
+
+
+
+class TUIFIProfile: #█┇▓┃▒┃░┃ 
+    """
+    Profile/Icon/Type
+    """
+    def __init__(self, text, color_map=1, open_with=None):
+        self.text         = text
+        self.color_map    = color_map  # I know it's not but i will in the future maybe?
+        self.open_with    = open_with 
+        temp_profileSplit = self.text.split('\n')
+        self.width        = len(max(temp_profileSplit, key=len))
+        self.height       = len(temp_profileSplit)
+        #self.open_with    = ['']
+
+
+
+DEFAULT_OPENER = 'start' if 'Windows' == OPERATING_SYSTEM else 'open' if 'Darwin' == OPERATING_SYSTEM  else 'xdg-open'   # meh.. # TODO: make an enviromental variable insted of those 2 vars, for everything
+DEFAULT_EDITOR = which('nvim') or which('emacs') or which('vim') or which('micro') or which('nano')  or DEFAULT_OPENER 
+
+TUIFIProfiles = { # TODO: open zip rar and etc. files on __init__.py as if they where kind of folders?
+    'folder':TUIFIProfile((
+        ' █████▒⎫⎫ \n'
+        ' █████▒▐┇ \n'
+        ' █████▒▐┃ \n'
+        ' ▀▀▀▀▀  ┘ '
+    ),2),
+    'empty_folder':TUIFIProfile((
+        ' █████ ⎫⎫ \n'
+        ' █████ ┇┇ \n'
+        ' █████ ┃┃ \n'
+        ' ▀▀▀▀▀  ┘ '
+    ),2),
+    '.xlsx':TUIFIProfile((
+        ' ┏━━━━┳┳┓ \n'
+        ' ┃██▓░┣╋┫ \n'
+        ' ┃EXCEL╋┫ \n' 
+        ' ╰━━━━┻┻┛ ' 
+    ),5, DEFAULT_OPENER),
+    '.ino':TUIFIProfile((
+        ' ╭━┳┳┳++╮ \n' 
+        ' ┃ARDINO┃ \n' 
+        ' ┃╭╯╰╯+╮┃ \n' 
+        ' ╰┻━━┻┻┻╯ '
+    ),4, DEFAULT_EDITOR),
+    'file':TUIFIProfile((
+        ' ┏┏━━━━┓┓ \n'
+        ' ┇┛FILE ┃ \n'  # 𝗙𝗜𝗟𝗘
+        ' ┃┋┇┃┃┇┋┃ \n'
+        ' ┗━━━━━━┛ ' 
+    ),2, DEFAULT_EDITOR),
+    '.h':TUIFIProfile((
+        ' ┏━━━━━━┓ \n'
+        ' ┇ █▂▂█ ┃ \n'  
+        ' ┃ █▔▔█ ┃ \n'
+        ' ┗━━━━━━┛ ' 
+    ),2, DEFAULT_EDITOR),
+    '.c':TUIFIProfile((
+        ' ┏━━━━━━┓ \n'
+        ' ┇ █▀▀▀ ┃ \n'  
+        ' ┃ █▄▄▄ ┃ \n'
+        ' ┗━━━━━━┛ ' 
+    ),2, DEFAULT_EDITOR),
+    '.cpp':TUIFIProfile((
+        ' ┏━━━━++┓ \n'
+        ' ┇ █▀▀▀ ┃ \n'  
+        ' ┃ █▄▄▄ ┃ \n'
+        ' ┗━━━━━━┛ ' 
+    ),2, DEFAULT_EDITOR),
+    '.js':TUIFIProfile(( 
+        '  ▃▃▃ ▃▃▃ \n'
+        'JS▔█▔ █▃▃ \n'  
+        ' ▃ █  ▔▔█ \n'
+        ' ▀▀▀  ▀▀▀ '
+    ),2, DEFAULT_EDITOR),
+    '.html':TUIFIProfile((
+        ' ┏━━━━━━┓ \n'
+        ' ┇ HTML ┃ \n'  
+        ' ┃ </ > ┃ \n'
+        ' ╰━━━━━━╯ ' 
+    ),2, DEFAULT_EDITOR),
+    '.vb':TUIFIProfile(( 
+        ' █ █ █▀▆  \n'
+        ' █ █ █▀▆  \n'  
+        ' ▀▃▀ █▃▀  \n'
+        '          '                         
+    ),4, DEFAULT_EDITOR),
+    '.vbs':TUIFIProfile(( 
+        '▃ ▃ ▃▃  ▃▃\n'
+        '█ █ █▃█ █▃\n'  
+        '▀▃▀ █▃▀ ▃█\n'
+        '          '                         
+    ),4, DEFAULT_EDITOR),
+    '.apk':TUIFIProfile(( 
+        ' ▃  ▃▃ ▃ ▃\n'
+        '█▃█ ██ █▃▀\n'  
+        '█ █ █  █ █\n'
+        '          '                         
+    ),5, DEFAULT_OPENER),
+    '.vim':TUIFIProfile(( 
+        ' ┏━━━━━┳┓ \n'
+        ' ┃VI━━━┫┇ \n'  
+        ' ┃█▀▅▀▄┃┃ \n'
+        ' ╰━━━━━┻┛ '                         
+    ),5, DEFAULT_EDITOR),
+    '.md':TUIFIProfile((
+        ' ┏━━━━━━┓ \n'
+        ' ┃█▀▅▀▄/┇ \n'  
+        ' ┃DOWN//┃ \n'
+        ' ╰━━━━━━┛ ' 
+    ),4, DEFAULT_EDITOR),
+    '.json':TUIFIProfile((
+        ' ┏━━━━┓━┓ \n'
+        ' ┃┏┇┏┏┓┓┃ \n'
+        ' ┃┇┛JSON┃ \n'
+        ' ┗━━━━┛━┛ '
+    ),1, DEFAULT_EDITOR),
+    '.lua':TUIFIProfile(( 
+        '▃  ▃ ▃ ▃▃▃\n'
+        '█  █ █ █▃█\n'  
+        '█▃▖█▃█ █ █\n'
+        '          '                         
+    ),4, DEFAULT_EDITOR),
+    '.java':TUIFIProfile(( 
+        '    Šƨ    \n'
+        '  ⊏█▇▇█   \n'  
+        ' ▗▃▃▃▃▃▃▖ \n'
+        '  ▔▔▔▔▔▔  '                         
+    ),3, DEFAULT_EDITOR),
+    '.jar':TUIFIProfile(( 
+        '    Šƨ    \n'
+        '  ⊏█▇▇█   \n'  
+        ' ▗▃▃▃▃▃▃▖ \n'
+        '  ▔▔▔▔▔▔  '                         
+    ),1, DEFAULT_EDITOR),
+    '.cs':TUIFIProfile(( 
+        '▃▃▃▃  ▃ ▃ \n'
+        '█▔▔▔ ▀█▀█▀\n'  
+        '█▃▃▃ ▀█▀█▀\n'
+        '▔▔▔▔      '
+    ),5, DEFAULT_EDITOR),
+    '.fs':TUIFIProfile(( 
+        '▃▃▃▃  ▃ ▃ \n'
+        '█▃▃  ▀█▀█▀\n'  
+        '█    ▀█▀█▀\n'
+        '▔         '
+    ),4, DEFAULT_EDITOR),
+    '.go':TUIFIProfile(( 
+        '▄▄▄▄ ▄▄▄▄-\n'
+        '█ ▄▄ █ ▒█/\n'  
+        '█▃▃█/█▃▃█/\n'
+        '▔▔▔▔ ▔▔▔▔ '
+    ),4, DEFAULT_EDITOR),
+    '.rs':TUIFIProfile(( 
+        ' ▅▅▅▅▄▃▂  \n'
+        '  █▄▃▃▟▛  \n'  
+        ' ▃█▃ ▀▆▄▅ \n'
+        ' ▔▔▔   ▔  '
+    ),3, DEFAULT_EDITOR),
+    '.py':TUIFIProfile(( 
+        ' ▃▃▃ ▃ ▃. \n'
+        ' █▃█▒█▃█▒ \n'
+        ' █▒·   █▒ \n'
+        '          '
+    ),2, DEFAULT_EDITOR),
+    '.txt':TUIFIProfile((
+        ' ┏━━┓┓┓┓┓ \n'
+        ' ┃┏┇┃☵☲┃┇ \n'
+        ' ┃┇┛┃☲☵┃┃ \n'
+        ' ┗━━┛━━┛┛ '
+    ),1, DEFAULT_EDITOR),
+    '.css':TUIFIProfile((
+        ' ╭━━━━╮╮╮ \n'
+        ' ┃CSS┃▅▅┃ \n'
+        ' ┃┇▒▒┃☲☰┃ \n'
+        ' ┗━━━╯━━╯ '
+    ),2, DEFAULT_EDITOR),
+    '.scss':TUIFIProfile((
+        ' ╭━━━━╮╮╮ \n'
+        ' ┃SCS┃▅▅┃ \n'
+        ' ┃┇▒▒┃S☲┃ \n'
+        ' ┗━━━╯━━╯ '
+    ),2, DEFAULT_EDITOR),
+    '.pdf':TUIFIProfile((
+        ' ╭━━━━╮━╮ \n'
+        ' ┃PDF┃☲ ┃ \n'
+        ' ┃┇▒█┃▒▒┃ \n'
+        ' ┗━━━━━━╯ '
+    ),3, DEFAULT_OPENER),
+    '.bin':TUIFIProfile((
+        ' ╭━━━━━━╮ \n'
+        ' ┃☴☱☶☵ ∟┇ \n'
+        ' ┇☱☶ BIN┃ \n'
+        ' ╰━━━━━━╯ '
+    ),2, DEFAULT_OPENER),
+    '.sh':TUIFIProfile((
+        ' ╭━━━━━━╮ \n'
+        ' ┃ $_   ┃ \n'
+        ' ┇ BASH ┇ \n'
+        ' ╰━━━━━━╯ '
+    ),2, DEFAULT_EDITOR),
+    '.cmd':TUIFIProfile((
+        ' ╭━━━━━━╮ \n'
+        ' ┃ C:\>_┃ \n'
+        ' ┇ CMD  ┇ \n'
+        ' ╰━━━━━━╯ '
+    ),2, DEFAULT_EDITOR),
+    '.bat':TUIFIProfile((
+        ' ╭━━━━━━╮ \n'
+        ' ┃ C:\>_┃ \n'
+        ' ┇ BAT  ┇ \n'
+        ' ╰━━━━━━╯ '
+    ),2, DEFAULT_EDITOR),
+    '.so':TUIFIProfile((
+        ' ╭━┓━┓╭━┓ \n'
+        ' │S┃O┃│Ξ┃ \n'
+        ' │█┃▒││▓╿ \n'
+        ' ╰━┘━┘╰━┘ ' 
+    ),2, DEFAULT_OPENER),
+    '.dll':TUIFIProfile((
+        ' ╭━┓━┓╭━┓ \n'
+        ' │D┃L┃│L┃ \n'
+        ' │█┃▒││▓╿ \n'
+        ' ╰━┘━┘╰━┘ ' 
+    ),2, DEFAULT_OPENER),
+    '.rar':TUIFIProfile((
+        ' ▃▃RAR▃▃╮ \n'
+        ' ▒▒░ ░▒▒┃ \n'
+        ' ▓▓░ ░▓▓┃ \n'
+        ' ▀▀░ ░▀▀┘ '
+    ),2, DEFAULT_OPENER),
+    '.zip':TUIFIProfile((
+        ' ▃▃▃░░▃▃▃ \n'
+        ' ▒▒▒░░▒▒▒ \n' 
+        ' ▓▓▓░░▓▓▓ \n' 
+        ' ZIP░░▀▀▀ '
+    ),2, DEFAULT_OPENER),
+    '.tar':TUIFIProfile((
+        ' ▃▃▃░░▃▃▃ \n'
+        ' ▒▒▒░░▒▒▒ \n' 
+        ' ▓▓▓░░▓▓▓ \n' 
+        ' TAR░░▀▀▀ '
+    ),2, DEFAULT_OPENER),
+    '.gz':TUIFIProfile((
+        ' ▃▃▃░░▃▃▃ \n'
+        ' ▒▒▒░░▒▒▒ \n' 
+        ' ▓▓▓░░▓▓▓ \n' 
+        ' TAR░░▀▀▀ '
+    ),2, DEFAULT_OPENER),
+    '.xz':TUIFIProfile((
+        ' ▃▃▃░░▃▃▃ \n'
+        ' ▒▒▒░░▒▒▒ \n' 
+        ' ▓▓▓░░▓▓▓ \n' 
+        ' TAR░░▀▀▀ '
+    ),2, DEFAULT_OPENER),
+    '.bz2':TUIFIProfile((
+        ' ▃▃▃░░▃▃▃ \n'
+        ' ▒▒▒░░▒▒▒ \n' 
+        ' ▓▓▓░░▓▓▓ \n' 
+        ' TAR░░▀▀▀ '
+    ),2, DEFAULT_OPENER),
+    '.zst':TUIFIProfile((
+        ' ▃▃▃░░▃▃▃ \n'
+        ' ▒▒▒░░▒▒▒ \n' 
+        ' ▓▓▓░░▓▓▓ \n' 
+        ' TAR░░▀▀▀ '
+    ),2, DEFAULT_OPENER),
+    
+    '.gif':TUIFIProfile((
+        ' ┏━━━┳┳┳┓ \n'
+        ' ┃GIF:∵◖┇ \n'
+        ' ┠┓░▃_▓▆┇ \n'
+        ' ╰━━━┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    
+    '.mp4':TUIFIProfile((
+        ' ┏┳┳┳┳┳┳┓ \n'
+        ' ┇MP4∴∵◖┇ \n'
+        ' ┇_░▃_▓▆┇ \n'
+        ' ┗┻┻┻┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    '.3gp':TUIFIProfile((
+        ' ┏┳┳┳┳┳┳┓ \n'
+        ' ┇3GP∴∵◖┇ \n'
+        ' ┇_░▃_▓▆┇ \n'
+        ' ┗┻┻┻┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    
+    '.avi':TUIFIProfile((
+        ' ┏┳┳┳┳┳┳┓ \n'
+        ' ┇AVI∴∵◖┇ \n'
+        ' ┇_░▃_▓▆┇ \n'
+        ' ┗┻┻┻┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    '.webm':TUIFIProfile((
+        ' ┏┳┳┳┳┳┳┓ \n'
+        ' ┇WEBM∵◖┇ \n'
+        ' ┇_░▃_▓▆┇ \n'
+        ' ┗┻┻┻┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    '.ogv':TUIFIProfile((
+        ' ┏┳┳┳┳┳┳┓ \n'
+        ' ┇OGV∴∵◖┇ \n'
+        ' ┇_░▃_▓▆┇ \n'
+        ' ┗┻┻┻┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    '.mov':TUIFIProfile((
+        ' ┏┳┳┳┳┳┳┓ \n'
+        ' ┇MOV∴∵◖┇ \n'
+        ' ┇_░▃_▓▆┇ \n'
+        ' ┗┻┻┻┻┻┻┛ '
+    ),2, DEFAULT_OPENER),
+    '.jpg':TUIFIProfile(( 
+        ' ┏━━━JPG╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.png':TUIFIProfile(( 
+        ' ┏━━━PNG╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.jpeg':TUIFIProfile(( 
+        ' ┏━━JPEG╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.bmp':TUIFIProfile(( 
+        ' ┏━━━BMP┓ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ┗━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.webp':TUIFIProfile(( 
+        ' ┏━━WEBP╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.tiff':TUIFIProfile(( 
+        ' ┏━━TIFF╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.tif':TUIFIProfile(( 
+        ' ┏━━TIFF╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.tga':TUIFIProfile(( 
+        ' ┏━TARGA╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.heif':TUIFIProfile(( 
+        ' ┏━━HEIF╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.heifs':TUIFIProfile(( 
+        ' ┏━━HEIF╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    '.heic':TUIFIProfile(( 
+        ' ┏━━HEIC╮ \n'
+        ' ┇*.∴:∵◖┃ \n'
+        ' ┣┓░▃┏▓▆┇ \n'
+        ' ╰━━━━━━┛ '
+    ),2, DEFAULT_OPENER),
+    
+    '.mp3':TUIFIProfile(( 
+        ' ╭┓━━━━┏╮ \n'
+        ' ┣╯MP3♪╰┫ \n'
+        ' ┇┓♯⦇⦈♯┏┇ \n'
+        ' ╰┛━━━━┗╯ '
+    ),8, DEFAULT_OPENER),
+    '.wav':TUIFIProfile(( 
+        ' ╭┓━━━━┏╮ \n'
+        ' ┣╯WAV♪╰┫ \n'
+        ' ┇┓♯⦇⦈♯┏┇ \n'
+        ' ╰┛━━━━┗╯ '
+    ),8, DEFAULT_OPENER),
+    '.mid':TUIFIProfile(( 
+        ' ╭┓━━━━┏╮ \n'
+        ' ┣╯MID♪╰┫ \n'
+        ' ┇┓♯⦇⦈♯┏┇ \n'
+        ' ╰┛━━━━┗╯ '
+    ),8, DEFAULT_OPENER),
+    '.aac':TUIFIProfile(( 
+        ' ╭┓━━━━┏╮ \n'
+        ' ┣╯AAC♪╰┫ \n'
+        ' ┇┓♯⦇⦈♯┏┇ \n'
+        ' ╰┛━━━━┗╯ '
+    ),8, DEFAULT_OPENER),
+    
+    '.psd':TUIFIProfile((
+        ' ╭▃▃▃ ▃▃╮ \n'
+        ' ┃█▂█ █▂┃ \n'
+        ' ┃█   ▃█┃ \n'
+        ' ┗━PSD━━┛ '
+    ),4, DEFAULT_OPENER),
+    '.mdf':TUIFIProfile((
+        ' ╭━━━━━━╮ \n'
+        ' ┃┇DB╭▒█┫ \n'
+        ' ┠┇██┻▓▒┃ \n'
+        ' ┗━━━╯━━╯ '
+    ),2, DEFAULT_OPENER),
+    '.tuifi':TUIFIProfile((
+        '             \                                      [            \n' 
+        '              @                 ⟡                  ╢             \n' 
+        '      /       ╣▒                                  ]▒       \     \n' 
+        '     ╔       ]Ñ▒                                  ╟╣┐       ▓    \n' 
+        '    ╢╣       ╣▓            √          t            ▓╣       ▓╣   \n' 
+        '   ▓╣▒╖    ╓╫╜           ╥▓   #TUIFI   ▓@           ╙▓╖    ╔╣╢║  \n' 
+        '   ▓▓▓▓  ,p▓,,,,,,      ╜╙▓▄╖,      ,╓╥╜╙╙    ,,,,,,,,▓▓,  ▀▓▓╣U \n' 
+        '   ▀▓Ö   ╙█▓▓▓▓▓▓╢╫╣▓▓▓▓▓╦, ▀▓▓╗  g╢▓╝ ,╓H╢╢╢╢╢╢▓▓▓▓▓▓▒▓╜   ]▓▓  \n' 
+        '    ▓▓▓╦╥╖ ╙╙╙╙`     `""▀▓▓@ ▐█▓L]▓╫╛ Æ▒╨╜"       ""╙╙` ╓╖∩▒▒▓   \n' 
+        ' ╒▓▒╜""╙▀▓▓                ▀  █▒Γ▐▓▓  ╩                ▓╢╜""╙▀█╫L\n' 
+        ' ▐▌`      └╝                  ▓▒` █▓                  ╜       └█▓\n' 
+        '▐▓                            ▓▒  █╢                           ▐▓\n' 
+        ' ▐Γ                            ╛  ▐"                           ▐[\n' 
+        ' ¬U                                                            jU\n' 
+        '  C                                                            j \n' 
+        '   L                                                          ]  \n'
+        '                                                                 '     
+    ),2),
+    '.tuifi3':TUIFIProfile((
+        '              .^        \n'
+        '            ~GB^        \n'
+        '          7B&G.         \n'
+        '        !B&&J   oOo     \n'
+        '      :G&&#~    oOo     \n'
+        '     ?&&&G.             \n'
+        '   :G&&&B7:  ^^^^^^^    \n'
+        '  7#&&&&&&#7J&#&&&##G^  \n'
+        '.5&&&G!!Y######J77P###? \n'
+        'J&&##:   ^G##G:   .BBB#7\n'
+        ' ?#&#B^    ??    ^GBBB7 \n'
+        '  :G&##J        7BB#G:  \n'
+        '    Y##&~     .5#BBJ    \n'
+        '     ~BP.    ^GBBG^     \n'
+        '  oOo       ?BBBJ       \n'
+        '  oOo     .P#B5:        \n'
+        '         ~B#P^          \n'
+        '        JBY^            \n'
+        '       .^.              \n'
+        '                        '
+    ),3)
+}
+DEFAULT_PROFILE   = TUIFIProfiles['file'] 
+LINK_SYMBOL       = '↩'  # Potential:⤶ ⤾ ↲ ⎌ ☍ ⧉ 
+LINK_SYMBOL_COLOR = 1
---
+++ packages/TUIFIManager/TUIFIManager-1.3.3.tar.gz/TUIFIManager-1.3.3/TUIFIManager/TUIFile.py
@@ -0,0 +1,89 @@
+import unicurses
+from .TUIFIProfile import *
+ 
+
+ 
+class TUIFile:
+    profile = DEFAULT_PROFILE
+    #h,w = 4,11 # 10        
+    x,y         = 0,0
+    name_height = 1  
+    is_selected = False
+    is_cut      = False # This is pointless for now, until i find a way of efficiently drawing/managing cuted  files 
+      
+    def chunkStr(self,text, n): # sorry  for this :P 
+        counter2 = 0
+        counter1 = 0
+        tempTxT  = ''
+        for i in range(0,len(text)):
+            counter1 += 1
+            tempTxT += text[i]
+            if counter1 == n:
+                counter2 += 1
+                tempTxT  += '\n'
+                counter1  = 0
+            
+        if tempTxT.endswith('\n'):
+            counter2 -= 1
+            tempTxT   = tempTxT[0:-1]
+            
+        self.name_height = counter2 + 1
+        return tempTxT    
+    
+                         
+    def __init__(self, name, y=0, x=0, profile=DEFAULT_PROFILE, name_color=1, is_link=False):
+        assert isinstance(profile, TUIFIProfile),'profile needs to be of type class TUIFIProfile'
+        self.name_color  = name_color
+        self.profile     = profile
+        self.name_color  = name_color
+        self.y           = y
+        self.x           = x
+        self.name        = name
+        self.is_link     = is_link
+        self.chunkStr(name, self.profile.width)
+
+
+    def clear(self,atpad):
+        pass
+    
+    
+    def draw_name(self, atpad, name, prename, chgatXY, attr=unicurses.A_NORMAL):  # fuck, lol
+        """
+        DON'T USE IT
+        """
+        y = chgatXY // self.profile.width
+        x = chgatXY - y * (self.profile.width)
+        for offY, ln in enumerate(self.chunkStr(prename + ' ',self.profile.width).split('\n'), self.profile.height):
+            unicurses.mvwaddwstr(atpad,offY + self.y,self.x, ' ' * len(ln)) # A_BOLD | 
+        for offY, ln in enumerate(self.chunkStr(name,self.profile.width).split('\n'), self.profile.height):
+            unicurses.mvwaddwstr(atpad,offY + self.y,self.x, ln, unicurses.COLOR_PAIR(self.name_color) | attr) # A_BOLD | 
+        unicurses.mvwchgat(atpad,self.y + self.profile.height + y, self.x +x, 1, unicurses.A_NORMAL, 6)
+        
+
+    def __draw_file(self, atpad):
+        for offY, ln in enumerate((self.profile.text + '\n').split('\n')):
+            unicurses.mvwaddwstr(atpad,offY + self.y,self.x, ln, unicurses.COLOR_PAIR(self.profile.color_map) ) 
+        for offY, ln in enumerate(self.chunkStr(self.name,self.profile.width).split('\n'), offY):
+            unicurses.mvwaddwstr(atpad,offY + self.y,self.x, ln, unicurses.COLOR_PAIR(self.name_color) )                  
+        if self.is_link: # no idea why but mvwadd_wch misbehaves ...
+            unicurses.mvwaddwstr(atpad, self.y + self.profile.height -1 , self.x + self.profile.width -1, LINK_SYMBOL, unicurses.COLOR_PAIR(LINK_SYMBOL_COLOR))  # | A_BOLD
+
+      
+    def draw(self,atpad, y=None, x=None, redraw_icon=False): #  na valw NEW giati sto resort() xanei to icon kai text h kati tetoio !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if y: self.y = y
+        if x: self.x = x
+        
+        pLINES, pCOLS = unicurses.getmaxyx(atpad)
+
+        if self.is_selected:
+            if redraw_icon:self.__draw_file(atpad)
+            for y in range(self.y, self.y + self.profile.height):
+                unicurses.mvwchgat(atpad,y, self.x, self.profile.width,unicurses.A_REVERSE,7)             
+        elif self.is_cut:
+            if redraw_icon:self.__draw_file(atpad)
+            for y in range(self.y, self.y + self.profile.height):
+                unicurses.mvwchgat(atpad,y, self.x, self.profile.width,unicurses.A_DIM,1)
+        else:
+            self.__draw_file(atpad)
+
+
