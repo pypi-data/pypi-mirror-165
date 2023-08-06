5c284ef2831383f63d5e9942ea0979c8e647169a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:07 +0000

    Add package JovyTesting/0.1.1/JovyTesting-0.1.1.tar.gz

---
+++ packages/JovyTesting/JovyTesting-0.1.1.tar.gz/JovyTesting-0.1.1/JovyTesting/jmaths/jmaths.py
@@ -0,0 +1,26 @@
+
+import math
+import numpy as np
+
+def add(*args):
+    """
+    Add numbers together
+    """
+    args = np.array(list(args)).flat
+    all_number_list = list(map(float,args))
+    sum_result = sum(all_number_list)
+    remainders = sum_result%int(sum_result)
+    return int(sum_result) if remainders==0 else sum_result
+
+
+def sub(*args):
+    """
+    Subtract numbers from each other
+    """
+    args = np.array(list(args)).flat
+    all_number_list = list(map(float,args))
+    diff_result = all_number_list[0]
+    for number in all_number_list[1::]:
+        diff_result -= number
+    remainders = diff_result%int(diff_result)
+    return int(diff_result) if remainders==0 else diff_result
