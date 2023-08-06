9586742195fdbd572c619b842f501d92504786bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:47 +0000

    Add package OpenFisca-UK/0.29.0/OpenFisca-UK-0.29.0.tar.gz

---
+++ packages/OpenFisca-UK/OpenFisca-UK-0.29.0.tar.gz/OpenFisca-UK-0.29.0/openfisca_uk/parameters/__init__.py
@@ -0,0 +1,4 @@
+from openfisca_uk.system import CountryTaxBenefitSystem
+from openfisca_uk.tools.simulation import prepare_parameters
+
+parameters = prepare_parameters(CountryTaxBenefitSystem()).parameters
