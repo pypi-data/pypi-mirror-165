d4756e07c4ff1122edfca582110fd1b00699c452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:50 +0000

    Add package skime/0.2.1/skime-0.2.1.tar.gz

---
+++ packages/skime/skime-0.2.1.tar.gz/skime-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['skime', 'skime.compiler', 'skime.types']
+
+package_data = \
+{'': ['*'], 'skime': ['scheme/*']}
+
+setup_kwargs = {
+    'name': 'skime',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
