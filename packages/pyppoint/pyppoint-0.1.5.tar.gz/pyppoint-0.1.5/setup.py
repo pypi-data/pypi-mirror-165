34514c25a690e59a9415e6589ef3f646ac6ee9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:31 +0000

    Add package pyppoint/0.1.5/pyppoint-0.1.5.tar.gz

---
+++ packages/pyppoint/pyppoint-0.1.5.tar.gz/pyppoint-0.1.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="pyppoint",
+    version="0.1.005",
+    author="Gaetan Desrues",
+    author_email="gdesrues@gmail.com",
+    packages=["pyppoint"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
