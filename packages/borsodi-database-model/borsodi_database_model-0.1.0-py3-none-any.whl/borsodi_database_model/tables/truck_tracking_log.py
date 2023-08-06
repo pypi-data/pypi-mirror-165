77c0ea070a5e5652733c402547bae2f766fe11cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:23 +0000

    Add package borsodi-database-model/0.1.0/borsodi_database_model-0.1.0-py3-none-any.whl

---
+++ packages/borsodi-database-model/borsodi_database_model-0.1.0-py3-none-any.whl/borsodi_database_model/tables/truck_tracking_log.py
@@ -0,0 +1,28 @@
+import enum
+
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Enum
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from .base import Base
+
+
+class Cameras(enum.Enum):
+    one = 1
+    two = 2
+    three = 3
+
+
+class TruckTrackingLog(Base):
+    __tablename__ = 'truck_tracking_log'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    ts = Column(DateTime, nullable=False)
+    camera_id = Column(Enum(Cameras), nullable=False)
+    licence_plate = Column(String(15), nullable=False)
+    arrived = Column(DateTime, nullable=False)
+    waiting = Column(DateTime, nullable=False)
+    finished = Column(DateTime, nullable=False)
+    status = Column(String(10), nullable=False)
