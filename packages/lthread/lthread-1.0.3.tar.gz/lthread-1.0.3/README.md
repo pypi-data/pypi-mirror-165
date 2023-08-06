6ee3f25763aa08a2be4fe661b85879a0ab9f31e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:03 +0000

    Add package lthread/1.0.3/lthread-1.0.3.tar.gz

---
+++ packages/lthread/lthread-1.0.3.tar.gz/lthread-1.0.3/README.md
@@ -0,0 +1,4 @@
+lthread
+=======
+
+NOTE: `lthread` is basically obsoltete. Everything that it can do can be done much more easily with Python 3.7's `asyncio`.
---
+++ packages/lthread/lthread-1.0.3.tar.gz/lthread-1.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="lthread",
+      version="1.0.3",
+      description="(OBSOLETE) lightweight threads in Python using extended generators",
+      author="Katie Rust",
+      author_email="katie@ktpanda.org",
+      license="MIT License",
+      packages=["lthread"],
+      classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+        ]
+
+     )
