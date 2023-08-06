18284f80eda15774019db9ac69d75b41db33f015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package linesieve/1.0a10/linesieve-1.0a10-py3-none-any.whl

---
+++ packages/linesieve/linesieve-1.0a10-py3-none-any.whl/linesieve/__init__.py
@@ -0,0 +1,37 @@
+"""
+Read standard input, process it, and write to standard output.
+
+Split input into sections, and output selected sections.
+Filter output lines, globally or per-section.
+Compress runs of blank lines.
+
+All patterns use the Python regular expression syntax.
+
+Example:
+
+\b
+  $ cat file.txt
+  0
+  one:
+  1...
+  two:
+  2 (two)
+  three:
+  3, three
+  fail
+
+\b
+  $ cat file.txt | linesieve --section '(\\S+):$' --failure fail \\
+  > show --ignore-case ^O \\
+  > match --section one --only-matching '\\d+' \\
+  > sub '([a-z])' '\\1\\1\\1'
+  d\bone
+  1
+  d\b..
+  d\bthree
+  3, ttthhhrrreeeeee
+  r\bfail
+
+"""
+
+__version__ = '1.0a10'
