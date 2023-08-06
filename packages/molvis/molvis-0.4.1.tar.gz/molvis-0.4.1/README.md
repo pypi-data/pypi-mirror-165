c519bfc08296258fd41ed42b98fa50740e0b0c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:30 +0000

    Add package molvis/0.4.1/molvis-0.4.1.tar.gz

---
+++ packages/molvis/molvis-0.4.1.tar.gz/molvis-0.4.1/README.md
@@ -0,0 +1,15 @@
+# molvis
+
+A simple python module for creating html/js files that use [3dmol.js](https://3dmol.csb.pitt.edu/) to visualise structures
+
+## Installation
+
+To install, use `pip`
+
+
+```shell
+pip install molvis
+```
+
+## Documentation
+Documentation for this package can be found here, and documentation for 3dmol.js can be found [here](https://3dmol.csb.pitt.edu/)
---
+++ packages/molvis/molvis-0.4.1.tar.gz/molvis-0.4.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+__version__ = "0.4.1"
+
+setuptools.setup(
+    name="molvis",
+    version=__version__,
+    author="Jon Kragskow",
+    author_email="jonkragskow@gmail.com",
+    description="A package for generating html scripts to visualise chemical structures using 3dmol.js",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/jonkragskow/molvis",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/jonkragskow/molvis/-/issues",
+        "Documentation": "https://jonkragskow.gitlab.io/molvis"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "deprecation",
+        "xyz_py"
+    ],
+    entry_points={
+        'console_scripts': [
+            'molvis = molvis.cli:main'
+        ]
+    }
+)
