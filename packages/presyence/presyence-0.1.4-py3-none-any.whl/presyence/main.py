6cf378d34c8e71d97cb3ee910abe346ca9e8296e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:35 +0000

    Add package presyence/0.1.4/presyence-0.1.4-py3-none-any.whl

---
+++ packages/presyence/presyence-0.1.4-py3-none-any.whl/presyence/main.py
@@ -0,0 +1,29 @@
+import typer
+from .testrunner.discovery import discover_test_files, extract_tests_from_file
+
+app = typer.Typer()
+
+
+@app.command()
+def main():
+    print("Looking for tests")
+    test_files = discover_test_files()
+    print(test_files)
+    if not test_files:
+        print("No test file found. Test file names must end with '.spec.sy.py'")
+        exit()
+    tests = []
+    for test_file in test_files:
+        _tests = extract_tests_from_file(test_file)
+        if not _tests:
+            print(f"No tests found in file {test_file.absolute()}")
+            exit()
+        tests += _tests
+    print("Running tests")
+    test_results = []
+    for test in tests:
+        test_results += [test.run()]
+    print("Creating the test report")
+    for each in test_results:
+        print(each, end="\n"*2)
+    print("Serving the test report on http://localhost:5000/")
