f24b7bff12d361350c88b6569c3ef604226dc723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:33 +0000

    Add package pylya/0.0.7/pylya-0.0.7.tar.gz

---
+++ packages/pylya/pylya-0.0.7.tar.gz/pylya-0.0.7/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pylya",
+    version="0.0.7",
+    author="Konstantinos Filopoulos",
+    description="Coarse-grained Dynamic Analysis for Python",
+    packages=setuptools.find_packages()
+)
