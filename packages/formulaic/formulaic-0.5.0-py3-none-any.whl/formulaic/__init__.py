3cc9d6a570dcc5488d2877bf3a29ab6a7ff6e5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:31 +0000

    Add package formulaic/0.5.0/formulaic-0.5.0-py3-none-any.whl

---
+++ packages/formulaic/formulaic-0.5.0-py3-none-any.whl/formulaic/__init__.py
@@ -0,0 +1,20 @@
+from ._version import __author__, __author_email__, __version__
+from .formula import Formula, FormulaSpec
+from .materializers import FactorValues
+from .model_matrix import ModelMatrix, ModelMatrices
+from .model_spec import ModelSpec, ModelSpecs
+from .sugar import model_matrix
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__version__",
+    "Formula",
+    "FormulaSpec",
+    "ModelMatrix",
+    "ModelMatrices",
+    "ModelSpec",
+    "ModelSpecs",
+    "model_matrix",
+    "FactorValues",
+]
