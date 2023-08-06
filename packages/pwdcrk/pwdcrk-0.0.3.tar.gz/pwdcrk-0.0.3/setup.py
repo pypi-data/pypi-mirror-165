aea3abe36f7dd6eb606041c9346ca4a975b15177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:45 +0000

    Add package pwdcrk/0.0.3/pwdcrk-0.0.3.tar.gz

---
+++ packages/pwdcrk/pwdcrk-0.0.3.tar.gz/pwdcrk-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = "A password cracking tool."
+LONG_DESCRIPTION = "A tool that will allow you to crack passwords. This library can generate password dictionaries for a brute-force attack and can also automatically crack any hash for you."
+
+# Setting up
+setup(
+    name="pwdcrk",
+    version=VERSION,
+    author="thecarpetjasp",
+    author_email="thecarpetjasp@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['hashlib'],
+    keywords=['python', 'password', 'password cracker', 'brute-force', 'dictionary'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
