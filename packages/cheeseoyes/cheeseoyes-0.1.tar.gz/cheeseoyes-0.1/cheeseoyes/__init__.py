48998d0032041721231c59d9cb7b4ed92d873593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:55 +0000

    Add package cheeseoyes/0.1/cheeseoyes-0.1.tar.gz

---
+++ packages/cheeseoyes/cheeseoyes-0.1.tar.gz/cheeseoyes-0.1/cheeseoyes/__init__.py
@@ -0,0 +1,1 @@
+﻿import cheeseoyes.main
---
+++ packages/cheeseoyes/cheeseoyes-0.1.tar.gz/cheeseoyes-0.1/cheeseoyes/setup.py
@@ -0,0 +1,21 @@
+﻿from distutils.core import setup
+setup(
+  name = 'cheeseoyes',         
+  packages = ['cheeseoyes'],   
+  version = '0.1',      
+  license='LICENSE.txt',        
+  description = '',   
+  author = 'oliverstech',                   
+  author_email = 'mail@oliverstech.tk',      
+  url = 'http://oliverstech.tk',   
+  install_requires=[''],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
