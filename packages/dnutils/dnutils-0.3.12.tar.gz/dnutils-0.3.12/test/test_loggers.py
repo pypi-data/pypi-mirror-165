4d442f0bd2853116f590981c09dbfce6877570b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:45 +0000

    Add package dnutils/0.3.12/dnutils-0.3.12.tar.gz

---
+++ packages/dnutils/dnutils-0.3.12.tar.gz/dnutils-0.3.12/test/test_loggers.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from dnutils import getlogger, logs, err
+
+try:
+    import pymongo
+    import mongomock
+except ModuleNotFoundError:
+    err('You must have "pymongo" and "mongomock" installed to run the tests.')
+    raise
+
+
+from dnutils.logs import MongoHandler, colored_console
+
+
+class MongoHandlerTest(TestCase):
+
+    def test_mongo_handler_insert(self):
+        handler = MongoHandler(mongomock.MongoClient(), 'db', 'log')
+        coll = handler.coll
+        logger = getlogger('/dnutils/test', level=logs.DEBUG)
+        logger.add_handler(handler)
+        logger.info('Test1')
+        logger.debug('Test2')
+        self.assertEqual(2, coll.count_documents({}))
+        messages = {tuple(doc['message']) for doc in coll.find({})}
+        self.assertSetEqual(messages, {('Test1',), ('Test2',)})
+
+
+class LoggerTest(TestCase):
+
+    def test_logger(self):
+        logger = getlogger('test', level=logs.DEBUG, handlers=[colored_console])
+        logger.debug('This is the DEBUG message.')
+        logger.info('This is the INFO message.')
+        logger.warning('This is the WARNING message.')
+        logger.error('This is the ERROR message.')
+        logger.exception('This is the EXCEPTION message.')
+        self.assertTrue(True)
