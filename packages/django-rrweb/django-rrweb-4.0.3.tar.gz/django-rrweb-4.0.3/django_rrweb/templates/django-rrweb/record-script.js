d340a23ce6add287680233e4b5f310bf0f6a726e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:36 +0000

    Add package django-rrweb/4.0.3/django-rrweb-4.0.3.tar.gz

---
+++ packages/django-rrweb/django-rrweb-4.0.3.tar.gz/django-rrweb-4.0.3/django_rrweb/templates/django-rrweb/record-script.js
@@ -0,0 +1,45 @@
+{% load static %}(function () {
+    function rrwebSetup() {
+        let rrwebEvents = [];
+        let rrwebPageKey = "{{ page_key }}";
+        let rrwebSessionKey = window.rrwebSessionKey || "{{ session_key }}";
+        window.rrwebSessionKey = rrwebSessionKey;
+
+        window.rrwebRecord({
+            emit(event) {
+                rrwebEvents.push(event);
+            }
+        });
+
+        function rrwebSave() {
+            if (rrwebEvents.length === 0) {
+                return;
+            }
+            const body = JSON.stringify({ rrwebEvents, rrwebPageKey, rrwebSessionKey });
+            rrwebEvents = [];
+            fetch("//{{ request.get_host }}{% url 'django-rrweb-record-events' %}", {
+                body,
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                mode: "cors"
+            });
+        }
+
+        setInterval(rrwebSave, 1000);
+        window.addEventListener("beforeunload", rrwebSave);
+        window.addEventListener("unload", rrwebSave);
+    }
+
+    if (window.rrwebRecord === undefined) {
+        const scriptTag = document.createElement("script");
+        scriptTag.onload = rrwebSetup;
+        scriptTag.src = "//{{ request.get_host }}{% static 'django-rrweb/rrweb-record-1-1-3.js' %}";
+        document.head.appendChild(scriptTag);
+    }
+    else {
+        rrwebSetup();
+    }
+}());
