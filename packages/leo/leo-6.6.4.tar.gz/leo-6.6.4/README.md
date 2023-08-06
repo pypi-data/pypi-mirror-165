50c8fb52ccb3ba06fdad60e3bf7eada86dc89081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:23 +0000

    Add package leo/6.6.4/leo-6.6.4.tar.gz

---
+++ packages/leo/leo-6.6.4.tar.gz/leo-6.6.4/README.md
@@ -0,0 +1,28 @@
+Leo http://leoeditor.com 6.6.4 is now available on
+[GitHub](https://github.com/leo-editor/leo-editor/releases) and
+[pypi](https://pypi.org/project/leo/).
+
+Leo, http://leoeditor.com is an [IDE, outliner and PIM](http://leoeditor.com/preface.html).
+
+**The highlights of Leo 6.6.4**
+
+- Enhance leoserver.py to support leoInteg and leoJS.
+- Simplify all of Leo's importers.
+- Importers no longer generate underindented escape strings.
+- Begin annotating Leo's core files.
+- The usual assortment of minor bug fixes.
+
+**Links**
+
+- 6.6.3 issues: https://github.com/leo-editor/leo-editor/issues?q=is%3Aissue+milestone%3A6.6.3+
+- [Documentation](http://leoeditor.com/leo_toc.html)
+- [Tutorials](http://leoeditor.com/tutorial.html)
+- [Video tutorials](http://leoeditor.com/screencasts.html)
+- [Forum](http://groups.google.com/group/leo-editor)
+- [Download](http://sourceforge.net/projects/leo/files/)
+- [Leo on GitHub](https://github.com/leo-editor/leo-editor)
+- [LeoVue](https://github.com/kaleguy/leovue#leo-vue)
+- [What people are saying about Leo](http://leoeditor.com/testimonials.html)
+- [A web page that displays .leo files](http://leoeditor.com/load-leo.html)
+- [More links](http://leoeditor.com/leoLinks.html)
+
---
+++ packages/leo/leo-6.6.4.tar.gz/leo-6.6.4/leo/plugins/nav_qt.py
@@ -0,0 +1,101 @@
+#@+leo-ver=5-thin
+#@+node:ville.20090518182905.5419: * @file ../plugins/nav_qt.py
+#@+<< docstring >>
+#@+node:ville.20090518182905.5420: ** << docstring >>
+"""Adds "Back" and "Forward" buttons (Qt only).
+
+Creates "back" and "forward" buttons on button bar. These navigate
+the node history.
+
+This plugin does not need specific setup. If the plugin is loaded, the buttons
+will be available. The buttons use the icon specified in the active Qt style
+
+Note it may be practical to put this plugin before mod_scripting.py in
+@enabled-plugins list. That way buttons "back" and "forward" will be placed on
+the left side of toolbar.
+
+"""
+#@-<< docstring >>
+#@+<< imports >>
+#@+node:ville.20090518182905.5422: ** << imports >> (nav_qt.py)
+from typing import Any, Dict
+from leo.core import leoGlobals as g
+from leo.core.leoQt import QAction, StandardPixmap
+#
+# Fail fast, right after all imports.
+g.assertUi('qt')  # May raise g.UiTypeException, caught by the plugins manager.
+#@-<< imports >>
+controllers: Dict[str, Any] = {}  # keys are c.hash(), values are NavControllers
+#@+others
+#@+node:ville.20090518182905.5423: ** init
+def init():
+    """Return True if the plugin has loaded successfully."""
+    ok = g.app.gui.guiName() == "qt"
+    if ok:
+        g.registerHandler(('new', 'open2'), onCreate)
+        g.registerHandler('close-frame', onClose)
+        g.plugin_signon(__name__)
+    return ok
+#@+node:ville.20090518182905.5424: ** onCreate
+def onCreate(tag, keys):
+
+    global controllers
+
+    c = keys.get('c')
+    if not c:
+        return
+
+    h = c.hash()
+
+    nc = controllers.get(h)
+    if not nc:
+        controllers[h] = NavController(c)
+#@+node:vitalije.20170712192502.1: ** onClose
+def onClose(tag, keys):
+    global controllers
+    c = keys.get('c')
+    h = c.hash()
+    nc = controllers.get(h)
+    if nc:
+        nc.removeButtons()
+        del controllers[h]
+#@+node:ville.20090518182905.5425: ** class NavController
+class NavController:
+
+    #@+others
+    #@+node:ville.20090518182905.5426: *3* __init__
+    def __init__(self, c):
+
+        self.c = c
+        c._prev_next = self
+        self._buttons = self.makeButtons()
+
+    #@+node:ville.20090518182905.5427: *3* makeButtons (NavController)
+    def makeButtons(self):
+
+        c = self.c
+        w = c.frame.iconBar.w
+        if not w:
+            return []  # EKR: can be an empty list when unit testing.
+        icon_l = w.style().standardIcon(StandardPixmap.SP_ArrowLeft)
+        icon_r = w.style().standardIcon(StandardPixmap.SP_ArrowRight)
+        # Create the actions.
+        act_l = QAction(icon_l, 'prev', w)
+        act_r = QAction(icon_r, 'next', w)
+        # Use the new commands.
+        act_l.triggered.connect(lambda checked: c.goToPrevHistory())
+        act_r.triggered.connect(lambda checked: c.goToNextHistory())
+        # Don't execute the command twice.
+        self.c.frame.iconBar.add(qaction=act_l)
+        self.c.frame.iconBar.add(qaction=act_r)
+        return act_l, act_r
+
+    def removeButtons(self):
+        for b in self._buttons:
+            self.c.frame.iconBar.deleteButton(b)
+        self._buttons = []
+    #@-others
+#@-others
+#@@language python
+#@@tabwidth -4
+#@-leo
