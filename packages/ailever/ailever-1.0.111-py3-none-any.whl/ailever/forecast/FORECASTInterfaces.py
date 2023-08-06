4b1ded2cacd8d4e8c58ad09bf5fa32dab23aabae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:09 +0000

    Add package ailever/1.0.111/ailever-1.0.111-py3-none-any.whl

---
+++ packages/ailever/ailever-1.0.111-py3-none-any.whl/ailever/forecast/FORECASTInterfaces.py
@@ -0,0 +1,42 @@
+from .machine_learning_based_modeling import LightMLOps
+from .tsa import TSA
+
+from .stockprophets import StockProphet
+
+def FeatureSelection(X):
+    r"""
+    Example:
+        >>> from ailever.forecast import FeatureSelection
+        >>> fs = FeatureSelection(OneColumnData)
+        >>> transformed_data = fs.univariate_feature_selection()
+        >>> transformed_data = fs.recursive_feature_elimination()
+        >>> transformed_data = fs.principal_component_analysis()
+    """
+    from .feature_selection import FeatureSelection
+    return FeatureSelection(X)
+
+def ModelSelection():
+    r"""
+    Example:
+        >>> from ailever.forecast import ModelSelection
+        >>> ms = ModelSelection(OneColumnData)
+    """
+    return None
+
+
+
+def regressor(norm):
+    r"""
+    """
+    from ._stattools import regressor
+    return regressor(norm=norm)
+
+
+def RSDA():
+    r"""
+    """
+    from .ts_residual import RSDA
+    return RSDA()
+
+
+
