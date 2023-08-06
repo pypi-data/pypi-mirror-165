6d8ee023bd79b5c4ae626423fdf280369828da94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:25 +0000

    Add package django-okta-sso/0.1.5/django_okta_sso-0.1.5.tar.gz

---
+++ packages/django-okta-sso/django_okta_sso-0.1.5.tar.gz/django_okta_sso-0.1.5/django_okta_sso/__init__.py
@@ -0,0 +1,5 @@
+"""
+Okta SSO with Django Permissions 
+"""
+__version__ = "0.1.5"
+__author__ = 'Andy Gannaway'
---
+++ packages/django-okta-sso/django_okta_sso-0.1.5.tar.gz/django_okta_sso-0.1.5/django_okta_sso/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OktaOauth2Config(AppConfig):
+    name = "django_okta_sso"
