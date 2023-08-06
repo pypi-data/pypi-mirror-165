78e6baa4d2f60ff9924f8f3451a71799a03b3be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:49 +0000

    Add package dask-checkpoint/0.2/dask_checkpoint-0.2-py2.py3-none-any.whl

---
+++ packages/dask-checkpoint/dask_checkpoint-0.2-py2.py3-none-any.whl/dask_checkpoint/hasher.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+
+try:
+    from typing import TypeAlias
+except ImportError:
+    from typing_extensions import TypeAlias
+
+from dask.base import tokenize as dask_tokenize
+
+FunctionHasher: TypeAlias = Callable[[Callable], str]
+ArgumentHasher: TypeAlias = Callable[[dict], str]
+
+
+def function_name(func):
+    """Function name with a trailing slash."""
+    return f"{func.__name__}/"
+
+
+def tokenize(kwargs):
+    return dask_tokenize(**kwargs)
+
+
+def exclude(*names: str, hasher: ArgumentHasher = tokenize):
+    def exclude_hasher(kwargs):
+        for name in names:
+            del kwargs[name]
+        return hasher(kwargs)
+
+    return exclude_hasher
