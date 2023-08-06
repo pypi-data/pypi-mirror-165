b5a00aefd13ab5926e3c3251c3e8b9402cf690b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:41 +0000

    Add package idasen-controller/2.0.2rc1/idasen-controller-2.0.2rc1.tar.gz

---
+++ packages/idasen-controller/idasen-controller-2.0.2rc1.tar.gz/idasen-controller-2.0.2rc1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['idasen_controller']
+
+package_data = \
+{'': ['*'], 'idasen_controller': ['example/*']}
+
+install_requires = \
+['PyYAML>=5.4.1,<=6.0.0',
+ 'aiohttp==3.8.1',
+ 'appdirs==1.4.4',
+ 'bleak>=0.10.0,<=0.16.0']
+
+entry_points = \
+{'console_scripts': ['idasen-controller = idasen_controller.main:init']}
+
+setup_kwargs = {
+    'name': 'idasen-controller',
+    'version': '2.0.2rc1',
+    'description': 'Command line tool for controlling the Ikea Idasen (Linak) standing desk',
+    'long_description': None,
+    'author': 'Rhys Tyers',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
