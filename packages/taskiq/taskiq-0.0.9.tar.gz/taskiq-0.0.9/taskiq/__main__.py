e6d50799300a2129e91101a31a4f78581e8cfbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:46 +0000

    Add package taskiq/0.0.9/taskiq-0.0.9.tar.gz

---
+++ packages/taskiq/taskiq-0.0.9.tar.gz/taskiq-0.0.9/taskiq/__main__.py
@@ -0,0 +1,12 @@
+from taskiq.cli.args import TaskiqArgs
+from taskiq.cli.worker import run_worker
+
+
+def main() -> None:
+    """Main entrypoint for CLI."""
+    args = TaskiqArgs.from_cli()
+    run_worker(args)
+
+
+if __name__ == "__main__":
+    main()
