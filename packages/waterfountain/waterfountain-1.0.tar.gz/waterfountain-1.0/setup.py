cc815a390042f5ed50951fe4c1d3f70fa0588dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:39 +0000

    Add package waterfountain/1.0/waterfountain-1.0.tar.gz

---
+++ packages/waterfountain/waterfountain-1.0.tar.gz/waterfountain-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="waterfountain",
+    version="1.0",
+    description="A useful library for all your waterfountain needs",
+    packages=find_packages(),
+    url="https://github.com/grihithmanchanda/waterfountain",
+    license="MIT",
+    install_requires=["requests"]
+)
