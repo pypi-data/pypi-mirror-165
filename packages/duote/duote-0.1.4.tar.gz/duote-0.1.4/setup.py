34395c1f6df1764a1296205bc5a34c612577f339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:41 +0000

    Add package duote/0.1.4/duote-0.1.4.tar.gz

---
+++ packages/duote/duote-0.1.4.tar.gz/duote-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+
+setup(
+    name='duote',  # 对外模块的名字
+    version='0.1.4',  # 版本号
+    description='测试版本,基本功能',  # 描述
+    author='JinxAndLux',  # 作者
+    author_email='yby1234@hotmail.com',
+    py_modules=['duote.crawler.article','duote.crawler.question','duote.web.func'],  # 要发布的模块
+
+)
