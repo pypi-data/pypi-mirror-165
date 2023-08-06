f5b54652a04ae003d3bf0538bc192f3eec4f4ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:06 +0000

    Add package streamlit-cytoscapejs/0.0.2/streamlit_cytoscapejs-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-cytoscapejs/streamlit_cytoscapejs-0.0.2-py3-none-any.whl/streamlit_cytoscapejs/frontend/build/precache-manifest.0f9784b7513a9f54198051f6ae173c37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9967f95aa617193fd2e5416d00b5b750",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a62cd4f92c2f694c13d7",
+    "url": "./static/js/2.ab48aa38.chunk.js"
+  },
+  {
+    "revision": "18588af34a2cc04d7aabb9379717638c",
+    "url": "./static/js/2.ab48aa38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a872a85ff95a17c398d",
+    "url": "./static/js/main.35eabfb4.chunk.js"
+  },
+  {
+    "revision": "0f5420640edf03cabd03",
+    "url": "./static/js/runtime-main.64c5e2cb.js"
+  }
+]);
