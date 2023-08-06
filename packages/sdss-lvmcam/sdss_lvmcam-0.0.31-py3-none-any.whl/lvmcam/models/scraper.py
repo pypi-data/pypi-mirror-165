729cf81fc3051cd7086dcea20fcba0dde03a61cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:33 +0000

    Add package sdss-lvmcam/0.0.31/sdss_lvmcam-0.0.31-py3-none-any.whl

---
+++ packages/sdss-lvmcam/sdss_lvmcam-0.0.31-py3-none-any.whl/lvmcam/models/scraper.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+# @Author: Florian Briegel (briegel@mpia.de)
+# @Date: 2021-08-18
+# @Filename: models/scraper.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import abc
+from math import nan
+
+from datetime import datetime
+
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+from basecam.exposure import Exposure
+from basecam.models import (
+    Card,
+    CardGroup,
+    Extension,
+    FITSModel,
+    HeaderModel,
+    MacroCard,
+    WCSCards,
+)
+from clu.legacy.types.pvt import PVT
+from sdsstools.time import get_sjd
+from sdsstools import get_logger
+
+
+
+class ScraperDataStore(object):
+    def __init__(self, config={}):
+        self.actor_key_maps = config
+        self.data = {}
+
+    def copy(self):
+        o = type(self).__new__(self.__class__)
+        o.actor_key_maps = self.actor_key_maps.copy()
+        o.data = self.data.copy()
+        return o
+
+    def __repr__(self):
+        return self.data.__repr__()
+
+    def keys(self):
+         return self.data.keys()
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __setitem__(self, key, value):
+        return self.set(key, value)
+
+    def actors(self):
+        return list(self.actor_key_maps.keys()) if self.actor_key_maps else []
+
+    def set(self, key, val, timestamp=datetime.utcnow()):
+        self.data[key] = (val, timestamp)
+        
+    def get(self, key, default=None):
+        return self.data.get(key, (default, None))[0]
+        
+    def update(self, data:dict, timestamp=datetime.utcnow()):
+        self.data.update({k:(v, timestamp) for k, v in data.items()})
+
+    def update_with_actor_key_maps(self, actor, data:dict, timestamp=datetime.utcnow()):
+        akm = self.actor_key_maps.get(actor, None)
+        self.data.update({akm[k]:(v, timestamp) for k, v in data.items() if k in akm.keys()})
+
+    def items(self):
+        return self.data.items()
+
+    def items(self):
+        return self.data.items()
+
+
+
+class ScraperParamCards(MacroCard):
+    def macro(self, exposure, context={}):
+        from sdsstools.logger import get_logger
+        logger = get_logger("ScraperParamCards")
+#        logger.warning(f"########### {exposure.scraper_store}")
+
+        return [
+            ('RA', exposure.scraper_store.get('ra_h', 0.0)*15, '[deg] Right Ascension of the observation'),
+            ('DEC', exposure.scraper_store.get('dec_d', 90.0), '[deg] Declination of the observation'),
+            ('FIELDROT', exposure.scraper_store.get('field_angle_d', -999.9), '[deg] Field angle from PW'),
+            ('KMIRDROT', exposure.scraper_store.get('km_d', -999.9), '[deg] Rotation angle kmirror'),
+            ('FOCUSUM', exposure.scraper_store.get('foc_um', -999.9), '[um] Focus stage position'),
+        ]
+
