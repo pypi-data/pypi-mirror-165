ebc14227c76abe50819e9a6e04064eae16daa7bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:06 +0000

    Add package SwifTCR/0.4.7/SwifTCR-0.4.7.tar.gz

---
+++ packages/SwifTCR/SwifTCR-0.4.7.tar.gz/SwifTCR-0.4.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.4.7' 
+DESCRIPTION = 'Cluster sequences by distance one.'
+LONG_DESCRIPTION = 'Cluster sequences based on edit distance (hamming/levenshtein) equal to one.'
+REQUIREMENTS = ["findspark", "pyspark", "scipy", "numpy", "pandas"],
+
+setup(
+        name="SwifTCR", 
+        version=VERSION,
+        author="ido",
+        author_email="ido.hasson.5@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=REQUIREMENTS,
+        keywords=['cluster', 'TCR', 'edit', 'distance'],
+)
