1181ac09010c4e920c9eca5aab3956bd6982170c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:22 +0000

    Add package python-syncthing-client/0.1.1/python-syncthing-client-0.1.1.tar.gz

---
+++ packages/python-syncthing-client/python-syncthing-client-0.1.1.tar.gz/python-syncthing-client-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['syncthing']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil>=2.8.2,<3.0.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'python-syncthing-client',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'luke lombardi',
+    'author_email': 'luke@slai.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
