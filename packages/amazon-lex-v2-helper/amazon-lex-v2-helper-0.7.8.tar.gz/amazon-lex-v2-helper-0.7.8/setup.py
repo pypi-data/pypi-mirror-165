530ff16eefd2149bb5feb541809e342387f2bc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:07 +0000

    Add package amazon-lex-v2-helper/0.7.8/amazon-lex-v2-helper-0.7.8.tar.gz

---
+++ packages/amazon-lex-v2-helper/amazon-lex-v2-helper-0.7.8.tar.gz/amazon-lex-v2-helper-0.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="amazon-lex-v2-helper",
+    version="0.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="amazon-lex-v2-helper of amazon-lex-v2-helper of amazon-lex-v2-helper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
