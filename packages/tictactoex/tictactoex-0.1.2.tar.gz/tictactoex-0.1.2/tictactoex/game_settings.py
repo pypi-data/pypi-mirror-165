b69500dd0ff62b52ce871aa90fac29d4bf9ded7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:41 +0000

    Add package tictactoex/0.1.2/tictactoex-0.1.2.tar.gz

---
+++ packages/tictactoex/tictactoex-0.1.2.tar.gz/tictactoex-0.1.2/tictactoex/game_settings.py
@@ -0,0 +1,7 @@
+P1_MARKER = 'X'
+P2_MARKER = 'O'
+P1_WON = "Player 1 is the winner."
+P2_WON = "Player 2 is the winner."
+DRAW_GAME = "It's a tie"
+OCCUPIED_POSITION = "Can't place marker. Please choose an empty position."
+MARK_WAS_PLACED = "Mark_was_placed"
