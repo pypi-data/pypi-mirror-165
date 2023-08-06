6c738f3ca5617d9f61b5d596f16d66f58838b521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:04 +0000

    Add package aws-lambda-typing/2.15.0/aws_lambda_typing-2.15.0-py3-none-any.whl

---
+++ packages/aws-lambda-typing/aws_lambda_typing-2.15.0-py3-none-any.whl/aws_lambda_typing/responses/__init__.py
@@ -0,0 +1,15 @@
+from .alb import ALBResponse as ALBResponse
+from .api_gateway_authorizer import APIGatewayAuthorizerResponse
+from .api_gateway_proxy import (
+    APIGatewayProxyResponseV1 as APIGatewayProxyResponseV1,
+)
+from .api_gateway_proxy import (
+    APIGatewayProxyResponseV2 as APIGatewayProxyResponseV2,
+)
+from .iot import (
+    IoTPreProvisioningHookResponse as IoTPreProvisioningHookResponse,
+)
+from .kinesis_firehose import (
+    KinesisFirehoseTransformationResponse as KinesisFirehoseTransformationResponse,
+)
+from .s3_batch import S3BatchResponse as S3BatchResponse
