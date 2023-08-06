3807486eac8f836d951095b5064a756c0866c3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:30 +0000

    Add package testing-tools/1.0.3/testing_tools-1.0.3-py3-none-any.whl

---
+++ packages/testing-tools/testing_tools-1.0.3-py3-none-any.whl/testing_tools/common_path.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+SRC_PATH = Path(__file__).parent.parent
+ROOT_DIR = SRC_PATH.parent
+TESTS_DATA_PATH = Path(ROOT_DIR, "tests", "testing_tools")
+NOTEBOOK_PATH = Path(ROOT_DIR, "notebook")
