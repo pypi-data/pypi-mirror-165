828e27250db8c5479f0637110626aed025721bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:16 +0000

    Add package liteapi/0.2.1/liteapi-0.2.1.tar.gz

---
+++ packages/liteapi/liteapi-0.2.1.tar.gz/liteapi-0.2.1/src/liteapi/BaseAPIRequest.py
@@ -0,0 +1,41 @@
+class BaseAPIRequest:
+    __definition = None
+    __request = None
+    __response = None
+    __methods = None
+    __methods_keys = None
+    __uriVars = None
+
+    @property
+    def definition(self):
+        return self.__definition
+
+    @property
+    def request(self):
+        return self.__request
+    
+    @property
+    def response(self):
+        return self.__response
+    
+    @property
+    def responseHeader(self):
+        header = ""
+        for key in self.__response:
+            header = '{}{}: {}\r\n'.format(header, key, self.__response[key])
+        return header
+
+    @property
+    def methods(self):
+        return self.__methods
+    
+    @property
+    def methods_keys(self):
+        return self.__methods_keys
+
+    @property
+    def vars(self):
+        return self.__uriVars
+    
+    def __getitem__(self, k):
+        return self.__request[k]
