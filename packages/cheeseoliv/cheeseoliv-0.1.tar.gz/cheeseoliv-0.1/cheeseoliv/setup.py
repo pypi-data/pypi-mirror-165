399131f7b11045c6e8a7191306b09dd037dd0543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:53 +0000

    Add package cheeseoliv/0.1/cheeseoliv-0.1.tar.gz

---
+++ packages/cheeseoliv/cheeseoliv-0.1.tar.gz/cheeseoliv-0.1/cheeseoliv/setup.py
@@ -0,0 +1,21 @@
+﻿from distutils.core import setup
+setup(
+  name = 'cheeseoliv',         
+  packages = ['cheeseoliv'],   
+  version = '0.1',      
+  license='LICENSE.txt',        
+  description = '',   
+  author = 'oliverstech',                   
+  author_email = 'imrolii9876@gmail.com',      
+  url = 'http://oliverstech.tk',   
+  install_requires=[''],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
