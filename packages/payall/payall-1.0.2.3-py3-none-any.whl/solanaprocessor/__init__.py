2b3c29e2260dbdaf7ab1a715a4d8cc0893fb95c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package payall/1.0.2.3/payall-1.0.2.3-py3-none-any.whl

---
+++ packages/payall/payall-1.0.2.3-py3-none-any.whl/solanaprocessor/__init__.py
@@ -0,0 +1,13 @@
+import os, subprocess
+
+def get_balance(public_key):
+    output = subprocess.getoutput(f"solana balance {public_key}")
+    return output
+
+def create_wallet(from_mob):
+    output = subprocess.getoutput(f"solana-keygen new --no-passphrase -o /root/solanakey/{from_mob}.json")
+    return output
+
+def transfer(from_mob,to_public_key,amount):
+    output=subprocess.getoutput(f"solana transfer --from /root/solanakey/{from_mob}.json {to_public_key} 1 --allow-unfunded-recipient --url https://api.devnet.solana.com --fee-payer /root/solanakey/{from_mob}.json")
+    return output
