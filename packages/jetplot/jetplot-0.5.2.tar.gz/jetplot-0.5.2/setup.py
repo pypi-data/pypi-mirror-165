1055f53151a98d912290fef0ccca678e0e9d64a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:02 +0000

    Add package jetplot/0.5.2/jetplot-0.5.2.tar.gz

---
+++ packages/jetplot/jetplot-0.5.2.tar.gz/jetplot-0.5.2/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import find_packages, setup
+
+global __version__
+__version__ = None
+
+with open('jetplot/version.py') as f:
+  exec(f.read(), globals())
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.md')).read()
+except IOError:
+    README = ''
+
+setup(name='jetplot',
+      version=__version__,
+      author='Niru Maheswaranathan',
+      author_email='niru@hey.com',
+      url='https://github.com/nirum/jetplot.git',
+      install_requires=['numpy', 'scipy', 'matplotlib'],
+      python_requires='>=3.7',
+      long_description=README,
+      packages=find_packages(),
+      license='LICENSE.md'
+      )
