1e419ed1238e160fa623adead1c7f7c39c51a0ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:45 +0000

    Add package olivcheese/0.1/olivcheese-0.1.tar.gz

---
+++ packages/olivcheese/olivcheese-0.1.tar.gz/olivcheese-0.1/olivcheese/setup.py
@@ -0,0 +1,21 @@
+﻿from distutils.core import setup
+setup(
+  name = 'olivcheese',         
+  packages = ['olivcheese'],   
+  version = '0.1',      
+  license='LICENSE.txt',        
+  description = '',   
+  author = 'oliverstech',                   
+  author_email = 'imrolii9876@gmail.com',      
+  url = 'http://google.com',   
+  install_requires=[''],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
