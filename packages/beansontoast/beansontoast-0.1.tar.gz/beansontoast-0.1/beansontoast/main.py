d3979dceb29111d2af8adfc0525cd90e60a50618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:33 +0000

    Add package beansontoast/0.1/beansontoast-0.1.tar.gz

---
+++ packages/beansontoast/beansontoast-0.1.tar.gz/beansontoast-0.1/beansontoast/main.py
@@ -0,0 +1,13 @@
+# \n = new line
+
+# Weird windows way of working I just don't get
+
+# so this will work fine right
+print("Something on one line" + "\n" + "Something on the other")
+# and so will this
+print("Something on one line\nSomething on the other")
+# but for some weird reason Windows doesn't like directly plonking \n on ANY coding language
+
+# example:
+# this won't work
+print("Something on one line" + \n + "Something on the other")
---
+++ packages/beansontoast/beansontoast-0.1.tar.gz/beansontoast-0.1/setup.py
@@ -0,0 +1,20 @@
+﻿from distutils.core import setup
+setup(
+  name = 'beansontoast',         
+  packages = ['beansontoast'],   
+  version = '0.1',        
+  description = 'This is a demo project with the OTC PyPI Uploader Tool!',   
+  author = 'johnbeans',                   
+  author_email = 'john.beans@gmail.com',      
+  url = 'https://www.google.com',   
+  install_requires=[''],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
