140242d33af41f83751b00276307e48f5831d2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:26 +0000

    Add package jdv-funcutils/1.0.0/jdv_funcutils-1.0.0-py3-none-any.whl

---
+++ packages/jdv-funcutils/jdv_funcutils-1.0.0-py3-none-any.whl/jdv_funcutils/signature/typedefs.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2022 Justin Vrana. All Rights Reserved.
+#  You may use, distribute, and modify this code under the terms of the MIT license.
+from inspect import Parameter
+from inspect import Signature
+from typing import Callable
+from typing import List
+from typing import Union
+
+SignatureLike = Union[Callable, Signature, List[Parameter]]
