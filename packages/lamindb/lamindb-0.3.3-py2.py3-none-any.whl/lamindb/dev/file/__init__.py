b0d3a12c261b0f27477367b27fa20ba7ef306a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:20 +0000

    Add package lamindb/0.3.3/lamindb-0.3.3-py2.py3-none-any.whl

---
+++ packages/lamindb/lamindb-0.3.3-py2.py3-none-any.whl/lamindb/dev/file/__init__.py
@@ -0,0 +1,21 @@
+"""Storage → object.
+
+File → memory:
+
+.. autosummary::
+   :toctree: .
+
+   h5ad_to_anndata
+
+Store files:
+
+.. autosummary::
+   :toctree: .
+
+   store_file
+   store_png
+"""
+
+from ._file import load_to_memory, store_file
+from ._h5ad import h5ad_to_anndata
+from ._images import store_png
