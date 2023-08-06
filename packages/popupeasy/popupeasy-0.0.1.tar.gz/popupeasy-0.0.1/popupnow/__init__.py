6a7b9248d8bb5d72409f530d81aff3c1611b710a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:08 +0000

    Add package popupeasy/0.0.1/popupeasy-0.0.1.tar.gz

---
+++ packages/popupeasy/popupeasy-0.0.1.tar.gz/popupeasy-0.0.1/popupnow/__init__.py
@@ -0,0 +1,18 @@
+from doctest import master
+import tkinter
+from tkinter import *
+import os
+import time
+
+class popupeasy:
+    def newpopup(popupheight=False, popupwidth=False):
+        master = Tk()
+        canvas = Canvas(height=popupheight, width=popupwidth)
+        canvas.pack()
+        master.mainloop()
+    def newframe(popuprelx=False, popuprely=False, popuprelheight=False, popuprelwidth=False, bgcolor=False):
+        global master
+        frame = Frame(master, bg=bgcolor)
+        frame.place(relx=popuprelx, rely=popuprely, relheight=popuprelheight, relwidth=popuprelwidth)
+
+popupeasy.newpopup(popupheight=450, popupwidth=750)
