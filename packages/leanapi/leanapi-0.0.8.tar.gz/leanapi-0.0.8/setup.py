f78e82169f7f3c95c97b16a12804bb03a0cc0ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:43 +0000

    Add package leanapi/0.0.8/leanapi-0.0.8.tar.gz

---
+++ packages/leanapi/leanapi-0.0.8.tar.gz/leanapi-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "leanapi",
+    version = "0.0.8",
+    author = "developerkitchen python team",
+    author_email = "topluluk@developerkitchen.dev",
+    description = ("Lean Api is a class layer for FastApi"),
+    license = "MIT",
+    keywords = "api, leanapi, fastapi",
+    url = "http://github.com/developerkitchentr/leanapi",
+    packages=['leanapi'],
+    long_description_content_type = "text/markdown",
+    long_description= read('README.md'),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'fastapi>=0.63.0',
+        'uvicorn>=0.13.3'
+        'python-dotenv',
+        'pydantic>=1.7.2'
+    ]
+)
