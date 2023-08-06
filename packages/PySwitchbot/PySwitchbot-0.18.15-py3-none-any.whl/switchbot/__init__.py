38dc625e4be7707dd3ccb4a2d7101d6b20796f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:44 +0000

    Add package PySwitchbot/0.18.15/PySwitchbot-0.18.15-py3-none-any.whl

---
+++ packages/PySwitchbot/PySwitchbot-0.18.15-py3-none-any.whl/switchbot/__init__.py
@@ -0,0 +1,26 @@
+"""Library to handle connection with Switchbot."""
+from __future__ import annotations
+
+from .adv_parser import SwitchbotSupportedType, parse_advertisement_data
+from .const import SwitchbotModel
+from .devices.bot import Switchbot
+from .devices.bulb import ColorMode, SwitchbotBulb
+from .devices.curtain import SwitchbotCurtain
+from .devices.device import SwitchbotDevice
+from .devices.plug import SwitchbotPlugMini
+from .discovery import GetSwitchbotDevices
+from .models import SwitchBotAdvertisement
+
+__all__ = [
+    "parse_advertisement_data",
+    "GetSwitchbotDevices",
+    "SwitchBotAdvertisement",
+    "ColorMode",
+    "SwitchbotBulb",
+    "SwitchbotDevice",
+    "SwitchbotCurtain",
+    "Switchbot",
+    "SwitchbotPlugMini",
+    "SwitchbotSupportedType",
+    "SwitchbotModel",
+]
