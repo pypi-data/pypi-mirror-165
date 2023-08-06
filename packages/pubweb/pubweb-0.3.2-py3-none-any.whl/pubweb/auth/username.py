cd3da6f3bd7219e4f0ab720c18d01253f48972a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:29 +0000

    Add package pubweb/0.3.2/pubweb-0.3.2-py3-none-any.whl

---
+++ packages/pubweb/pubweb-0.3.2-py3-none-any.whl/pubweb/auth/username.py
@@ -0,0 +1,41 @@
+import boto3
+from pycognito import AWSSRP
+from requests.auth import AuthBase
+
+from pubweb.auth.base import AuthInfo
+from pubweb.config import config
+
+
+class UsernameAndPasswordAuth(AuthInfo):
+    """
+    Uses your username & password to authenticate
+    Note: this does not work with federated identities (Fred Hutch login)
+    You must contact HDC to manually create an account
+    """
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def get_request_auth(self) -> AuthBase:
+        return self.RequestAuth(self._get_token()['AccessToken'])
+
+    def get_current_user(self) -> str:
+        return self.username
+
+    def _get_token(self):
+        cognito = boto3.client('cognito-idp', region_name=config.region)
+        aws = AWSSRP(username=self.username,
+                     password=self.password,
+                     pool_id=config.user_pool_id,
+                     client_id=config.app_id,
+                     client=cognito)
+        resp = aws.authenticate_user()
+        return resp['AuthenticationResult']
+
+    class RequestAuth(AuthBase):
+        def __init__(self, token):
+            self.token = token
+
+        def __call__(self, request):
+            request.headers['Authorization'] = self.token
+            return request
