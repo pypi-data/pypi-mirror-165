2672881ccd67dbde440a7b52af90d94972687e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:45 +0000

    Add package weco-deploy/5.7.2/weco-deploy-5.7.2.tar.gz

---
+++ packages/weco-deploy/weco-deploy-5.7.2.tar.gz/weco-deploy-5.7.2/src/deploy/exceptions.py
@@ -0,0 +1,14 @@
+class WecoDeployError(Exception):
+    pass
+
+
+class ConfigError(WecoDeployError):
+    pass
+
+
+class EcrError(WecoDeployError):
+    pass
+
+
+class NothingToReleaseError(Exception):
+    pass
