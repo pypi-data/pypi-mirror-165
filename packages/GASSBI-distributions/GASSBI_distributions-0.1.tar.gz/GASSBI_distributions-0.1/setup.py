fd1fbe7d88f91cc525cbea323b501a5b519d13b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package GASSBI-distributions/0.1/GASSBI_distributions-0.1.tar.gz

---
+++ packages/GASSBI-distributions/GASSBI_distributions-0.1.tar.gz/GASSBI_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='GASSBI_distributions',
+      version='0.1',
+      description='Gaussian distributions and binomial distribution',
+      packages=['distributions'],
+      zip_safe=False)
