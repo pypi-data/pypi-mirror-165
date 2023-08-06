d8b1fe4ebedf68352e8f5453d1d3e5f933d1d021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:08 +0000

    Add package hurpetools/0.0.3/hurpetools-0.0.3.tar.gz

---
+++ packages/hurpetools/hurpetools-0.0.3.tar.gz/hurpetools-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hurpetools',
+      version='0.0.3',
+      description='close ssl verify and ignore warning',
+      url='https://github.com/BigDataFounder/string2date',
+      author='hurpe',
+      author_email='hurpe.ma@gmail.com',
+      license='MIT',
+      packages=['hurpetools'])
