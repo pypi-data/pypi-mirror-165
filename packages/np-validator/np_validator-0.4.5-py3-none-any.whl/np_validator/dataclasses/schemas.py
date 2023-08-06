2e206d8aa7b1e7cbfa3bd2d347a15904991f94f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:48 +0000

    Add package np-validator/0.4.5/np_validator-0.4.5-py3-none-any.whl

---
+++ packages/np-validator/np_validator-0.4.5-py3-none-any.whl/np_validator/dataclasses/schemas.py
@@ -0,0 +1,44 @@
+from marshmallow import Schema, fields, post_load, validate
+
+from .. import processors, validators
+from .utils import list_functions
+
+
+class ProcessorSchema(Schema):
+
+    """Serialized builtin callable"""
+
+    name = fields.String(validate=validate.OneOf(list_functions(processors)))
+    args = fields.Dict(load_default={})
+
+
+class ValidatorSchema(Schema):
+
+    """Serialized builtin callable"""
+
+    name = fields.String(validate=validate.OneOf(list_functions(validators)))
+    args = fields.Dict(load_default={})
+
+
+class ValidationStepSchema(Schema):
+
+    """Serialized validation step"""
+
+    path_suffix = fields.String()
+    processor = fields.Nested(ProcessorSchema, many=False)
+    validators = fields.Nested(ValidatorSchema, many=True)
+
+
+class ValidationResultSchema(Schema):
+
+    """Serialized validation result"""
+
+    filepath = fields.String()
+    results = fields.List(
+        fields.Tuple(
+            (
+                fields.Raw(),
+                fields.Bool(),
+            )
+        )
+    )
