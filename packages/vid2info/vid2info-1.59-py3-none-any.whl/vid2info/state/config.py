dd348f1a96969de878c7e7b52d0d467ce4f1f025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:10 +0000

    Add package vid2info/1.59/vid2info-1.59-py3-none-any.whl

---
+++ packages/vid2info/vid2info-1.59-py3-none-any.whl/vid2info/state/config.py
@@ -0,0 +1,11 @@
+BUFFER_SIZE = 100
+ELEMENT_BUFFER_SIZE = 10
+
+# HISTORY KEYS
+
+INITIAL_CLASS_NAME = "initial_class_name"
+OUT_CLASS_NAME = "out_class_name"
+INITIAL_TIMESTAMP = "initial_timestamp"
+OUT_TIMESTAMP = "out_timestamp"
+FINITE_STATE_MACHINE = "finite_state_machine"
+TRACK_LENGTH = "track_length"
