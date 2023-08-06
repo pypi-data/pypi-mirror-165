079ad0f9bcd380d9c35cbfc86e2bd061543abc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:00 +0000

    Add package deeplabel-sdk/0.3.9/deeplabel-sdk-0.3.9.tar.gz

---
+++ packages/deeplabel-sdk/deeplabel-sdk-0.3.9.tar.gz/deeplabel-sdk-0.3.9/deeplabel/label/labels.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from typing import Any, Dict, List, Optional
+from pydantic import validator
+
+from pydantic import Field
+from deeplabel.basemodel import DeeplabelBase
+import deeplabel.client
+
+
+class DetectionLabelType(Enum):
+    OBJECT = "OBJECT"
+    ACTION = "ACTION"
+
+
+class DetectionLabelCategory(Enum):
+    DETECTION = "DETECTION"
+    CLASSIFICATION = "CLASSIFICATION"
+
+class DetectionLabel(DeeplabelBase):
+    """Detection Label"""
+    color: str  # hashvalue eg. \#efbg17
+    name: str
+    type: DetectionLabelType
+    category: DetectionLabelCategory
+    id:Optional[str]
+    label_id: Optional[str]
+    is_deleted:bool = False
+    client:Any = Field(None, exclude=True)
+
+    @validator('label_id', always=True)
+    def validate_label_id(cls, v:Optional[str], values)->str: #type: ignore
+        if v is None:
+            assert 'id' in values, f"DetectionLabel should either have label_id or id key. {v} ; {values}"
+            return values['id'] #type: ignore
+        return v
+
+    @classmethod
+    def from_search_params(
+        cls, params: Dict[str, Any], client: "deeplabel.client.BaseClient"
+    ) -> List["DetectionLabel"]:
+        resp = client.get("/labels", params=params)
+        labels = resp.json()["data"]["labels"]
+        return [cls(**label, client=client) for label in labels]
