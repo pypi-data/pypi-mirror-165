a4a88bb55e8c27285dcbefe9ed3821b865224b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:49 +0000

    Add package aspeak/3.0.0.dev1/aspeak-3.0.0.dev1-py3-none-any.whl

---
+++ packages/aspeak/aspeak-3.0.0.dev1-py3-none-any.whl/aspeak/__init__.py
@@ -0,0 +1,7 @@
+"""
+aspeak is a CLI tool as well as a Python library that
+uses trial auth token of Azure Cognitive Services to do speech synthesis for you.
+"""
+
+from .api import *
+from .errors import AspeakError, TokenRetrievalError
