b491200cfd3893fd6f88e66940d2a48595021c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:28 +0000

    Add package Winstree/0.0.1a1/Winstree-0.0.1a1.tar.gz

---
+++ packages/Winstree/Winstree-0.0.1a1.tar.gz/Winstree-0.0.1a1/src/Winstreer/__init__.py
@@ -0,0 +1,11 @@
+from Winkit import KApplication, KWindow
+
+
+def empty_demo():
+    App = KApplication()
+    Window = KWindow()
+    App.Run(Window)
+
+
+if __name__ == '__main__':
+    empty_demo()
