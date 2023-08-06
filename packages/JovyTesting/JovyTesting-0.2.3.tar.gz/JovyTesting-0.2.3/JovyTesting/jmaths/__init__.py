0b4f3da38e88d1a1e13219bae42be1ae2d27690c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:30 +0000

    Add package JovyTesting/0.2.3/JovyTesting-0.2.3.tar.gz

---
+++ packages/JovyTesting/JovyTesting-0.2.3.tar.gz/JovyTesting-0.2.3/JovyTesting/jmaths/__init__.py
@@ -0,0 +1,61 @@
+
+
+import math
+import numpy as np
+
+__all__ = ["add", "sub", "mult", "div", "subtract", "divide", "multiply"]
+
+#Defining Functions
+def add(*args):
+    """
+    Add numbers together
+    """
+    args2 = list(np.array(args).flat)
+    all_number_list = list(map(float,args2))
+    sum_result = sum(all_number_list)
+    remainders = sum_result%int(sum_result)
+    return int(sum_result) if remainders==0 else sum_result
+
+
+def sub(*args):
+    """
+    Subtract numbers from each other
+    """
+    args = np.array(args).flat
+    all_number_list = list(map(float,args))
+    diff_result = all_number_list[0]
+    for number in all_number_list[1::]:
+        diff_result -= number
+    remainders = diff_result%int(diff_result)
+    return int(diff_result) if remainders==0 else diff_result
+
+
+def mult(*args):
+    """
+    Multiply numbers with each other
+    """
+    args = np.array(args).flat
+    all_number_list = list(map(float,args))
+    mult_result = all_number_list[0]
+    for number in all_number_list[1::]:
+        mult_result *= number
+    remainders = mult_result%int(mult_result)
+    return int(mult_result) if remainders==0 else mult_result
+
+def div(*args):
+    """
+    Divide numbers with each other
+    """
+    args = np.array(args).flat
+    all_number_list = list(map(float,args))
+    div_result = all_number_list[0]
+    for number in all_number_list[1::]:
+        div_result /= number
+    remainders = div_result%int(div_result)
+    return int(div_result) if remainders==0 else div_result
+    
+
+#Cloning Functions
+subtract = sub
+multiply = mult
+divide = div
