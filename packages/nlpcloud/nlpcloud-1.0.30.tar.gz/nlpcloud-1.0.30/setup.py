a24bee994e0b6a0c548df59c88b93dd03ccc7c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:47 +0000

    Add package nlpcloud/1.0.30/nlpcloud-1.0.30.tar.gz

---
+++ packages/nlpcloud/nlpcloud-1.0.30.tar.gz/nlpcloud-1.0.30/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='nlpcloud',
+    version='1.0.30',
+    description='Python client for the NLP Cloud API',
+    long_description="NLP Cloud serves high performance pre-trained or custom models for NER, sentiment-analysis, classification, summarization, paraphrasing, grammar and spelling correction, keywords and keyphrases extraction, chatbot, product description and ad generation, intent classification, text generation, image generation, blog post generation, code generation, question answering, machine translation, language detection, semantic similarity, tokenization, POS tagging, embeddings, and dependency parsing. It is ready for production, served through a REST API.\n\nThis is the Python client for the API.\n\nMore details here: https://nlpcloud.io\n\nDocumentation: https://docs.nlpcloud.io\n\nGithub: https://github.com/nlpcloud/nlpcloud-python",
+    packages=['nlpcloud'],
+    author='Julien Salinas',
+    author_email='all@juliensalinas.com',
+    license='MIT',
+    keywords=['api', 'NLP', 'spacy', 'Hugging Face', 'deep learning', 'machine learning',
+              'data science', 'nlpcloud'],
+    url='https://github.com/nlpcloud/nlpcloud-python',
+    install_requires=['requests']
+)
