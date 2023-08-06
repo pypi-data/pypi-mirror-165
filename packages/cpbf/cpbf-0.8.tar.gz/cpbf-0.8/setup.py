6d45e19c272aa3a9a1cc5e730e138c5ef5b21be9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:15 +0000

    Add package cpbf/0.8/cpbf-0.8.tar.gz

---
+++ packages/cpbf/cpbf-0.8.tar.gz/cpbf-0.8/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import setuptools
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+setup(
+    name='cpbf',
+    version='0.8',
+    license='MIT',
+    author="mahdi",
+    author_email='sellingavl@gmail.com.com',
+    url='',
+    keywords='example project',
+    packages = ['cpbf'],
+    setup_requires = [
+        'pytest-runner',
+        'pypandoc>=1.4',
+    ],
+    install_requires = [
+        'matplotlib>=2.0.0',  # This should be a dependency of `scikit-image`
+        'numpy>=1.11.1',
+        'scikit-image>=0.12.3',
+        'scipy>=0.18.1',
+    ],
+    tests_require = ['pytest'],
+    extras_require = {
+        'dev': [
+            'pandas>=0.19.2',
+            'pytest>=3.0.0',
+            'scikit-learn>=0.18.1',
+            'tox>=2.9.1',
+        ],
+       },
+)
