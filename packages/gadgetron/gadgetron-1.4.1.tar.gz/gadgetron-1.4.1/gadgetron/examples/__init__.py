3649c9c43ef19499dbb0dde00f64d7c1c34bf711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:57 +0000

    Add package gadgetron/1.4.1/gadgetron-1.4.1.tar.gz

---
+++ packages/gadgetron/gadgetron-1.4.1.tar.gz/gadgetron-1.4.1/gadgetron/examples/__init__.py
@@ -0,0 +1,10 @@
+
+from .recon_acquisitions import recon_acquisitions
+from .recon_buffers import recon_buffers
+from .pass_through import pass_through
+
+__all__ = [
+    recon_acquisitions,
+    recon_buffers,
+    pass_through
+]
---
+++ packages/gadgetron/gadgetron-1.4.1.tar.gz/gadgetron-1.4.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from pathlib import Path
+
+version = {}
+with open("gadgetron/version.py") as fp:
+    exec(fp.read(), version)
+# later on we use: version['__version__']
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='gadgetron',
+    version=version['version'],
+    packages=[
+        'gadgetron',
+        'gadgetron.util',
+        'gadgetron.external',
+        'gadgetron.examples',
+        'gadgetron.legacy',
+        'gadgetron.types'
+    ],
+    url='',
+    license='MIT',
+    author='Kristoffer Langeland Knudsen',
+    author_email='kristofferlknudsen@gradientsoftware.net',
+    description='Python interface for Gadgetron',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'numpy>=1.22',
+        'ismrmrd>=1.12.5',
+        'pyFFTW>=0.11',
+        'multimethod>=1.0'
+    ]
+)
