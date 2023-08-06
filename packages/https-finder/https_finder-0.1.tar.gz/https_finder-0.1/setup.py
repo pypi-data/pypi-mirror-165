0e98a70f66a41be61acaa2aaff9900811395663f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:43 +0000

    Add package https-finder/0.1/https_finder-0.1.tar.gz

---
+++ packages/https-finder/https_finder-0.1.tar.gz/https_finder-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='https_finder',
+      version='0.1',
+      description='this script can search for https proxies and immediately checks them for validity',
+      packages=['https_finder'],
+      author_email='Prostoegorich2@gmail.com',
+      zip_safe=False)
