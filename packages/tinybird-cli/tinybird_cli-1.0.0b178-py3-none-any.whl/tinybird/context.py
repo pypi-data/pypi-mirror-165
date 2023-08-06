82127b929f5cd652d06a05d671a7e76d9c3431d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:59 +0000

    Add package tinybird-cli/1.0.0b178/tinybird_cli-1.0.0b178-py3-none-any.whl

---
+++ packages/tinybird-cli/tinybird_cli-1.0.0b178-py3-none-any.whl/tinybird/context.py
@@ -0,0 +1,8 @@
+import contextvars
+
+
+workspace_id = contextvars.ContextVar('workspace_id')
+workspace = contextvars.ContextVar('workspace')
+table_id = contextvars.ContextVar('table_id')
+hfi_frequency = contextvars.ContextVar('hfi_frequency')
+disable_template_security_validation = contextvars.ContextVar('disable_template_security_validation')
