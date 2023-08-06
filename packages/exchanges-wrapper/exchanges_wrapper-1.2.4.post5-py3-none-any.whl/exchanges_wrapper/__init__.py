31befc11bc93330fb796e1d628085a33fb97c02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:34 +0000

    Add package exchanges-wrapper/1.2.4.post5/exchanges_wrapper-1.2.4.post5-py3-none-any.whl

---
+++ packages/exchanges-wrapper/exchanges_wrapper-1.2.4.post5-py3-none-any.whl/exchanges_wrapper/__init__.py
@@ -0,0 +1,13 @@
+"""
+REST API and WebSocket asyncio wrapper with grpc powered multiplexer server for crypto exchanges
+ (Binance, FTX, Bitfinex,)
+Utilizes one connection for many accounts and trading pairs.
+For SPOT market only
+"""
+__authors__ = ["Th0rgal", "Jerry Fedorenko"]
+__license__ = "MIT"
+__maintainer__ = "Jerry Fedorenko"
+__contact__ = "https://github.com/DogsTailFarmer"
+__email__ = "jerry.fedorenko@yahoo.com"
+__credits__ = ["https://github.com/DanyaSWorlD"]
+__version__ = "1.2.4-5"
