c9cefd90294ba25687b3c861d78baef46abdcfdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:58 +0000

    Add package fb-tools/2.0.2/fb_tools-2.0.2-py3-none-any.whl

---
+++ packages/fb-tools/fb_tools-2.0.2-py3-none-any.whl/fb_tools/__init__.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+
+DDNS_CFG_BASENAME = 'ddns.ini'
+__version__ = '2.0.2'
+
+from .mailaddress import MailAddress, QualifiedMailAddress, MailAddressList      # noqa
+
+# vim: ts=4 et list
