e2db60186aaaa1e3836ab66b535f2509590ee444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:17 +0000

    Add package napari-turing/0.2.0/napari_turing-0.2.0-py3-none-any.whl

---
+++ packages/napari-turing/napari_turing-0.2.0-py3-none-any.whl/napari_turing/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.2.0"
+from ._widget import TuringViewer
+from ._TuringPattern import TuringPattern
+from ._model_list import *
+
+# __all__ = ("TuringViewer", "TuringPattern", "GrayScott", "Brusselator", "FitzHughNagumo")
