99e0e2f5c904d1b96ede50fb7ed6dc2855a1627b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:41 +0000

    Add package dvtm-sources/0.15.1/dvtm-sources-0.15.1.tar.gz

---
+++ packages/dvtm-sources/dvtm-sources-0.15.1.tar.gz/dvtm-sources-0.15.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='dvtm-sources',
+    version='0.15.1',
+    setup_requires='setupmeta',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    include_package_data=True,
+    license='MIT',
+)
