cef558352c9c01878c5252e71e7c6f725263de9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:06 +0000

    Add package pystddic/0.0.2/pystddic-0.0.2.tar.gz

---
+++ packages/pystddic/pystddic-0.0.2.tar.gz/pystddic-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(name='pystddic',
+		version='0.0.2',
+		description='Standard Dictionary Management(Korean Ver)',
+		author='oh seung',
+		author_email='seung.oh17@gmail.com',
+		url='https://github.com/oh-seung/pystddic',
+		python_requires='>=3.8',
+		include_package_data=True,
+		packages = setuptools.find_packages(),
+		install_requires= ['pandas', 
+				   'numpy',
+				   'string',
+				   'copy',
+				   'psutil',
+				   'ray',
+				   'pickle',
+				   'tqdm',
+				  ]
+    )
