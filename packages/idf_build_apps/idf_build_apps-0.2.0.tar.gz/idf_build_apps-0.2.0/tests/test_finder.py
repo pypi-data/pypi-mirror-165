865a300e747b9efe5ef315a50430882fee126ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:19 +0000

    Add package idf_build_apps/0.2.0/idf_build_apps-0.2.0.tar.gz

---
+++ packages/idf_build_apps/idf_build_apps-0.2.0.tar.gz/idf_build_apps-0.2.0/tests/test_finder.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+import inspect
+
+from idf_build_apps.constants import IDF_PATH
+from idf_build_apps.main import find_apps
+
+
+def test_finder(tmpdir):
+    test_dir = str(IDF_PATH / 'examples')
+    apps = find_apps(test_dir, 'esp32', recursive=True)
+    assert apps
+
+    yaml_file = str(tmpdir / 'test.yml')
+    with open(yaml_file, 'w') as fw:
+        fw.write(
+            inspect.cleandoc(
+                f'''
+            {test_dir}:
+                enable:
+                    - if: IDF_TARGET == "esp32s2"
+        '''
+            )
+        )
+
+    filtered_apps = find_apps(
+        test_dir, 'esp32', recursive=True, manifest_files=yaml_file
+    )
+    assert not filtered_apps
+    assert filtered_apps != apps
