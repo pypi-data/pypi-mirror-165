a3dd43188258492fe3598add5f52317fb93b6cd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:22 +0000

    Add package eea.banner/1.4/eea.banner-1.4.zip

---
+++ packages/eea.banner/eea.banner-1.4.zip/eea.banner-1.4/eea/banner/browser/controlpanel.py
@@ -0,0 +1,17 @@
+"""Control panel module"""
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+
+from eea.banner.interfaces import IBannerSettings
+
+
+class BannerRegistryEditForm(RegistryEditForm):
+    """Banner Registry Edit Form"""
+    schema = IBannerSettings
+    id = "banner"
+    label = "Banner Settings"
+
+
+class BannerControlPanelFormWrapper(ControlPanelFormWrapper):
+    """Banner Control Panel Form Wrapper"""
+    form = BannerRegistryEditForm
