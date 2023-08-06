0266f6116dce8e4fad01ed12118ccc2ba37d4124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:52 +0000

    Add package express-integrations-monday/1.0.3/express_integrations_monday-1.0.3-py3-none-any.whl

---
+++ packages/express-integrations-monday/express_integrations_monday-1.0.3-py3-none-any.whl/monday/resources/account.py
@@ -0,0 +1,23 @@
+from monday.resources.base import BaseResource
+from monday.query_joins import get_apps_monetization_supported_query, get_app_subscription_query, get_account_query, get_self_query
+
+
+class AccountResource(BaseResource):
+    def __init__(self, token):
+        super().__init__(token)
+
+    def get_apps_monetization_supported(self):
+        query = get_apps_monetization_supported_query()
+        return self.client.execute(query)
+
+    def get_app_subscription(self):
+        query = get_app_subscription_query()
+        return self.client.execute(query)
+
+    def get_account(self):
+        query = get_account_query()
+        return self.client.execute(query)
+
+    def get_self(self):
+        query = get_self_query()
+        return self.client.execute(query)
