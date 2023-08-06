4d927808fef3b66c0bac69ff382fc3bf51ce2a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:50 +0000

    Add package typed-graphql/0.23.0/typed_graphql-0.23.0-py3-none-any.whl

---
+++ packages/typed-graphql/typed_graphql-0.23.0-py3-none-any.whl/typed_graphql/__init__.py
@@ -0,0 +1,19 @@
+from .core import (
+    ReturnTypeMissing,
+    TypedGraphqlMiddlewareManager,
+    graphql_input_type,
+    graphql_type,
+    resolver,
+    resolverclass,
+    staticresolver,
+)
+
+__all__ = [
+    "ReturnTypeMissing",
+    "TypedGraphqlMiddlewareManager",
+    "graphql_input_type",
+    "graphql_type",
+    "resolver",
+    "resolverclass",
+    "staticresolver",
+]
