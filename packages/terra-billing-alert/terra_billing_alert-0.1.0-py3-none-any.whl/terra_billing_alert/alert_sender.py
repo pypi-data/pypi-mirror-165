159939feea5d63fbaa8be93549332f7240fd0ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:18 +0000

    Add package terra-billing-alert/0.1.0/terra_billing_alert-0.1.0-py3-none-any.whl

---
+++ packages/terra-billing-alert/terra_billing_alert-0.1.0-py3-none-any.whl/terra_billing_alert/alert_sender.py
@@ -0,0 +1,24 @@
+from slack import WebClient
+from slack.errors import SlackApiError
+
+
+class AlertSender:
+    def send_message(self, title, message):
+        print(title)
+        print(message)
+
+
+class SlackSender(AlertSender):
+    def __init__(self, channel, token):
+        self.client = WebClient(token)
+        self.channel = channel
+        self.token = token
+
+    def send_message(self, title, message):
+        try:
+            self.client.chat_postMessage(
+                channel=self.channel,
+                text='\n'.join([title, message])
+            )
+        except SlackApiError as e:
+            print(f'Failed to send slack message due to error {e.response["error"]}')
