60c609134e80317bf1d6c5a48b3931068381766b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package pytorch-tutorials/0.2.17/pytorch_tutorials-0.2.17.tar.gz

---
+++ packages/pytorch-tutorials/pytorch_tutorials-0.2.17.tar.gz/pytorch_tutorials-0.2.17/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch_tutorials',
+    version='0.2.17',    
+    description='A resource for learning about PyTorch and deep learning.',
+    long_description=
+    """
+    ## [Project Documentation](https://github.com/drewbyron/pytorch-tutorials/blob/main/README.md)\n\n
+    ## [Project YouTube Channel](https://www.youtube.com/channel/UCORZQS8pVWrPyY3-OpvNkcg/featured)\n\n
+    """,
+    long_description_content_type='text/markdown',
+    url='https://github.com/drewbyron/pytorch-tutorials',
+    author='William (Drew) Byron',
+    author_email='william.andrew.byron@gmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires= [
+        "torch >= 1.12.1",
+        "torchvision >= 0.13.1",
+        "pytorch-lightning >= 1.6.4",
+        "torchmetrics >= 0.9.1",
+        "matplotlib>=3.1.3",
+        "numpy>= 1.21.6",
+        "opencv-python"
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent"
+    ],
+)
