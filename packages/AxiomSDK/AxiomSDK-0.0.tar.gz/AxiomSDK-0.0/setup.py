b2ba2fe62c89a4fdda5607199daffba934276477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:53 +0000

    Add package AxiomSDK/0.0/AxiomSDK-0.0.tar.gz

---
+++ packages/AxiomSDK/AxiomSDK-0.0.tar.gz/AxiomSDK-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='AxiomSDK',
+      version='0.0',
+      description='KLMG',
+      packages=['AxiomSDK'],
+      author_email='Pogrebnykh.Max@yandex.ru',
+      zip_safe=False)
