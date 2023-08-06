b608cfd88b596a4a8567b96b977dbecbf45aa7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:39 +0000

    Add package greenplum-python-fork/1.0.0b5/greenplum-python-fork-1.0.0b5.tar.gz

---
+++ packages/greenplum-python-fork/greenplum-python-fork-1.0.0b5.tar.gz/greenplum-python-fork-1.0.0b5/greenplumpython/__init__.py
@@ -0,0 +1,10 @@
+from greenplumpython.db import Database, database
+from greenplumpython.func import create_aggregate  # type: ignore
+from greenplumpython.func import create_array_function  # type: ignore
+from greenplumpython.func import create_function  # type: ignore
+from greenplumpython.func import aggregate, function
+from greenplumpython.op import operator
+from greenplumpython.table import Table, table, values
+from greenplumpython.type import get_type
+
+__version__ = "1.0.0-beta5"
