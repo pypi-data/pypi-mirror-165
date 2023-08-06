e653c0df046f37909f027a0bdf50395a75bdf779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:51 +0000

    Add package pendo/0.0.4/pendo-0.0.4.tar.gz

---
+++ packages/pendo/pendo-0.0.4.tar.gz/pendo-0.0.4/pendo/types.py
@@ -0,0 +1,35 @@
+from enum import Enum, unique
+from typing import Dict, Union
+
+from pydantic import BaseModel, StrictInt, StrictStr
+
+
+@unique
+class PendoEventType(str, Enum):
+    track = "track"
+
+
+class PendoProperties(BaseModel):
+    ...
+
+
+class PendoContext(BaseModel):
+    ...
+
+
+class PendoTrackEvent(BaseModel):
+    event: StrictStr
+    visitorId: StrictStr
+    accountId: StrictStr
+    timestamp: StrictInt
+    type: PendoEventType = PendoEventType.track
+    properties: Union[Dict, PendoProperties] = dict()
+    context: Union[Dict, PendoContext] = dict()
+
+
+class Response(BaseModel):
+    status_code: StrictInt
+
+
+class PendoResponse(Response):
+    pass
