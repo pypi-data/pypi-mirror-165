be2c30d8952801a86c9860a0000c6183c8d60c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:51 +0000

    Add package pytrify/0.1.0a2/pytrify-0.1.0a2.tar.gz

---
+++ packages/pytrify/pytrify-0.1.0a2.tar.gz/pytrify-0.1.0a2/pytrify/__init__.py
@@ -0,0 +1,4 @@
+from .errors import ImmutableAttributeError
+from .set_once import SetOnce
+from .list_view import ListView
+from .immutable_wrapper import IMMUTABLE_TYPES, pytrify
---
+++ packages/pytrify/pytrify-0.1.0a2.tar.gz/pytrify-0.1.0a2/pytrify/errors.py
@@ -0,0 +1,4 @@
+class ImmutableAttributeError(TypeError):
+    """Raised when something tries to modify an immutable attribute"""
+
+    pass
