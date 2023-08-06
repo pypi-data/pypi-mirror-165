0d834be011afd12070bf386ae874227bdab556da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:56 +0000

    Add package MukeshTheBull/1.1/MukeshTheBull-1.1-py3-none-any.whl

---
+++ packages/MukeshTheBull/MukeshTheBull-1.1-py3-none-any.whl/MukeshTheBull/__init__.py
@@ -0,0 +1,45 @@
+class Moo:
+  def __init__(self):
+    print("Moooo! Welcome to my GauShala aka Cowshed!! Mooo ")
+    print("Mooooooooooooo! Get some grass and ladoos! ")
+    from PIL import Image
+    import urllib.request 
+    import certifi
+    
+
+  def SeeMe(self):
+    print("Why not! Have a look ")
+    from PIL import Image
+    import urllib.request 
+    import certifi
+    url = "https://i.ibb.co/hgNYffS/mukesh.png"
+    im = Image.open(urllib.request.urlopen(url,  cafile=certifi.where()))
+    im.show()
+    
+
+  def SeeMom(self):
+    print("Why not! Have a look ")
+    from PIL import Image
+    import urllib.request 
+    import certifi
+    url = "https://i.ibb.co/7JrJf9w/mom.png"
+    im = Image.open(urllib.request.urlopen(url, cafile=certifi.where()))
+    im.show()
+
+  def SeeDad(self):
+    print("Why not! Have a look ")
+    from PIL import Image
+    import urllib.request 
+    import certifi
+    url = "https://i.ibb.co/x5FwTQt/dad.png"
+    im = Image.open(urllib.request.urlopen(url,  cafile=certifi.where()))
+    im.show()
+
+  def SeeFriends(self):
+    print("Why not! Have a look ")
+    from PIL import Image
+    import urllib.request 
+    import certifi
+    url = "https://i.ibb.co/5LCjZJx/friends.png"
+    im = Image.open(urllib.request.urlopen(url,  cafile=certifi.where()))
+    im.show()
