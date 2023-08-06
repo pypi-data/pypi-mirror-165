3a346d9109b21ee2fa2f62e1570414ec07193f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:16 +0000

    Add package ultralytics/0.0.34/ultralytics-0.0.34.tar.gz

---
+++ packages/ultralytics/ultralytics-0.0.34.tar.gz/ultralytics-0.0.34/src/ultralytics/main.py
@@ -0,0 +1,31 @@
+# Trains and Updates Ultralytics HUB Models
+
+from .auth import Auth
+from .trainer import Trainer
+from .yolov5_utils.general import PREFIX
+from .yolov5_wrapper import clone_yolo
+
+AUTH = Auth()
+CONNECTED = False
+
+
+def train_model(api_key='', model_id='') -> None:
+    """Starts training from next in queue"""
+    clone_yolo()
+    # connect_to_hub(api_key=api_key)  # deprecated
+    AUTH.api_key = api_key
+    trainer = Trainer(model_id=model_id, auth=AUTH)  # No model so next in train queue is fetched
+    if trainer.model is not None:
+        trainer.start()
+
+
+def connect_to_hub(api_key='', verbose=False) -> bool:
+    """Authenticates user with Ultralytics HUB"""
+    global CONNECTED
+
+    if CONNECTED and verbose:
+        print(f'{PREFIX}Already logged in.')
+    elif not CONNECTED:
+        CONNECTED = AUTH.attempt_api_key(api_key=api_key)
+
+    return CONNECTED
