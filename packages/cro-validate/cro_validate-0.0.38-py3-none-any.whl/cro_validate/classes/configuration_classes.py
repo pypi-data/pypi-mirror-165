1123169497b304ad394fa02099583394bf74f6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:13 +0000

    Add package cro-validate/0.0.38/cro_validate-0.0.38-py3-none-any.whl

---
+++ packages/cro-validate/cro_validate-0.0.38-py3-none-any.whl/cro_validate/classes/configuration_classes.py
@@ -0,0 +1,8 @@
+class Config:
+	exception_factory = None
+	example_generator_factory = None
+	default_examples_provider = None
+	definition_name_resolver = None
+	parameter_name_resolver = None
+	definition_name_strategy = None
+	instantiate_value_provider = None
