9a82e2d13ed4e818de2b1b5ae0cdd725a8069e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:37 +0000

    Add package oplib/212/oplib-212-py3-none-any.whl

---
+++ packages/oplib/oplib-212-py3-none-any.whl/op/clt.py
@@ -0,0 +1,36 @@
+# This file is placed in the Public Domain.
+
+"client"
+
+
+from .hdl import Handler, dispatch
+
+
+def __dir__():
+    return (
+            'Client',
+           )
+
+
+class Client(Handler):
+
+    "input/output handler."
+
+    def __init__(self):
+        Handler.__init__(self)
+        self.ignore = []
+        self.orig = repr(self)
+        self.register("event", dispatch)
+
+    def announce(self, txt):
+        "annouce text."
+        self.raw(txt)
+
+    def raw(self, txt):
+        "raw echo of text."
+        raise NotImplementedError("raw")
+
+    def say(self, channel, txt):
+        "say text into channel."
+        if channel not in self.ignore:
+            self.raw(txt)
