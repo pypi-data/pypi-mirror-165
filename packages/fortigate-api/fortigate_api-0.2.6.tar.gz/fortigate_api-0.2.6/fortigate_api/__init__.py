3121a03239d5fe88e3a9d3ee3c515d2f5e61a0d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:06 +0000

    Add package fortigate-api/0.2.6/fortigate_api-0.2.6.tar.gz

---
+++ packages/fortigate-api/fortigate_api-0.2.6.tar.gz/fortigate_api-0.2.6/fortigate_api/__init__.py
@@ -0,0 +1,20 @@
+"""fortigate-api"""
+
+from fortigate_api.fortigate import Fortigate
+from fortigate_api.fortigate_api import FortigateAPI
+
+__all__ = [
+    "Fortigate",
+    "FortigateAPI",
+]
+
+__version__ = "0.2.6"
+__date__ = "2022-09-01"
+__title__ = "fortigate-api"
+
+__summary__ = "Python package to configure Fortigate (Fortios) devices using REST API"
+__author__ = "Vladimir Prusakov"
+__email__ = "vladimir.prusakovs@gmail.com"
+__url__ = "https://github.com/vladimirs-git/fortigate-api"
+__download_url__ = f"{__url__}/archive/refs/tags/{__version__}.tar.gz"
+__license__ = "MIT"
