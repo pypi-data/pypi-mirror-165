0b9264f251dee3fac262963d82f2379e1ae63799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:22 +0000

    Add package pix-apidata/1.3.3/pix_apidata-1.3.3.tar.gz

---
+++ packages/pix-apidata/pix_apidata-1.3.3.tar.gz/pix_apidata-1.3.3/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup ,find_packages
+from os import path
+# The directory containing this file
+#HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+#README = (HERE / "README.md").read_text()
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="pix_apidata",
+    version="1.3.3",
+    author="Coumar Pandourangane",
+    author_email="pscoumar@gmail.com",
+    description="Python library to connect and stream the market data.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pscoumar",
+    license="MIT",
+    classifiers=[ 
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords = 'stock market, apidata, accelpix, ticanalytics',
+    packages=['pix_apidata'],
+    include_package_data=True,
+    install_requires=['signalrcore-async', 'urllib3'],
+
+)
