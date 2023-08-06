d411cf562bf263a6104f466ac9812ad4e4311fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:02 +0000

    Add package s2spy/0.2.0/s2spy-0.2.0.tar.gz

---
+++ packages/s2spy/s2spy-0.2.0.tar.gz/s2spy-0.2.0/s2spy/__init__.py
@@ -0,0 +1,19 @@
+"""
+s2spy: integrating expert knowledge and ai to boost S2S forecasting.
+
+This package is a high-level python package integrating expert knowledge
+and artificial intelligence to boost (sub) seasonal forecasting.
+"""
+import logging
+from . import time
+from . import traintest
+from .rgdr.rgdr import RGDR
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Yang Liu"
+__email__ = "y.liu@esciencecenter.nl"
+__version__ = "0.2.0"
+
+__all__ = ["time", "traintest", "RGDR"]
