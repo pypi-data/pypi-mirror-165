29a3af49122800f17bef6f26781b56af7d16eb83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:54 +0000

    Add package pgx/0.0.5/pgx-0.0.5.tar.gz

---
+++ packages/pgx/pgx-0.0.5.tar.gz/pgx-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pgx",
+    version="0.0.5",
+    description="",
+    url="",
+    author="Sotetsu KOYAMADA",
+    author_email="sotetsu.koyamada@gmail.com",
+    keywords="",
+    packages=find_packages(),
+    install_requires=["jax", "flax", "gym"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+)
