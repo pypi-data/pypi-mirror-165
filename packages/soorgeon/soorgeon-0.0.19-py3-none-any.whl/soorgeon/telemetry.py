d6dcf56dad591f253fa1f75f097b0b1364698d5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:02 +0000

    Add package soorgeon/0.0.19/soorgeon-0.0.19-py3-none-any.whl

---
+++ packages/soorgeon/soorgeon-0.0.19-py3-none-any.whl/soorgeon/telemetry.py
@@ -0,0 +1,9 @@
+from soorgeon import __version__
+
+from ploomber_core.telemetry.telemetry import Telemetry
+
+telemetry = Telemetry(
+    api_key="phc_P9SpSeypyPwxrMdFn2edOOEooQioF2axppyEeDwtMSP",
+    package_name="soorgeon",
+    version=__version__,
+)
