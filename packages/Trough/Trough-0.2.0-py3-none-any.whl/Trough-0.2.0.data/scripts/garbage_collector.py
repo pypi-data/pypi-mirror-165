b79a0d616017438859302e2337bfda280ba28338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:47 +0000

    Add package Trough/0.2.0/Trough-0.2.0-py3-none-any.whl

---
+++ packages/Trough/Trough-0.2.0-py3-none-any.whl/Trough-0.2.0.data/scripts/garbage_collector.py
@@ -0,0 +1,10 @@
+#!python
+import trough
+from trough.settings import settings
+import logging
+import time
+
+if __name__ == '__main__':
+    controller = trough.sync.get_controller(False)
+    controller.check_config()
+    controller.collect_garbage()
