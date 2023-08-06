68f20e6c8c76f41e8342c15a702ef59270f34680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:46 +0000

    Add package aidans-common-functions/0.1.0/aidans_common_functions-0.1.0-py3-none-any.whl

---
+++ packages/aidans-common-functions/aidans_common_functions-0.1.0-py3-none-any.whl/aidans_common_functions/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.0'
+
+class Pet:
+  def __init__(self, name, type, age):
+    self.name = name
+    self.type = type
+    self.age = age
+  def name(self):
+    return self.name
+    
+    
+    7LJzz4*HC@CF8a$
