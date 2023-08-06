b88383db04bd94580fcc62402080bdac2b8c36cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:31 +0000

    Add package checkov/2.1.175/checkov-2.1.175-py3-none-any.whl

---
+++ packages/checkov/checkov-2.1.175-py3-none-any.whl/checkov/common/bridgecrew/severities.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+class Severity:
+    __slots__ = ("level", "name")
+
+    def __init__(self, name: str, level: int) -> None:
+        self.name = name
+        self.level = level
+
+
+@dataclass
+class BcSeverities:
+    NONE = 'NONE'
+    LOW = 'LOW'
+    MEDIUM = 'MEDIUM'
+    HIGH = 'HIGH'
+    CRITICAL = 'CRITICAL'
+    MODERATE = 'MODERATE'
+    IMPORTANT = 'IMPORTANT'
+    OFF = 'OFF'
+
+
+Severities = {
+    BcSeverities.NONE: Severity(BcSeverities.NONE, 0),
+    BcSeverities.LOW: Severity(BcSeverities.LOW, 1),
+    BcSeverities.MEDIUM: Severity(BcSeverities.MEDIUM, 2),
+    BcSeverities.MODERATE: Severity(BcSeverities.MEDIUM, 2),
+    BcSeverities.HIGH: Severity(BcSeverities.HIGH, 3),
+    BcSeverities.IMPORTANT: Severity(BcSeverities.HIGH, 3),
+    BcSeverities.CRITICAL: Severity(BcSeverities.CRITICAL, 4),
+    BcSeverities.OFF: Severity(BcSeverities.OFF, 999),
+}
+
+
+def get_severity(severity: Optional[str]) -> Optional[Severity]:
+    if not severity:
+        return None
+    return Severities.get(severity.upper())
