9049ba1b80ae2dd7fbd2d3f9f0ef3df779cbaa80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:59 +0000

    Add package pytiers/2.0.2/pytiers-2.0.2.tar.gz

---
+++ packages/pytiers/pytiers-2.0.2.tar.gz/pytiers-2.0.2/pytiers/parameters.py
@@ -0,0 +1,15 @@
+"""Parameter settings."""
+
+parameters = {
+    'write_to_file':{
+        'replace':False,
+        'duplicate':False,
+        'duplicated name extension':'modified'
+    },
+    'points':{
+        'ignore_missing_point':False
+    },
+    'to_plot':{
+        'size':(15, 10)
+    }
+}
