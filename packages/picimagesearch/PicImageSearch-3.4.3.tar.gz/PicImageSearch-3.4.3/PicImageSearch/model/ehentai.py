cb94fccd2e109e897600e7ad6e6e42ac39757a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package picimagesearch/3.4.3/PicImageSearch-3.4.3.tar.gz

---
+++ packages/picimagesearch/PicImageSearch-3.4.3.tar.gz/PicImageSearch-3.4.3/PicImageSearch/model/ehentai.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from lxml.html import HTMLParser, fromstring
+from pyquery import PyQuery
+
+
+class EHentaiItem:
+    def __init__(self, data: PyQuery):
+        self.origin: PyQuery = data  # 原始数据
+        self.title: str = ""
+        self.url: str = ""
+        self.thumbnail: str = ""
+        self.type: str = ""
+        self.date: str = ""
+        self.tags: List[str] = []
+        self._arrange(data)
+
+    def _arrange(self, data: PyQuery) -> None:
+        glink = data.find(".glink")
+        self.title = glink.text()
+        if glink.parent("div"):
+            self.url = glink.parent("div").parent("a").attr("href")
+        else:
+            self.url = glink.parent("a").attr("href")
+        self.thumbnail = data.find("img").attr("src")
+        self.type = data.find(".cn").eq(0).text()
+        self.date = data.find("[id^='posted']").eq(0).text()
+        self.tags = [
+            i.text() for i in data.find("div[class=gt],div[class=gtl]").items()
+        ]
+
+
+class EHentaiResponse:
+    def __init__(self, resp_text: str, resp_url: str):
+        self.origin: str = resp_text  # 原始数据
+        utf8_parser = HTMLParser(encoding="utf-8")
+        data = PyQuery(fromstring(self.origin, parser=utf8_parser))
+        self.raw: List[EHentaiItem] = [
+            EHentaiItem(i)
+            for i in data.find(".itg").children("tr").items()
+            if i.children("td")
+        ]
+        self.url: str = resp_url
