7c223ef9c0f34f1df3cd6991bbfc3cd2ccbd912c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:14 +0000

    Add package OMP-soil-database/0.0.3/OMP_soil_database-0.0.3.tar.gz

---
+++ packages/OMP-soil-database/OMP_soil_database-0.0.3.tar.gz/OMP_soil_database-0.0.3/README.rst
@@ -0,0 +1,30 @@
+SUTRA
+============
+.. image:: https://readthedocs.org/projects/sutra/badge/?version=latest&style=flat
+                    :target: https://sutra.readthedocs.io
+.. image:: https://img.shields.io/travis/KWR-Water/sutra
+                    :target: https://travis-ci.com/KWR-Water/sutra
+.. image:: https://img.shields.io/codecov/c/gh/KWR-Water/sutra
+                    :target: https://codecov.io/gh/KWR-Water/sutra
+
+A tool to calculate the fate of Organic Micropollutant and pathogens in the subsoil.
+
+See documentation_ for more information.
+
+
+Installation
+------------
+To get the latest stable version, use::
+
+    python -m pip install git+https://github.com/KWR-Water/OMP_soil_database.git@main
+
+or::
+
+    pip install git+ssh://git@github.com/KWR-Water/OMP_soil_database.git@main
+
+License
+-------
+
+This package is MIT licensed. See `License File <https://github.com/KWR-Water/OMP_soil_database/blob/master/LICENSE>`__.
+
+.. _documentation: https://OMP_soil_database.readthedocs.io/en/latest/
---
+++ packages/OMP-soil-database/OMP_soil_database-0.0.3.tar.gz/OMP_soil_database-0.0.3/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    name='OMP_soil_database',
+    version='0.0.3',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A python package for calculating the removal of microbial organisms in the subsurface',
+    long_description=read('README.rst'),
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Hydrology',
+    ],
+    python_requires='>=3.7',
+    project_urls={
+    'Source': 'https://github.com/steven-ros/OMP_soil_database',
+    'Documentation': 'http://OMP_soil_database.readthedocs.io/en/latest/',
+    'Tracker': 'https://github.com/KWR-Water/OMP_soil_database/issues',
+    'Help': 'https://github.com/KWR-Water/OMP_soil_database/issues',
+    # 'Help': 'https://stackoverflow.com/questions/tagged/OMP_soil_database'
+    },
+    install_requires=[
+        'pandas>=0.23',
+
+        ],
+    include_package_data=True,
+    url='https://github.com/KWR-Water/OMP_soil_database',
+    author='KWR Water Research Institute',
+    author_email='vincent.post@kwrwater.nl, martin.korevaar@kwrwater.nl, martin.van.der.schans@kwrwater.nl, steven.ros@kwrwater.nl'
+)
