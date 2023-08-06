f1752e67a7eb8a833ba34b7687b1c4d26f3912ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:27 +0000

    Add package metacity/0.5.2/metacity-0.5.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/metacity/metacity-0.5.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/metacity-0.5.2.data/data/_skbuild/linux-x86_64-3.9/cmake-install/_skbuild/linux-x86_64-3.9/cmake-build/CMakeFiles/geometry.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geometry.dir/src/cpp/geometry.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/geometry.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/attribute.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/attribute.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bbox.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bbox.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bvh.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/bvh.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/graham.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/graham.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/grid.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/grid.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/layer.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/layer.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/model.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/model.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/modifiers.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/modifiers.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/triangulation.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/mesh_pipeline/triangulation.cpp.o.d"
+  "CMakeFiles/geometry.dir/src/cpp/vector_pipeline/graph.cpp.o"
+  "CMakeFiles/geometry.dir/src/cpp/vector_pipeline/graph.cpp.o.d"
+  "geometry.cpython-39-x86_64-linux-gnu.so"
+  "geometry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
