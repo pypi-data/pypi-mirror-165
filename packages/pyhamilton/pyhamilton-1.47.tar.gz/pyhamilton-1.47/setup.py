279d20f8d874cdb6b243038664adabd92ec0181b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:44 +0000

    Add package pyhamilton/1.47/pyhamilton-1.47.tar.gz

---
+++ packages/pyhamilton/pyhamilton-1.47.tar.gz/pyhamilton-1.47/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+try:
+    print("here")
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+    print(long_description)
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='pyhamilton',
+    version='1.47',
+    packages=find_packages(exclude=['tests*', 'examples*']),
+    license='MIT',
+    description='Python for Hamilton liquid handling robots',
+    long_description='Forthcoming due to markdown incompatibility',
+    install_requires=['requests', 'pythonnet', 'pywin32', 'pyserial'],
+    package_data={'pyhamilton': ['star-oem/*', 'star-oem/VENUS_Method/*', 'bin/*','library/*']},
+    url='https://github.com/dgretton/pyhamilton.git',
+    author='Dana Gretton',
+    author_email='dgretton@mit.edu',
+    entry_points={
+        'console_scripts': [
+            'pyhamilton-quickstart = pyhamilton.cmd.quickstart:main',
+            'pyhamilton-config = pyhamilton.__init__:autoconfig'
+        ],
+    },
+)
