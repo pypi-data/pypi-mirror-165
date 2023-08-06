f99c5d5604a30dc5d7a5dccd8fdccb1f4e72faf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:10 +0000

    Add package bc-time/3.0.0/bc_time-3.0.0.tar.gz

---
+++ packages/bc-time/bc_time-3.0.0.tar.gz/bc_time-3.0.0/src/bc_time/api/objects/employee_leave.py
@@ -0,0 +1,8 @@
+from bc_time.api.objects.object_base import ObjectBase
+from bc_time.api.api import Api
+from bc_time.api.enumerators.content_type import ContentType
+
+class EmployeeLeave(ObjectBase):
+    def __init__(self, api: Api=None) -> None:
+        super().__init__(api)
+        self._content_type_id = ContentType.employee_leave
