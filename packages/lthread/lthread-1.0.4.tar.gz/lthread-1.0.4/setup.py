e6c530d27bdfe084d3761c342c94e476d05d2c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:03 +0000

    Add package lthread/1.0.4/lthread-1.0.4.tar.gz

---
+++ packages/lthread/lthread-1.0.4.tar.gz/lthread-1.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="lthread",
+      version="1.0.4",
+      description="(OBSOLETE) lightweight threads in Python using extended generators",
+      author="Katie Rust",
+      author_email="katie@ktpanda.org",
+      readme="README.md",
+      license="MIT License",
+      packages=["lthread"],
+      classifiers = [
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+        ]
+
+     )
