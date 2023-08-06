0bb13b98a2d144efe0cebcc2cb07b821b595bd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:24 +0000

    Add package pyRoomOSxAPI/0.0.2/pyRoomOSxAPI-0.0.2.tar.gz

---
+++ packages/pyRoomOSxAPI/pyRoomOSxAPI-0.0.2.tar.gz/pyRoomOSxAPI-0.0.2/pyRoomOSxAPI/xCommand.py
@@ -0,0 +1,540 @@
+class Top_Level:
+    def __init__(self) -> str:
+        pass
+
+class Audio:
+    def __init__(self) -> str:
+        pass
+
+        class AecReverb:
+            pass
+
+            def Reset():
+                pass
+
+            def Run():
+                pass
+
+    class Microphones:
+        pass
+
+        def Mute():
+            pass
+
+        def ToggleMute():
+            pass
+
+        def Unmute():
+            pass
+
+        class MusicMode:
+            pass
+
+            def Start():
+                pass
+
+            def Stop():
+                pass
+
+        class NoiseRemoval:
+            pass
+
+            def Activate():
+                pass
+
+            def Deactivate():
+                pass
+
+    class Sound:
+        pass
+
+        def Stop():
+            pass
+
+        class Ringtone:
+            pass
+
+            def List():
+                pass
+
+            def Stop():
+                pass
+
+    class Volume:
+        pass
+
+        def Mute():
+            pass
+
+        def ToggleMute():
+            pass
+
+        def Unmute():
+            pass
+
+    class VuMeter:
+        pass
+
+        def StopAll():
+            pass
+
+class Bluetooth:
+    def __init__(self) -> str:
+        pass
+
+class Bookings:
+    def __init__(self) -> str:
+        pass
+
+class Call:
+    def __init__(self) -> str:
+        pass
+
+class CallHistory:
+    def __init__(self) -> str:
+        pass
+
+class CallLog:
+    def __init__(self) -> str:
+        pass
+
+class CallTransfer:
+    def __init__(self) -> str:
+        pass
+
+class Camera:
+    def __init__(self) -> str:
+        pass
+
+class Cameras:
+    def __init__(self) -> str:
+        pass
+
+    class SpeakerTrack:
+        pass
+
+        def Activate():
+            pass
+
+        def Deactivate():
+            pass
+
+        class Diagnostics:
+            pass
+
+            def Stop():
+                pass
+
+        class ViewLimits:
+            pass
+
+            def Activate():
+                pass
+
+            def Deactivate():
+                pass
+
+            def StorePosition():
+                pass
+
+class Capabilities:
+    def __init__(self) -> str:
+        pass
+
+class Conference:
+    def __init__(self) -> str:
+        pass
+
+    class DoNotDisturb:
+        pass
+
+        def Deactivate():
+            pass
+
+    class SpeakerLock:
+        pass
+
+        def Release():
+            pass
+
+class Diagnostics:
+    def __init__(self) -> str:
+        pass
+
+class FacilityService:
+    def __init__(self) -> str:
+        pass
+
+class FarEndMessage:
+    def __init__(self) -> str:
+        pass
+
+class FeccReceive:
+    def __init__(self) -> str:
+        pass
+
+class Files:
+    def __init__(self) -> str:
+        pass
+
+class H320:
+    def __init__(self) -> str:
+        pass
+
+class H323:
+    def __init__(self) -> str:
+        pass
+
+class HttpClient:
+    def __init__(self) -> str:
+        pass
+
+        class Hostname:
+            pass
+
+            def Clear():
+                pass
+
+            def List():
+                pass
+
+class HttpFeedback:
+    def __init__(self) -> str:
+        pass
+
+class ICE:
+    def __init__(self) -> str:
+        pass
+
+class Logging:
+    def __init__(self) -> str:
+        pass
+
+class Macros:
+    def __init__(self) -> str:
+        pass
+
+    class Log:
+        pass
+
+        def Clear():
+            pass
+
+    class Macro:
+        pass
+
+        def RemoveAll():
+            pass
+
+    class Runtime:
+        pass
+
+        def Restart():
+            pass
+
+        def Start():
+            pass
+
+        def Status():
+            pass
+
+        def Stop():
+            pass
+
+class MediaChannels:
+    def __init__(self) -> str:
+        pass
+
+class Message:
+    def __init__(self) -> str:
+        pass
+
+class Network:
+    def __init__(self) -> str:
+        pass
+
+    class Wifi:
+        pass
+
+        def List():
+            pass
+
+        class Scan:
+            pass
+
+            def Stop():
+                pass
+
+class NetworkServices:
+    def __init__(self) -> str:
+        pass
+
+class Peripherals:
+    def __init__(self) -> str:
+        pass
+
+        class PinPairing:
+            pass
+
+            def Stop():
+                pass
+
+class Phonebook:
+    def __init__(self) -> str:
+        pass
+
+class Presentation:
+    def __init__(self) -> str:
+        pass
+
+class Provisioning:
+    def __init__(self) -> str:
+        pass
+
+        class ExtensionMobility:
+            pass
+
+            def Logout():
+                pass
+
+class Proximity:
+    def __init__(self) -> str:
+        pass
+
+    class Services:
+        pass
+
+        def Activate():
+            pass
+
+        def Deactivate():
+            pass
+
+class RoomAnalytics:
+    def __init__(self) -> str:
+        pass
+
+class RoomCleanup:
+    def __init__(self) -> str:
+        pass
+
+class RoomPreset:
+    def __init__(self) -> str:
+        pass
+
+class RoomReset:
+    def __init__(self) -> str:
+        pass
+
+class RoomScheduler:
+    def __init__(self) -> str:
+        pass
+
+class SIP:
+    def __init__(self) -> str:
+        pass
+
+class Security:
+    def __init__(self) -> str:
+        pass
+
+        class ThirdParty:
+            pass
+
+            def List():
+                pass
+
+    class Ciphers:
+        pass
+
+        def List():
+            pass
+
+    class Session:
+        pass
+
+        def Get():
+            pass
+
+        def List():
+            pass
+
+class SerialPort:
+    def __init__(self) -> str:
+        pass
+
+class Standby:
+    def __init__(self) -> str:
+        pass
+
+class SystemUnit:
+    def __init__(self) -> str:
+        pass
+
+    class DeveloperPreview:
+        pass
+
+        def Activate():
+            pass
+
+        def Deactivate():
+            pass
+
+    class FirstTimeWizard:
+        pass
+
+        def Stop():
+            pass
+
+    class Notifications:
+        pass
+
+        def RemoveAll():
+            pass
+
+    class OptionKey:
+        pass
+
+        def List():
+            pass
+
+    class SignInBanner:
+        pass
+
+        def Clear():
+            pass
+
+        def Get():
+            pass
+
+    class WelcomeBanner:
+        pass
+
+        def Clear():
+            pass
+
+        def Get():
+            pass
+
+class Time:
+    def __init__(self) -> str:
+        pass
+
+    class DateTime:
+        pass
+
+        def Get():
+            pass
+
+class UserInterface:
+    def __init__(self) -> str:
+        pass
+
+    class Branding:
+        pass
+
+        def Clear():
+            pass
+
+        class Icon:
+            pass
+
+            def List():
+                pass
+
+        class Panel:
+            pass
+
+            def Close():
+                pass
+
+        class Alert:
+            pass
+
+            def Clear():
+                pass
+
+        class TextLine:
+            pass
+
+            def Clear():
+                pass
+
+        class ExternalSource:
+            pass
+
+            def List():
+                pass
+
+            def RemoveAll():
+                pass
+
+        class Override:
+            pass
+
+            def Clear():
+                pass
+
+            def Get():
+                pass
+
+    class WallpaperBundle:
+        pass
+
+        def Clear():
+            pass
+
+        def List():
+            pass
+
+class UserManagement:
+    def __init__(self) -> str:
+        pass
+
+    class RemoteSupportUser:
+        pass
+
+        def Delete():
+            pass
+
+        def GetState():
+            pass
+
+class Video:
+    def __init__(self) -> str:
+        pass
+
+        class MainVideo:
+            pass
+
+            def Mute():
+                pass
+
+            def Unmute():
+                pass
+
+class VoiceControl:
+    def __init__(self) -> str:
+        pass
+
+class WebEngine:
+    def __init__(self) -> str:
+        pass
+
+class WebRTC:
+    def __init__(self) -> str:
+        pass
+
+class Webex:
+    def __init__(self) -> str:
+        pass
+
+    class Registration:
+        pass
+
+        def Cancel():
+            pass
+
+        def Logout():
+            pass
+
