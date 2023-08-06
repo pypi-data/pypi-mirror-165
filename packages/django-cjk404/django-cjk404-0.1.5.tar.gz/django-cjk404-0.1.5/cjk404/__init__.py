aaf119aaf0f1182a5a5c368384bf1a710cb532e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:25 +0000

    Add package django-cjk404/0.1.5/django-cjk404-0.1.5.tar.gz

---
+++ packages/django-cjk404/django-cjk404-0.1.5.tar.gz/django-cjk404-0.1.5/cjk404/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cjk404."""
+
+__author__ = """Grzegorz Król, Filip Woźniak"""
+__email__ = "gk@kachange.eu; fw@kachange.eu"
+__version__ = "0.1.0"
