0b7353e5c2b7fc45093bd3d2e1a055ee4687c128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:03 +0000

    Add package audups/0.1.post4/audups-0.1.post4.tar.gz

---
+++ packages/audups/audups-0.1.post4.tar.gz/audups-0.1.post4/audups/common.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+import contextlib
+
+from tqdm.contrib.logging import tqdm_logging_redirect
+
+
+logger = logging.getLogger('audups')
+
+
+@contextlib.contextmanager
+def dynamic_tqdm(*tqdm_args, **tqdm_kwargs):
+  """Context manager that returns a tqdm object or None depending on context."""
+  with contextlib.ExitStack() as cm:
+    if sys.stderr.isatty() and logger.isEnabledFor(logging.INFO):
+      progress = cm.enter_context(tqdm_logging_redirect(
+        *tqdm_args,
+        **tqdm_kwargs
+      ))
+    else:
+      progress = None
+    yield progress
