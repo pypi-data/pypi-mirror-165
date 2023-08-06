5196a97258014c574587309ddaeb01d65268493e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:44 +0000

    Add package PySwitchbot/0.18.15/PySwitchbot-0.18.15.tar.gz

---
+++ packages/PySwitchbot/PySwitchbot-0.18.15.tar.gz/PySwitchbot-0.18.15/switchbot/devices/plug.py
@@ -0,0 +1,32 @@
+"""Library to handle connection with Switchbot."""
+from __future__ import annotations
+
+from typing import Any
+
+from .device import SwitchbotDevice
+
+# Plug Mini keys
+PLUG_ON_KEY = "570f50010180"
+PLUG_OFF_KEY = "570f50010100"
+
+
+class SwitchbotPlugMini(SwitchbotDevice):
+    """Representation of a Switchbot plug mini."""
+
+    async def update(self, interface: int | None = None) -> None:
+        """Update state of device."""
+        await self.get_device_data(retry=self._retry_count, interface=interface)
+
+    async def turn_on(self) -> bool:
+        """Turn device on."""
+        result = await self._sendcommand(PLUG_ON_KEY)
+        return result[1] == 0x80
+
+    async def turn_off(self) -> bool:
+        """Turn device off."""
+        result = await self._sendcommand(PLUG_OFF_KEY)
+        return result[1] == 0x00
+
+    def is_on(self) -> bool | None:
+        """Return switch state from cache."""
+        return self._get_adv_value("isOn")
