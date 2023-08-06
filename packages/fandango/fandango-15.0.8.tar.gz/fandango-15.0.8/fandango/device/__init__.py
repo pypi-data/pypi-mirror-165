3c71e012b65264908695ee235c728761d6acfd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:34 +0000

    Add package fandango/15.0.8/fandango-15.0.8.tar.gz

---
+++ packages/fandango/fandango-15.0.8.tar.gz/fandango-15.0.8/fandango/device/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+from __future__ import absolute_import
+
+try:
+    from . import DDebug as debug
+    from .DDebug import *
+except:
+    print("Unable to import DDebug")
+try:
+    from . import Dev4Tango as dev4tango
+    from .Dev4Tango import *
+except:
+    print("Unable to import Dev4Tango")
+try:
+    from . import FolderDS as folders
+    from .FolderDS import FolderDS, FolderAPI
+except:
+    print("Unable to import FolderDS")
+try:
+    from . import WorkerDS as workers
+except:
+    print("Unable to import WorkerDS")
