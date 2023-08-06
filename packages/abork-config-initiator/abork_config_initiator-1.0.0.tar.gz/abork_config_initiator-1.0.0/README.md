887a7e73a6bc70932bf7bdcdf99412162444b628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:14 +0000

    Add package abork-config-initiator/1.0.0/abork_config_initiator-1.0.0.tar.gz

---
+++ packages/abork-config-initiator/abork_config_initiator-1.0.0.tar.gz/abork_config_initiator-1.0.0/README.md
@@ -0,0 +1,2 @@
+# py_config_initor
+Config initializator, creates config file with specified variables if not exists
---
+++ packages/abork-config-initiator/abork_config_initiator-1.0.0.tar.gz/abork_config_initiator-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='abork_config_initiator',
+    version='1.0.0', 
+    packages=['py_config_initiator'], 
+    author='Alex Bork',
+    license=open('LICENSE', 'r').read(),
+    long_description_content_type='text/markdown',
+    long_description=open('README.md', 'r', encoding="utf-8").read(),
+)
