fd5268b595cb582676b86c976591607d27d66acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:01 +0000

    Add package cyslf/0.2.2/cyslf-0.2.2-py3-none-any.whl

---
+++ packages/cyslf/cyslf-0.2.2-py3-none-any.whl/cyslf/utils.py
@@ -0,0 +1,24 @@
+# Skill rating divisions
+FIRST_ROUND_SKILL = 1
+TOP_TIER_SKILLS = [2, 3]
+MID_TIER_SKILLS = [4, 5, 6]
+BOTTOM_TIER_SKILLS = [7, 8, 9, 10]
+
+DAY_MAP = {
+    "Monday": "M",
+    "Tuesday": "T",
+    "Wednesday": "W",
+    "Thursday": "R",
+    "Friday": "F",
+}
+
+FIELD_MAP = {
+    "East": ["Ahern", "Donnelly"],
+    "Central": ["Common"],
+    "Cambridgeport": ["Pacific", "Magazine"],
+    "West": ["Danehy", "Raymond", "Maher"],
+    "North": ["Danehy", "Raymond", "Russell"],
+}
+
+# Threshold below which a player counts as a goalie
+GOALIE_THRESHOLD = 3
