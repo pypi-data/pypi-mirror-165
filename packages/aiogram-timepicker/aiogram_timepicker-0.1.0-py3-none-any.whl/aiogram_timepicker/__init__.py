3cd681cc5e345433d3ea30c93a52771fa640958f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:49 +0000

    Add package aiogram-timepicker/0.1.0/aiogram_timepicker-0.1.0-py3-none-any.whl

---
+++ packages/aiogram-timepicker/aiogram_timepicker-0.1.0-py3-none-any.whl/aiogram_timepicker/__init__.py
@@ -0,0 +1,17 @@
+import aiogram_timepicker.result
+from aiogram_timepicker.full import \
+    timepicker_callback as full_timep_callback, \
+    TimePicker as FullTimePicker, \
+    default as full_timep_default
+from aiogram_timepicker.single.hour import \
+    timepicker_callback as hour_timep_callback, \
+    TimePicker as HourTimePicker, \
+    default as hour_timep_default
+from aiogram_timepicker.single.minute import \
+    timepicker_callback as minute_timep_callback, \
+    TimePicker as MinuteTimePicker, \
+    default as minute_timep_default
+from aiogram_timepicker.single.second import \
+    timepicker_callback as second_timep_callback, \
+    TimePicker as SecondTimePicker, \
+    default as second_timep_default
