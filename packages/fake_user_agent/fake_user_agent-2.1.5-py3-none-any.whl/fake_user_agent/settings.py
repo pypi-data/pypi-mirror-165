44bc07b54cff6c836da7d57145e976c991ceee93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:43 +0000

    Add package fake_user_agent/2.1.5/fake_user_agent-2.1.5-py3-none-any.whl

---
+++ packages/fake_user_agent/fake_user_agent-2.1.5-py3-none-any.whl/fake_user_agent/settings.py
@@ -0,0 +1,52 @@
+"""Setting static data."""
+
+import os
+import re
+import tempfile
+
+from .log import logger
+
+VERSION = "2.1.5"
+
+TEMP_DIR = tempfile.gettempdir()  # str type
+
+
+def find_tempfile(dir):
+    for _, _, files in os.walk(dir):
+        for f in files:
+            match = re.search(r"^fake_useragent_", f)
+            if match:
+                logger.debug(f"{f} is found.")
+                return os.path.join(dir, f)  # str type
+
+    logger.debug("No cache is found.")
+    return ""
+
+
+DB = os.path.join(
+    TEMP_DIR, "fake_useragent_{version}.json".format(version=VERSION)
+)  # str type
+
+BROWSER_BASE_PAGE = (
+    "http://useragentstring.com/pages/useragentstring.php?name={browser}"
+)
+
+BROWSERS = {
+    "chrome": 80.7,
+    "edge": 5.6,
+    "firefox": 6.1,
+    "safari": 3.7,
+    "opera": 2.4,
+}
+
+SHORTCUTS = {
+    "internet explorer": "edge",
+    "ie": "edge",
+    "google": "chrome",
+    "googlechrome": "chrome",
+    "ff": "firefox",
+}
+
+BROWSERS_COUNT_LIMIT = 50
+
+HTTP_TIMEOUT = 10
