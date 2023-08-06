5039c6b3f0cc3e757c82a7d260d7ec7b90dda22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package tomoe/3.0.12/tomoe-3.0.12.tar.gz

---
+++ packages/tomoe/tomoe-3.0.12.tar.gz/tomoe-3.0.12/tomoe/hentai2read.py
@@ -0,0 +1,124 @@
+import asyncio
+import janda
+import requests
+import os
+import re
+import time
+from .utils.misc import (
+    choose,
+    split_name,
+    get_size,
+    convert_html_to_pdf,
+    project,
+    get_size_folder,
+    log_data,
+    log_file,
+    log_final,
+)
+from inputimeout import inputimeout, TimeoutOccurred
+
+h2r = janda.Hentai2read()
+t: str = "tomoe.html"
+
+
+async def get_h2r(ids=choose().hentai2read):
+    for id in ids:
+        par = id.split(":")
+        await asyncio.gather(process_h2r(par[0], par[1]))
+        print(f"Complete process {par[0]}, chapter {par[1]}")
+
+
+async def process_h2r(id: str, chapter: int):
+    initial = time.time()
+    data = await h2r.get(id, chapter)
+    parser = janda.resolve(data)
+
+    title = re.sub(r"[^\w\s]", "", parser["data"]["title"])
+    img = parser["data"]["image"]
+    slug = parser["data"]["id"]
+
+    log_data("TITLE", title)
+    log_data("ID", slug)
+    log_data("SOURCE", parser["current_url"])
+    log_data("TOTAL", f"{len(parser['data']['image'])} pages")
+
+    neat_dir = split_name(__file__) + " - " + title
+    if not os.path.exists(neat_dir):
+        os.makedirs(neat_dir)
+
+    if len(img) == len(os.listdir(neat_dir)):
+        print(
+            "All images already downloaded! If you're doubt kindly remove this folder and re-download"
+        )
+        print(f"Directory: {os.path.abspath(neat_dir)}")
+        return
+
+    for i in img:
+        start = time.time()
+        img_url = i
+        img_name = img_url.rsplit("/", 1)[-1]
+
+        r = requests.get(img_url)
+        with open(neat_dir + "/" + img_name, "wb") as f:
+
+            f.write(r.content)
+
+            if os.path.exists(neat_dir + "/" + img_name):
+                log_file(
+                    img_name,
+                    get_size(neat_dir + "/" + img_name),
+                    f"{time.time() - start:.2f}",
+                )
+
+            if len(img) == len(os.listdir(neat_dir)):
+
+                log_final(
+                    f"{(time.time() - initial) / 60:.2f}", get_size_folder(neat_dir)
+                )
+                print(f"Directory: {os.path.abspath(neat_dir)}")
+
+                with open(neat_dir + "/" + t, "x", encoding="utf-8") as f:
+                    f.write("<html><center><body>")
+                    f.write(f"<h1>{parser['data']['title']}</h1>")
+
+                    for i in img:
+                        file = i.rsplit("/", 1)[-1]
+
+                        f.write(f'<img src="{neat_dir}/{file}"><p></p>')
+                    f.write(f"{project()}")
+                    f.write("</body></center></html>")
+                    f.close()
+
+                try:
+                    desired = inputimeout(
+                        prompt="Do you want to render it all to .pdf? (y/n) ",
+                        timeout=10,
+                    )
+                    to_pdf = desired
+
+                    if to_pdf == "y":
+                        try:
+                            source = open(f"{neat_dir}/{t}")
+                            output = f"{neat_dir}/{parser['data']['title']}.pdf"
+                            filepdf = output.rsplit("/", 1)[-1]
+
+                            convert_html_to_pdf(source, output)
+                            print(
+                                f"Successfully rendered to {filepdf} | {get_size(output)} MB"
+                            )
+
+                        except Exception as e:
+                            print(f"Something went wrong while converting to pdf: {e}")
+
+                    elif to_pdf == "n":
+                        print("Okay")
+                        os.remove(neat_dir + "/" + t)
+                        return
+
+                    else:
+                        print("Invalid input")
+                        os.remove(neat_dir + "/" + t)
+                        return
+
+                except TimeoutOccurred:
+                    print(f"Timeout occurred, not rendering pdf {id}, chapter: {chapter}")
---
+++ packages/tomoe/tomoe-3.0.12.tar.gz/tomoe-3.0.12/tomoe/nhentai.py
@@ -0,0 +1,130 @@
+import asyncio
+import janda
+import requests
+import os
+import re
+import time
+from .utils.misc import (
+    choose,
+    split_name,
+    get_size,
+    convert_html_to_pdf,
+    project,
+    get_size_folder,
+    log_data,
+    log_file,
+    log_final,
+)
+from inputimeout import inputimeout, TimeoutOccurred
+
+nh = janda.Nhentai()
+t: str = "tomoe.html"
+
+
+async def get_nh(ids=choose().nhentai):
+    for id in ids:
+        await asyncio.gather(process_nhentai(id))
+        print(f"Complete process {id}")
+
+
+async def process_nhentai(id: int):
+    initial = time.time()
+    data = await nh.get(id)
+    parser = janda.resolve(data)
+
+    title = re.sub(r"[^\w\s]", "", parser["data"]["title"])
+    img = parser["data"]["image"]
+    number = parser["data"]["id"]
+    tags = parser["data"]["tags"]
+    tags = [tag for tag in tags]
+    to_tags = ", ".join(tags)
+
+    log_data("TITLE", title)
+    log_data("TAGS", to_tags)
+    log_data("ID", number)
+    log_data("SOURCE", parser["source"])
+    log_data("TOTAL", f"{parser['data']['total']} pages")
+
+    neat_dir = f"{split_name(__file__)}-{number}-{title}"
+    neat_dir = re.sub("[^A-Za-z0-9-]+", " ", neat_dir)
+
+    neat_dir = re.sub(r"\s+", "_", neat_dir)
+
+    if not os.path.exists(neat_dir):
+        os.makedirs(neat_dir)
+
+    if len(os.listdir(neat_dir)) >= len(img):
+        print(
+            "All images already downloaded! If you're doubt kindly remove this folder and re-download"
+        )
+        print(f"Directory: {os.path.abspath(neat_dir)}")
+        return
+
+    for i in img:
+        start = time.time()
+        img_url = i
+        img_name = img_url.rsplit("/", 1)[-1]
+
+        r = requests.get(img_url)
+        with open(neat_dir + "/" + img_name, "wb") as f:
+            f.write(r.content)
+
+            if os.path.exists(neat_dir + "/" + img_name):
+                log_file(
+                    img_name,
+                    get_size(neat_dir + "/" + img_name),
+                    f"{time.time() - start:.2f}",
+                )
+
+            if len(img) == len(os.listdir(neat_dir)):
+                log_final(
+                    f"{(time.time() - initial) / 60:.2f}", get_size_folder(neat_dir)
+                )
+
+                with open(neat_dir + "/" + t, "x", encoding="utf-8") as f:
+                    f.write("<html><center><body>")
+                    f.write(f"<h1>{parser['data']['id']}</h1>")
+
+                    for i in img:
+                        file = i.rsplit("/", 1)[-1]
+
+                        f.write(f'<img src="{neat_dir}/{file}"><p></p>')
+                    f.write(f"{project()}")
+                    f.write("</body></center></html>")
+                    f.close()
+
+                    print(f"Directory: {os.path.abspath(neat_dir)}")
+
+                try:
+                    desired = inputimeout(
+                        prompt="Do you want to render it all to .pdf? (y/n) ",
+                        timeout=10,
+                    )
+                    to_pdf = desired
+
+                    if to_pdf == "y":
+                        try:
+                            source = open(f"{neat_dir}/{t}")
+                            output = f"{neat_dir}/{parser['data']['id']}.pdf"
+                            filepdf = output.rsplit("/", 1)[-1]
+
+                            convert_html_to_pdf(source, output)
+                            print(
+                                f"Successfully rendered to {filepdf} | {get_size(output)} MB"
+                            )
+
+                        except Exception as e:
+                            print(f"Something went wrong while converting to pdf: {e}")
+
+                    elif to_pdf == "n":
+                        print("Okay")
+                        os.remove(neat_dir + "/" + t)
+                        return
+
+                    else:
+                        print("Invalid input")
+                        os.remove(neat_dir + "/" + t)
+                        return
+
+                except TimeoutOccurred:
+                    print(f"Timeout occurred, not rendering pdf {id}")
