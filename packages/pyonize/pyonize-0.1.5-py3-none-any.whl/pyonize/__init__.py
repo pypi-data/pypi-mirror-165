ddbe7ebc3bb52585ad0edf00a1d41ab9630df004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:57 +0000

    Add package pyonize/0.1.5/pyonize-0.1.5-py3-none-any.whl

---
+++ packages/pyonize/pyonize-0.1.5-py3-none-any.whl/pyonize/__init__.py
@@ -0,0 +1,12 @@
+""" convert json|dict to python object """
+
+__version__ = "0.1.5"
+
+
+from .__main__ import pyonize, Pyon, PyonList
+
+__all__ = [
+    "pyonize",
+    "Pyon",
+    "PyonList"
+]
