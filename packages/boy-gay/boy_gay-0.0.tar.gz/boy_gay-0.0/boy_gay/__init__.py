a38a3a936d7295a48c79e09cb2f2e6b4bf95b9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:49 +0000

    Add package boy-gay/0.0/boy_gay-0.0.tar.gz

---
+++ packages/boy-gay/boy_gay-0.0.tar.gz/boy_gay-0.0/boy_gay/__init__.py
@@ -0,0 +1,10 @@
+from .masagedistribution import masage
+
+
+
+
+def masage(digit):
+
+    mas = 'Tu pidor !!!!!!!'
+
+    return mas
---
+++ packages/boy-gay/boy_gay-0.0.tar.gz/boy_gay-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='boy_gay',
+      version='0.0',
+      description='masage distributions',
+      packages=['boy_gay'],
+      author_email='valedol64@gmail.com',
+      zip_safe=False)
