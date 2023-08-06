c0ad0ddfa797c9eee42755a5397fb5d537fe6211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:49 +0000

    Add package bzip3/0.0.5/bzip3-0.0.5-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/bzip3/bzip3-0.0.5-cp38-cp38-macosx_10_15_x86_64.whl/bz3/backends/cython/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
+"""
+from bz3.backends.cython._bz3_cy import (
+    BZ3Compressor,
+    BZ3Decompressor,
+    compress_file,
+    crc32,
+    decompress_file,
+    test_file,
+)
