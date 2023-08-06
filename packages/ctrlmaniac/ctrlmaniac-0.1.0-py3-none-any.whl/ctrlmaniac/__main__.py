df46bb0253f5276e35bff22158e117b37c836cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:49 +0000

    Add package ctrlmaniac/0.1.0/ctrlmaniac-0.1.0-py3-none-any.whl

---
+++ packages/ctrlmaniac/ctrlmaniac-0.1.0-py3-none-any.whl/ctrlmaniac/__main__.py
@@ -0,0 +1,10 @@
+"""Type python -m ctrlmanaic"""
+from ctrlmaniac.ctrlmaniac import CtrlManiac
+
+me = CtrlManiac()
+
+me.greet("en_US")
+
+me.learn_new_coding_languange("Java")
+
+me.learn_new_coding_languange("Typescript")
