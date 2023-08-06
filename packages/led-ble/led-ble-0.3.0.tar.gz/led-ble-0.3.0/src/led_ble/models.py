f0aa70984b928a46b94153469edb2df1512b2cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:44 +0000

    Add package led-ble/0.3.0/led-ble-0.3.0.tar.gz

---
+++ packages/led-ble/led-ble-0.3.0.tar.gz/led-ble-0.3.0/src/led_ble/models.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class LEDBLEState:
+
+    power: bool = False
+    rgb: tuple[int, int, int] = (0, 0, 0)
+    w: int = 0
