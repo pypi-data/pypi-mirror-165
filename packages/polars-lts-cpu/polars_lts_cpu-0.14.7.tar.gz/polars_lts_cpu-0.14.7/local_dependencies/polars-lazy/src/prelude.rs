309cd2dbee429c057f1245b1a49ca1208b469a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:06 +0000

    Add package polars-lts-cpu/0.14.7/polars_lts_cpu-0.14.7.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.14.7.tar.gz/polars_lts_cpu-0.14.7/local_dependencies/polars-lazy/src/prelude.rs
@@ -0,0 +1,41 @@
+#[cfg(feature = "temporal")]
+pub(crate) use polars_time::in_nanoseconds_window;
+#[cfg(feature = "dynamic_groupby")]
+pub(crate) use polars_time::{DynamicGroupOptions, PolarsTemporalGroupby, RollingGroupOptions};
+pub(crate) use polars_utils::arena::{Arena, Node};
+#[cfg(not(feature = "dynamic_groupby"))]
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[cfg(not(feature = "dynamic_groupby"))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Clone, Debug)]
+pub struct DynamicGroupOptions {
+    pub index_column: String,
+}
+
+pub(crate) use polars_time::prelude::*;
+
+#[cfg(not(feature = "dynamic_groupby"))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[derive(Clone, Debug)]
+pub struct RollingGroupOptions {
+    pub index_column: String,
+}
+
+pub(crate) use polars_ops::prelude::*;
+
+pub use crate::{
+    dsl::*,
+    frame::*,
+    logical_plan::{
+        optimizer::{type_coercion::TypeCoercionRule, Optimize, *},
+        options::*,
+        *,
+    },
+    physical_plan::{expressions::*, planner::PhysicalPlanner, Executor},
+};
+pub(crate) use crate::{
+    logical_plan::{aexpr::*, alp::*, conversion::*, iterator::*},
+    utils::*,
+};
