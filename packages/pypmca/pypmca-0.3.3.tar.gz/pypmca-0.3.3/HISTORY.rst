010fdfe5ef36a84cd6204b68c413e52d2b40b1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:42 +0000

    Add package pypmca/0.3.3/pypmca-0.3.3.tar.gz

---
+++ packages/pypmca/pypmca-0.3.3.tar.gz/pypmca-0.3.3/HISTORY.rst
@@ -0,0 +1,142 @@
+=======
+History
+=======
+
+0.1 (2020-05-12)
+------------------
+
+* First release on PyPI.
+* 0.1.1 - minor fix to __init__.py
+* 0.1.2 - bug fix in evolve_expectations
+* 0.1.3 - remove min/max parameter check
+* 0.1.4 - revised the likelihood calculation for MCMC (now shape and normalization treated separately)
+
+0.2 (2020-05-19)
+----------------
+
+* 0.2.1 beta release on PyPI
+  (includes new reference model 2.1, and copy methods)
+* 0.2.2 (2020-06-16)
+
+  * fix issue with in_icu for reference model #1
+  * allow no reports on some days of week (eg. BC: Sundays)
+  * add fit statistics to help tune reporting noise
+  * fix bugs in Ensemble
+
+* 0.2.3 (2020-07-30)
+
+  * allow cumulative to start from 0 at first point in fit
+  * allow modifier to change parameter linearly with time
+  * add code for submitting .csv file for US Forecast Hub
+  * do interpolation with log(alpha)
+  * fix bugs
+
+* 0.2.4 (2020-07-31)
+
+  * minor bug fix
+
+* 0.2.5 (2020-09-11)
+
+  * bug fixes for ensemble
+  * updates for forecast hub
+
+* 0.2.6 (2020-10-04)
+
+ * addition functionality to model vaccination
+
+* 0.2.7 (2020-11-21)
+
+ * condition simulation ensembles
+
+* 0.2.8 (2020-11-28)
+
+ * add tool to make intervals
+
+* 0.2.9 (2020-11-29)
+
+ * minor corrections and improvements
+
+* 0.2.10 (2020-12-04)
+
+ * allow gamma delay distributions
+ * new reference model
+ * smearing parameters for intervals
+ * weekly noise reporting
+
+* 0.2.11 (2020-12-12)
+
+ * produce intervals for multiple categories at once
+
+* 0.2.12 (2020-12-27)
+
+ * add AgeModeller tool to automatically fit age groups
+ * improve functionality of IntervalMaker
+ * fix chi^2 for cumul_reset = True
+
+* 0.2.13 (2020-12-29)
+
+ * modify linear modifier
+ * fix chi^2 for daily data
+ * new reference model
+
+* 0.2.15 (2020-12-30)
+
+ * minor corrections and additions
+
+* 0.2.16 (2021-01-02)
+
+ * fix for interval maker for scenarios with future transitions
+ * modified file format for scenario hub
+
+* 0.2.17 (2021-01-27)
+
+ * fixes for backup parameter for interval maker
+ * set protocol level for pickling models
+ * add new model (2_8) with second variant infection cycle
+
+* 0.2.18 (2021-01-31)
+
+ * let fit skip specified dates (xmas period for example)
+
+* 0.2.19 (2021-02-14)
+
+ * add option to produce simulated weekly data
+ * minor fix for copy
+
+* 0.2.20 (2021-03-10)
+
+ * fix issues with Ensemble
+
+* 0.2.21 (2021-03-28)
+
+ * fix mistake in Ensemble transitions
+
+* 0.2.22 (2021-08-22)
+
+ * add feature to combine evolve_expectations and generate_data
+ * improve optimization for local fits (cumul_reset)
+
+* 0.2.23 (2021-09-05)
+
+ * add method to convert population future expectations to Poisson variates
+ * add option for optimizer to skip zeros (no report days) for fit to cumulative
+
+* 0.3.0 (2021-12-09)
+
+ * add new connector: Collector
+ * add new class: Operator
+ * produce model 3.1 (4.1) for omicron with escape
+
+* 0.3.1 (2022-01-14)
+
+ * fix issue with Collector
+ * updates to reference models
+ * add option to optimizer to specify start day for iterations
+
+* 0.3.2 (2022-01-22)
+
+ * fix issue with Adder and Subtractor: prevent pop ratios from exceeding 1.
+
+* 0.3.3 (2022-08-30)
+
+ * fix issue with Population and Propagator: add checks against negative expectation or probability
---
+++ packages/pypmca/pypmca-0.3.3.tar.gz/pypmca-0.3.3/pypmca/tools/Forecast_hub_eu.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+"""
+Produce .csv file suitable for submission to the EU covid19-forecast-hub
+(https://github.com/epiforecasts/covid19-forecast-hub-europe)
+
+@author: karlen
+"""
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from scipy import stats
+import requests
+import datetime
+from datetime import timedelta
+import copy
+
+from pypmca import Model, Population, Delay, Parameter, Multiplier, Propagator, \
+    Splitter, Adder, Subtractor, Chain, Modifier, Injector, Ensemble
+
+
+class Forecast_hub:
+    """ Forecast_hub: produce .csv file for covid19-forecast-hub
+    """
+
+    def __init__(self, folder, model_names):
+
+        self.model_dir = Path(folder).resolve()
+        self.model_names = model_names
+        self.pd_dict = self.get_data()
+        self.buff = [['forecast_date', 'target', 'target_end_date', 'location', 'type', 'quantile', 'value']]
+
+        self.regional_abbreviations_all = {
+        'Austria': 'AT',
+        'Belgium': 'BE',
+        'Bulgaria': 'BG',
+        'Croatia': 'HR',
+        'Cyprus': 'CY',
+        'Czechia': 'CZ',
+        'Denmark': 'DK',
+        'Estonia': 'EE',
+        'Finland': 'FI',
+        'France': 'FR',
+        'Germany': 'DE',
+        'Greece': 'GR',
+        'Hungary': 'HU',
+        'Iceland': 'IS',
+        'Ireland': 'IE',
+        'Italy': 'IT',
+        'Latvia': 'LV',
+        'Liechtenstein': 'LI',
+        'Lithuania': 'LT',
+        'Luxembourg': 'LU',
+        'Malta': 'MT',
+        'Netherlands': 'NL',
+        'Norway': 'NO',
+        'Poland': 'PL',
+        'Portugal': 'PT',
+        'Romania': 'RO',
+        'Slovakia': 'SK',
+        'Slovenia': 'SI',
+        'Spain': 'ES',
+        'Sweden': 'SE',
+        'Switzerland': 'CH',
+        'United Kingdom': 'GB'
+        }
+
+        self.regional_abbreviations = {
+        'Belgium': 'BE',
+        'France': 'FR',
+        'Ireland': 'IE',
+        'Norway': 'NO',
+        'Switzerland': 'CH',
+        'United Kingdom': 'GB'
+        }
+
+        #self.hospitalization_states = ['BE','HR','DK','EE','FR','NL','NO','SI','ES','GB','IE']
+        # Starting Nov 7, 2021 (CH and PT added on Nov 14) (IT and PT removed on Nov 28) (NL removed Dec 5)
+        # 2022-07-20: Removed GB, since that generates a github submission error
+        self.hospitalization_states = ['BE', 'HR', 'DK', 'EE', 'FR', 'NO', 'SI',
+                                       'IE', 'CZ', 'LV', 'CY', 'CH', 'GR']
+
+    def get_data(self):
+        data_folder = 'data/covid19/EU'
+
+        success = True
+        pd_dict = {}
+        try:
+            data_desc_resp = requests.get('http://data.ipypm.ca/get_data_desc/' + data_folder)
+        except requests.exceptions.RequestException as error:
+            print('Error retrieving data description over network:')
+            success = False
+        if success:
+            data_description = data_desc_resp.json()
+            data_description['folder'] = data_folder
+            print(data_description['description'])
+
+            # load the data into a panda dictionary
+            for filename in data_description['files']:
+                path = data_folder + '/' + filename
+                success = True
+                try:
+                    csv_resp = requests.get('http://data.ipypm.ca/get_csv/' + path, stream=True)
+                except requests.exceptions.RequestException as error:
+                    print('Error retrieving data over network:')
+                    print()
+                    print(error)
+                    success = False
+
+                if success:
+                    pd_dict[filename] = pd.read_csv(csv_resp.raw)
+
+        return pd_dict
+
+    def get_csv(self, forecast_date):
+        t0 = datetime.date(2020, 3, 1)
+        forecast_date_text = forecast_date.isoformat()
+        day_of_week = forecast_date.weekday()
+        days_after_t0 = (forecast_date - t0).days
+        first_sunday = days_after_t0
+        if day_of_week == 0:
+            first_sunday -= 1
+        elif day_of_week < 6:
+            first_sunday += 6 - day_of_week
+
+        period = 'wk'
+        inc_type = 'inc'
+
+        for state in self.regional_abbreviations:
+            abbrev = self.regional_abbreviations[state].lower()
+            location = self.regional_abbreviations[state]
+
+            categories = ['case','death']
+            if location in self.hospitalization_states:
+                categories = ['case', 'death', 'hospitalization']
+
+            for category in categories:
+
+                success = False
+                model = None
+
+                if category == 'case':
+                    for model_name in self.model_names:
+                        try:
+                            filename = abbrev + model_name + '.pypm'
+                            path_model = self.model_dir / filename
+                            model = Model.open_file(path_model)
+                            success = True
+                            break
+                        except:
+                            pass
+                elif category == 'death':
+                    for model_name in self.model_names:
+                        for model_suffix in ['_d','_h','']:
+                            try:
+                                filename = abbrev + model_name + model_suffix + '.pypm'
+                                path_model = self.model_dir / filename
+                                model = Model.open_file(path_model)
+                                success = True
+                                break
+                            except:
+                                pass
+                elif category == 'hospitalization':
+                    for model_name in self.model_names:
+                        for model_suffix in ['_h','']:
+                            try:
+                                filename = abbrev + model_name + model_suffix + '.pypm'
+                                path_model = self.model_dir / filename
+                                model = Model.open_file(path_model)
+                                success = True
+                                break
+                            except:
+                                pass
+
+                if not success:
+                    print('No model for: ', abbrev)
+                else:
+                    print(category,model.name)
+
+                    hub_dict = model.user_dict['forecast_hub'][forecast_date][category]
+                    point_est_dict = hub_dict['point_estimates']
+                    quantile_dict = hub_dict['quantiles']
+
+                    for i_period in point_est_dict:
+                        # eu has different label for hospitalization...
+                        label = category
+                        if category == 'hospitalization':
+                            label = 'hosp'
+                        target = i_period+' '+period+' ahead '+inc_type+' '+label
+                        days = int(i_period)
+                        if period == 'wk':
+                            days *= 7
+                        else:
+                            days += 1
+                        target_end_date = (t0 + timedelta(days=(first_sunday + days - 1))).isoformat()
+
+                        # point estimates
+                        value = point_est_dict[i_period]
+                        self.add_record(forecast_date_text,target,target_end_date,location,'point','NA',value)
+
+                        # quantiles
+                        for quant in quantile_dict[i_period]:
+                            value = quantile_dict[i_period][quant]
+                            self.add_record(forecast_date_text, target, target_end_date, location, 'quantile', quant, value)
+
+        return self.buff
+
+    def add_record(self, forecast_date, target, target_end_date, location, record_type, quantile_text, value):
+        record = [forecast_date, target, target_end_date, location, record_type, quantile_text]
+        record.append('{0:0.0f}'.format(value))
+        self.buff.append(record)
+
+my_forecast = Forecast_hub('/Users/karlen/pypm-temp/eu', ['_4_4_0821'])
+
+my_csv = my_forecast.get_csv(datetime.date(2022, 8, 21))
+pass
+with open('/Users/karlen/pypm-temp/test-eu-forecast.csv','w') as out:
+    for line in my_csv:
+        record = ','.join(line)
+        out.write(record + '\n')
