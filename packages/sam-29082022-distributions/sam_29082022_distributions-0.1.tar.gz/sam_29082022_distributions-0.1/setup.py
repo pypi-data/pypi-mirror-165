05137d882c2e693be1f29cfb1910f41ef47f0896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:25 +0000

    Add package sam-29082022-distributions/0.1/sam_29082022_distributions-0.1.tar.gz

---
+++ packages/sam-29082022-distributions/sam_29082022_distributions-0.1.tar.gz/sam_29082022_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sam_29082022_distributions',
+      version='0.1',
+      description='Gaussian, Binomial distributions',
+      packages=['sam_29082022_distributions'],
+      zip_safe=False)
