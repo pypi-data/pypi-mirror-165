fcc84f95f561a5163d6e0dd37845c7969fe0980b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:57 +0000

    Add package MukeshTheBull/0.7/MukeshTheBull-0.7.tar.gz

---
+++ packages/MukeshTheBull/MukeshTheBull-0.7.tar.gz/MukeshTheBull-0.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='MukeshTheBull',
+version='0.7',
+description='Welcome to Mukesh the Bulls world, full of milk and fun!!',
+Long_description = 'Welcome to Mukesh the Bulls world, full of milk and fun!! Location at Ghaziabad Urban Forest, village Karhera. Sri Krishna Gaushala',
+author='Friends of Cheeku',
+packages = ['MukeshTheBull'],
+install_requires=['Pillow'])
