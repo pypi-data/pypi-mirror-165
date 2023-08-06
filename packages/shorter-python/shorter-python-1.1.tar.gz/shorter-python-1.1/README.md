c59f49fadd4edebf9273e224d2a55e27adb56fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:15 +0000

    Add package shorter-python/1.1/shorter-python-1.1.tar.gz

---
+++ packages/shorter-python/shorter-python-1.1.tar.gz/shorter-python-1.1/README.md
@@ -0,0 +1,65 @@
+# python-plus
+Shorten your Python code with concise syntax.
+
+## Installation
+
+From PIP:
+
+```
+$ pip install shorter-python
+```
+
+## Running code from command line
+
+Use the `pythonplus` command, followed by a filename
+
+```
+$ pythonplus my_file.py
+```
+
+Note: the file can have any extension, not just `.py`
+
+## Running code from Python
+
+Use the `convert` function to convert a string of Python+ code to Python:
+
+```python
+from python_plus import convert
+exec(convert(PYTHON_PLUS_CODE_HERE))
+```
+
+## Syntax
+
+Here is a table to show the difference between Python and Python+
+
+|Python|Python+|Examples|Characters saved|
+|-|-|-|-|
+|`print`|`$`|`print(x)` -> `$(x)`|4|
+|`input`|`§`|`input(x)` -> `§(x)`|4|
+|`for` ... `in`|`£` ... `:`\*|`for x in y:` -> `£x:y`|7|
+|`in`|`:`|`x in y` -> `x:y`|3|
+|`while`|`€`\*|`while x` -> `€x`|5|
+|`if`|`?`\*|`if x:` -> `?x`|3|
+|`else`|`!`\*|`else:` -> `!`|4|
+|`elif`|`±`\*|`elif x:` -> `±x`|5|
+|`and`|`&&`|`x and y` -> `x&&y`|3|
+|`or`|`\|\|`|`x or y` -> `x\|\|y`|2|
+|`not`|`~~`|`not x` -> `~~x`|2|
+
+\* Colons are not needed at the end of a line in Python+
+
+## Example
+
+This Python script to get the factors of a given number, n, is 43 characters:
+
+```python
+print([i for i in range(1,n+1) if not n%i])
+```
+
+The equivalent Python+ code is 27 characters (a 37% reduction)
+
+```python
+$([i£i:range(1,n+1)?~~n%i])
+```
+
+Python+ can save you time and characters.
---
+++ packages/shorter-python/shorter-python-1.1.tar.gz/shorter-python-1.1/python_plus/compiler.py
@@ -0,0 +1,82 @@
+import argparse
+
+def convert(s):
+    r = ''
+    for l in s.splitlines():
+        string = ''
+        newline = True
+        colon = False
+        check = True
+        for p, q in zip(l, list(l[1:])+[None]):
+            if check:
+                if not string:
+                    if p == '$':
+                        r += 'print'
+                    elif p == '§':
+                        r += 'input'
+                    elif p == '£':
+                        if not newline:
+                            r += ' '
+                        else:
+                            colon = True
+                        r += 'for '
+                    elif p == '€':
+                        if not newline:
+                            r += ' '
+                        else:
+                            colon = True
+                        r += 'while '
+                    elif p == '?':
+                        if not newline:
+                            r += ' '
+                        else:
+                            colon = True
+                        r += 'if '
+                    elif p == '!':
+                        if not newline:
+                            r += ' '
+                        else:
+                            colon = True
+                        r += 'else '
+                    elif p == '±':
+                        colon = True
+                        r += 'elif '
+                    elif p == ':':
+                        r += ' in '
+                    elif p == q == '&':
+                        r += ' and '
+                        check = False
+                    elif p == q == '|':
+                        r += ' or '
+                        check = False
+                    elif p == q == '~':
+                        r += ' not '
+                        check = False
+                    elif p in '"\'':
+                        string = p
+                    elif p in ' \t':
+                        r += p
+                        newline = newline and True
+                        continue
+                    else:
+                        r += p
+                    newline = False
+                    continue
+                r += p
+            else:
+                check = True
+        r += colon * ':' + '\n'
+    return r
+
+def from_file():
+    parser = argparse.ArgumentParser(prog ='pythonplus',
+                                     description ='Run a Python+ file using the pythonplus command')
+    parser.add_argument('filename', metavar ='filename', type=str, nargs=1, help= 'The filename of the file to run using the Python+ Compiler')
+    args = parser.parse_args()
+    try:
+        with open(args.filename[0]) as f:
+            exec(convert(f.read()))
+            return 0
+    except Exception as e:
+        print('An error occurred:', e)
+    return 1
