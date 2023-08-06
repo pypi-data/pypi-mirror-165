7f907feb167ef91c9af211af620d2a70e130175a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:47 +0000

    Add package slack-machine/0.28.1/slack-machine-0.28.1.tar.gz

---
+++ packages/slack-machine/slack-machine-0.28.1.tar.gz/slack-machine-0.28.1/machine/asyncio/models/core.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import re
+from dataclasses import dataclass, field
+from typing import Callable, Any, Awaitable
+
+from machine.asyncio.plugins.base import MachineBasePlugin
+
+
+@dataclass
+class HumanHelp:
+    command: str
+    help: str
+
+
+@dataclass
+class Manual:
+    human: dict[str, dict[str, HumanHelp]]
+    robot: dict[str, list[str]]
+
+
+@dataclass
+class MessageHandler:
+    class_: MachineBasePlugin
+    class_name: str
+    function: Callable[..., Awaitable[None]]
+    regex: re.Pattern[str]
+
+
+@dataclass
+class RegisteredActions:
+    listen_to: dict[str, MessageHandler] = field(default_factory=dict)
+    respond_to: dict[str, MessageHandler] = field(default_factory=dict)
+    process: dict[str, dict[str, Callable[[dict[str, Any]], Awaitable[None]]]] = field(default_factory=dict)
