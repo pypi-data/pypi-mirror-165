fd34774660ad5064276c23bd064e0918bb58b542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:07 +0000

    Add package function-burger/1.1.0/function_burger-1.1.0-py3-none-any.whl

---
+++ packages/function-burger/function_burger-1.1.0-py3-none-any.whl/function_burger/__init__.py
@@ -0,0 +1,12 @@
+# function-burger 🍔 by shinya_sun_sun, MIT license
+
+from .function_burger import (
+    LogColor,
+    LogLevel,
+    LogPosition,
+    bottom_log,
+    burger_log,
+    top_log,
+)
+
+__version__ = "1.1.0"
