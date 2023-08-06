a5951fa71e07283de7c1efb4fd10766175e861e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:42 +0000

    Add package lbz/0.5.15/lbz-0.5.15.tar.gz

---
+++ packages/lbz/lbz-0.5.15.tar.gz/lbz-0.5.15/lbz/events/broker.py
@@ -0,0 +1,43 @@
+from copy import deepcopy
+from typing import Callable, Dict, List
+
+from lbz.events.event import Event
+from lbz.misc import get_logger
+
+logger = get_logger(__name__)
+
+
+class EventBroker:
+    def __init__(
+        self,
+        mapper: Dict[str, List[Callable]],
+        raw_event: dict,
+        *,
+        type_key: str = "detail-type",
+        data_key: str = "detail",
+    ) -> None:
+        self.mapper = mapper
+        self.event = Event(raw_event[data_key], event_type=raw_event[type_key])
+
+    def handle(self) -> None:
+        self.pre_handle()
+
+        for handler in self._get_handlers():
+            try:
+                handler(deepcopy(self.event))
+            except Exception:  # pylint: disable=broad-except
+                logger.exception("Handling event failed, event: %s", self.event)
+
+        self.post_handle()
+
+    def _get_handlers(self) -> List[Callable]:
+        try:
+            return self.mapper[self.event.type]
+        except KeyError as err:
+            raise NotImplementedError(f"No handlers implemented for {self.event.type}") from err
+
+    def pre_handle(self) -> None:
+        pass
+
+    def post_handle(self) -> None:
+        pass
