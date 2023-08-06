1127a7f8ad2a9bb9ca5a995bfc0df890881bfbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package CheckUtil/0.4/CheckUtil-0.4.tar.gz

---
+++ packages/CheckUtil/CheckUtil-0.4.tar.gz/CheckUtil-0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="CheckUtil",
+    version="0.4",
+    author="Nidhi Sharma",
+    description="This package is use to check the RAM and CPU Usage of Current Device.",
+    packages=["CheckUtil"],
+    url = "https://github.com/Nidhi0829/CheckUtil"
+)
