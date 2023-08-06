4b15a0f2dae8b7b8d64d5822b7a0d48bee1bdb74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:59 +0000

    Add package lk-logger/5.2.4/lk_logger-5.2.4-py3-none-any.whl

---
+++ packages/lk-logger/lk_logger-5.2.4-py3-none-any.whl/lk_logger/__init__.py
@@ -0,0 +1,11 @@
+from .general import builtin_print
+from .general import builtin_print as bprint  # alias
+from .global_control import disable
+from .global_control import enable
+from .global_control import setup
+from .global_control import start_ipython
+from .global_control import unload
+from .global_control import update
+from .logger import lk
+
+__version__ = '5.2.4'
