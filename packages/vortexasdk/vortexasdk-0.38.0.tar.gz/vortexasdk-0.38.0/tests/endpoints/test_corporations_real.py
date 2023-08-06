8ed0768869748bdb0cca7cec1417969ccc21ca11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:56 +0000

    Add package vortexasdk/0.38.0/vortexasdk-0.38.0.tar.gz

---
+++ packages/vortexasdk/vortexasdk-0.38.0.tar.gz/vortexasdk-0.38.0/tests/endpoints/test_corporations_real.py
@@ -0,0 +1,13 @@
+from tests.testcases import TestCaseUsingRealAPI
+from docs.utils import to_markdown
+from vortexasdk import Corporations
+
+
+class TestCorporationsReal(TestCaseUsingRealAPI):
+    def test_search(self):
+        Corporations().search().to_df()
+
+    def test_load_all(self):
+        all_corporations = Corporations().load_all()
+
+        assert len(all_corporations) > 1000
