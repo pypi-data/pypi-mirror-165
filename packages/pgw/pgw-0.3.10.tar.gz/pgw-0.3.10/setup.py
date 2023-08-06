e201e5037b8ab5ffcdedda9cd2212532fe9fa1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:54 +0000

    Add package pgw/0.3.10/pgw-0.3.10.tar.gz

---
+++ packages/pgw/pgw-0.3.10.tar.gz/pgw-0.3.10/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='pgw',packages=['pygwin'],version='0.3.10',
+author='themixray',description='A library for creating Python applications.',
+license='MIT',install_requires=['attrdict','cython','pywin32','pygame','inputs','randstr',
+'pydub','wxPython','pyautogui','moviepy','pipwin','wave','opencv-python','mutagen'])
