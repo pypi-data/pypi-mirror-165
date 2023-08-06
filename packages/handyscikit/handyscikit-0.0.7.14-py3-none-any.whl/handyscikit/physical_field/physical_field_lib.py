f1eea6e61d782a72e05c65ded048d1f3ee23cc3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:04 +0000

    Add package handyscikit/0.0.7.14/handyscikit-0.0.7.14-py3-none-any.whl

---
+++ packages/handyscikit/handyscikit-0.0.7.14-py3-none-any.whl/handyscikit/physical_field/physical_field_lib.py
@@ -0,0 +1,43 @@
+from .physical_field_base import PhysicalFieldBase
+import numpy as np
+
+class AbstractField(PhysicalFieldBase):
+    def __init__(self, mesh):
+        PhysicalFieldBase.__init__(self, mesh)
+
+        self._cell_info = np.zeros([mesh.cell_num, 1], dtype=np.float32)
+        self._name = "empty"
+
+
+class ConvectionDiffusionField(PhysicalFieldBase):
+    def __init__(self, mesh):
+        PhysicalFieldBase.__init__(self, mesh)
+
+        # axis 1 - (source, diffusion coefficient)
+        self._cell_info = np.zeros([mesh.cell_num, 10], dtype=np.float32)
+        self._name = "convection_diffusion"
+
+        self._velocity = np.array([0.0, 0.0, 0.0], dtype=np.float32)
+
+    def set_velocity(self, velocity):
+        self._velocity = velocity
+
+
+class DarcyField(PhysicalFieldBase):
+    def __init__(self, mesh):
+        PhysicalFieldBase.__init__(self, mesh)
+
+        # axis 1 - (source, storage, permeability_matrix)
+        self._cell_info = np.zeros([mesh.cell_num, 11], dtype=np.float32)
+        self._name = "darcy"
+
+        self._aquifer_thick = 1  # There is no influence on 3D, because its value is 1.
+
+    def set_aquifer_thick(self, value):
+        assert mesh.dim==2, "Darcy field in 3D doesn't have aquifer thick parameter."
+
+        self._aquifer_thick = value
+
+    @property
+    def aquifer_thick(self):
+        return self._aquifer_thick
