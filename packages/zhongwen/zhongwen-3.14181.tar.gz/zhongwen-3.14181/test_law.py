e75c486de222d595348f4d803b3ca2f94f5a0c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:41 +0000

    Add package zhongwen/3.14181/zhongwen-3.14181.tar.gz

---
+++ packages/zhongwen/zhongwen-3.14181.tar.gz/zhongwen-3.14181/test_law.py
@@ -0,0 +1,20 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test(self):
+        from zhongwen.law import 法條查詢, 法條展開
+
+        self.assertEqual(法條查詢('職業安全衛生法第33條').條文內容.iloc[0], 
+                '雇主應負責宣導本法及有關安全衛生之規定，使勞工周知。')
+
+        self.assertEqual(法條查詢('職業安全衛生法33').條文內容.iloc[0], 
+                '雇主應負責宣導本法及有關安全衛生之規定，使勞工周知。')
+
+        self.assertEqual(法條查詢('職業安全衛生法[合格]').條號.iloc[0], '8')
+
+        self.assertEqual(法條展開('職業安全衛生法33'), 
+                '職業安全衛生法第33條規定：「雇主應負責宣導本法及有關安全衛生之規定，使勞工周知。」'
+                )
+
+if __name__ == '__main__':
+    unittest.main()
