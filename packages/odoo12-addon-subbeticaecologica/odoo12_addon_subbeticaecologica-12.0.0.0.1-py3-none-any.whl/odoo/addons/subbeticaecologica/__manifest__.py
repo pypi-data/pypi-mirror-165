ffa96e390124666a04a920d61d919b814b47cd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:13 +0000

    Add package odoo12-addon-subbeticaecologica/12.0.0.0.1/odoo12_addon_subbeticaecologica-12.0.0.0.1-py3-none-any.whl

---
+++ packages/odoo12-addon-subbeticaecologica/odoo12_addon_subbeticaecologica-12.0.0.0.1-py3-none-any.whl/odoo/addons/subbeticaecologica/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Odoo Subbética Ecológica customizations",
+    'version': '12.0.0.0.1',
+    'depends': ['portal'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Odoo Subbética Ecológica customizations.
+    """,
+    "license": "AGPL-3",
+    'data': [
+        'templates/portal_templates.xml'
+    ],
+}
