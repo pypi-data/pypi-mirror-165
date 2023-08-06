e942c95af08e490be271d27be91a22d70c04b8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:25 +0000

    Add package nodeweb/1.0.3/nodeweb-1.0.3.tar.gz

---
+++ packages/nodeweb/nodeweb-1.0.3.tar.gz/nodeweb-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='nodeweb',
+    version='1.0.3',
+    author='zhoufangfang',
+    author_email='xczff001@163.com',
+    maintainer='zhoufangfang',
+    maintainer_email='xczff001@163.com',
+    description='这是一个可以搭建web服务器的库，使用前需安装nodejs',
+    keywords=['web','nodejs'],
+    py_modules=['nodeweb'],
+    python_requires='>=3'
+)
+
