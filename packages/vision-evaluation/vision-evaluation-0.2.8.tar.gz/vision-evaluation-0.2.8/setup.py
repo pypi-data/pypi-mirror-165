50f3f6e7701c62274175a5f296c818811a520c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:14 +0000

    Add package vision-evaluation/0.2.8/vision-evaluation-0.2.8.tar.gz

---
+++ packages/vision-evaluation/vision-evaluation-0.2.8.tar.gz/vision-evaluation-0.2.8/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+from os import path
+
+VERSION = '0.2.8'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(name='vision-evaluation',
+                 author='Ping Jin, Shohei Ono',
+                 description="Evaluation code for vision tasks.",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url='https://github.com/pj-ms/vision-evaluation',
+                 version=VERSION,
+                 license='MIT',
+                 python_requires='>=3.6',
+                 packages=setuptools.find_packages(),
+                 keywords='vision metric evaluation classification detection',
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Programming Language :: Python :: 3.9',
+                     'Programming Language :: Python :: 3.10',
+                 ],
+                 install_requires=[
+                     'numpy',
+                     'sklearn',
+                     'pycocotools',
+                     'pycocoevalcap',
+                     'opencv-python-headless',
+                     'Pillow>=6.2.2'
+                 ])
