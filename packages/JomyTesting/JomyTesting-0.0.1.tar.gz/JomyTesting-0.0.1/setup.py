081f0663912ba5636343fe48cd4cf1b8ac221c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:21 +0000

    Add package JomyTesting/0.0.1/JomyTesting-0.0.1.tar.gz

---
+++ packages/JomyTesting/JomyTesting-0.0.1.tar.gz/JomyTesting-0.0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import *
+
+
+setup(
+    name="JomyTesting",
+    description= "The testing package for Jomy",
+    version= "0.0.1",
+    author = "Muheko Nikolas",
+    packages = find_packages()
+    
+)
