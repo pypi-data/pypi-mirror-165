312f57e88a5f985d34525eab9992555370cd412e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:54:56 +0000

    Add package googlewrapper/0.2.11/googlewrapper-0.2.11.tar.gz

---
+++ packages/googlewrapper/googlewrapper-0.2.11.tar.gz/googlewrapper-0.2.11/src/googlewrapper/__init__.py
@@ -0,0 +1,9 @@
+"""General API Connector Classes for Google Products"""
+
+from .cal import GoogleCalendar
+from .ga import GoogleAnalytics
+from .gsc import GoogleSearchConsole
+from .gbq import GoogleBigQuery
+from .sheets import GoogleSheets
+from .docs import GoogleDocs
+from .pagespeed import PageSpeed
